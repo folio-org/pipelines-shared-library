41b43e42f3086dbe3289bd62ef684e79fb908df6
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-03-28 17:55:46 +0300

    RANCHER-232 change okapi-host, baseurl

--- pipelines/cypress.groovy
+++ pipelines/cypress.groovy
@@ -10,8 +10,8 @@ properties([
      strategy: [$class: 'LogRotator', numToKeepStr: '10']],
     disableConcurrentBuilds(),
     parameters([
-        string(name: 'baseUrl', description: 'Choose what you want', defaultValue: "https://folio-snapshot.dev.folio.org"),
-        string(name: 'OKAPI_HOST', description: 'Choose what you want', defaultValue: "https://folio-snapshot-okapi.dev.folio.org"),
+        string(name: 'baseUrl', description: 'Choose what you want', defaultValue: "https://cypress.ci.folio.org"),
+        string(name: 'OKAPI_HOST', description: 'Choose what you want', defaultValue: "https://cypress-okapi.ci.folio.org"),
         string(name: 'OKAPI_TENANT', description: 'Choose what you want', defaultValue: "diku"),
         string(name: 'diku_login', description: 'Choose what you want', defaultValue: "diku_admin"),
         password(name: 'diku_password', description: 'Choose what you want', defaultValue: "admin"),
