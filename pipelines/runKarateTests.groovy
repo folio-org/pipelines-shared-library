27185d9794c4a64c628f2747f59eee13d92c108e
Vitaly Demchenko <Vitaly_Demchenko@epam.com>
2022-04-04 17:47:15 +0300

    no message

--- pipelines/runKarateTests.groovy
+++ pipelines/runKarateTests.groovy
@@ -123,9 +123,8 @@ pipeline {
                         }
                     }
 
-                    def jobBuildStatus = BUILD_STATUS ?: 'SUCCESS'
                     teamResults.each { entry ->
-                        def msg = "${jobBuildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+                        def msg = "${currentBuild.result}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
                         entry.value.each { moduleTestResult ->
                             if (moduleTestResult.getExecutionResult() == KarateExecutionResult.FAIL) {
                                 message += "Module '${moduleTestResult.getName()}' has ${moduleTestResult.getFailedCount()} failures of ${moduleTestResult.getTotalCount()}.\n"
