f093a3a264e1a4cb73814e23373070a3b0170a96
Vitaly Demchenko <Vitaly_Demchenko@epam.com>
2022-04-05 14:23:41 +0300

    RANCHER-251 fix

--- pipelines/karateTestsPipeline.groovy
+++ pipelines/karateTestsPipeline.groovy
@@ -94,12 +94,14 @@ pipeline {
 
         stage('Archive artifacts') {
             steps {
-                // archive artifacts for upstream job
-                if (currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)) {
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "**/target/karate-reports*/*.json", fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "**/target/karate-reports*/*.xml", fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: '**/target/karate-reports*/karate-summary-json.txt', fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: 'teams-assignment.json', fingerprint: true, defaultExcludes: false
+                script {
+                    // archive artifacts for upstream job
+                    if (currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)) {
+                        archiveArtifacts allowEmptyArchive: true, artifacts: "**/target/karate-reports*/*.json", fingerprint: true, defaultExcludes: false
+                        archiveArtifacts allowEmptyArchive: true, artifacts: "**/target/karate-reports*/*.xml", fingerprint: true, defaultExcludes: false
+                        archiveArtifacts allowEmptyArchive: true, artifacts: '**/target/karate-reports*/karate-summary-json.txt', fingerprint: true, defaultExcludes: false
+                        archiveArtifacts allowEmptyArchive: true, artifacts: 'teams-assignment.json', fingerprint: true, defaultExcludes: false
+                    }
                 }
             }
         }
