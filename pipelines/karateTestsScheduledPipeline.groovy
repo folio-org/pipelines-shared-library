61683f69d39ef7dae765f215865254750b73df6b
Vitaly Demchenko <Vitaly_Demchenko@epam.com>
2022-05-16 15:12:08 +0300

    RANCHER-252 fix

--- pipelines/karateTestsScheduledPipeline.groovy
+++ pipelines/karateTestsScheduledPipeline.groovy
@@ -7,7 +7,11 @@ import org.folio.version.semantic.Order
 import org.folio.version.semantic.SemanticVersionComparator
 import org.jenkinsci.plugins.workflow.libs.Library
 
-def okapiUrl, tenant, user, password
+def projectName = "test"
+def okapiUrl = "https://${projectName}-okapi.ci.folio.org"
+def tenant = "karate"
+def user = "admin"
+def password = "q3fui4ht3490fg"
 def spinUpEnvironmentJobName = "/Rancher/Project"
 def spinUpEnvironmentJob
 def tearDownEnvironmentJob
@@ -34,28 +38,31 @@ pipeline {
                 script {
                     def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, uiImageVersion)
 
-                    spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: true
+                    spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
                 }
             }
         }
 
-//        stage("Run karate tests") {
-//            steps {
-//                script {
-//                    def jobParameters = [
-//                        string(name: 'branch', value: params.branch),
-//                        string(name: 'threadsCount', value: "4"),
-//                        string(name: 'modules', value: ""),
-//                        string(name: 'okapiUrl', value: okapiUrl),
-//                        string(name: 'tenant', value: tenant),
-//                        string(name: 'adminUserName', value: user),
-//                        string(name: 'adminPassword', value: password)
-//                    ]
-//
-//                    karateTestsJob = build job: karateTestsJobName, parameters: jobParameters, wait: true, propagate: false
-//                }
-//            }
-//        }
+        stage("Run karate tests") {
+            when {
+                equals actual: spinUpEnvironmentJob.result, expected: 'SUCCESS'
+            }
+            steps {
+                script {
+                    def jobParameters = [
+                        string(name: 'branch', value: params.branch),
+                        string(name: 'threadsCount', value: "4"),
+                        string(name: 'modules', value: ""),
+                        string(name: 'okapiUrl', value: okapiUrl),
+                        string(name: 'tenant', value: tenant),
+                        string(name: 'adminUserName', value: okapiSettings.adminUser().username),
+                        string(name: 'adminPassword', value: okapiSettings.adminUser().password)
+                    ]
+
+                    karateTestsJob = build job: karateTestsJobName, parameters: jobParameters, wait: true, propagate: false
+                }
+            }
+        }
 
         stage("Parallel") {
             parallel {
@@ -70,6 +77,9 @@ pipeline {
                 }
                 stage("Collect test results") {
                     stages {
+                        when {
+                            equals actual: spinUpEnvironmentJob.result, expected: 'SUCCESS'
+                        }
                         stage("Test parallel 1") {
                             steps {
                                 echo "bla 1"
@@ -150,18 +160,18 @@ pipeline {
     }
 }
 
-private List getEnvironmentJobParameters(String action, String okapiVersion, String uiImageVersion) {
+private List getEnvironmentJobParameters(String action, String okapiVersion, String uiImageVersion, projectName, tenant) {
     [
         string(name: 'branch', value: 'master'),
         string(name: 'action', value: action),
         string(name: 'okapi_version', value: okapiVersion),
         string(name: 'rancher_cluster_name', value: "folio-test"),
-        string(name: 'project_name', value: "test"),
+        string(name: 'project_name', value: projectName),
         string(name: 'folio_repository', value: "complete"),
         string(name: 'folio_branch', value: "R1-2022"),
         string(name: 'stripes_image_tag', value: uiImageVersion),
-        string(name: 'tenant_id', value: "diku"),
-        string(name: 'tenant_name', value: "Master karate tenant"),
+        string(name: 'tenant_id', value: tenant),
+        string(name: 'tenant_name', value: "Karate tenant"),
         string(name: 'tenant_description', value: "Karate tests main tenant"),
         booleanParam(name: 'load_reference', value: true),
         booleanParam(name: 'load_sample', value: false)
