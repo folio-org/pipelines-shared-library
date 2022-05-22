ec5697bd7678cd4b55edec742482741d53e4188c
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2022-05-23 01:18:18 +0300

    Refactoring

--- terraform/rancher/project/elasticsearch.tf
+++ terraform/rancher/project/elasticsearch.tf
@@ -33,4 +33,150 @@ resource "rancher2_app" "elasticsearch" {
     "plugins"                                = "analysis-icu, analysis-kuromoji, analysis-smartcn, analysis-nori, analysis-phonetic"
   }
 }
-# TODO Add opensearch provisioning
+
+#variable "use-es" {
+#  default = false
+#}
+#
+#variable "type-es" {
+#  default = "service"
+#}
+#
+#variable "es_domain_name" {}
+#
+#variable "es_version" {
+#  default = "7.10"
+#}
+#
+#variable "es_ebs_volumes_size" {
+#  default = "500"
+#}
+#
+#variable "es_instance_type" {
+#  default = "m5.xlarge.elasticsearch"
+#}
+#
+#variable "es_instance_count" {
+#  default = "2"
+#}
+#
+#variable "es_dedicated_master" {
+#  default = false
+#}
+#
+#variable "create_service_link_role" {
+#  default = false
+#}
+#
+#variable "se_username" {
+#  default = "esadmin"
+#}
+#
+#resource "random_password" "se_password" {
+#  length = 16
+#  special = true
+#  number = true
+#  upper = true
+#  lower = true
+#  min_lower = 1
+#  min_numeric = 1
+#  min_special = 1
+#  min_upper = 1
+#}
+#
+#
+#resource "aws_security_group" "allow_es" {
+#  name = "allow_es-${var.name_prefix}"
+#  vpc_id =  module.vpc.vpc_id
+#  description = "${var.cluster_name}-${var.name_prefix}-es-service ports"
+#  ingress {
+#    from_port = 443
+#    to_port = 443
+#    protocol = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port = 0
+#    to_port = 0
+#    protocol = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  tags = {
+#    Environment = "dev"
+#    Terraform   = "true"
+#    Name = "allow_es"
+#  }
+#
+#}
+#
+#module "aws_es" {
+#  source = "github.com/lgallard/terraform-aws-elasticsearch"
+#
+#  domain_name = "${var.es_domain_name}-${var.name_prefix}"
+#  elasticsearch_version = var.es_version
+#
+#  domain_endpoint_options_enforce_https = true
+#  create_service_link_role = var.create_service_link_role
+#
+#  advanced_options = {
+#    "rest.action.multi.allow_explicit_index" = "true"
+#  }
+#
+#  cluster_config = {
+#    dedicated_master_enabled = var.es_dedicated_master
+#    instance_count = var.es_instance_count
+#    instance_type = var.es_instance_type
+#    zone_awareness_enabled = "false"
+#    availability_zone_count = 2
+#    dedicated_master_count = 0
+#  }
+#
+#  advanced_security_options = {
+#    enabled = true
+#    internal_user_database_enabled = true
+#    master_user_options = {
+#      master_user_name = var.se_username
+#      master_user_password = random_password.se_password.result
+#    }
+#  }
+#
+#  ebs_options = {
+#    ebs_enabled = var.es_ebs_volumes_size > 0 ? "true" : "false"
+#    volume_size = var.es_ebs_volumes_size
+#  }
+#
+#  #  encrypt_at_rest = {
+#  #    enabled = "true"
+#  #    kms_key_id = "alias/aws/es"
+#  #  }
+#
+#  cognito_options_enabled = false
+#
+#  cognito_options = {
+#    enabled = false
+#  }
+#
+#  vpc_options = {
+#    subnet_ids = tolist([module.vpc.private_subnets[0]])
+#    security_group_ids = tolist([aws_security_group.allow_es.id])
+#  }
+#
+#  node_to_node_encryption_enabled = true
+#  snapshot_options_automated_snapshot_start_hour = 23
+#
+#  access_policies = templatefile("${path.module}/resources/es-access-policies.tpl", {
+#    region = var.aws_region,
+#    account = data.aws_caller_identity.current.account_id,
+#    domain_name = "${var.es_domain_name}-${var.name_prefix}"
+#  })
+#
+#  timeouts_update = "60m"
+#
+#  tags = {
+#    service = "ElasticSearch"
+#    name = "${var.es_domain_name}-${var.name_prefix}"
+#    domain = var.es_domain_name
+#    version = var.es_version
+#  }
+#
+#}
--- terraform/rancher/project/kafka.tf
+++ terraform/rancher/project/kafka.tf
@@ -24,4 +24,68 @@ resource "rancher2_app" "kafka" {
     "readinessProbe.enabled"     = "false"
   }
 }
-# TODO Add MSK provisioning support
+
+#resource "aws_security_group" "kafka_sg" {
+#  name        = "kafka_sg"
+#  description = "Allow connection to Kafka"
+#  vpc_id      = module.vpc.vpc_id
+#
+#  ingress {
+#    description      = "Allow 9092 port"
+#    from_port        = 9092
+#    to_port          = 9092
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow connection to Kafka"
+#  }
+#}
+#resource "aws_msk_cluster" "kafka" {
+#  depends_on             = [module.rds]
+#  cluster_name           = "Kafka-${var.name_prefix}"
+#  kafka_version          = "2.8.0"
+#  number_of_broker_nodes = 2
+#  encryption_info {
+#    encryption_in_transit {
+#      client_broker = "PLAINTEXT"
+#    }
+#  }
+#
+#  broker_node_group_info {
+#    instance_type   = "kafka.m5.large"
+#    ebs_volume_size = 100
+#    client_subnets = [
+#      module.vpc.private_subnets[0],
+#      module.vpc.private_subnets[1],
+#    ]
+#    security_groups = [aws_security_group.kafka_sg.id]
+#  }
+#
+#  configuration_info {
+#    arn      = aws_msk_configuration.kafka_configuration.arn
+#    revision = aws_msk_configuration.kafka_configuration.latest_revision
+#  }
+#
+#  tags = {
+#    service = "Kafka"
+#    name = "KAFKA-PERF-${var.name_prefix}"
+#  }
+#}
+#resource "aws_msk_configuration" "kafka_configuration" {
+#  kafka_versions    = ["2.6.1"]
+#  name              = "Kafka-configuration-${var.name_prefix}"
+#  description       = "Kafka configuration which is used for perf rancher env"
+#  server_properties = <<PROPERTIES
+#auto.create.topics.enable = true
+#PROPERTIES
+#}
