dfa29ffa2dbd93dc8d22f1410387c8c058753491
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-27 09:23:03 +0300

    rancher-282 add configuration files

--- terraform/rancher/project/config_dev.tf
+++ terraform/rancher/project/config_dev.tf
@@ -1,10 +1,9 @@
 
 locals {
-  dev            = "${var.env_type == "development" ? "local.module_configs_dev" : ""}"
-  perf           = "${var.env_type == "performance" ? "local.module_configs_perf" : ""}"
-  test           = "${var.env_type != "development" && var.env_type != "performance" ? "local.module_configs_test" : ""}"
- # module_configs = "${([local.module_configs_dev, local.module_configs_perf, local.module_configs_test])}"
-  module_configs = "${setproduct(["development", "performance", "testing"], ["replicaCount", "resources.requests.memory", "resources.limits.memory"])}"
+  dev  = "${var.env_type == "development" ? "local.module_configs_dev" : ()}"
+  perf = "${var.env_type == "performance" ? "local.module_configs_perf" : ""}"
+  test = "${var.env_type != "development" && var.env_type != "performance" ? "local.module_configs_test" : ""}"
+  module_configs = "${coalesce(local.dev,local.perf, local.test)}"
 }
 
 locals {
