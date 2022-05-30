fbb7fab5fd0625223df07476b3292e86e3c0c378
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-30 15:36:05 +0300

    rancher-282 add configuration files

--- terraform/rancher/project/variables.tf
+++ terraform/rancher/project/variables.tf
@@ -173,7 +173,7 @@ variable "env_type" {
 
   validation {
     condition     = var.env_type == "performance" || var.env_type == "testing"
-    error_message = "Must be a valid env type"
+    error_message = "Must be a valid env type for perf or test env."
    }
 }
 
