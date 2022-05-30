8764dad0c8da50d5d7b5fb0af06fa6af23b79822
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-30 18:24:53 +0300

    rancher-282 add configuration files

--- terraform/rancher/project/config_dev.tf
+++ terraform/rancher/project/config_dev.tf
@@ -3,7 +3,7 @@ locals {
   performance = local.module_configs_perf
   testing     = local.module_configs_test
   development = local.module_configs_dev
-  module_configs = var.env_type == "development" || "performance" ? local.development : local.performance
+  module_configs = var.env_type == "development" && var.env_type == "performance" ? local.development : local.performance
 }
 
 locals {
