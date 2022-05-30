53b3ae575e0986b4412f196188151f7c138fb682
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-30 12:51:17 +0300

    rancher-282 add configuration files

--- terraform/rancher/project/variables.tf
+++ terraform/rancher/project/variables.tf
@@ -167,7 +167,7 @@ variable "stripes_image_tag" {
 
 variable "env_type" {
   type        = list(string)
-  default     = "development"
+  default     = ["development", "performance", "testing"]
   description = "config file for dev, perf, test env"
   }
 
