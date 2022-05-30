085ec3888175a449358f689ebb397528bccb2c2a
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-30 15:58:29 +0300

    rancher-282 add configuration files

--- pipelines/rancher/project.groovy
+++ pipelines/rancher/project.groovy
@@ -82,7 +82,7 @@ ansiColor('xterm') {
                     terraform.tfWorkspaceSelect(tfWorkDir, params.project_name)
                     terraform.tfStatePull(tfWorkDir)
                     if (params.action == 'apply') {
-                        terraform.tfPlan(tfWorkDir, tfVars, tf_var_file='env_type.tfvars')
+                        terraform.tfPlan(tfWorkDir, tfVars)
                         terraform.tfApply(tfWorkDir)
                         okapiUrl = terraform.tfOutput(tfWorkDir, 'okapi_url')
                         stripesUrl = terraform.tfOutput(tfWorkDir, 'stripes_url')
--- terraform/rancher/project/env_type.tfvars
+++ terraform/rancher/project/env_type.tfvars
@@ -1,11 +1,5 @@
 module_congifs = {
-  "dev" = {
-    "development" = "local.module_configs_perf"
-  }
-  "perf" = {
+    "development" = "local.module_configs_dev"
     "performance" = "local.module_configs_perf"
-  }
-  "test" = {
     "testing" = "local.module_configs_test"
-  }
 }
