296a43f5b9e637e676fb05e30846c2fda03b5ef1
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-05-30 20:02:01 +0300

    rancher-282 add configuration files

--- terraform/rancher/project/config_dev.tf
+++ terraform/rancher/project/config_dev.tf
@@ -58,6 +58,7 @@ locals {
         }
       },
       replicaCount = 1,
+      javaOptions  = "-XX:MaxRAMPercentage=80.0"
       javaArgs  = "-Dokapi_url=http://pvt.lb.$CLUSTER.$DOMAIN_PREFIX.$REGION:$OKAPI_PORT -Dsecure_store=AwsSsm -Dsecure_store_props=/usr/ms/aws_ss.properties -Dhttp.port=$INTERNAL_DOCKER_PORT -Dlog.level=debug"
     },
     "edge-dematic" = {
