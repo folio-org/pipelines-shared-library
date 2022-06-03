8c77f618d972a52703469834744601ab7c485660
Vitaly Demchenko <Vitaly_Demchenko@epam.com>
2022-06-03 17:08:15 +0300

    RANCHER-329 extract tenant creation to separate class

--- pipelines/rancher/cluster.groovy
+++ pipelines/rancher/cluster.groovy
@@ -2,6 +2,7 @@
 @Library('pipelines-shared-library') _
 
 import org.folio.Constants
+import org.jenkinsci.plugins.workflow.libs.Library
 
 properties([
     buildDiscarder(logRotator(numToKeepStr: '20')),
--- src/org/folio/rest/Deployment.groovy
+++ src/org/folio/rest/Deployment.groovy
@@ -1,6 +1,6 @@
 package org.folio.rest
 
-import hudson.AbortException
+
 import org.folio.rest.model.Email
 import org.folio.rest.model.GeneralParameters
 import org.folio.rest.model.OkapiTenant
@@ -59,40 +59,14 @@ class Deployment extends GeneralParameters {
     }
 
     void main() {
-        if (tenant && admin_user) {
-            enableList = gitHubUtility.buildEnableList(repository, branch)
-            discoveryList = gitHubUtility.buildDiscoveryList(repository, branch)
-            okapi.publishModuleDescriptors(enableList)
-            okapi.createTenant(tenant)
-            okapi.enableDisableUpgradeModulesForTenant(tenant, okapi.buildInstallList(["okapi"], "enable"))
-            okapi.registerServices(discoveryList)
-            okapi.enableDisableUpgradeModulesForTenant(tenant, enableList, 900000)
-            String authtokenModId = okapi.getModuleId(tenant, 'authtoken')
-            List authtokenDisableDependenciesList = okapi.enableDisableUpgradeModulesForTenant(tenant, okapi.buildInstallList([authtokenModId], "disable"))
-            users.createUser(tenant, admin_user)
-            auth.createUserCredentials(tenant, admin_user)
-            permissions.createUserPermissions(tenant, admin_user)
-            String servicePointsUsersModId = okapi.getModuleId(tenant, 'service-points-users')
-            if (servicePointsUsersModId) {
-                if (servicePoints.getServicePointsUsersRecords(tenant, admin_user).totalRecords == 0) {
-                    servicePoints.createServicePointsUsersRecord(tenant, admin_user, servicePoints.getServicePointsIds(tenant))
-                }
-            } else {
-                logger.warning("Module service-points-users does not installed")
-            }
-            okapi.enableDisableUpgradeModulesForTenant(tenant, authtokenDisableDependenciesList.reverse().collect { [id: it.id, action: "enable"] })
-            auth.login(tenant, admin_user)
-            permissions.assignUserPermissions(tenant, admin_user, permissions.getAllPermissions(tenant))
-            users.setPatronGroup(tenant, admin_user, users.getPatronGroupId(tenant, admin_user))
-            okapi.secure(super_admin)
-            okapi.secure(testing_admin)
-            edge.createEdgeUsers(tenant, enableList)
-            tenantConfiguration.modInventoryMods(tenant)
-            tenantConfiguration.ebscoRmapiConfig(tenant, kb_api_key)
-            tenantConfiguration.worldcat(tenant)
-            tenantConfiguration.configurations(tenant, email, stripesUrl)
-        } else {
-            throw new AbortException('Tenant or admin user not set')
-        }
+        enableList = gitHubUtility.buildEnableList(repository, branch)
+        discoveryList = gitHubUtility.buildDiscoveryList(repository, branch)
+        okapi.publishModuleDescriptors(enableList)
+        okapi.registerServices(discoveryList)
+        okapi.secure(super_admin)
+        okapi.secure(testing_admin)
+
+        def tenant = new Tenant(steps, okapiUrl)
+        tenant.createTenant(tenant, admin_user, enableList, email, stripesUrl, kb_api_key)
     }
 }
