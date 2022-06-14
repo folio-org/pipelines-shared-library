129227285cc9275c00da7aa97811e9d0d2b8bc04
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-06-14 14:56:09 +0300

    check def test

--- src/org/folio/rest/Deployment.groovy
+++ src/org/folio/rest/Deployment.groovy
@@ -67,10 +67,10 @@ class Deployment extends GeneralParameters {
     void main() {
         enableList = gitHubUtility.buildEnableList(repository, branch)
         discoveryList = gitHubUtility.buildDiscoveryList(repository, branch)
-//        okapi.publishModuleDescriptors(enableList)
-//        okapi.registerServices(discoveryList)
-//        okapi.secure(super_admin)
-//        okapi.secure(testing_admin)
+        okapi.publishModuleDescriptors(enableList)
+        okapi.registerServices(discoveryList)
+        okapi.secure(super_admin)
+        okapi.secure(testing_admin)
 
         def tenantService = new TenantService(steps, okapiUrl, super_admin)
         tenantService.createTenant(tenant, admin_user, enableList, email, stripesUrl, kb_api_key, reIndexElasticsearch, recreateIndexElasticsearch)
