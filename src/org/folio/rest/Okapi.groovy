9d1b8289397bb5809a8ee045c7b25d0b969a766e
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-06-14 16:54:39 +0300

    check def test

--- src/org/folio/rest/Okapi.groovy
+++ src/org/folio/rest/Okapi.groovy
@@ -161,7 +161,7 @@ class Okapi extends GeneralParameters {
         String body = "{\"recreateIndexElasticsearch\": ${recreateIndexElasticsearch} }"
         def res = http.postRequest(url, body, headers)
         if (res.status == HttpURLConnection.HTTP_OK) {
-            return tools.jsonParse(res.content) + "Elastic Search reindex successfully run"
+            return tools.jsonParse(res.content)
         } else {
             throw new AbortException("Error during Elastic Search reindex." + http.buildHttpErrorMessage(res))
         }
