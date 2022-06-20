f3564bc49cd39d95ee6792b9cc6e20b0f95f7fdd
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-06-20 09:35:09 +0300

    for test reindex return

--- src/org/folio/rest/Okapi.groovy
+++ src/org/folio/rest/Okapi.groovy
@@ -172,16 +172,15 @@ class Okapi extends GeneralParameters {
      * @param admin_user
      * @return
      */
-    void reindexId(tenant, admin_user) {
-        auth.getOkapiToken(tenant, admin_user)
+    void reindexId(tenant) {
+        auth.getOkapiToken(tenant, tenant.admin_user)
         String url = okapiUrl + "/instance-storage/reindex/${reIndexElasticsearch(res.content).id}"
         ArrayList headers = [
             [name: 'Content-type', value: "application/json"],
             [name: 'X-Okapi-Tenant', value: tenant.getId()],
             [name: 'X-Okapi-Token', value: tenant.getAdmin_user().getToken() ? tenant.getAdmin_user().getToken() : '', maskValue: true]
         ]
-        def jobId = tools.jsonParse(res.content).id
-        def res = http.getRequest(url, jobId, headers)
+        def res = http.getRequest(url, headers)
         if (res.status == HttpURLConnection.HTTP_OK) {
             logger.info(tools.jsonParse(res.content).status)
         }else {
