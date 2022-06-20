58a39268f2c35c2a8088f77fed3bb6dc17354b38
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-06-20 12:52:18 +0300

    RANCHER-316 add runix /pgadmin helm

--- terraform/rancher/project/postgresql.tf
+++ terraform/rancher/project/postgresql.tf
@@ -127,21 +127,21 @@ resource "rancher2_app" "pgadmin4" {
   template_name = "pgadmin4"
   template_version = "1.9.11"
 
-  answers = {
-    "env.email"                                                  = var.pgadmin_username
-    "env.password"                                               = var.pgadmin_password
-    "service.type"                                               = "NodePort"
-    "ingress.enabled"                                            = "true"
-    "ingress.annotations.kubernetes\\.io/ingress\\.class"        = "alb"
-    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/scheme" = "internet-facing"
-    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/group\\.name" = join(".", [
-      data.rancher2_cluster.cluster.name, rancher2_project.project.name
-    ])
-    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/listen-ports"  = "[{\"HTTPS\":443}]"
-    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/success-codes" = "200-399"
-    "ingress.hosts[0].paths[0]"                                         = "/*"
-    "ingress.hosts[0].host" = join(".", [
-      join("-", [data.rancher2_cluster.cluster.name, rancher2_project.project.name, "pgadmin"]), var.root_domain
-    ])
-  }
+#  answers = {
+#    "env.email"                                                  = var.pgadmin_username
+#    "env.password"                                               = var.pgadmin_password
+#    "service.type"                                               = "NodePort"
+#    "ingress.enabled"                                            = "true"
+#    "ingress.annotations.kubernetes\\.io/ingress\\.class"        = "alb"
+#    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/scheme" = "internet-facing"
+#    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/group\\.name" = join(".", [
+#      data.rancher2_cluster.cluster.name, rancher2_project.project.name
+#    ])
+#    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/listen-ports"  = "[{\"HTTPS\":443}]"
+#    "ingress.annotations.alb\\.ingress\\.kubernetes\\.io/success-codes" = "200-399"
+#    "ingress.hosts[0].paths[0]"                                         = "/*"
+#    "ingress.hosts[0].host" = join(".", [
+#      join("-", [data.rancher2_cluster.cluster.name, rancher2_project.project.name, "pgadmin"]), var.root_domain
+#    ])
+#  }
 }
