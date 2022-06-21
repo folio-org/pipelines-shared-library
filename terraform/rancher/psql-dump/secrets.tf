8e43702ad25a50be424dac0b819171e9051ec820
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-06-21 10:49:23 +0300

    RANCHER-313

--- terraform/rancher/psql-dump/secrets.tf
+++ terraform/rancher/psql-dump/secrets.tf
@@ -3,9 +3,9 @@ resource "rancher2_secret" "s3-postgres-backups-credentials" {
   project_id   = data.terraform_remote_state.project_state.outputs.rancher2_project_id
   namespace_id = data.terraform_remote_state.project_state.outputs.rancher2_project_namespace
   data = {
-    POSTGRES_USER                = base64encode(data.terraform_remote_state.project_state.outputs.pg_username)
-    POSTGRES_PASSWORD            = base64encode(data.terraform_remote_state.project_state.outputs.pg_password)
-    POSTGRES_DATABASE            = base64encode(data.terraform_remote_state.project_state.outputs.pg_dbname)
+    POSTGRES_USER                = base64encode(var.pg_username)
+    POSTGRES_PASSWORD            = base64encode(var.pg_password)
+    POSTGRES_DATABASE            = base64encode(var.pg_dbname)
     POSTGRES_HOST                = base64encode("pg-folio")
     S3_BACKUP_PATH               = base64encode("s3://folio-postgresql-backups")
     RANCHER_CLUSTER_PROJECT_NAME = base64encode(join("-", [var.rancher_cluster_name, var.rancher_project_name]))
