660c5a32596d1bf455fc7a78b797588d77f32122
maryia lazavik <maryia_lazavik@epam.com>
2022-06-28 11:07:26 +0400

    add servers to pdamin4 answers

--- terraform/rancher/project/postgresql.tf
+++ terraform/rancher/project/postgresql.tf
@@ -156,5 +156,10 @@ resource "rancher2_app" "pgadmin4" {
     "ingress.hosts[0].host" = join(".", [
       join("-", [data.rancher2_cluster.this.name, rancher2_project.this.name, "pgadmin"]), var.root_domain
     ])
+    "serverDefinitions.enabled"                             = "true"
+    "serverDefinitions.servers.username"                    = var.pg_username
+    "serverDefinitions.servers.host"                        = var.pg_embedded
+    "serverDefinitions.servers.MaintenanceDB"               = var.pg_dbname
+    "serverDefinitions.servers.password"                    = var.pg_password
   }
 }
