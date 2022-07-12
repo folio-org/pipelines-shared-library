68f4ca2322e3b7bfbfeb005c7cf2a207a4bc83c1
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2022-07-12 23:32:27 +0300

    Add lib version

--- pipelines/rancher/cluster.groovy
+++ pipelines/rancher/cluster.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@OLD_RANCHER') _
 
 import org.folio.Constants
 import org.jenkinsci.plugins.workflow.libs.Library
--- pipelines/rancher/project.groovy
+++ pipelines/rancher/project.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@OLD_RANCHER') _
 
 import org.folio.Constants
 import org.folio.rest.Deployment
