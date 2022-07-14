a5903a0872ef5c45ed4301da7c7c862753c0c53e
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-07-14 22:35:56 +0300

    RANCHER-319

--- pipelines/rancher/psql-dump.groovy
+++ pipelines/rancher/psql-dump.groovy
@@ -19,6 +19,7 @@ def date_time = LocalDateTime.now().toString()
 String started_by_user = "volodymyr-kartsev"
 //String db_backup_name = "backup_${date_time}-${started_by_user}.pgdump"
 String db_backup_name = params.restore_postgresql_from_backup ? params.restorePostgresqlBackupName : "backup_${date_time}-${started_by_user}.psql"
+String db_backup_name2 = restore_postgresql_from_backup ? true : false
 
 ansiColor('xterm') {
     if (params.refreshParameters) {
@@ -42,6 +43,7 @@ ansiColor('xterm') {
                 docker.image(Constants.PSQL_DUMP_DOCKER_CLIENT).inside("-u 0:0 --entrypoint=") {
                     psqlDumpMethods.configureKubectl(Constants.RANCHER_CLUSTERS_DEFAULT_REGION, params.rancher_cluster_name)
                     psqlDumpMethods.configureHelm(Constants.FOLIO_HELM_REPOSITORY_NAME, Constants.FOLIO_HELM_REPOSITORY_URL)
+                    println(params.restore_postgresql_from_backup)
                     try {
                         if (params.restore_postgresql_from_backup == false) {
                             psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_REPOSITORY_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name)
