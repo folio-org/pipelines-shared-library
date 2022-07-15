988cc0753ae22f1351bb4ddb8c4501a7ec105e60
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-07-15 12:27:39 +0300

    RANCHER-319

--- terraform/rancher/project/secrets.tf
+++ terraform/rancher/project/secrets.tf
@@ -100,7 +100,6 @@ resource "rancher2_secret" "s3-postgres-backups-credentials" {
   project_id   = rancher2_project.this.id
   namespace_id = rancher2_namespace.this.name
   data = {
-    RANCHER_CLUSTER_PROJECT_NAME = base64encode(join("/", [data.rancher2_cluster.this.name, rancher2_project.this.name]))
     AWS_BUCKET                   = base64encode(var.s3_postgres-backups-bucket-name)
     AWS_ACCESS_KEY_ID            = base64encode(var.s3_postgres_backups_access_key)
     AWS_SECRET_ACCESS_KEY        = base64encode(var.s3_postgres_backups_secret_key)
