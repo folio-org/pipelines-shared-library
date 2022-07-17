e5e9496b12e4cbb08fddebf0ec09f41444db085d
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-07-17 18:48:28 +0300

    RANCHER-319

--- pipelines/rancher/psql-dump.groovy
+++ pipelines/rancher/psql-dump.groovy
@@ -16,7 +16,7 @@ properties([
     ])
 ])
 
-def date_time = LocalDateTime.now().toString()
+def date_time = LocalDateTime.now().withNano(0).toString()
 String started_by_user = params.restore_postgresql_from_backup ? '' : currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]['userId']
 String db_backup_name = params.restore_postgresql_from_backup ? params.restore_postgresql_backup_name : "backup_${date_time}-${started_by_user}.psql"
 
