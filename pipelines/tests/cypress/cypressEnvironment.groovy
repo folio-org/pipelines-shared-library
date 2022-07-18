6896c6bb7197e28955729ff818159dba5184bb40
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2022-07-19 00:43:32 +0300

    RANCHER-369 Create wrapper job for cypress and karate environment set up

---
+++ pipelines/tests/cypress/cypressEnvironment.groovy
@@ -0,0 +1,71 @@
+#!groovy
+import org.folio.Constants
+
+@Library('pipelines-shared-library@RANCHER-369') _
+
+properties([
+    buildDiscarder(logRotator(numToKeepStr: '20')),
+    disableConcurrentBuilds(),
+    parameters([
+        booleanParam(name: 'refreshParameters', defaultValue: false, description: 'Do a dry run and refresh pipeline configuration'),
+        choice(name: 'action', choices: ['apply', 'destroy'], description: '(Required) Choose what should be done with cluster'),
+        jobsParameters.repository(),
+        jobsParameters.folioBranch(),
+        jobsParameters.okapiVersion(),
+        string(name: 'github_teams', defaultValue: '', description: 'Coma separated list of GitHub teams who need access to project')
+    ])
+])
+
+def clusterName = "folio-testing"
+def projectName = "cypress"
+def okapiUrl = "https://${clusterName}-${projectName}-okapi.ci.folio.org"
+def frontendUrl = "https://${clusterName}-${projectName}.ci.folio.org"
+def tenantId = "diku"
+
+ansiColor("xterm") {
+    if (params.refreshParameters) {
+        currentBuild.result = "ABORTED"
+        error("DRY RUN BUILD, NO STAGE IS ACTIVE!")
+    }
+    node("jenkins-agent-java11") {
+        try {
+            stage("Create environment") {
+                build job: Constants.JENKINS_JOB_PROJECT,
+                    parameters: [
+                        booleanParam(name: 'refreshParameters', value: false),
+                        string(name: 'action', value: params.action),
+                        string(name: 'folio_repository', value: params.folio_repository),
+                        string(name: 'folio_branch', value: params.folio_branch),
+                        string(name: 'okapi_version', value: params.okapi_version),
+                        string(name: 'rancher_cluster_name', value: clusterName),
+                        string(name: 'rancher_project_name', value: projectName),
+                        booleanParam(name: 'build_ui', value: true),
+                        string(name: 'env_config', value: "development"),
+                        booleanParam(name: 'enable_modules', value: true),
+                        string(name: 'tenant_id', value: tenantId),
+                        string(name: 'tenant_name', value: "Cypress tenant"),
+                        string(name: 'tenant_description', value: "Cypress tests main tenant"),
+                        booleanParam(name: 'reindex_elastic_search', value: true),
+                        booleanParam(name: 'recreate_index_elastic_search', value: false),
+                        booleanParam(name: 'load_reference', value: true),
+                        booleanParam(name: 'load_sample', value: true),
+                        string(name: 'github_teams', value: params.github_teams),
+                        booleanParam(name: 'pg_embedded', value: true),
+                        booleanParam(name: 'kafka_embedded', value: true),
+                        booleanParam(name: 'es_embedded', value: true),
+                        booleanParam(name: 's3_embedded', value: true),
+                        booleanParam(name: 'pgadmin4', value: true)
+                    ]
+            }
+        } catch (exception) {
+            println(exception)
+            error(exception.getMessage())
+        } finally {
+            stage("Cleanup") {
+                println(okapiUrl)
+                println(frontendUrl)
+                cleanWs notFailBuild: true
+            }
+        }
+    }
+}
