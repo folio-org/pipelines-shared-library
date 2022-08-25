fface988de072ff1012c6f50834b8154ced8cd2d
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-08-25 17:42:56 +0300

    RANCHER-396

--- pipelines/rancher/psql-dump-restore.groovy
+++ pipelines/rancher/psql-dump-restore.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-396') _
 
 import org.folio.Constants
 import org.jenkinsci.plugins.workflow.libs.Library
@@ -22,6 +22,7 @@ def date_time = LocalDateTime.now().withNano(0).toString()
 String started_by_user = params.restore_postgresql_from_backup ? '' : currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]['userId']
 String db_backup_name = params.restore_postgresql_from_backup ? params.restore_postgresql_backup_name : "backup_${date_time}-${started_by_user}"
 OkapiUser admin_user = okapiSettings.adminUser()
+String s3_postgres_backups_bucket_name = "s3://folio-postgresql-backups"
 
 ansiColor('xterm') {
     if (params.refreshParameters) {
@@ -48,6 +49,7 @@ ansiColor('xterm') {
                     try {
                         if (params.restore_postgresql_from_backup == false) {
                             psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_REPOSITORY_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, admin_user.username, admin_user.password)
+                            psqlDumpMethods.savePlatformCompleteImageTag(params.rancher_project_name, params.rancher_cluster_name, db_backup_name, s3_postgres_backups_bucket_name, params.tenant_id_to_backup_modules_versions)
                             psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
                             println("\n\n\n" + "\033[32m" + "PostgreSQL backup process SUCCESSFULLY COMPLETED\nYou can find your backup in AWS s3 bucket folio-postgresql-backups/" +
                                 "${params.rancher_cluster_name}/${params.rancher_project_name}/${db_backup_name}" + "\n\n\n" + "\033[0m")
--- vars/psqlDumpMethods.groovy
+++ vars/psqlDumpMethods.groovy
@@ -10,8 +10,17 @@ def configureHelm(String repo_name, String repo_url) {
     }
 }
 
+def savePlatformCompleteImageTag(String project_namespace, String cluster_name, String db_backup_name, String s3_postgres_backups_bucket_name, String tenant_id) {
+    stage('Save platform complete image tag') {
+        sh "PLATFORM_COMPLETE_POD_LIST=\$(kubectl get pods --no-headers=true -o custom-columns=NAME_OF_MY_POD:.metadata.name -n ${project_namespace} | \
+        grep platform-complete); for IMAGE in $PLATFORM_COMPLETE_POD_LIST; \
+        do IMAGE_TAG=\$(kubectl get pod $IMAGE -n ${project_namespace} -o jsonpath=\"{.spec.containers[*].image}\" | \
+        sed 's/.*://' | grep ${tenant_id});if [ ! -z $IMAGE_TAG  ];then break;fi;done; \
+        echo $IMAGE_TAG > ${db_backup_name}_image_tag.txt; aws s3 cp ${db_backup_name}_image_tag.txt ${s3_postgres_backups_bucket_name}/${cluster_name}/${project_namespace}/${db_backup_name}/"
+    }
+}
 
-def backupHelmInstall(String build_id, String repo_name, String chart_name, String chart_version, String project_namespace, String cluster_name, db_backup_name, String tenant_id_to_backup_modules_versions, String tenant_admin_username_to_backup_modules_versions, String tenant_admin_password_to_backup_modules_versions) {
+def backupHelmInstall(String build_id, String repo_name, String chart_name, String chart_version, String project_namespace, String cluster_name, String db_backup_name, String tenant_id_to_backup_modules_versions, String tenant_admin_username_to_backup_modules_versions, String tenant_admin_password_to_backup_modules_versions) {
     stage('Helm install') {
         sh "helm install psql-dump-build-id-${build_id} ${repo_name}/${chart_name} --version ${chart_version} --set psql.projectNamespace=${project_namespace} \
         --set psql.jenkinsDbBackupName=${db_backup_name} --set psql.job.action='backup' --set psql.clusterName=${cluster_name} \
