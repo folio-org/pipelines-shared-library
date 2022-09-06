6d0eae83767aaadd676b094baaa07322f0909ef8
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-09-06 17:28:07 +0300

    RANCHER-433

--- vars/psqlDumpMethods.groovy
+++ vars/psqlDumpMethods.groovy
@@ -55,8 +55,8 @@ def getPlatformCompleteImageTag(String filePathName) {
     }
 }
 
-def getOkapiVersion(String installJson) {
-    String okapi = new JsonSlurperClassic().parseText(installJson)*.id.find{it ==~ /okapi-.*/}
+def getOkapiVersion(def installJson) {
+    String okapi = new JsonSlurperClassic().parseText(installJson.toString())*.id.find{it ==~ /okapi-.*/}
     return okapi.trim()
 }
 
