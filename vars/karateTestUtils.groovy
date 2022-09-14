599330a39fe99dbf4e0f7a1f1deb466d86c102e0
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-09-14 19:42:05 +0300

    test

--- vars/karateTestUtils.groovy
+++ vars/karateTestUtils.groovy
@@ -178,7 +178,7 @@ def getSlackColor(def buildStatus) {
 void syncJiraIssues(KarateTestsExecutionSummary karateTestsExecutionSummary, TeamAssignment teamAssignment) {
     JiraClient jiraClient = getJiraClient()
     println("TEST2")
-    printl teamAssignment.toString()
+    println teamAssignment.toString()
 
     // find existing karate issues
     List<JiraIssue> issues = jiraClient.searchIssues(KarateConstants.KARATE_ISSUES_JQL, ["summary", "status"])
