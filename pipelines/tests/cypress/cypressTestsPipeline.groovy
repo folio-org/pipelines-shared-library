9d4a623090a39eb017dc730313ddbfc75f35bc8e
Shokhrukhmirzo Gulomov <Shokhrukhmirzo_Gulomov@epam.com>
2022-09-16 15:18:23 +0500

    Test

--- pipelines/tests/cypress/cypressTestsPipeline.groovy
+++ pipelines/tests/cypress/cypressTestsPipeline.groovy
@@ -1,7 +1,7 @@
 import org.folio.Constants
 import org.jenkinsci.plugins.workflow.libs.Library
 
-@Library('pipelines-shared-library@RANCHER-449') _
+@Library('pipelines-shared-library') _
 
 def cypressRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/stripes-testing.git"
 
