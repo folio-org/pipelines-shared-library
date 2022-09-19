f999ffbf82ee17c3575b65c17f223bb045b94df0
Shokhrukhmirzo Gulomov <Shokhrukhmirzo_Gulomov@epam.com>
2022-09-19 14:24:41 +0500

    Added ProjectID parameter

--- pipelines/tests/cypress/cypressTestsPipeline.groovy
+++ pipelines/tests/cypress/cypressTestsPipeline.groovy
@@ -47,6 +47,7 @@ properties([
         string(name: 'cypressParameters', defaultValue: "--env grepTags=smoke,grepFilterSpecs=true", description: 'Cypress execution parameters'),
         string(name: 'customBuildName', defaultValue: "", description: 'Custom name for build'),
         string(name: 'timeout', defaultValue: "4", description: 'Custom timeout for build. Set in hours'),
+        string(name: 'testrailProjectID', defaultValue: "", description: 'Project ID to be set', trim: true),
         string(name: 'testrailRunID', defaultValue: "", description: 'To enable TestRail integration, enter RunID from TestRail, ex. 2048', trim: true),
     ])
 ])
@@ -117,7 +118,7 @@ pipeline {
                                 if (params.testrailRunID) {
                                     // Run with TesTrail Integration
                                     env.TESTRAIL_HOST = "https://foliotest.testrail.io"
-                                    // env.TESTRAIL_PROJECTID = "22"
+                                    env.TESTRAIL_PROJECTID = "${params.testrailProjectID}"
                                     withCredentials([usernamePassword(credentialsId: 'testrail-ut56', passwordVariable: 'TESTRAIL_PASSWORD', usernameVariable: 'TESTRAIL_USERNAME')]) {
                                         sh "cypress run --headless --browser ${browserName} ${params.cypressParameters} --env testRailRunId=${params.testrailRunID}"
                                     }
