96a78827d0bbd90c6dba5c9e96ed5f8715229c2d
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-09-21 10:27:25 +0300

    Move slack notification in the end

--- pipelines/tests/karate/karateTestsScheduledPipeline.groovy
+++ pipelines/tests/karate/karateTestsScheduledPipeline.groovy
@@ -148,18 +148,18 @@ pipeline {
                             }
                         }
 
-                        stage("Send slack notifications") {
-                            steps {
-                                script {
-                                    karateTestUtils.sendSlackNotification(karateTestsExecutionSummary, teamAssignment)
-                                }
-                            }
-                        }
-
                         stage("Sync jira tickets") {
                             steps {
                                 script {
                                     karateTestUtils.syncJiraIssues(karateTestsExecutionSummary, teamAssignment)
+                                }
+                            }
+                        }
+                        
+                        stage("Send slack notifications") {
+                            steps {
+                                script {
+                                    karateTestUtils.sendSlackNotification(karateTestsExecutionSummary, teamAssignment)
                                 }
                             }
                         }
