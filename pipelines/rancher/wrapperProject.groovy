d9c6f4ea7d749b5da969d1e9b1459794a09e8ed3
Renat Safin <renat.k.safin@gmail.com>
2022-09-23 16:46:19 +0500

    RANCHER-444 Final v.1

--- pipelines/rancher/wrapperProject.groovy
+++ pipelines/rancher/wrapperProject.groovy
@@ -1,14 +1,7 @@
 #!groovy
 @Library('pipelines-shared-library') _
 
-import org.folio.Constants
-import org.folio.rest.Deployment
-import org.folio.rest.model.Email
-import org.folio.rest.model.OkapiUser
-import org.folio.rest.model.OkapiTenant
-import org.folio.utilities.Tools
 import org.jenkinsci.plugins.workflow.libs.Library
-
 
 properties([
     buildDiscarder(logRotator(numToKeepStr: '20')),
@@ -19,6 +12,7 @@ properties([
         jobsParameters.folioBranch(),
         jobsParameters.okapiVersion(),
 //        jobsParameters.projectName(),
+        // Question 1: How should i get projectName list dynamically ?
         choice(name: 'projectName', choices: ["bama","concorde","core-platform","ebsco-core","falcon","firebird","folijet",
                                               "metadata","prokopovych","scout","spanish","spitfire","sprint-testing",
                                               "stripes-force","thor","thunderjet","unam","vega","volaris","volaris-2nd"],
@@ -27,113 +21,35 @@ properties([
         jobsParameters.loadSample()
     ])
 ])
-["bama","concorde","core-platform","ebsco-core","falcon","firebird","folijet","metadata","prokopovych","scout","spanish","spitfire","sprint-testing","stripes-force","thor","thunderjet","unam","vega","volaris","volaris-2nd"]
 def rancherClusters = "folio-dev"
-//def frontendImageTag = "folio-testing-karate-diku-e025d02"
 def tenantId = "diku"
 def tenantName = "Datalogisk Institut"
 def tenantDescription = "Danish Library Technology Institute"
-//def pgPassword = "postgres_password_123!"
-//def pgAdminPassword    = "SuperSecret"
+//def pgPassword = "**********"
+//def pgAdminPassword    = "********"
+//Question 2: Should I write passwd here in open mode ? Or reused this parameters from Rancher/Project ?
 def envConfig = "development"
 def agents = "jenkins-agent-java11"
 
-//def testrefreshParameters = false
-//def testaction = params.action
-//def testloadReference = params.loadReference
-//def testfolio_repository = params.repository
-//def testfolio_branch = params.folioBranch
-//def testokapi_version = params.okapiVersion
-//def testrancher_cluster_name = rancherClusters
-//def testrancher_project_name = params.projectName
-//def testbuild_ui = true
-//def testfrontendImageTag = frontendImageTag
-//def testenvType = envConfig
-//def testenableModules = true
-//def testagents = agents
-//def testtenantId = tenantId
-//def testtenantName = tenantName
-//def testtenantDescription = tenantDescription
-//def testreindexElasticsearch = true
-//def testrecreateindexElasticsearch = false
-//def testload_reference = params.loadReference
-//def testload_sample = params.loadSample
-//def testpgPassword = pgPassword
-//def testpgAdminPassword = pgAdminPassword
-//def testgithub_teams = ''
-//def testrestorePostgresqlFromBackup = false
-//def testtenantIdToRestoreModulesVersions = tenantId
-//def testrestorePostgresqlBackupName = ''
-//def testpg_embedded = true
-//def testkafka_embedded = true
-//def testes_embedded = true
-//def tests3_embedded = true
-//def testpgadmin4 = true
 node('jenkins-agent-java11') {
-//    stage('Test') {
-//        println(testaction)
-//        println(testloadReference)
-//        println(testfolio_repository)
-//        println(testfolio_branch)
-//        println(testokapi_version)
-//        println(testrancher_cluster_name)
-//        println(testrancher_project_name)
-//        println(testbuild_ui)
-//        println(testload_sample)
-//        println(testload_reference)
-//        println(testrefreshParameters)
-//        println(testfrontendImageTag)
-//        println(testenvType)
-//        println(testenableModules)
-//        println(testagents)
-//        println(testtenantId)
-//        println(testtenantName)
-//        println(testtenantDescription)
-//        println(testreindexElasticsearch)
-//        println(testrecreateindexElasticsearch)
-//        println(testpgPassword)
-//        println(testpgAdminPassword)
-//        println(testgithub_teams)
-//        println(testrestorePostgresqlFromBackup)
-//        println(testtenantIdToRestoreModulesVersions)
-//        println(testrestorePostgresqlBackupName)
-//        println(testpg_embedded)
-//        println(testkafka_embedded)
-//        println(testes_embedded)
-//        println(tests3_embedded)
-//        println(testpgadmin4)
-//    }
-//}
         try {
             stage('Build project Job') {
-                if (params.action == 'apply') {
+                if (params.action == 'apply' || params.action == 'destroy') {
                     build job: 'Rancher/Project',
                         parameters: [
-//                            booleanParam(name: 'refreshParameters', value: false),
+                            // Question 3 : Should I put here all parmeters or only parameters from properties.params ?
                             string(name: 'action', value: params.action),
                             string(name: 'folio_repository', value: params.folio_repository),
                             string(name: 'folio_branch', value: params.folio_branch),
                             string(name: 'okapi_version', value: params.okapi_version),
                             string(name: 'rancher_cluster_name', value: rancherClusters),
                             string(name: 'rancher_project_name', value: params.projectName),
-//                            booleanParam(name: 'build_ui', value: true),
-//                            string(name: 'frontend_image_tag', value: frontendImageTag),
                             string(name: 'env_config', value: envConfig),
                             booleanParam(name: 'enable_modules', value: true),
-//                            string(name: 'agent', value: agents),
-//                            string(name: 'tenant_id', value: tenantId),
-//                            string(name: 'tenant_name', value: tenantName),
-//                            string(name: 'tenant_description', value: tenantDescription),
                             booleanParam(name: 'reindex_elastic_search', value: true),
                             booleanParam(name: 'recreate_index_elastic_search', value: false),
                             booleanParam(name: 'load_reference', value: params.load_reference),
                             booleanParam(name: 'load_sample', value: params.load_sample),
-//                            string(name: 'pg_password', value: pgPassword),
-//                            string(name: 'pgadmin_password', value: pgAdminPassword),
-//                            string(name: 'github_teams', value: ''),
-//                            booleanParam(name: 'restore_postgresql_from_backup', value: false),
-//                            string(name: 'restore_tenant_id', value: tenantId),
-//                            booleanParam(name: 'restore_postgresql_backup_name', value: ''),
                             booleanParam(name: 'pg_embedded', value: true),
                             booleanParam(name: 'kafka_embedded', value: true),
                             booleanParam(name: 'es_embedded', value: true),
