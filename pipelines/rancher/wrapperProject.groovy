1edeb98e23c4cf68b6bd1b44bf278288a51e61fe
Renat Safin <renat.k.safin@gmail.com>
2022-09-26 23:10:55 +0500

    RANCHER-444 Final v.2

--- pipelines/rancher/wrapperProject.groovy
+++ pipelines/rancher/wrapperProject.groovy
@@ -16,13 +16,6 @@ properties([
         jobsParameters.loadSample()
     ])
 ])
-//def rancherClusters = "folio-dev"
-//def tenantId = "diku"
-//def tenantName = "Datalogisk Institut"
-//def tenantDescription = "Danish Library Technology Institute"
-//def envConfig = "development"
-//def agents = "jenkins-agent-java11"
-
 node('jenkins-agent-java11') {
         try {
             stage('Build project Job') {
@@ -33,19 +26,9 @@ node('jenkins-agent-java11') {
                             string(name: 'folio_repository', value: params.folio_repository),
                             string(name: 'folio_branch', value: params.folio_branch),
                             string(name: 'okapi_version', value: params.okapi_version),
-//                            string(name: 'rancher_cluster_name', value: rancherClusters),
                             string(name: 'rancher_project_name', value: params.projectName),
-//                            string(name: 'env_config', value: envConfig),
-//                            booleanParam(name: 'enable_modules', value: true),
-//                            booleanParam(name: 'reindex_elastic_search', value: true),
-//                            booleanParam(name: 'recreate_index_elastic_search', value: false),
                             booleanParam(name: 'load_reference', value: params.load_reference),
-                            booleanParam(name: 'load_sample', value: params.load_sample),
-//                            booleanParam(name: 'pg_embedded', value: true),
-//                            booleanParam(name: 'kafka_embedded', value: true),
-//                            booleanParam(name: 'es_embedded', value: true),
-//                            booleanParam(name: 's3_embedded', value: true),
-//                            booleanParam(name: 'pgadmin4', value: true)
+                            booleanParam(name: 'load_sample', value: params.load_sample)
                         ]
                 }
             }
