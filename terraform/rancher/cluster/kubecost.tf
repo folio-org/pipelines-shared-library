e89fa1b98516bd313bbcfbf8e9aee3304836dfef
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-09-27 14:19:41 +0300

    RANCHER-451 Deploy Kubecost to cluster

---
+++ terraform/rancher/cluster/kubecost.tf
@@ -0,0 +1,60 @@
+#Creating a new project in Rancher.
+resource "rancher2_project" "this" {
+  depends_on                = [rancher2_cluster_sync.this]
+  count                     = var.register_in_rancher ? 1 : 0
+  provider                  = rancher2
+  name                      = "Kubecost"
+  cluster_id                = rancher2_cluster_sync.this[0].cluster_id
+  enable_project_monitoring = false
+  container_resource_limit {
+    limits_memory   = "512Mi"
+    requests_memory = "400Mi"
+  }
+}
+
+# Create a new rancher2 Namespace assigned to cluster project
+resource "rancher2_namespace" "this" {
+  depends_on  = [rancher2_cluster_sync.this]
+  count       = var.register_in_rancher ? 1 : 0
+  name        = "kubecost"
+  project_id  = rancher2_project.this[0].id
+  description = "Project kubecost namespace"
+  container_resource_limit {
+    limits_memory   = "512Mi"
+    requests_memory = "400Mi"
+  }
+}
+
+# Create rancher2 Kubecost app in kubecost namespace
+resource "rancher2_app_v2" "kubecost" {
+  depends_on    = [rancher2_catalog_v2.kubecost]
+  count         = var.register_in_rancher ? 1 : 0
+  cluster_id    = rancher2_cluster_sync.this[0].cluster_id
+  namespace     = rancher2_namespace.this[0].name
+  name          = "kubecost"
+  repo_name     = "kubecost"
+  chart_name    = "cost-analyzer"
+  chart_version = "1.97.0"
+  force_upgrade = "true"
+  values        = <<-EOT
+    ingress:
+      enabled: true
+      hosts: 
+        - "${module.eks_cluster.cluster_id}-kubecost.${var.root_domain}"
+      paths: ["/*"]
+      annotations:
+        kubernetes.io/ingress.class: alb
+        alb.ingress.kubernetes.io/scheme: internet-facing
+        alb.ingress.kubernetes.io/group.name: ${module.eks_cluster.cluster_id}
+        alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
+        alb.ingress.kubernetes.io/success-codes: 200-399
+        alb.ingress.kubernetes.io/healthcheck-path: /
+        alb.ingress.kubernetes.io/load-balancer-attributes: idle_timeout.timeout_seconds=4000
+        alb.ingress.kubernetes.io/auth-idp-cognito: '{"UserPoolArn":"arn:aws:cognito-idp:us-west-2:732722833398:userpool/us-west-2_pz4VAvmj1","UserPoolClientId":"5qugmova8vbn1ilr7p9a2mv3aj", "UserPoolDomain":"folio-testing-kubecost"}'
+        alb.ingress.kubernetes.io/auth-on-unauthenticated-request: authenticate
+        alb.ingress.kubernetes.io/auth-scope: openid
+        alb.ingress.kubernetes.io/auth-session-cookie: AWSELBAuthSessionCookie
+        alb.ingress.kubernetes.io/auth-session-timeout: "3600"
+        alb.ingress.kubernetes.io/auth-type: cognito
+  EOT
+}
