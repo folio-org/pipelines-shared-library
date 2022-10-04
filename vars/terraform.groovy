660684fa13f7a37bf7ee10a821c03ba9d02f70a6
Renat Safin <renat.k.safin@gmail.com>
2022-10-04 14:43:33 +0500

    Fixed input message

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -119,7 +119,7 @@ void tfApplyFlow(Closure body) {
     retry(2) {
         sleep(60)
         tfPlan(config.working_dir, config.tf_vars)
-        input message: "test"
+        input 'test'
         tfApply(config.working_dir)
     }
     if(config.postAction){
