507f42414603f745dd52c0a74b24860b68a0a9ac
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-04 10:33:11 +0300

    RANCHER-466

---
+++ pipelines/automation/ecr-cleanup-images.groovy
@@ -0,0 +1,40 @@
+@Library('pipelines-shared-library@RANCHER-466') _
+
+import org.folio.utilities.Tools
+import org.jenkinsci.plugins.workflow.libs.Library
+
+pipeline {
+    agent { label 'jenkins-agent-java11' }
+
+    triggers {
+        cron('H 0 * * 1-6')
+    }
+
+    options {
+        disableConcurrentBuilds()
+    }
+
+    /*parameters {
+        string(name: 'branch', defaultValue: 'master', description: '')
+    }*/
+
+    stages {
+        stage("Cleanup us-west-2 ui-bundle repo") {
+            steps {
+                script {
+                    jobsParameters.clustersList().each {val->
+                        println(val)
+                    }
+                }
+            }
+        }
+
+        stage("Cleanup us-west-2 mod-* and okapi repos") {
+            steps {
+                script {
+
+                }
+            }
+        }
+    }
+}
