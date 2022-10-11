4449f9bcab268d7c88aeec73846bcba85d4e94d6
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2022-10-11 13:37:08 +0300

    Edge module support for jobsParameters.backendModule()

--- vars/jobsParameters.groovy
+++ vars/jobsParameters.groovy
@@ -188,7 +188,7 @@ String patternModuleVersion = /^(?<moduleName>.*)-(?<moduleVersion>(0|[1-9]\\d*)
 def installJson = new URL('https://raw.githubusercontent.com/folio-org/platform-complete/snapshot/install.json').openConnection()
 if (installJson.getResponseCode().equals(200)) {
     List modules_list = ['okapi']
-    new JsonSlurperClassic().parseText(installJson.getInputStream().getText())*.id.findAll { it ==~ /mod-.*/ }.each { value ->
+    new JsonSlurperClassic().parseText(installJson.getInputStream().getText())*.id.findAll { it ==~ /mod-.*|edge-.*/ }.each { value ->
         def matcherModule = value =~ patternModuleVersion
         assert matcherModule.matches()
         modules_list.add(matcherModule.group(nameGroup))
