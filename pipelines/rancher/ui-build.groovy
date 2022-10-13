b36d833bb7051a6611ecb5c0c7af250749d34a7e
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-13 16:05:41 +0300

    RANCHER-492

--- pipelines/rancher/ui-build.groovy
+++ pipelines/rancher/ui-build.groovy
@@ -7,6 +7,8 @@ import org.jenkinsci.plugins.workflow.libs.Library
 
 @Library('pipelines-shared-library@RANCHER-492') _
 
+}}
+
 properties([
     buildDiscarder(logRotator(numToKeepStr: '20')),
     disableConcurrentBuilds(),
