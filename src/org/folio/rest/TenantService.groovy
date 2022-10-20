90a645e63f49accdeb4d842a261783b5717fa794
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-20 10:49:46 +0300

    RANCHER-472

--- src/org/folio/rest/TenantService.groovy
+++ src/org/folio/rest/TenantService.groovy
@@ -31,7 +31,7 @@ class TenantService extends GeneralParameters {
 
     void createTenant(OkapiTenant tenant, OkapiUser admin_user, List enableList, Email email, String stripes_url) {
         if (tenant && admin_user) {
-            okapi.getInstalledModules('test6')
+            println(okapi.getInstalledModules('test6'))
             /*okapi.createTenant(tenant)
             okapi.enableDisableUpgradeModulesForTenant(tenant, okapi.buildInstallList(["okapi"], "enable"))
             okapi.enableDisableUpgradeModulesForTenant(tenant, enableList, 900000)
