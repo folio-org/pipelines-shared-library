98a206cd247c15a1b93479ed0cb340cd365a04a9
Renat Safiulin <112557825+renatsafiulin@users.noreply.github.com>
2022-10-21 17:31:11 +0500

    Rancher 477 (#112)
    * Update project.groovy and terraform.groovy
    
    * Updated project.groovy
    
    * Update kafka.tf
    
    * 1-st version of task
    
    * Update project.groovy
    
    * Update kafka.tf
    
    * Update kafka.tf
    
    * Updated kafka.tf and added repo_name to catalogs.tf
    
    * Update kafka.tf
    
    * Update kafka.tf
    
    * Update kafka.tf
    
    * Change kafka-ui ver. to 0.4.3
    
    * kafka-ui ver 0.4.1
    
    * Update kafka-ui version to 0.4.2
    
    * Update kafka-ui version to 0.4.3
    
    * Update kafka.tf
    
    * revert changes in project.groovy
    
    Co-authored-by: Renat Safin <renat.k.safin@gmail.com>

--- terraform/rancher/project/kafka.tf
+++ terraform/rancher/project/kafka.tf
@@ -33,7 +33,6 @@ resource "rancher2_app_v2" "kafka" {
     heapOpts: "-Xmx3277m -Xms1024m"
   EOT
 }
-
 resource "aws_security_group" "kafka" {
   count       = var.kafka_embedded ? 0 : 1
   name        = "allow-kafka-${local.env_name}"
@@ -59,9 +58,8 @@ resource "aws_security_group" "kafka" {
     var.tags,
     {
       Name = "allow-kafka"
-  })
+    })
 }
-
 resource "aws_msk_configuration" "this" {
   count             = var.kafka_embedded ? 0 : 1
   kafka_versions    = [var.kafka_version]
@@ -71,7 +69,6 @@ resource "aws_msk_configuration" "this" {
 auto.create.topics.enable = true
 PROPERTIES
 }
-
 resource "aws_msk_cluster" "this" {
   count                  = var.kafka_embedded ? 0 : 1
   cluster_name           = "kafka-${local.env_name}"
@@ -86,7 +83,7 @@ resource "aws_msk_cluster" "this" {
 
   broker_node_group_info {
     instance_type   = var.kafka_instance_type
-#    client_subnets  = data.aws_subnets.private.ids
+    #    client_subnets  = data.aws_subnets.private.ids
     client_subnets  = slice(data.aws_subnets.private.ids, 0, var.kafka_number_of_broker_nodes)
     security_groups = [aws_security_group.kafka[count.index].id]
     storage_info {
@@ -108,5 +105,41 @@ resource "aws_msk_cluster" "this" {
       name    = "kafka-${local.env_name}"
       version = var.kafka_version
       devTeam = var.rancher_project_name
-  })
+    })
+}
+resource "rancher2_app_v2" "kafka_ui" {
+  count         = var.kafka_ui ? 1 : 0
+  cluster_id    = data.rancher2_cluster.this.id
+  namespace     = rancher2_namespace.this.name
+  name          = "kafka-ui"
+  repo_name     = "provectus"
+  chart_name    = "kafka-ui"
+  chart_version = "0.4.3"
+  force_upgrade = "true"
+  values        = <<-EOT
+    service:
+      type: NodePort
+    ingress:
+      host: ${join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "kafka-ui"]), var.root_domain])}
+      path: "/"
+      enabled: true
+      annotations:
+        kubernetes.io/ingress.class: alb
+        alb.ingress.kubernetes.io/scheme: internet-facing
+        alb.ingress.kubernetes.io/group.name: ${local.group_name}
+        alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
+        alb.ingress.kubernetes.io/success-codes: 200-399
+
+    yamlApplicationConfig:
+      kafka:
+        clusters:
+          - name: ${data.rancher2_cluster.this.name}
+            bootstrapServers: ${var.kafka_embedded ? "kafka" : element(split(":", aws_msk_cluster.this[0].bootstrap_brokers), 0)}:9092
+      auth:
+        type: disabled
+      management:
+        health:
+          ldap:
+            enabled: false
+  EOT
 }
