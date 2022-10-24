0c5c02b35f537b86314512a962bc1ae5995a2579
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-24 19:25:06 +0300

    RANCHER-472

--- vars/jobsParameters.groovy
+++ vars/jobsParameters.groovy
@@ -279,8 +279,8 @@ def tenantDescription() {
     return _paramString('tenant_description', defaultTenant().description, 'Description used for tenant creation')
 }
 
-def referenceTenantId(String ref_tenant_id = defaultTenant().id) {
-    return _paramString('reference_tenant_id', ref_tenant_id, 'Id used to extract list of installed modules. For Rancher environments it\'s usually diku')
+def referenceTenantId(String reference_tenant_id = defaultTenant().id) {
+    return _paramString('reference_tenant_id', reference_tenant_id, 'Id used to extract list of installed modules. For Rancher environments it\'s usually diku')
 }
 
 def additionalTenantId() {
