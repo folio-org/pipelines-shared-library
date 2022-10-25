03b4b6d793e24113f56601899f9d0063b3309adf
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-25 16:50:02 +0300

    RANCHER-472

--- pipelines/rancher/create-tenant.groovy
+++ pipelines/rancher/create-tenant.groovy
@@ -38,7 +38,10 @@ List modules_to_install = []
 String core_modules = "mod-permissions, mod-users, mod-authtoken"
 
 if (params.install_list && !params.refresh_parameters){
+    println(params.install_list.toString())
+    println(core_modules)
     core_modules = core_modules + params.install_list.toString()
+    println(core_modules)
     core_modules.split(',').each {module->
         modules_to_install.add(okapi.getModuleIdFromInstallJson(installed_modules, module.toString().trim()))}
     modules_to_install = okapi.buildInstallList(modules_to_install, 'enable')
