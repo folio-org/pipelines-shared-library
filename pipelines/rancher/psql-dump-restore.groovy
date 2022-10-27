b4505656ca36e47e64f7d7646f53a8a36999c1fb
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-27 15:29:16 +0300

    RANCHER-503

--- pipelines/rancher/psql-dump-restore.groovy
+++ pipelines/rancher/psql-dump-restore.groovy
@@ -42,6 +42,8 @@ ansiColor('xterm') {
             }
 
             helm.k8sClient {
+                psqlDumpMethods.configureKubectl(Constants.RANCHER_CLUSTERS_DEFAULT_REGION, params.rancher_cluster_name)
+                psqlDumpMethods.configureHelm(Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.FOLIO_HELM_HOSTED_REPO_URL)
                 try {
                     if (params.restore_postgresql_from_backup == false) {
                         psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, admin_user.username, admin_user.password)
