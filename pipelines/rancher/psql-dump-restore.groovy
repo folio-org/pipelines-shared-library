6f7aaa84cd537d0267c14700ca1b3105462b711e
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-31 14:59:09 +0200

    Revert "RANCHER-503"
    This reverts commit 0907c3a8bdbb482fdf3d0cae198677dc693c44c4.

--- pipelines/rancher/psql-dump-restore.groovy
+++ pipelines/rancher/psql-dump-restore.groovy
@@ -47,7 +47,7 @@ ansiColor('xterm') {
                 psqlDumpMethods.configureHelm(Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.FOLIO_HELM_HOSTED_REPO_URL)
                 try {
                     if (params.restore_from_backup == false) {
-                        psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, params.admin_username, params.admin_password, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
+                        psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, admin_user.username, admin_user.password, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
                         psqlDumpMethods.savePlatformCompleteImageTag(params.rancher_project_name, params.rancher_cluster_name, db_backup_name, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, params.tenant_id_to_backup_modules_versions)
                         psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
                         println("\n\n\n" + "\033[32m" + "PostgreSQL backup process SUCCESSFULLY COMPLETED\nYou can find your backup in AWS s3 bucket folio-postgresql-backups/" +
