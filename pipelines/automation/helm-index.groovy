d412b4e8a8eb6c43b9f179c0996da50fd37a7ff8
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-11-22 17:37:55 +0200

    add const

--- pipelines/automation/helm-index.groovy
+++ pipelines/automation/helm-index.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-535') _
 
 import org.folio.Constants
 import org.folio.utilities.Tools
@@ -51,7 +51,7 @@ ansiColor('xterm') {
                         chartsForIndex.each {
                             sh """
                             CHART_PACKAGE="\$(helm package ${it} --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-                            curl -is -u "\$NEXUS_USERNAME:\$NEXUS_PASSWORD" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
+                            curl -is -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" ${Constants.FOLIO_HELM_V2_REPO_NAME} --upload-file "${CHART_PACKAGE}"
                             """
                         }
                     }
