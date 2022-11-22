ae1d0ba95979a00b450e99bcbdd7e55074c9539d
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-11-22 16:59:20 +0200

    Add chartsForIndex

--- pipelines/automation/helm-index.groovy
+++ pipelines/automation/helm-index.groovy
@@ -35,7 +35,7 @@ ansiColor('xterm') {
                         userRemoteConfigs: [[url: chartsRepositoryUrl]]
                     ])
                     if (params.indexAllCharts) {
-                        chartsForIndex = sh(script: "ls charts/", returnStdout: true).split('\\n')
+                        chartsForIndex = sh(script: "ls -d charts/*", returnStdout: true).split('\\n')
                     } else {
                         chartsForIndex = sh(script: "git diff HEAD~1 --name-only | cut -d'/' -f1-2 | sort | uniq", returnStdout: true).split('\\n')
                     }
@@ -44,32 +44,20 @@ ansiColor('xterm') {
                     }
                 }
             }
-            // stage("Test") {
-            //     withCredentials([
-            //         usernamePassword(credentialsId: Constants.NEXUS_PUBLISH_CREDENTIALS_ID, usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD'),
-            //     ]) {
-            //         if (params.indexAllCharts) {
-            //             helm.k8sClient {
-            //                 sh """
-            //                 for dir in charts/*;
-            //                     do 
-            //                         CHART_PACKAGE="\$(helm package \$dir --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-            //                         curl -is -u "\$NEXUS_USERNAME:\$NEXUS_PASSWORD" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
-            //                     done;
-            //                 """
-            //             }
-            //         } else {
-            //             helm.k8sClient {
-            //                 chartsForIndex.each {
-            //                     sh """
-            //                     CHART_PACKAGE="\$(helm package ${it} --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-            //                     curl -is -u "\$NEXUS_USERNAME:\$NEXUS_PASSWORD" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
-            //                     """
-            //                 }
-            //             }                        
-            //         }
-            //     }
-            // }
+            stage("Test") {
+                withCredentials([
+                    usernamePassword(credentialsId: Constants.NEXUS_PUBLISH_CREDENTIALS_ID, usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD'),
+                ]) {
+                    helm.k8sClient {
+                        chartsForIndex.each {
+                            sh """
+                            CHART_PACKAGE="\$(helm package ${it} --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
+                            curl -is -u "\$NEXUS_USERNAME:\$NEXUS_PASSWORD" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
+                            """
+                        }
+                    }
+                }
+            }
         } catch (exception) {
             println(exception)
             error(exception.getMessage())
@@ -80,77 +68,3 @@ ansiColor('xterm') {
         }
     }
 }
-
-def getChangedRegulations(regulationType) {
-    try {
-        sh(script: "git diff HEAD~1 HEAD -m -1 --name-only --diff-filter=ACMRT --pretty='format:' ", returnStdout: true).split('\\n')
-    } catch (Exception e) {
-        println("No changed ${regulationType} files found")
-    }
-}
-
-                            // for dir in charts/*;
-                            //     do 
-                            //         CHART_PACKAGE="\$(helm package \$dir --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-                            //         curl -is -u "\$NEXUS_USERNAME:\$NEXUS_PASSWORD" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
-                            //     done;
-
-
-
-//  CHART_PACKAGE="\$(helm package edge-caiasoft/ --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-//                             echo "DEBUG 2"
-//                             echo \$CHART_PACKAGE
-//                             ls
-//                             echo "DEBUG 3"
-//                             echo "Pushing \$CHART_PACKAGE to repo Nexus ..."
-//                             curl -is -u "\$AUTH" https://repository.folio.org/repository/folio-helm-v2-test/ --upload-file "\$CHART_PACKAGE"
-
-
-// import org.folio.Constants
-// import org.jenkinsci.plugins.workflow.libs.Library
-// import hudson.util.Secret
-
-// @Library('pipelines-shared-library') _
-
-// def chartsRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/folio-helm-v2.git"
-
-// pipeline {
-//     agent { label 'jenkins-agent-java11' }
-
-//     stages {
-//         stage('Checkout') {
-//             steps {
-//                 script {
-//                     sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
-//                         checkout([
-//                             $class           : 'GitSCM',
-//                             branches         : [[name: "*/master"]],
-//                             extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
-//                                                                   disableSubmodules  : false,
-//                                                                   parentCredentials  : false,
-//                                                                   recursiveSubmodules: true,
-//                                                                   reference          : '',
-//                                                                   trackingSubmodules : false]],
-//                             userRemoteConfigs: [[url: chartsRepositoryUrl]]
-//                         ])
-//                     }
-//                 }
-//             }
-//         }
-
-//         stage('Build tests') {
-//             steps {
-//                 helm.k8sClient {
-//                     sh "ls"
-//                     sh """
-//                         CHART_PACKAGE="\$(helm package edge-caiasoft/ --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
-//                         echo \$CHART_PACKAGE
-//                         ls
-//                     """
-//                     cleanWs()
-//                 }
-//             }
-//         }
-//     }
-// }
-
