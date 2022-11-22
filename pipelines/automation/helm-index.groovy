2b3a7571060a816363ef9cf3fde8c7008c6d3e5a
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-11-22 11:56:49 +0200

    test

--- pipelines/automation/helm-index.groovy
+++ pipelines/automation/helm-index.groovy
@@ -1,39 +1,40 @@
-import org.folio.Constants
-import org.jenkinsci.plugins.workflow.libs.Library
-import hudson.util.Secret
+#!groovy
 
 @Library('pipelines-shared-library') _
 
+import org.folio.Constants
+import org.folio.utilities.Tools
+import groovy.json.JsonSlurperClassic
+import org.jenkinsci.plugins.workflow.libs.Library
+
 def chartsRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/folio-helm-v2.git"
 
-pipeline {
-    agent { label 'jenkins-agent-java11' }
-
-    stages {
-        stage('Checkout') {
-            steps {
-                script {
-                    sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
-                        checkout([
-                            $class           : 'GitSCM',
-                            branches         : [[name: "*/master"]],
-                            extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
-                                                                  disableSubmodules  : false,
-                                                                  parentCredentials  : false,
-                                                                  recursiveSubmodules: true,
-                                                                  reference          : '',
-                                                                  trackingSubmodules : false]],
-                            userRemoteConfigs: [[url: chartsRepositoryUrl]]
-                        ])
+ansiColor('xterm') {
+    node('jenkins-agent-java11') {
+        try {
+            stage('Checkout') {
+                steps {
+                    script {
+                        sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
+                            checkout([
+                                $class           : 'GitSCM',
+                                branches         : [[name: "*/master"]],
+                                extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
+                                                                    disableSubmodules  : false,
+                                                                    parentCredentials  : false,
+                                                                    recursiveSubmodules: true,
+                                                                    reference          : '',
+                                                                    trackingSubmodules : false]],
+                                userRemoteConfigs: [[url: chartsRepositoryUrl]]
+                            ])
+                        }
                     }
                 }
             }
-        }
-
-        stage('Build tests') {
-            steps {
+            stage("Test") {
                 helm.k8sClient {
                     sh "ls"
+                    sh "pwd"
                     sh """
                         CHART_PACKAGE="\$(helm package edge-caiasoft/ --dependency-update | cut -d":" -f2 | tr -d '[:space:]')"
                         echo \$CHART_PACKAGE
@@ -42,6 +43,15 @@ pipeline {
                     cleanWs()
                 }
             }
+        } catch (exception) {
+            println(exception)
+            error(exception.getMessage())
+        } finally {
+            stage('Cleanup') {
+                cleanWs notFailBuild: true
+            }
         }
     }
 }
+
+
