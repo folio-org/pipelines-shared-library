a4299b438516f0112d9df07b33534680294bde48
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-12-05 18:20:56 +0200

    prepare for commit

--- vars/compare.groovy
+++ vars/compare.groovy
@@ -1,65 +1,5 @@
 import groovy.json.JsonOutput
 
-// def mapCurrent = '''[ {
-//   "id" : "folio_developer-6.2.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_handler-stripes-registry-1.2.0",
-//   "action" : "enable"
-// } ,{
-//   "id" : "folio_myprofile-7.1.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-erm-usage-data-provider-4.1.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-fund-1.1.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-package-title-4.1.0",
-//   "action" : "enable"
-// }]'''
-// def mapNew = '''[ {
-//   "id" : "folio_developer-6.2.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_handler-stripes-registry-SNAPSHOT-1.3.0",
-//   "action" : "enable"
-// } , {
-//   "id" : "folio_myprofile-7.1.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-erm-usage-data-provider-4.1.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-fund-1.3.0",
-//   "action" : "enable"
-// }, {
-//   "id" : "folio_plugin-find-package-title-4.0.0",
-//   "action" : "enable"
-// }]'''
-
-//def getContentAsJson (String inputMap) {
-//  def slurper = new groovy.json.JsonSlurper()
-//  return slurper.parseText(inputMap)
-//}
-
-// def getModuleMap(String inputString) {
-//   def slurper = new groovy.json.JsonSlurper()
-//   def moduleList = slurper.parseText(inputString)
-//   String nameGroup = "moduleName"
-//   String versionGroup = "moduleVersion"
-//   Map moduleMap = [:]
-//   def patternModuleVersion = /^(?<moduleName>.*)-(?<moduleVersion>(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*)$/
-
-//   moduleList.each { value ->
-//     def matcherModule = value.id =~ patternModuleVersion
-//     assert matcherModule.matches()
-//     moduleMap[matcherModule.group(nameGroup)] = matcherModule.group(versionGroup)
-//   }
-//   return moduleMap
-// }
-
 String compareVersion(String inA, String inB){
   List verA = inA.minus("-SNAPSHOT").tokenize('.') + '0'
   List verB = inB.minus("-SNAPSHOT").tokenize('.') + '0'
@@ -107,8 +47,3 @@ def createActionMaps(oldMap, newMap) {
     actionMaps.downgradeMap = downgradeMap
     return actionMaps
 }
-
-// def (Map updateMap, Map disableMap, Map downgradeMap) = createActionMaps(getModuleMap(mapCurrent), getModuleMap(mapNew)).values()
-// println updateMap
-// println disableMap
-// println downgradeMap
