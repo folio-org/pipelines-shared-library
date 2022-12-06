52cc79a8d34a51ee770c4dcacb109d3ee45b0d8b
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-12-06 17:30:27 +0200

    Fix service monitor

--- terraform/rancher/project/serviceMonitor.tf
+++ terraform/rancher/project/serviceMonitor.tf
@@ -1,15 +1,13 @@
 # Create ServiceMonitor for monitoring jvm metrics on 9991 port
 resource "kubectl_manifest" "service_monitor" {
-  depends_on         = [rancher2_app_v2.elasticsearch]
   count              = var.register_in_rancher ? 1 : 0
   provider           = kubectl
-  override_namespace = rancher2_namespace.logging[0].name
+  override_namespace = rancher2_namespace.this.name
   yaml_body          = <<YAML
 apiVersion: monitoring.coreos.com/v1
 kind: ServiceMonitor
 metadata:
   name: ${var.rancher_project_name}-metrics
-  namespace: ${var.rancher_project_name}
 spec:
   endpoints:
   - interval: 30s
