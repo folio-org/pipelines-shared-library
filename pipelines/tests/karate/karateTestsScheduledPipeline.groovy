9346b7fe85510812402ac8fef1eeababd6f85cec
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-12-14 11:40:20 +0200

    for tests

--- pipelines/tests/karate/karateTestsScheduledPipeline.groovy
+++ pipelines/tests/karate/karateTestsScheduledPipeline.groovy
@@ -139,30 +139,30 @@ pipeline {
                             }
                         }
 
-                        stage("Parse teams assignment") {
-                            steps {
-                                script {
-                                    def jsonContents = readJSON file: "teams-assignment.json"
-                                    teamAssignment = new TeamAssignment(jsonContents)
-                                }
-                            }
-                        }
+                        // stage("Parse teams assignment") {
+                        //     steps {
+                        //         script {
+                        //             def jsonContents = readJSON file: "teams-assignment.json"
+                        //             teamAssignment = new TeamAssignment(jsonContents)
+                        //         }
+                        //     }
+                        // }
 
-                        stage("Sync jira tickets") {
-                            steps {
-                                script {
-                                    karateTestUtils.syncJiraIssues(karateTestsExecutionSummary, teamAssignment)
-                                }
-                            }
-                        }
+                        // stage("Sync jira tickets") {
+                        //     steps {
+                        //         script {
+                        //             karateTestUtils.syncJiraIssues(karateTestsExecutionSummary, teamAssignment)
+                        //         }
+                        //     }
+                        // }
 
-                        stage("Send slack notifications") {
-                            steps {
-                                script {
-                                    karateTestUtils.sendSlackNotification(karateTestsExecutionSummary, teamAssignment)
-                                }
-                            }
-                        }
+                        // stage("Send slack notifications") {
+                        //     steps {
+                        //         script {
+                        //             karateTestUtils.sendSlackNotification(karateTestsExecutionSummary, teamAssignment)
+                        //         }
+                        //     }
+                        // }
                     }
                 }
             }
