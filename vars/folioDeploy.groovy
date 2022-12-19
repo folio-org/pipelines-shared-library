79d47f972f890f8341643a97b78b7866670f1cd8
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-12-19 13:21:13 +0200

    RANCHER-257

--- vars/folioDeploy.groovy
+++ vars/folioDeploy.groovy
@@ -98,6 +98,6 @@ void greenmail(Project project_config) {
     helm.k8sClient {
         awscli.getKubeConfig(Constants.AWS_REGION, project_config.getClusterName())
         helm.addRepo(Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.FOLIO_HELM_HOSTED_REPO_URL)
-        helm.upgrade("greenmail", project_config.getProjectName(), """""", Constants.FOLIO_HELM_HOSTED_REPO_NAME, "greenmail")
+        helm.upgrade("greenmail", project_config.getProjectName(), "''", Constants.FOLIO_HELM_HOSTED_REPO_NAME, "greenmail")
     }
 }
