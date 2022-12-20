6d4cf83cd698dabb712d1672f1cc616bcf232c18
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-12-20 16:19:24 +0200

    Changed group.name for grafana

--- terraform/rancher/cluster/monitoring.tf
+++ terraform/rancher/cluster/monitoring.tf
@@ -100,7 +100,7 @@ resource "rancher2_app_v2" "prometheus" {
         annotations:
           kubernetes.io/ingress.class: alb
           alb.ingress.kubernetes.io/scheme: internet-facing
-          alb.ingress.kubernetes.io/group.name: folio-tmp
+          alb.ingress.kubernetes.io/group.name: ${module.eks_cluster.cluster_id}
           alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
           alb.ingress.kubernetes.io/success-codes: 200-399
           alb.ingress.kubernetes.io/healthcheck-path: /
