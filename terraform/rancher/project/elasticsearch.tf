c6eaea1176c19b37fbf70db493945ac0d379d0ef
renatsafiulin <renat_safiulin@epam.com>
2022-12-28 15:52:24 +0500

    Added ingress rule for opensearch

--- terraform/rancher/project/elasticsearch.tf
+++ terraform/rancher/project/elasticsearch.tf
@@ -177,6 +177,13 @@ resource "aws_security_group" "es" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 9200
+    to_port   = 9200
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
