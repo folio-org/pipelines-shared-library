7f74f292f50f9493932166e2dc28a68b377354b6
renatsafiulin <renat_safiulin@epam.com>
2023-01-09 19:47:22 +0500

    Update cypressTestsPipeline.groovy

--- pipelines/tests/cypress/cypressTestsPipeline.groovy
+++ pipelines/tests/cypress/cypressTestsPipeline.groovy
@@ -93,7 +93,7 @@ pipeline {
 //                    def CyprImageVer = readJSON(text: readFile("${workspace}/stripes-testing/package.json"))
 //                    println(CyprImageVer.dependencies.cypress)
                     println("$workspace")
-                    sh "ls -la ${workspace}/stripes-testing"
+                    sh "ls -la ${workspace}"
                 }
 
             }
