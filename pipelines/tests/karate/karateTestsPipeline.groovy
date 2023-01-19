48a805c57882bc19ae62f59a4edb64c96455e920
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-01-19 13:46:15 +0200

    fix

--- pipelines/tests/karate/karateTestsPipeline.groovy
+++ pipelines/tests/karate/karateTestsPipeline.groovy
@@ -144,9 +144,9 @@ pipeline {
     post {
         always {
             script {
+                def files = findFiles(glob: "**/*-ephemeral-properties")
                 helm.k8sClient {
                     awscli.getKubeConfig(Constants.AWS_REGION, clusterName)
-                    def files = findFiles(glob: "**/*-ephemeral-properties")
                     files.each { file ->
                         kubectl.recreateConfigMap(file, projectName, "./${file}")  
                     }
