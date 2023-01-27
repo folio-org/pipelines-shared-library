37c3b242b317a4ceb52a1c52e3d90d04ef5c546f
vitalii-ustalu <vitalii_ustalu@epam.com>
2023-01-27 16:45:55 +0400

    test new approach karate

--- pipelines/tests/spring/springScheduledTestsPipeline.groovy
+++ pipelines/tests/spring/springScheduledTestsPipeline.groovy
@@ -1,32 +1,14 @@
 @Library('pipelines-shared-library@RANCHER-608') _
 
-import org.folio.utilities.Tools
 import org.jenkinsci.plugins.workflow.libs.Library
-import org.folio.karate.results.KarateTestsExecutionSummary
-import org.folio.karate.teams.TeamAssignment
-
-def allureVersion = "2.17.2"
 
 def clusterName = "folio-testing"
 def projectName = "spring"
-def folio_repository = "platform-complete"
-def folio_branch = "snapshot"
 def tenant = "diku"
 def uiUrl = "https://${clusterName}-${projectName}-${tenant}.ci.folio.org"
 def okapiUrl = "https://${clusterName}-${projectName}-okapi.ci.folio.org"
 def edgeUrl = "https://${clusterName}-${projectName}-edge.ci.folio.org"
-
 def prototypeTenant = "diku"
-
-def karateTestsJobName = "/Testing/Karate tests"
-def karateTestsJob
-
-KarateTestsExecutionSummary karateTestsExecutionSummary
-def teamAssignment
-
-Tools tools = new Tools(this)
-List<String> versions = tools.eval(jobsParameters.getOkapiVersions(), ["folio_repository": folio_repository, "folio_branch": folio_branch])
-String okapiVersion = versions[0]
 
 pipeline {
     agent { label 'jenkins-agent-java11' }
@@ -46,90 +28,45 @@ pipeline {
     }
 
     stages {
-//        stage("Run cypress tests") {
-//            steps {
-//                script {
-//                    def jobParameters = [
-//                        branch: params.branch,
-//                        uiUrl: uiUrl,
-//                        okapiUrl: okapiUrl,
-//                        tenant: tenant,
-//                        user: 'diku_admin',
-//                        password: 'admin',
-//                        cypressParameters: "--env grepTags=\"smoke criticalPth\",grepFilterSpecs=true",
-//                        customBuildName: JOB_BASE_NAME,
-//                        timeout: '6',
-//                        testrailProjectID: '14',
-//                        testrailRunID: '2108'
-//                    ]
-//                    cypressStages(jobParameters)
-//                }
-//            }
-//        }
+        stage("Run cypress tests") {
+            steps {
+                script {
+                    def jobParameters = [
+                        branch           : params.branch,
+                        uiUrl            : uiUrl,
+                        okapiUrl         : okapiUrl,
+                        tenant           : tenant,
+                        user             : 'diku_admin',
+                        password         : 'admin',
+                        cypressParameters: "--env grepTags=\"smoke criticalPth\",grepFilterSpecs=true",
+                        customBuildName  : JOB_BASE_NAME,
+                        timeout          : '6',
+                        testrailProjectID: '14',
+                        testrailRunID    : '2108'
+                    ]
+                    cypressStages(jobParameters)
+                }
+            }
+        }
 
         stage("Run karate tests") {
             steps {
                 script {
                     def jobParameters = [
-                        branch: params.branch,
-                        threadsCount: "4",
-                        modules: "",
-                        okapiUrl: okapiUrl,
-                        edgeUrl: edgeUrl,
-                        tenant: 'supertenant',
-                        adminUserName: 'super_admin',
-                        adminPassword: 'admin',
+                        branch         : params.branch,
+                        threadsCount   : "4",
+                        modules        : "",
+                        okapiUrl       : okapiUrl,
+                        edgeUrl        : edgeUrl,
+                        tenant         : 'supertenant',
+                        adminUserName  : 'super_admin',
+                        adminPassword  : 'admin',
                         prototypeTenant: prototypeTenant
                     ]
                     karateStages(jobParameters)
                 }
             }
         }
-//
-//        stage("Parallel Karate results") {
-//            parallel {
-//                stage("Collect test results") {
-//                    stages {
-//                        stage("Copy downstream job artifacts") {
-//                            steps {
-//                                script {
-//                                    def jobNumber = karateTestsJob.number
-//                                    copyArtifacts(projectName: karateTestsJobName, selector: specific("${jobNumber}"), filter: "cucumber.zip")
-//                                    copyArtifacts(projectName: karateTestsJobName, selector: specific("${jobNumber}"), filter: "junit.zip")
-//                                    copyArtifacts(projectName: karateTestsJobName, selector: specific("${jobNumber}"), filter: "karate-summary.zip")
-//                                    copyArtifacts(projectName: karateTestsJobName, selector: specific("${jobNumber}"), filter: "teams-assignment.json")
-//
-//                                    unzip zipFile: "cucumber.zip", dir: "results"
-//                                    unzip zipFile: "junit.zip", dir: "results"
-//                                    unzip zipFile: "karate-summary.zip", dir: "results"
-//                                }
-//                            }
-//                        }
-//
-//                        stage('Publish tests report Karate') {
-//                            steps {
-//                                script {
-//                                    cucumber buildStatus: "UNSTABLE",
-//                                        fileIncludePattern: "results/**/target/karate-reports*/*.json",
-//                                        sortingMethod: "ALPHABETICAL"
-//
-//                                    junit testResults: 'results/**/target/karate-reports*/*.xml'
-//                                }
-//                            }
-//                        }
-//
-//                        stage("Collect execution results") {
-//                            steps {
-//                                script {
-//                                    karateTestsExecutionSummary = karateTestUtils.collectTestsResults("results/**/target/karate-reports*/karate-summary-json.txt")
-//
-//                                    karateTestUtils.attachCucumberReports(karateTestsExecutionSummary)
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+
     }
 }
--- vars/karateStages.groovy
+++ vars/karateStages.groovy
@@ -5,74 +5,69 @@ import org.jenkinsci.plugins.workflow.libs.Library
 
 def call(params) {
     stage("Checkout") {
-            script {
-                sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
-                    checkout([
-                        $class           : 'GitSCM',
-                        branches         : [[name: "*/${params.branch}"]],
-                        extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
-                                                              disableSubmodules  : false,
-                                                              parentCredentials  : false,
-                                                              recursiveSubmodules: true,
-                                                              reference          : '',
-                                                              trackingSubmodules : false]],
-                        userRemoteConfigs: [[url: "${Constants.FOLIO_GITHUB_URL}/folio-integration-tests.git"]]
-                    ])
-                }
+        script {
+            sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
+                checkout([$class           : 'GitSCM',
+                          branches         : [[name: "*/${params.branch}"]],
+                          extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
+                                                                disableSubmodules  : false,
+                                                                parentCredentials  : false,
+                                                                recursiveSubmodules: true,
+                                                                reference          : '',
+                                                                trackingSubmodules : false]],
+                          userRemoteConfigs: [[url: "${Constants.FOLIO_GITHUB_URL}/folio-integration-tests.git"]]])
             }
+        }
     }
 
     stage("Build karate config") {
-            script {
-                def files = findFiles(glob: '**/karate-config.js')
-                files.each { file ->
-                    echo "Updating file ${file.path}"
-                    writeFile file: file.path, text: karateTestUtils.renderKarateConfig(readFile(file.path), params)
-                }
-
+        script {
+            def files = findFiles(glob: '**/karate-config.js')
+            files.each { file ->
+                echo "Updating file ${file.path}"
+                writeFile file: file.path, text: karateTestUtils.renderKarateConfig(readFile(file.path), params)
             }
+        }
     }
 
     stage('Run karate tests') {
-            script {
-                def karateEnvironment = "folio-testing-karate"
-                withMaven(
-                    jdk: 'openjdk-11-jenkins-slave-all',
-                    maven: 'maven3-jenkins-slave-all',
-                    mavenSettingsConfig: 'folioci-maven-settings'
-                ) {
-                    def modules = ""
-                    if (params.modules) {
-                        modules = "-pl common,testrail-integration," + params.modules
-                    }
-                    sh "mvn test -T ${threadsCount} ${modules} -DfailIfNoTests=false -DargLine=-Dkarate.env=${karateEnvironment}"
+        script {
+            def karateEnvironment = "folio-testing-karate"
+            withMaven(jdk: 'openjdk-11-jenkins-slave-all',
+                maven: 'maven3-jenkins-slave-all',
+                mavenSettingsConfig: 'folioci-maven-settings') {
+                def modules = ""
+                if (params.modules) {
+                    modules = "-pl common,testrail-integration," + params.modules
                 }
+                sh "mvn test -T ${threadsCount} ${modules} -DfailIfNoTests=false -DargLine=-Dkarate.env=${karateEnvironment}"
             }
+        }
     }
 
     stage('Publish tests report') {
-            script {
-                cucumber buildStatus: "UNSTABLE",
-                    fileIncludePattern: "**/target/karate-reports*/*.json",
-                    sortingMethod: "ALPHABETICAL"
+        script {
+            cucumber buildStatus: "UNSTABLE",
+                fileIncludePattern: "**/target/karate-reports*/*.json",
+                sortingMethod: "ALPHABETICAL"
 
-                junit testResults: '**/target/karate-reports*/*.xml'
-            }
+            junit testResults: '**/target/karate-reports*/*.xml'
+        }
     }
 
     stage('Archive artifacts') {
-            script {
-                // archive artifacts for upstream job
-                if (currentBuild.getBuildCauses('org.jenkinsci.plugins.workflow.support.steps.build.BuildUpstreamCause')) {
-                    zip zipFile: "cucumber.zip", glob: "**/target/karate-reports*/*.json"
-                    zip zipFile: "junit.zip", glob: "**/target/karate-reports*/*.xml"
-                    zip zipFile: "karate-summary.zip", glob: "**/target/karate-reports*/karate-summary-json.txt"
+        script {
+            // archive artifacts for upstream job
+            if (currentBuild.getBuildCauses('org.jenkinsci.plugins.workflow.support.steps.build.BuildUpstreamCause')) {
+                zip zipFile: "cucumber.zip", glob: "**/target/karate-reports*/*.json"
+                zip zipFile: "junit.zip", glob: "**/target/karate-reports*/*.xml"
+                zip zipFile: "karate-summary.zip", glob: "**/target/karate-reports*/karate-summary-json.txt"
 
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "cucumber.zip", fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "junit.zip", fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "karate-summary.zip", fingerprint: true, defaultExcludes: false
-                    archiveArtifacts allowEmptyArchive: true, artifacts: "teams-assignment.json", fingerprint: true, defaultExcludes: false
-                }
+                archiveArtifacts allowEmptyArchive: true, artifacts: "cucumber.zip", fingerprint: true, defaultExcludes: false
+                archiveArtifacts allowEmptyArchive: true, artifacts: "junit.zip", fingerprint: true, defaultExcludes: false
+                archiveArtifacts allowEmptyArchive: true, artifacts: "karate-summary.zip", fingerprint: true, defaultExcludes: false
+                archiveArtifacts allowEmptyArchive: true, artifacts: "teams-assignment.json", fingerprint: true, defaultExcludes: false
             }
+        }
     }
 }
