835e07d76d48db128695e72613ab58077f78ac2f
vitalii-ustalu <vitalii_ustalu@epam.com>
2023-01-27 16:04:33 +0400

    test new approach

--- pipelines/tests/spring/springScheduledTestsPipeline.groovy
+++ pipelines/tests/spring/springScheduledTestsPipeline.groovy
@@ -15,13 +15,6 @@ def tenant = "diku"
 def uiUrl = "https://${clusterName}-${projectName}-${tenant}.ci.folio.org"
 def okapiUrl = "https://${clusterName}-${projectName}-okapi.ci.folio.org"
 
-
-def spinUpEnvironmentJobName = "/Rancher/Project"
-def spinUpEnvironmentJob
-def tearDownEnvironmentJob
-def cypressTestsJobName = "/Testing/Cypress tests"
-def cypressTestsJob
-
 def prototypeTenant = "diku"
 
 def karateTestsJobName = "/Testing/Karate tests"
@@ -52,52 +45,9 @@ pipeline {
     }
 
     stages {
-//        stage("Destroy environment before Cypress") {
-//            steps {
-//                script {
-//                    def jobParameters = getEnvironmentJobParameters('destroy', okapiVersion, clusterName,
-//                        projectName, tenant, folio_repository, folio_branch)
-//
-//                    tearDownEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-//                }
-//            }
-//        }
-//
-//        stage("Create environment before Karate") {
-//            steps {
-//                script {
-//                    def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
-//                        projectName, tenant, folio_repository, folio_branch)
-//
-//                    spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-//                }
-//            }
-//        }
-
         stage("Run cypress tests") {
-//            when {
-//                expression {
-//                    spinUpEnvironmentJob.result == 'SUCCESS'
-//                }
-//            }
             steps {
                 script {
-//                    def jobParameters = [
-//                        string(name: 'branch', value: params.branch),
-//                        string(name: 'uiUrl', value: uiUrl),
-//                        string(name: 'okapiUrl', value: okapiUrl),
-//                        string(name: 'tenant', value: tenant),
-//                        string(name: 'user', value: 'diku_admin'),
-//                        password(name: 'password', value: 'admin'),
-//                        string(name: 'cypressParameters', value: "--env grepTags=\"smoke criticalPth\",grepFilterSpecs=true"),
-//                        string(name: 'customBuildName', value: JOB_BASE_NAME),
-//                        string(name: 'timeout', value: '6'),
-//                        string(name: 'testrailProjectID', value: '14'),
-//                        string(name: 'testrailRunID', value: '2108')
-//                    ]
-//
-//                    cypressTestsJob = build job: cypressTestsJobName, parameters: jobParameters, wait: true, propagate: false
-
                     def jobParameters = [
                         branch: params.branch,
                         uiUrl: uiUrl,
@@ -116,71 +66,7 @@ pipeline {
             }
         }
 
-//        stage("Parallel Cypress results") {
-//            parallel {
-//                stage("Collect test results") {
-////                    when {
-////                        expression {
-////                            spinUpEnvironmentJob.result == 'SUCCESS'
-////                        }
-////                    }
-//                    stages {
-//                        stage("Copy downstream job artifacts") {
-//                            steps {
-//                                script {
-//                                    def jobNumber = cypressTestsJob.number
-//                                    copyArtifacts(projectName: cypressTestsJobName, selector: specific("${jobNumber}"), filter: "allure-results.zip")
-//
-//                                    unzip zipFile: "allure-results.zip", dir: "."
-//                                }
-//                            }
-//                        }
-//
-//                        stage('Publish tests report Cypress') {
-//                            steps {
-//                                allure([
-//                                    includeProperties: false,
-//                                    jdk              : '',
-//                                    commandline      : allureVersion,
-//                                    properties       : [],
-//                                    reportBuildPolicy: 'ALWAYS',
-//                                    results          : [[path: 'allure-results']]
-//                                ])
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-
-//        stage("Destroy environment before Karate") {
-//            steps {
-//                script {
-//                    def jobParameters = getEnvironmentJobParameters('destroy', okapiVersion, clusterName,
-//                        projectName, tenant, folio_repository, folio_branch)
-//
-//                    tearDownEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-//                }
-//            }
-//        }
-//
-//        stage("Create environment before Karate") {
-//            steps {
-//                script {
-//                    def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
-//                        projectName, tenant, folio_repository, folio_branch)
-//
-//                    spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-//                }
-//            }
-//        }
-
 //        stage("Run karate tests") {
-////            when {
-////                expression {
-////                    spinUpEnvironmentJob.result == 'SUCCESS'
-////                }
-////            }
 //            steps {
 //                script {
 //                    def jobParameters = [
@@ -202,11 +88,6 @@ pipeline {
 //        stage("Parallel Karate results") {
 //            parallel {
 //                stage("Collect test results") {
-////                    when {
-////                        expression {
-////                            spinUpEnvironmentJob.result == 'SUCCESS'
-////                        }
-////                    }
 //                    stages {
 //                        stage("Copy downstream job artifacts") {
 //                            steps {
@@ -249,42 +130,5 @@ pipeline {
 //                }
 //            }
 //        }
-
-//        stage("Set job execution result") {
-//            when {
-//                expression {
-//                    spinUpEnvironmentJob.result != 'SUCCESS'
-//                }
-//            }
-//            steps {
-//                script {
-//                    currentBuild.result = 'FAILURE'
-//                }
-//            }
-//        }
     }
-}
-
-private List getEnvironmentJobParameters(String action, String okapiVersion, clusterName, projectName, tenant,
-                                         folio_repository, folio_branch) {
-    [
-        string(name: 'action', value: action),
-        string(name: 'config_type', value: "testing"),
-        string(name: 'rancher_cluster_name', value: clusterName),
-        string(name: 'rancher_project_name', value: projectName),
-        string(name: 'okapi_version', value: okapiVersion),
-        booleanParam(name: 'ui_bundle_build', value: true),
-        booleanParam(name: 'enable_modules', value: true),
-        string(name: 'folio_repository', value: folio_repository),
-        string(name: 'folio_branch', value: folio_branch),
-        string(name: 'tenant_id', value: tenant),
-        string(name: 'tenant_name', value: "Cypress tenant"),
-        string(name: 'tenant_description', value: "Cypress tests main tenant"),
-        booleanParam(name: 'load_reference', value: true),
-        booleanParam(name: 'load_sample', value: true),
-        booleanParam(name: 'pg_embedded', value: true),
-        booleanParam(name: 'kafka_embedded', value: true),
-        booleanParam(name: 'es_embedded', value: true),
-        booleanParam(name: 's3_embedded', value: true)
-    ]
 }
--- vars/cypressStages.groovy
+++ vars/cypressStages.groovy
@@ -1,131 +1,106 @@
 import org.folio.Constants
 import org.jenkinsci.plugins.workflow.libs.Library
 import groovy.transform.Field
+
 @Library('pipelines-shared-library@RANCHER-608') _
 
 @Field def cypressImageVersion
 
 def call(params) {
     stage('Checkout Cypress repo') {
-            script {
-                def customBuildName = params.customBuildName?.trim() ? params.customBuildName + '.' + env.BUILD_ID : env.BUILD_ID
-                buildName customBuildName
-                sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
-                    checkout([
-                        $class           : 'GitSCM',
-                        branches         : [[name: "*/${params.branch}"]],
-                        extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
-                                                              disableSubmodules  : false,
-                                                              parentCredentials  : false,
-                                                              recursiveSubmodules: true,
-                                                              reference          : '',
-                                                              trackingSubmodules : false]],
-                        userRemoteConfigs: [[url: Constants.CYPRESS_REPOSITORY_URL]]
-                    ])
-                }
+        script {
+            def customBuildName = params.customBuildName?.trim() ? params.customBuildName + '.' + env.BUILD_ID : env.BUILD_ID
+            buildName customBuildName
+            sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
+                checkout([
+                    $class           : 'GitSCM',
+                    branches         : [[name: "*/${params.branch}"]],
+                    extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
+                                                          disableSubmodules  : false,
+                                                          parentCredentials  : false,
+                                                          recursiveSubmodules: true,
+                                                          reference          : '',
+                                                          trackingSubmodules : false]],
+                    userRemoteConfigs: [[url: Constants.CYPRESS_REPOSITORY_URL]]
+                ])
             }
+        }
     }
     stage('Cypress tests Image version') {
-            script {
-                def packageJson = readJSON(text: readFile("${workspace}/package.json"))
-                cypressImageVersion = packageJson.dependencies.cypress
-            }
+        script {
+            def packageJson = readJSON(text: readFile("${workspace}/package.json"))
+            cypressImageVersion = packageJson.dependencies.cypress
+        }
     }
     stage('Build tests') {
-//        environment {
-//            HOME = "${pwd()}/cache"
-//            CYPRESS_CACHE_FOLDER = "${pwd()}/cache"
-//        }
         script {
             env.HOME = "${pwd()}/cache"
             env.CYPRESS_CACHE_FOLDER = "${pwd()}/cache"
             sh "yarn config set @folio:registry ${Constants.FOLIO_NPM_REPO_URL}"
             sh "yarn add -D cypress-testrail-simple"
             sh "yarn install"
-            sh "pwd"
-            sh "ls -ltra"
         }
     }
 
     stage('Cypress tests execution') {
-//        agent {
-//            docker {
-//                image "cypress/included:${cypressImageVersion}"
-//                args '--entrypoint='
-//                reuseNode true
-//            }
-//        }
-//        environment {
-//            HOME = "${pwd()}/cache"
-//            CYPRESS_CACHE_FOLDER = "${pwd()}/cache"
-//
-//            CYPRESS_BASE_URL = "${params.uiUrl}"
-//            CYPRESS_OKAPI_HOST = "${params.okapiUrl}"
-//            CYPRESS_OKAPI_TENANT = "${params.tenant}"
-//            CYPRESS_diku_login = "${params.user}"
-//            CYPRESS_diku_password = "${params.password}"
-//        }
-            script {
-                docker.image("cypress/included:${cypressImageVersion}").inside("-v \"$WORKSPACE\":/project -w /project --entrypoint=") {
-                        env.HOME = "${pwd()}/cache"
-                        env.CYPRESS_CACHE_FOLDER = "${pwd()}/cache"
-                        env.CYPRESS_BASE_URL = "${params.uiUrl}"
-                        env.CYPRESS_OKAPI_HOST = "${params.okapiUrl}"
-                        env.CYPRESS_OKAPI_TENANT = "${params.tenant}"
-                        env.CYPRESS_diku_login = "${params.user}"
-                        env.CYPRESS_diku_password = "${params.password}"
-                        def browserName = "chrome"
-                        ansiColor('xterm') {
-                            timeout(time: "${params.timeout}", unit: 'HOURS') {
-                                catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
-                                    sh "touch test_file"
-                                    sh "pwd"
-                                    sh "ls -ltra"
-                                    if (params.testrailRunID && params.testrailProjectID) {
-                                        // Run with TesTrail Integration
-                                        env.TESTRAIL_HOST = "https://foliotest.testrail.io"
-                                        env.TESTRAIL_PROJECTID = "${params.testrailProjectID}"
-                                        env.TESTRAIL_RUN_ID = "${params.testrailRunID}"
-                                        env.CYPRESS_allureReuseAfterSpec = "true"
-                                        println "Test results will be send to TestRail. (ProjectID: ${params.testrailProjectID}, RunID: ${params.testrailRunID})"
-                                        withCredentials([usernamePassword(credentialsId: 'testrail-ut56', passwordVariable: 'TESTRAIL_PASSWORD', usernameVariable: 'TESTRAIL_USERNAME')]) {
-                                            sh "cypress run --headless --browser ${browserName} ${params.cypressParameters}"
-                                        }
-                                    } else {
-                                        sh "cypress run --headless --browser ${browserName} ${params.cypressParameters}"
-                                    }
+        script {
+            docker.image("cypress/included:${cypressImageVersion}").inside("-v \"$WORKSPACE\":/project -w /project --entrypoint=") {
+                env.HOME = "${pwd()}/cache"
+                env.CYPRESS_CACHE_FOLDER = "${pwd()}/cache"
+                env.CYPRESS_BASE_URL = "${params.uiUrl}"
+                env.CYPRESS_OKAPI_HOST = "${params.okapiUrl}"
+                env.CYPRESS_OKAPI_TENANT = "${params.tenant}"
+                env.CYPRESS_diku_login = "${params.user}"
+                env.CYPRESS_diku_password = "${params.password}"
+                def browserName = "chrome"
+                ansiColor('xterm') {
+                    timeout(time: "${params.timeout}", unit: 'HOURS') {
+                        catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+                            if (params.testrailRunID && params.testrailProjectID) {
+                                // Run with TesTrail Integration
+                                env.TESTRAIL_HOST = "https://foliotest.testrail.io"
+                                env.TESTRAIL_PROJECTID = "${params.testrailProjectID}"
+                                env.TESTRAIL_RUN_ID = "${params.testrailRunID}"
+                                env.CYPRESS_allureReuseAfterSpec = "true"
+                                println "Test results will be send to TestRail. (ProjectID: ${params.testrailProjectID}, RunID: ${params.testrailRunID})"
+                                withCredentials([usernamePassword(credentialsId: 'testrail-ut56', passwordVariable: 'TESTRAIL_PASSWORD', usernameVariable: 'TESTRAIL_USERNAME')]) {
+                                    sh "cypress run --headless --browser ${browserName} ${params.cypressParameters}"
                                 }
+                            } else {
+                                sh "cypress run --headless --browser ${browserName} ${params.cypressParameters}"
                             }
                         }
+                    }
                 }
             }
+        }
     }
 
     stage('Generate tests report') {
-            script {
-                def allure_home = tool type: 'allure', name: Constants.CYPRESS_ALLURE_VERSION
-                sh "${allure_home}/bin/allure generate --clean"
-            }
+        script {
+            def allure_home = tool type: 'allure', name: Constants.CYPRESS_ALLURE_VERSION
+            sh "${allure_home}/bin/allure generate --clean"
+        }
     }
 
-//    stage('Publish tests report') {
-//        script {
-//            allure([
-//                includeProperties: false,
-//                jdk              : '',
-//                commandline      : Constants.CYPRESS_ALLURE_VERSION,
-//                properties       : [],
-//                reportBuildPolicy: 'ALWAYS',
-//                results          : [[path: 'allure-results']]
-//            ])
-//        }
-//    }
+    stage('Publish tests report') {
+        script {
+            allure([
+                includeProperties: false,
+                jdk              : '',
+                commandline      : Constants.CYPRESS_ALLURE_VERSION,
+                properties       : [],
+                reportBuildPolicy: 'ALWAYS',
+                results          : [[path: 'allure-results']]
+            ])
+        }
+    }
 
     stage('Archive artifacts') {
-            script {
-                zip zipFile: "allure-results.zip", glob: "allure-results/*"
+        script {
+            zip zipFile: "allure-results.zip", glob: "allure-results/*"
 
-                archiveArtifacts allowEmptyArchive: true, artifacts: "allure-results.zip", fingerprint: true, defaultExcludes: false
-            }
+            archiveArtifacts allowEmptyArchive: true, artifacts: "allure-results.zip", fingerprint: true, defaultExcludes: false
+        }
     }
 }
