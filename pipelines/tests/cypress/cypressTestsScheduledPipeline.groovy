99f27a3b43e3e71664d5e2c961eb3450e55aaf3e
vitalii-ustalu <vitalii_ustalu@epam.com>
2023-02-22 12:05:47 +0400

    RANCHER-642 refactor scheduled cypress pipeline

--- pipelines/tests/cypress/cypressTestsScheduledPipeline.groovy
+++ pipelines/tests/cypress/cypressTestsScheduledPipeline.groovy
@@ -24,125 +24,6 @@ Tools tools = new Tools(this)
 List<String> versions = tools.eval(jobsParameters.getOkapiVersions(), ["folio_repository": folio_repository, "folio_branch": folio_branch])
 String okapiVersion = versions[0] //versions.toSorted(new SemanticVersionComparator(order: Order.DESC, preferredBranches: [VersionConstants.MASTER_BRANCH]))[0]
 
-pipeline {
-    agent { label 'rancher' }
-
-    triggers {
-        cron('H 0 * * 1-6')
-    }
-
-    options {
-        disableConcurrentBuilds()
-    }
-
-    parameters {
-        string(name: 'branch', defaultValue: 'master', description: 'Cypress tests repository branch to checkout')
-    }
-
-    stages {
-        stage("Destroy environment") {
-            steps {
-                script {
-                    def jobParameters = getEnvironmentJobParameters('destroy', okapiVersion, clusterName,
-                        projectName, tenant, folio_repository, folio_branch)
-
-                    tearDownEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-                }
-            }
-        }
-
-       stage("Create environment") {
-           steps {
-               script {
-                   def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
-                       projectName, tenant, folio_repository, folio_branch)
-
-                   spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-               }
-           }
-       }
-
-        stage("Run cypress tests") {
-           when {
-               expression {
-                   spinUpEnvironmentJob.result == 'SUCCESS'
-               }
-           }
-            steps {
-                script {
-                    def jobParameters = [
-                        string(name: 'branch', value: params.branch),
-                        string(name: 'uiUrl', value: uiUrl),
-                        string(name: 'okapiUrl', value: okapiUrl),
-                        string(name: 'tenant', value: tenant),
-                        string(name: 'user', value: 'diku_admin'),
-                        password(name: 'password', value: 'admin'),
-                        string(name: 'cypressParameters', value: "--env grepTags=\"smoke criticalPth\",grepFilterSpecs=true"),
-                        string(name: 'customBuildName', value: JOB_BASE_NAME),
-                        string(name: 'timeout', value: '6'),
-                        string(name: 'testrailProjectID', value: '14'),
-                        string(name: 'testrailRunID', value: '2108'),
-                        string(name: 'numberOfWorkers', value: '4'),
-                        string(name: 'agent', value: 'rancher||jenkins-agent-java11')
-                    ]
-
-                    cypressTestsJob = build job: cypressTestsJobName, parameters: jobParameters, wait: true, propagate: false
-                }
-            }
-        }
-
-        stage("Parallel") {
-            parallel {
-                stage("Collect test results") {
-                   when {
-                       expression {
-                           spinUpEnvironmentJob.result == 'SUCCESS'
-                       }
-                   }
-                    stages {
-                        stage("Copy downstream job artifacts") {
-                            steps {
-                                script {
-                                    def jobNumber = cypressTestsJob.number
-                                    copyArtifacts(projectName: cypressTestsJobName, selector: specific("${jobNumber}"), filter: "allure-results.zip")
-
-                                    unzip zipFile: "allure-results.zip", dir: "."
-                                }
-                            }
-                        }
-
-                        stage('Publish tests report') {
-                            steps {
-                                allure([
-                                    includeProperties: false,
-                                    jdk              : '',
-                                    commandline      : allureVersion,
-                                    properties       : [],
-                                    reportBuildPolicy: 'ALWAYS',
-                                    results          : [[path: 'allure-results']]
-                                ])
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-       stage("Set job execution result") {
-           when {
-               expression {
-                   spinUpEnvironmentJob.result != 'SUCCESS'
-               }
-           }
-           steps {
-               script {
-                   currentBuild.result = 'FAILURE'
-               }
-           }
-       }
-    }
-}
-
 private List getEnvironmentJobParameters(String action, String okapiVersion, clusterName, projectName, tenant,
                                          folio_repository, folio_branch) {
     [
@@ -166,3 +47,53 @@ private List getEnvironmentJobParameters(String action, String okapiVersion, clu
         booleanParam(name: 's3_embedded', value: true)
     ]
 }
+
+properties([
+    parameters([
+        string(name: 'branch', defaultValue: 'master', description: 'Cypress tests repository branch to checkout')
+    ]),
+    options([
+        disableConcurrentBuilds()
+    ]),
+    triggers([
+        cron('H 0 * * 1-6')
+    ])
+])
+
+node('rancher') {
+    stage("Destroy environment") {
+        script {
+            def jobParameters = getEnvironmentJobParameters('destroy', okapiVersion, clusterName,
+                projectName, tenant, folio_repository, folio_branch)
+
+            tearDownEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
+        }
+    }
+
+    stage("Create environment") {
+        script {
+            def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
+                projectName, tenant, folio_repository, folio_branch)
+
+            spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: true
+        }
+    }
+}
+
+def jobParameters = [
+    branch: params.branch,
+    uiUrl: uiUrl,
+    okapiUrl: okapiUrl,
+    tenant: tenant,
+    user: 'diku_admin',
+    password: 'admin',
+    cypressParameters: "--env grepTags=\"smoke criticalPth\",grepFilterSpecs=true",
+    customBuildName: JOB_BASE_NAME,
+    timeout: '6',
+    testrailProjectID: '14',
+    testrailRunID: '2108',
+    numberOfWorkers: '4',
+    agent: 'rancher||jenkins-agent-java11'
+]
+
+cypressStages(jobParameters)
