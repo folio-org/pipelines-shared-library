c436521b19163835ed4dd626f76428b5e6a5dec5
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-03-13 12:11:07 +0200

    Add json template

--- vars/dataMigrationReport.groovy
+++ vars/dataMigrationReport.groovy
@@ -5,110 +5,17 @@ import java.util.concurrent.*
 import java.util.Date
 
 def getESLogs(cluster, indexPattern, startDate) {
-    def elasticRequestBody = """
-    {
-        "track_total_hits": false,
-        "sort": [
-        {
-            "@timestamp": {
-            "order": "desc",
-            "unmapped_type": "boolean"
-            }
-        }
-        ],
-        "fields": [
-        {
-            "field": "log",
-            "include_unmapped": "true"
-        }
-        ],
-        "size": 500,
-        "version": true,
-        "_source": false,
-        "query": {
-        "bool": {
-            "must": [],
-            "filter": [
-            {
-                "bool": {
-                "should": [
-                    {
-                    "bool": {
-                        "filter": [
-                        {
-                            "multi_match": {
-                            "type": "phrase",
-                            "query": "Activation of module ",
-                            "lenient": true
-                            }
-                        },
-                        {
-                            "multi_match": {
-                            "type": "phrase",
-                            "query": "failed",
-                            "lenient": true
-                            }
-                        }
-                        ]
-                    }
-                    },
-                    {
-                    "bool": {
-                        "filter": [
-                        {
-                            "multi_match": {
-                            "type": "phrase",
-                            "query": "Activation of module ",
-                            "lenient": true
-                            }
-                        },
-                        {
-                            "multi_match": {
-                            "type": "phrase",
-                            "query": "completed successfully",
-                            "lenient": true
-                            }
-                        }
-                        ]
-                    }
-                    }
-                ],
-                "minimum_should_match": 1
-                }
-            },
-            {
-                "range": {
-                "@timestamp": {
-                    "format": "strict_date_optional_time",
-                    "gte": "${startDate}",
-                    "lte": "now"
-                }
-                }
-            },
-            {
-                "match_phrase": {
-                "kubernetes.container_name": "okapi"
-                }
-            }
-            ],
-            "should": [],
-            "must_not": []
-        }
-        },
-        "highlight": {
-        "pre_tags": [
-            "@kibana-highlighted-field@"
-        ],
-        "post_tags": [
-            "@/kibana-highlighted-field@"
-        ],
-        "fields": {
-            "log": {}
-        },
-        "fragment_size": 2147483647
-        }
-    }
-    """
+    def template = "es.json.template"
+    def binding = [
+        cluster     : cluster,
+        indexPattern: indexPattern,
+        startDate   : startDate
+    ]
+
+    tools.copyResourceFileToWorkspace("dataMigration/$template")
+    def content = steps.readFile template
+    String elasticRequestBody = new GStringTemplateEngine().createTemplate(content).make(binding).toString()
+
     def response = httpRequest httpMode: 'GET', url: "https://${cluster}-elasticsearch.ci.folio.org/${indexPattern}*/_search", validResponseCodes: '100:599', requestBody: elasticRequestBody, contentType: "APPLICATION_JSON"
     def result = new JsonSlurperClassic().parseText(response.content)
 
