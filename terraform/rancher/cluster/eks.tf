70adef981c99c5cde2818f8df4837da285189862
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-04-03 15:09:45 +0300

    RANCHER-717 Add tags to EKS

--- terraform/rancher/cluster/eks.tf
+++ terraform/rancher/cluster/eks.tf
@@ -116,5 +116,9 @@ module "eks_cluster" {
 
   aws_auth_users = local.admin_users_map
 
-  tags = var.tags
+  tags = merge(
+    var.tags,
+    {
+      kubernetes_cluster = terraform.workspace
+  })
 }
