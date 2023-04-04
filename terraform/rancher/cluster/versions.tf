22a01d84d9b2bef8f5c7810e1b4ff56cdc63c56f
Oleksandr Haimanov <43782513+OHaimanov@users.noreply.github.com>
2023-04-04 13:55:17 +0300

    RANCHER-713 Update kubernetes and add-on versions to latest in terraform/rancher/cluster (#211)

--- terraform/rancher/cluster/versions.tf
+++ terraform/rancher/cluster/versions.tf
@@ -3,27 +3,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.24.0"
+      version = "~>4.61.0"
     }
-    rancher2 = {
-      source  = "rancher/rancher2"
-      version = "1.24.2"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.14.0"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2.19.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.6.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "2.2.0"
+      version = "~>2.9.0"
     }
     elasticstack = {
       source  = "elastic/elasticstack"
       version = "~>0.3.3"
+    }
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "~>1.25.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~>1.14.0"
     }
   }
 }
