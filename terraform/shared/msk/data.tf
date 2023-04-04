20982f6fb2e4a1660a3d8526536d3473116a3690
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-04-04 18:39:38 +0300

    RANCHER-678 Add shared kafka

---
+++ terraform/shared/msk/data.tf
@@ -0,0 +1,29 @@
+data "aws_vpc" "rancher_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "availability-zone"
+    values = ["${var.aws_region}a", "${var.aws_region}b"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.rancher_vpc.id]
+  }
+  tags = {
+    Type : "private"
+  }
+}
+
+locals {
+  domain_name = "folio-kafka"
+  broker_urls = split(",", aws_msk_cluster.this.bootstrap_brokers)
+  data        = jsonencode({
+    KAFKA_HOST = element(split(":", local.broker_urls[0]), 0)
+    KAFKA_PORT = element(split(":", local.broker_urls[0]), 1)
+  })
+}
