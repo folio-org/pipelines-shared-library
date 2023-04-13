8589fabbbdd175dc9fa7cf0fca4da753e78a1cb1
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2023-04-13 22:11:47 +0300

    RANCHER-744 Create function for opensearch indices cleanup during project/namespace destroy

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -38,3 +38,41 @@ def checkDeploymentStatus(String name, String namespace) {
         println(e.getMessage())
     }
 }
+
+String getSecretValue(String namespace, String secret_name, String key_name) {
+    try {
+        return sh(script: "set +x && kubectl get secret --namespace=${namespace} ${secret_name} -o jsonpath='{.data.${key_name}}' | base64 -d",
+            returnStdout: true).trim()
+    } catch (Exception e) {
+        currentBuild.result = 'UNSTABLE'
+        println(e.getMessage())
+    }
+}
+
+void runPodWithCommand(String namespace = 'default', String pod_name, String pod_image, String command = 'sleep 10m') {
+    try {
+        sh "kubectl run --namespace=${namespace} ${pod_name} --image=${pod_image} --command -- ${command}"
+    } catch (Exception e) {
+        currentBuild.result = 'UNSTABLE'
+        println(e.getMessage())
+    }
+}
+
+void execCommand(String namespace = 'default', String pod_name, String command) {
+    try {
+        return sh(script: "set +x && kubectl exec --namespace=${namespace} ${pod_name} -- ${command}",
+            returnStdout: true).trim()
+    } catch (Exception e) {
+        currentBuild.result = 'UNSTABLE'
+        println(e.getMessage())
+    }
+}
+
+void deletePod(String namespace = 'default', String pod_name) {
+    try {
+        sh "kubectl delete pod --namespace=${namespace} ${pod_name}"
+    } catch (Exception e) {
+        currentBuild.result = 'UNSTABLE'
+        println(e.getMessage())
+    }
+}
