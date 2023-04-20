d09ac19d6fbb41a0c14894438a9365d0a6ee12db
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2023-04-20 16:20:30 +0300

    RANCHER-750

--- pipelines/automation/start-stop-project.groovy
+++ pipelines/automation/start-stop-project.groovy
@@ -33,7 +33,7 @@ ansiColor('xterm') {
             stage("Downscale namespace replicas") {
                 helm.k8sClient {
                     awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                    String deployments_list = awscli.getDeploymentsList(params.rancher_project_name)
+                    def deployments_list = awscli.getDeploymentsList(params.rancher_project_name)
                     deployments_list.each {deployment ->
                         awscli.setDeploymentCount(deployment.toString(), params.rancher_project_name, 0)
                     }
