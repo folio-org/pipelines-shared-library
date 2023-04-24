891329d204299cd839f1278a1706173f6b4a46cf
vitalii-ustalu <121868876+vitalii-ustalu@users.noreply.github.com>
2023-04-24 16:15:05 +0400

    Rancher 733 vpc peering (#234)
    * RANCHER-733 added vpc peering
    
    * RANCHER-733 fix rules

--- terraform/jenkins/network.tf
+++ terraform/jenkins/network.tf
@@ -45,3 +45,53 @@ module "vpc" {
     Type = "private"
   }
 }
+
+data "aws_vpc" "rancher" {
+  tags = {
+    Name = "folio-rancher-vpc"
+  }
+}
+
+data "aws_route_table" "rancher_private" {
+  tags = {
+    Name = "folio-rancher-vpc-private"
+  }
+}
+
+data "aws_route_table" "rancher_public" {
+  tags = {
+    Name = "folio-rancher-vpc-public"
+  }
+}
+
+resource "aws_vpc_peering_connection" "jenkins_rancher" {
+  peer_vpc_id   = data.aws_vpc.rancher.id
+  vpc_id        = module.vpc.vpc_id
+  auto_accept = true
+
+  tags = var.tags
+}
+
+resource "aws_route" "jenkins_to_rancher_private" {
+  route_table_id = module.vpc.private_route_table_ids[0]
+  destination_cidr_block = data.aws_vpc.rancher.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.jenkins_rancher.id
+}
+
+resource "aws_route" "jenkins_to_rancher_public" {
+  route_table_id = module.vpc.public_route_table_ids[0]
+  destination_cidr_block = data.aws_vpc.rancher.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.jenkins_rancher.id
+}
+
+resource "aws_route" "rancher_to_jenkins_private" {
+  route_table_id = data.aws_route_table.rancher_private.id
+  destination_cidr_block = var.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.jenkins_rancher.id
+}
+
+resource "aws_route" "rancher_to_jenkins_public" {
+  route_table_id = data.aws_route_table.rancher_public.id
+  destination_cidr_block = var.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.jenkins_rancher.id
+}
