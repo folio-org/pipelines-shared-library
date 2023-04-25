43d3baddf80ee0ed6e2ed996b83e54c550948bf1
vitalii-ustalu <121868876+vitalii-ustalu@users.noreply.github.com>
2023-04-25 16:21:30 +0400

    RANCHER-775 switched to embedded opensearch (#238)

--- pipelines/tests/cypress/cypressTestsScheduledPipeline.groovy
+++ pipelines/tests/cypress/cypressTestsScheduledPipeline.groovy
@@ -41,7 +41,7 @@ private List getEnvironmentJobParameters(String action, String okapiVersion, clu
         booleanParam(name: 'load_sample', value: true),
         booleanParam(name: 'pg_embedded', value: true),
         booleanParam(name: 'kafka_shared', value: true),
-        booleanParam(name: 'opensearch_shared', value: true),
+        booleanParam(name: 'opensearch_shared', value: false),
         booleanParam(name: 's3_embedded', value: true)
     ]
 }
