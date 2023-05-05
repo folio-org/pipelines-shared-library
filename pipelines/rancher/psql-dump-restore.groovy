6ede1d8f6935bd1bfb4214914b6c2cfa64655b6c
vitalii-ustalu <vitalii_ustalu@epam.com>
2023-05-05 10:59:59 +0400

    RANCHER-768 adapt pg backup pipelines

--- pipelines/rancher/psql-dump-restore.groovy
+++ pipelines/rancher/psql-dump-restore.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-768-adapt-for-kube') _
 
 import org.folio.Constants
 import org.jenkinsci.plugins.workflow.libs.Library
@@ -12,7 +12,6 @@ properties([
         booleanParam(name: 'refreshParameters', defaultValue: false, description: 'Do a dry run and refresh pipeline configuration'),
         jobsParameters.clusterName(),
         jobsParameters.projectName(),
-        jobsParameters.agents(),
         jobsParameters.tenantIdToBackupModulesVersions(),
         jobsParameters.adminUsername(),
         jobsParameters.adminPassword(),
@@ -31,45 +30,54 @@ ansiColor('xterm') {
         currentBuild.result = 'ABORTED'
         error('DRY RUN BUILD, NO STAGE IS ACTIVE!')
     }
-    node(params.agent) {
-        try {
-            stage('Ini') {
-                buildName params.rancher_cluster_name + '.' + params.rancher_project_name + '.' + env.BUILD_ID
-                buildDescription "rancher_cluster_name: ${params.rancher_cluster_name}\n" +
-                    "rancher_project_name: ${params.rancher_project_name}"
-            }
-            stage('Checkout') {
-                checkout scm
-            }
+    podTemplate(inheritFrom: 'rancher-kube', containers: [
+        containerTemplate(name: 'k8sclient', image: Constants.DOCKER_K8S_CLIENT_IMAGE, command: "sleep", args: "99999999")]
+    ) {
+        node(POD_LABEL) {
+            try {
+                stage('Ini') {
+                    buildName params.rancher_cluster_name + '.' + params.rancher_project_name + '.' + env.BUILD_ID
+                    buildDescription "rancher_cluster_name: ${params.rancher_cluster_name}\n" +
+                        "rancher_project_name: ${params.rancher_project_name}"
+                }
+                stage('Checkout') {
+                    checkout scm
+                }
 
-            helm.k8sClient {
-                psqlDumpMethods.configureKubectl(Constants.AWS_REGION, params.rancher_cluster_name)
-                psqlDumpMethods.configureHelm(Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.FOLIO_HELM_HOSTED_REPO_URL)
-                try {
-                    if (params.restore_from_backup == false) {
-                        psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, admin_user.username, admin_user.password, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
-                        psqlDumpMethods.savePlatformCompleteImageTag(params.rancher_project_name, db_backup_name, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory, params.tenant_id_to_backup_modules_versions)
-                        psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
-                        println("\n\n\n" + "\033[32m" + "PostgreSQL backup process SUCCESSFULLY COMPLETED\nYou can find your backup in AWS s3 bucket folio-postgresql-backups/" +
-                            "${params.rancher_cluster_name}/${params.rancher_project_name}/${db_backup_name}" + "\n\n\n" + "\033[0m")
-                    } else {
-                        psqlDumpMethods.restoreHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, db_backup_name, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
-                        psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
-                        println("\n\n\n" + "\033[32m" + "PostgreSQL restore process SUCCESSFULLY COMPLETED\n" + "\n\n\n" + "\033[0m")
+                container('k8sclient') {
+                    withCredentials([[$class           : 'AmazonWebServicesCredentialsBinding',
+                                      credentialsId    : Constants.AWS_CREDENTIALS_ID,
+                                      accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                        psqlDumpMethods.configureKubectl(Constants.AWS_REGION, params.rancher_cluster_name)
+                        psqlDumpMethods.configureHelm(Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.FOLIO_HELM_HOSTED_REPO_URL)
+                        try {
+                            if (params.restore_from_backup == false) {
+                                psqlDumpMethods.backupHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, params.rancher_cluster_name, db_backup_name, params.tenant_id_to_backup_modules_versions, admin_user.username, admin_user.password, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
+                                psqlDumpMethods.savePlatformCompleteImageTag(params.rancher_project_name, db_backup_name, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory, params.tenant_id_to_backup_modules_versions)
+                                psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
+                                println("\n\n\n" + "\033[32m" + "PostgreSQL backup process SUCCESSFULLY COMPLETED\nYou can find your backup in AWS s3 bucket folio-postgresql-backups/" +
+                                    "${params.rancher_cluster_name}/${params.rancher_project_name}/${db_backup_name}" + "\n\n\n" + "\033[0m")
+                            } else {
+                                psqlDumpMethods.restoreHelmInstall(env.BUILD_ID, Constants.FOLIO_HELM_HOSTED_REPO_NAME, Constants.PSQL_DUMP_HELM_CHART_NAME, Constants.PSQL_DUMP_HELM_INSTALL_CHART_VERSION, params.rancher_project_name, db_backup_name, "s3://" + Constants.PSQL_DUMP_BACKUPS_BUCKET_NAME, postgresql_backups_directory)
+                                psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
+                                println("\n\n\n" + "\033[32m" + "PostgreSQL restore process SUCCESSFULLY COMPLETED\n" + "\n\n\n" + "\033[0m")
+                            }
+                        }
+                        catch (exception) {
+                            psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
+                            println("\n\n\n" + "\033[1;31m" + "PostgreSQL backup/restore process was FAILED!!!\nPlease, check logs and try again.\n\n\n" + "\033[0m")
+                            throw exception
+                        }
                     }
                 }
-                catch (exception) {
-                    psqlDumpMethods.helmDelete(env.BUILD_ID, params.rancher_project_name)
-                    println("\n\n\n" + "\033[1;31m" + "PostgreSQL backup/restore process was FAILED!!!\nPlease, check logs and try again.\n\n\n" + "\033[0m")
-                    throw exception
+            } catch (exception) {
+                println(exception)
+                error(exception.getMessage())
+            } finally {
+                stage('Cleanup') {
+                    cleanWs notFailBuild: true
                 }
-            }
-        } catch (exception) {
-            println(exception)
-            error(exception.getMessage())
-        } finally {
-            stage('Cleanup') {
-                cleanWs notFailBuild: true
             }
         }
     }
