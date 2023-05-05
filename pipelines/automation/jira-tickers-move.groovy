8b7b121622ef382b3f4093e323e5b209a2ee9d28
vitalii-ustalu <vitalii_ustalu@epam.com>
2023-05-05 11:55:22 +0400

    RANCHER-768 adapt jira ticket pipelines

--- pipelines/automation/jira-tickers-move.groovy
+++ pipelines/automation/jira-tickers-move.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-768-adapt-for-kube') _
 
 import org.folio.client.jira.model.*
 import org.folio.client.jira.JiraClient
@@ -25,7 +25,6 @@ properties([
     disableConcurrentBuilds(),
     parameters([
         booleanParam(name: 'refreshParameters', defaultValue: false, description: 'Do a dry run and refresh pipeline configuration'),
-        jobsParameters.agents(),
         choice(name: 'BugfestEnv', choices: Bugfest_envs, description: 'Choose what Bugfest host you are going to use'),
         string(name: 'Release', defaultValue: 'Nolana (R3 2022)', description: 'Name of current Release', trim: true),
         string(name: 'JiraTaskStatus', defaultValue: 'Awaiting deployment', description: 'Current status of jira task', trim: true),
@@ -40,92 +39,93 @@ ansiColor('xterm') {
         currentBuild.result = 'ABORTED'
         error('DRY RUN BUILD, NO STAGE IS ACTIVE!')
     }
-    node(params.agent) {
-        try {
-            stage('Check input') {
-                double_check=0
-                if ((Release!=null) && (Release!="")){
-                    release_check="release = \"${Release}\""
-                    double_check++
-                }
-                if ((JiraTaskStatus!=null) && (JiraTaskStatus!="")){
-                    jira_status_check="status = \"${JiraTaskStatus}\""
-                    double_check++
-                }
-                if (double_check==2){
-                    search_pattern="${release_check} and ${jira_status_check}"
-                }
-                else {
-                    error("Error in input data")
-                }
-            }
-            stage('Search Jira tasks') {
-                List<JiraIssue> issues = jiraClient.searchIssues(search_pattern,
-                    ["key","status","fixVersions","project"])
-                list_of_found_jira_tasks=SortJiraTickersByVersion(issues)
-            }
-            stage('Get map with services from bugfest') {
-                String host = host_map[BugfestEnv]
-                def response = getRequest(host, tenant)
-                def json = new groovy.json.JsonSlurper().parseText(response)
-
-                json.each{ i ->
-                    list_key_value="${i}".split(':')
-                    value = list_key_value[-1].substring(0, list_key_value[-1].length() - 1)
-
-                    if (value.contains("folio_")){
-                        value=value.replace("folio_","ui-")
+    podTemplate(inheritFrom: 'rancher-kube') {
+        node(POD_LABEL) {
+            try {
+                stage('Check input') {
+                    double_check = 0
+                    if ((Release != null) && (Release != "")) {
+                        release_check = "release = \"${Release}\""
+                        double_check++
                     }
-                    pattern="-[0-9]"
-                    key="${value}".split(pattern)[0]
-                    value="${value}".split("${key}-")[-1]
-
-                    bugfest_map.put(key, value)
+                    if ((JiraTaskStatus != null) && (JiraTaskStatus != "")) {
+                        jira_status_check = "status = \"${JiraTaskStatus}\""
+                        double_check++
+                    }
+                    if (double_check == 2) {
+                        search_pattern = "${release_check} and ${jira_status_check}"
+                    } else {
+                        error("Error in input data")
+                    }
                 }
-            }
-            stage('Compare versions of modules') {
-                for(i in list_of_found_jira_tasks){
-                    check_is_less=false
-                    try {
-                        service_version=bugfest_map[i.project]
-                        jira_service_version=i.fixVersions.substring(1, i.fixVersions.length() - 1)
-                        if (service_version!=null){
-                            check_is_less=false
-                            service_version_list=service_version.replace(".","").split(",")
-                            jira_service_version_list=jira_service_version.replace(".","").split(",")
+                stage('Search Jira tasks') {
+                    List<JiraIssue> issues = jiraClient.searchIssues(search_pattern,
+                        ["key", "status", "fixVersions", "project"])
+                    list_of_found_jira_tasks = SortJiraTickersByVersion(issues)
+                }
+                stage('Get map with services from bugfest') {
+                    String host = host_map[BugfestEnv]
+                    def response = getRequest(host, tenant)
+                    def json = new groovy.json.JsonSlurper().parseText(response)
 
-                            if (service_version_list.length == jira_service_version_list.length){
-                                for (j=0 ; j < jira_service_version_list.size(); j++){
-                                    //Compare jira and bugfest versions
-                                    if (jira_service_version_list[j].toInteger() <= service_version_list[j].toInteger()){
-                                        check_is_less=true
-                                        break
+                    json.each { i ->
+                        list_key_value = "${i}".split(':')
+                        value = list_key_value[-1].substring(0, list_key_value[-1].length() - 1)
+
+                        if (value.contains("folio_")) {
+                            value = value.replace("folio_", "ui-")
+                        }
+                        pattern = "-[0-9]"
+                        key = "${value}".split(pattern)[0]
+                        value = "${value}".split("${key}-")[-1]
+
+                        bugfest_map.put(key, value)
+                    }
+                }
+                stage('Compare versions of modules') {
+                    for (i in list_of_found_jira_tasks) {
+                        check_is_less = false
+                        try {
+                            service_version = bugfest_map[i.project]
+                            jira_service_version = i.fixVersions.substring(1, i.fixVersions.length() - 1)
+                            if (service_version != null) {
+                                check_is_less = false
+                                service_version_list = service_version.replace(".", "").split(",")
+                                jira_service_version_list = jira_service_version.replace(".", "").split(",")
+
+                                if (service_version_list.length == jira_service_version_list.length) {
+                                    for (j = 0; j < jira_service_version_list.size(); j++) {
+                                        //Compare jira and bugfest versions
+                                        if (jira_service_version_list[j].toInteger() <= service_version_list[j].toInteger()) {
+                                            check_is_less = true
+                                            break
+                                        }
                                     }
                                 }
                             }
+                            if (check_is_less) {
+                                list_of_jira_tasks_to_change.add(i)
+                            }
+                        } catch (exception) {
+                            println(exception)
                         }
-                        if (check_is_less) {
-                            list_of_jira_tasks_to_change.add(i)
-                        }
-                    }catch (exception){
-                        println(exception)
                     }
                 }
-            }
-            stage('Change status of Jira ticket') {
-                for(i in list_of_jira_tasks_to_change) {
-                    jiraClient.addIssueComment(i.id, Comment)
-                    jiraClient.issueTransition(i.id, NextJiraTaskStatus)
-                    println "Jira ticket '${i.key}' status changed to '${NextJiraTaskStatus}'"
-                    println "Jira ticket version '${i.fixVersions}' bugfest version'${bugfest_map[i.project]}'"
+                stage('Change status of Jira ticket') {
+                    for (i in list_of_jira_tasks_to_change) {
+                        jiraClient.addIssueComment(i.id, Comment)
+                        jiraClient.issueTransition(i.id, NextJiraTaskStatus)
+                        println "Jira ticket '${i.key}' status changed to '${NextJiraTaskStatus}'"
+                        println "Jira ticket version '${i.fixVersions}' bugfest version'${bugfest_map[i.project]}'"
+                    }
                 }
-            }
-        } catch (exception) {
-            println(exception)
-            error(exception.getMessage())
-        } finally {
-            stage('Cleanup') {
-                cleanWs notFailBuild: true
+            } catch (exception) {
+                println(exception)
+                error(exception.getMessage())
+            } finally {
+                stage('Cleanup') {
+                    cleanWs notFailBuild: true
+                }
             }
         }
     }
