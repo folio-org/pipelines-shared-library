a0507fe1e332e4d08de5adef2ce794426446deb7
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2023-05-09 13:27:30 +0300

    RANCHER-751

--- pipelines/automation/start-stop-project.groovy
+++ pipelines/automation/start-stop-project.groovy
@@ -54,85 +54,89 @@ ansiColor('xterm') {
         println('REFRESH PARAMETERS!')
         return
     }
-    node('rancher') {
-        try {
-            if (params.action == 'stop') {
-                stage("Downscale namespace replicas") {
-                    helm.k8sClient {
-                        awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                        def deployments_list = kubectl.getKubernetesResourceList('deployment', params.rancher_project_name)
-                        def postgresql = kubectl.getKubernetesResourceList('statefulset',params.rancher_project_name).findAll{it.startsWith("postgresql-${params.rancher_project_name}")}
-                        kubectl.deleteConfigMap('deployments-replica-count-json', params.rancher_project_name)
-                        def deployments_replica_count_table = [:]
-                        deployments_list.each { deployment ->
-                            deployments_replica_count_table.put(deployment, kubectl.getKubernetesResourceCount('deployment', deployment, params.rancher_project_name))
-                        }
-                        def jsonString = new JsonBuilder(deployments_replica_count_table).toString()
-                        new Tools(this).createFileFromString('deployments_replica_count_table_json',jsonString)
-                        kubectl.createConfigMap('deployments-replica-count-json', params.rancher_project_name, './deployments_replica_count_table_json')
-                        deployments_list.each { deployment ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, '0')
-                        }
-                        if (!kubectl.checkKubernetesResourceExist('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name)){
-                            kubectl.setKubernetesResourceCount('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '0')
-                        }
-                        else {
-                            awscli.stopRdsCluster("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
-                        }
-                    }
-                }
-            }
-            else if (params.action == 'start') {
-                stage("Upscale namespace replicas") {
-                    helm.k8sClient {
-                        awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                        String configMap = kubectl.getConfigMap('deployments-replica-count-json', params.rancher_project_name, 'deployments_replica_count_table_json')
-                        def deployments_list = new groovy.json.JsonSlurperClassic().parseText(configMap)
-                        def services_list = deployments_list.findAll { key, value -> !["mod-", "edge-", "okapi", "ldp-server", "ui-bundle"].any { prefix -> key.startsWith(prefix) } }
-                        def core_modules_list = ["okapi", "mod-users", "mod-users-bl", "mod-login", "mod-permissions", "mod-authtoken"]
-                        def core_modules_list_map = deployments_list.findAll { key, value -> core_modules_list.any { prefix -> key.startsWith(prefix) } }
-                        def backend_module_list = deployments_list.findAll { key, value -> ["mod-"].any { prefix -> key.startsWith(prefix) } }
-                        def edge_module_list = deployments_list.findAll { key, value -> ["edge-"].any { prefix -> key.startsWith(prefix) } }
-                        def ui_bundle_list = deployments_list.findAll { key, value -> ["ui-bundle"].any { prefix -> key.contains(prefix) } }
-                        if (!kubectl.checkKubernetesResourceExist('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name)){
-                            kubectl.setKubernetesResourceCount('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '1')
-                            kubectl.waitKubernetesResourceStableState('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '1', '600')
-                        }
-                        else {
-                            awscli.startRdsCluster("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
-                            awscli.waitRdsClusterAvailable("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
-                            sleep 20
-                        }
-                        println(services_list)
+    podTemplate(inheritFrom: 'rancher-kube', containers: [
+        containerTemplate(name: 'k8sclient', image: Constants.DOCKER_K8S_CLIENT_IMAGE, command: "sleep", args: "99999999")]
+    ) {
+        node(POD_LABEL) {
+            try {
+                container('k8sclient') {
+                    withCredentials([[$class           : 'AmazonWebServicesCredentialsBinding',
+                                      credentialsId    : Constants.AWS_CREDENTIALS_ID,
+                                      accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                        if (params.action == 'stop') {
+                            stage("Downscale namespace replicas") {
+                                awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                def deployments_list = kubectl.getKubernetesResourceList('deployment', params.rancher_project_name)
+                                def postgresql = kubectl.getKubernetesResourceList('statefulset', params.rancher_project_name).findAll { it.startsWith("postgresql-${params.rancher_project_name}") }
+                                kubectl.deleteConfigMap('deployments-replica-count-json', params.rancher_project_name)
+                                def deployments_replica_count_table = [:]
+                                deployments_list.each { deployment ->
+                                    deployments_replica_count_table.put(deployment, kubectl.getKubernetesResourceCount('deployment', deployment, params.rancher_project_name))
+                                }
+                                def jsonString = new JsonBuilder(deployments_replica_count_table).toString()
+                                new Tools(this).createFileFromString('deployments_replica_count_table_json', jsonString)
+                                kubectl.createConfigMap('deployments-replica-count-json', params.rancher_project_name, './deployments_replica_count_table_json')
+                                deployments_list.each { deployment ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, '0')
+                                }
+                                if (!kubectl.checkKubernetesResourceExist('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name)) {
+                                    kubectl.setKubernetesResourceCount('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '0')
+                                } else {
+                                    awscli.stopRdsCluster("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
+                                }
+                            }
+                        } else if (params.action == 'start') {
+                            stage("Upscale namespace replicas") {
+                                awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                String configMap = kubectl.getConfigMap('deployments-replica-count-json', params.rancher_project_name, 'deployments_replica_count_table_json')
+                                def deployments_list = new groovy.json.JsonSlurperClassic().parseText(configMap)
+                                def services_list = deployments_list.findAll { key, value -> !["mod-", "edge-", "okapi", "ldp-server", "ui-bundle"].any { prefix -> key.startsWith(prefix) } }
+                                def core_modules_list = ["okapi", "mod-users", "mod-users-bl", "mod-login", "mod-permissions", "mod-authtoken"]
+                                def core_modules_list_map = deployments_list.findAll { key, value -> core_modules_list.any { prefix -> key.startsWith(prefix) } }
+                                def backend_module_list = deployments_list.findAll { key, value -> ["mod-"].any { prefix -> key.startsWith(prefix) } }
+                                def edge_module_list = deployments_list.findAll { key, value -> ["edge-"].any { prefix -> key.startsWith(prefix) } }
+                                def ui_bundle_list = deployments_list.findAll { key, value -> ["ui-bundle"].any { prefix -> key.contains(prefix) } }
+                                if (!kubectl.checkKubernetesResourceExist('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name)) {
+                                    kubectl.setKubernetesResourceCount('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '1')
+                                    kubectl.waitKubernetesResourceStableState('statefulset', "postgresql-${params.rancher_project_name}", params.rancher_project_name, '1', '600')
+                                } else {
+                                    awscli.startRdsCluster("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
+                                    awscli.waitRdsClusterAvailable("rds-${params.rancher_cluster_name}-${params.rancher_project_name}", Constants.AWS_REGION)
+                                    sleep 20
+                                }
+                                println(services_list)
 
-                        services_list.each { deployment, replica_count ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
-                            kubectl.checkDeploymentStatus(deployment, params.rancher_project_name, "600")
-                            sleep 10
-                        }
-                        core_modules_list_map.each { deployment, replica_count ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
-                            kubectl.checkDeploymentStatus(deployment, params.rancher_project_name, "600")
-                            sleep 10
-                        }
-                        backend_module_list.each { deployment, replica_count ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
-                        }
-                        edge_module_list.each { deployment, replica_count ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
-                        }
-                        ui_bundle_list.each { deployment ->
-                            kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, 1)
+                                services_list.each { deployment, replica_count ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
+                                    kubectl.checkDeploymentStatus(deployment, params.rancher_project_name, "600")
+                                    sleep 10
+                                }
+                                core_modules_list_map.each { deployment, replica_count ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
+                                    kubectl.checkDeploymentStatus(deployment, params.rancher_project_name, "600")
+                                    sleep 10
+                                }
+                                backend_module_list.each { deployment, replica_count ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
+                                }
+                                edge_module_list.each { deployment, replica_count ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, replica_count.toString())
+                                }
+                                ui_bundle_list.each { deployment ->
+                                    kubectl.setKubernetesResourceCount('deployment', deployment.toString(), params.rancher_project_name, 1)
+                                }
+                            }
                         }
                     }
                 }
-            }
-        } catch (exception) {
-            println(exception)
-            error(exception.getMessage())
-        } finally {
-            stage('Cleanup') {
-                cleanWs notFailBuild: true
+                    } catch (exception) {
+                        println(exception)
+                        error(exception.getMessage())
+                    } finally {
+                        stage('Cleanup') {
+                            cleanWs notFailBuild: true
+                }
             }
         }
     }
