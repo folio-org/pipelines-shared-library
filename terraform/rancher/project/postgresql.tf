f5b1cd71c2d859011e19af37abfa74c3fdd20877
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-05-16 13:04:01 +0300

    Fix host for pgadmin

--- terraform/rancher/project/postgresql.tf
+++ terraform/rancher/project/postgresql.tf
@@ -267,7 +267,7 @@ resource "rancher2_app_v2" "pgadmin4" {
           Group: Servers
           Port: 5432
           Username: ${var.pg_embedded ? var.pg_username : module.rds[0].cluster_master_username}
-          Host: ${var.pg_embedded ? join("-", ["postgresql", var.rancher_project_name]) : module.rds[0].cluster_endpoint}
+          Host: ${var.pg_embedded ? var.pg_service_writer : module.rds[0].cluster_endpoint}
           SSLMode: prefer
           MaintenanceDB: ${var.pg_dbname}
   EOT
