9cda893c076726cc4e086e628f941ddab5f65d46
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-05-25 11:59:26 +0300

    test

--- terraform/rancher/project/versions.tf
+++ terraform/rancher/project/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.62.0"
+      version = "~> 4.62"
     }
     rancher2 = {
       source  = "rancher/rancher2"
