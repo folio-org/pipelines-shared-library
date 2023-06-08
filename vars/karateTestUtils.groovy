2deb8f016b86161a1fd978f5f3fb14ea8a827613
Kseniia Dubniak <kseniia_dubniak@epam.com>
2023-06-08 21:25:28 +0300

    test

--- vars/karateTestUtils.groovy
+++ vars/karateTestUtils.groovy
@@ -295,6 +295,7 @@ private String getIssueDescription(KarateFeatureExecutionSummary featureSummary)
         .replaceAll("\\{", "&#125;")
 }
 
+@NonCPS
 private JiraClient getJiraClient() {
     withCredentials([
         usernamePassword(credentialsId: Constants.JIRA_CREDENTIALS_ID, usernameVariable: 'jiraUsername', passwordVariable: 'jiraPassword')
