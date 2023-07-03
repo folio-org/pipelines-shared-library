e213141a4b1487e227d81d076a9c3813951275ba
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2023-07-03 13:50:57 +0300

    RANCHER-876

--- pipelines/automation/manage-do-not-scale-label.groovy
+++ pipelines/automation/manage-do-not-scale-label.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-876') _
 
 import org.folio.Constants
 import groovy.json.JsonSlurperClassic
@@ -8,11 +8,13 @@ import org.folio.utilities.Tools
 import org.jenkinsci.plugins.workflow.libs.Library
 import groovy.json.JsonBuilder
 
-def labelKey = "do_not_scale_down"
+def labelKeyTonight = "do_not_scale_down_tonight"
+def labelKeyUpToNextMonday = "do_not_scale_down_up_to_next_monday"
 
 properties([
     buildDiscarder(logRotator(numToKeepStr: '20')),
     parameters([
+        choice(name: 'timeline', choices: ['tonight', 'up_to_next_monday'], description: '(Required) Choose what time period your environment should be UP. tonight - this night only, up_to_next_monday - up to next Monday'),
         choice(name: 'action', choices: ['get', 'add', 'delete'], description: '(Required) Choose what should be done with do_not_scale_down label in project'),
         jobsParameters.clusterName(),
         jobsParameters.projectName(),
@@ -31,50 +33,105 @@ ansiColor('xterm') {
                 buildName "${params.rancher_cluster_name}-${params.rancher_project_name}"
                 buildDescription "action: ${params.action} label\n"
             }
-            switch (params.action) {
-                case 'get':
-                    stage("Get list of label in project") {
-                        helm.k8sClient {
-                            awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                            String namespaceLabels = kubectl.getLabelsFromNamespace(params.rancher_project_name)
+            switch (params.timeline) {
+                case 'tonight':
+                    switch (params.action) {
+                        case 'get':
+                            stage("Get list of label in project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    String namespaceLabels = kubectl.getLabelsFromNamespace(params.rancher_project_name)
 
-                            // Parse the JSON string
-                            def labels = new groovy.json.JsonSlurperClassic().parseText(namespaceLabels)
-                            
-                            // Format the labels
-                            StringBuilder formattedLabels = new StringBuilder()
-                            labels.each { key, value ->
-                                formattedLabels.append("${key}:${value}\n")
-                                // Check if the label key is "do_not_scale_down"
-                                if (key == labelKey) {
-                                    currentBuild.description += "${key}: ${value}\n"
+                                    // Parse the JSON string
+                                    def labels = new groovy.json.JsonSlurperClassic().parseText(namespaceLabels)
+
+                                    // Format the labels
+                                    StringBuilder formattedLabels = new StringBuilder()
+                                    labels.each { key, value ->
+                                        formattedLabels.append("${key}:${value}\n")
+                                        // Check if the label key is "do_not_scale_down"
+                                        if (key == labelKeyTonight) {
+                                            currentBuild.description += "${key}: ${value}\n"
+                                        }
+                                    }
+
+                                    println "\u001B[32mLabels already set to project:\u001B[0m"
+                                    println formattedLabels.toString()
+
                                 }
                             }
-                            
-                            println "\u001B[32mLabels already set to project:\u001B[0m"
-                            println formattedLabels.toString()
+                            break
+                        case 'add':
+                            stage("Add do_not_scale_down label to project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    kubectl.addLabelToNamespace(params.rancher_project_name, labelKeyTonight, "true")
+                                }
+                            }
+                            break
+                        case 'delete':
+                            stage("Delete do_not_scale_down label to project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    kubectl.deleteLabelFromNamespace(params.rancher_project_name, labelKeyTonight)
+                                }
+                            }
+                            break
+                        default:
+                            throw new Exception("Action ${params.action} is unknown")
+                            break
+                    }
+                    break
+                case 'up_to_next_monday':
+                    switch (params.action) {
+                        case 'get':
+                            stage("Get list of label in project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    String namespaceLabels = kubectl.getLabelsFromNamespace(params.rancher_project_name)
 
-                        }
-                    }
-                    break
-                case 'add':
-                    stage("Add do_not_scale_down label to project") {
-                        helm.k8sClient {
-                            awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                            kubectl.addLabelToNamespace(params.rancher_project_name, labelKey, "true")
-                        }
-                    }
-                    break
-                case 'delete':
-                    stage("Delete do_not_scale_down label to project") {
-                        helm.k8sClient {
-                            awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
-                            kubectl.deleteLabelFromNamespace(params.rancher_project_name, labelKey)
-                        }
+                                    // Parse the JSON string
+                                    def labels = new groovy.json.JsonSlurperClassic().parseText(namespaceLabels)
+
+                                    // Format the labels
+                                    StringBuilder formattedLabels = new StringBuilder()
+                                    labels.each { key, value ->
+                                        formattedLabels.append("${key}:${value}\n")
+                                        // Check if the label key is "do_not_scale_down"
+                                        if (key == labelKeyUpToNextMonday) {
+                                            currentBuild.description += "${key}: ${value}\n"
+                                        }
+                                    }
+
+                                    println "\u001B[32mLabels already set to project:\u001B[0m"
+                                    println formattedLabels.toString()
+
+                                }
+                            }
+                            break
+                        case 'add':
+                            stage("Add do_not_scale_down label to project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    kubectl.addLabelToNamespace(params.rancher_project_name, labelKeyUpToNextMonday, "true")
+                                }
+                            }
+                            break
+                        case 'delete':
+                            stage("Delete do_not_scale_down label to project") {
+                                helm.k8sClient {
+                                    awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
+                                    kubectl.deleteLabelFromNamespace(params.rancher_project_name, labelKeyUpToNextMonday)
+                                }
+                            }
+                            break
+                        default:
+                            throw new Exception("Action ${params.action} is unknown")
+                            break
                     }
                     break
                 default:
-                    throw new Exception("Action ${params.action} is unknown")
+                    throw new Exception("Timeline ${params.timeline} is unknown")
                     break
             }
         } catch (exception) {
--- pipelines/automation/start-stop-project.groovy
+++ pipelines/automation/start-stop-project.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-@Library('pipelines-shared-library') _
+@Library('pipelines-shared-library@RANCHER-876') _
 
 import org.folio.Constants
 import groovy.json.JsonSlurperClassic
@@ -11,8 +11,10 @@ import java.util.Calendar
 
 def cluster_project_map = new JsonSlurperClassic().parseText(jobsParameters.generateProjectNamesMap())
 assert cluster_project_map instanceof Map
-def labelKey = "do_not_scale_down"
-def labelKeyExists = false
+def labelKeyTonight = "do_not_scale_down_tonight"
+def labelKeyUpToNextMonday = "do_not_scale_down_up_to_next_monday"
+def tonightLabelKeyExists = false
+def weekendsLabelKeyExists = false
 def postgresqlResources
 
 properties([
@@ -27,107 +29,7 @@ properties([
         0 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=bama
         30 22 * * 0 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=bama
 
-        0 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=firebird
-        30 22 * * 0 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=firebird
 
-        5 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=folijet
-        45 22 * * 0 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=folijet
-
-        5 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=nla
-        45 22 * * 0 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=nla
-
-        10 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=spanish
-        0 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=spanish
-
-        10 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=spitfire
-        0 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=spitfire
-
-        15 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=thunderjet
-        15 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=thunderjet
-
-        15 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=vega
-        15 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=vega
-
-        20 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=volaris
-        30 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=volaris
-
-        20 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=volaris-2nd
-        30 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=volaris-2nd
-
-
-
-        55 21 * * 5 %action=stop;rancher_cluster_name=folio-perf;rancher_project_name=vega
-        35 23 * * 1 %action=start;rancher_cluster_name=folio-perf;rancher_project_name=vega
-
-        55 21 * * 5 %action=stop;rancher_cluster_name=folio-perf;rancher_project_name=folijet
-        35 23 * * 1 %action=start;rancher_cluster_name=folio-perf;rancher_project_name=folijet
-
-
-
-        25 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=consortia
-        40 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=consortia
-
-        25 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=tamu
-        40 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=tamu
-
-        25 22 * * 5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=task-force
-        45 23 * * 1 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=task-force
-
-
-
-
-
-
-
-
-        0 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=bama
-        0 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=bama
-
-        0 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=firebird
-        0 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=firebird
-
-        0 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=folijet
-        0 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=folijet
-
-        10 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=nla
-        15 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=nla
-
-        10 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=spanish
-        15 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=spanish
-
-        10 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=spitfire
-        15 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=spitfire
-
-        15 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=thunderjet
-        30 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=thunderjet
-
-        15 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=vega
-        30 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=vega
-
-        15 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=volaris
-        30 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=volaris
-
-        20 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=volaris-2nd
-        45 05 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=volaris-2nd
-
-
-
-        55 23 * * 1-4 %action=stop;rancher_cluster_name=folio-perf;rancher_project_name=vega
-        45 05 * * 2-5 %action=start;rancher_cluster_name=folio-perf;rancher_project_name=vega
-
-        55 23 * * 1-4 %action=stop;rancher_cluster_name=folio-perf;rancher_project_name=folijet
-        45 05 * * 2-5 %action=start;rancher_cluster_name=folio-perf;rancher_project_name=folijet
-
-
-
-        20 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=consortia
-        00 06 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=consortia
-
-        20 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=tamu
-        00 06 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=tamu
-
-        25 00 * * 2-5 %action=stop;rancher_cluster_name=folio-dev;rancher_project_name=task-force
-        00 06 * * 2-5 %action=start;rancher_cluster_name=folio-dev;rancher_project_name=task-force
     ''')
     ])
 ])
@@ -143,14 +45,19 @@ ansiColor('xterm') {
             stage('Init') {
                 buildName "${params.rancher_cluster_name}-${params.rancher_project_name}.${params.action}"
                 buildDescription "action: ${params.action} project\n"
+                BUILD_TRIGGER_BY = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
+                echo "BUILD_TRIGGER_BY: ${BUILD_TRIGGER_BY}"
                 helm.k8sClient {
                     awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
                         String namespaceLabels = kubectl.getLabelsFromNamespace(params.rancher_project_name)
                         def labels = new groovy.json.JsonSlurperClassic().parseText(namespaceLabels)
                         labels.each { key, value ->
                             // Check if the label key is "do_not_scale_down"
-                            if (key == labelKey) {
-                                labelKeyExists = true
+                            if (key == labelKeyTonight) {
+                                tonightLabelKeyExists = true
+                            }
+                            if (key == labelKeyUpToNextMonday) {
+                                weekendsLabelKeyExists = true
                             }
                         }
                         postgresqlResources = kubectl.getKubernetesResourceList('statefulset',params.rancher_project_name).findAll{it.startsWith("postgresql-${params.rancher_project_name}")}
@@ -161,8 +68,8 @@ ansiColor('xterm') {
                     helm.k8sClient {
                         awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
 
-                        if (labelKeyExists) {
-                            println "\u001B[32mProject ${params.rancher_project_name} has label ${labelKey} and will not be disabled this time.\u001B[0m"
+                        if (tonightLabelKeyExists == true || weekendsLabelKeyExists == true) {
+                            println "\u001B[32mProject ${params.rancher_project_name} has label ${labelKeyTonight}/${labelKeyUpToNextMonday} and will not be disabled this time.\u001B[0m"
                             currentBuild.description += "Skip stop action"
                         } else {
                             println "Shutting down the project..."
@@ -191,6 +98,11 @@ ansiColor('xterm') {
                 }
             }
             else if (params.action == 'start') {
+                Calendar calendar = Calendar.getInstance()
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+
+                /*if ((dayOfWeek == Calendar.MONDAY || dayOfWeek == Calendar.SUNDAY) && labelKeyExists) {
+                }*/
                 stage("Upscale namespace replicas") {
                     helm.k8sClient {
                         awscli.getKubeConfig(Constants.AWS_REGION, params.rancher_cluster_name)
@@ -237,8 +149,6 @@ ansiColor('xterm') {
                         }
 
                         // Delete tag if Monday or Sunday
-                        Calendar calendar = Calendar.getInstance()
-                        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
                         if ((dayOfWeek == Calendar.MONDAY || dayOfWeek == Calendar.SUNDAY) && labelKeyExists) {
                             println "Deleting ${labelKey} label from project ${params.rancher_project_name}"
                             kubectl.deleteLabelFromNamespace(params.rancher_project_name, labelKey)
