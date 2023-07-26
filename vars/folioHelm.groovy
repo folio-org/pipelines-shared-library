d310d11844ded4ec2a471735e5a9391971ac4d5a
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2023-07-26 15:41:49 +0600

    Rancher-903 (#293)
    * RANCHER-903 test v1
    
    * RANCHER-903 test v1
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v2
    
    * RANCHER-903 test v3
    
    * RANCHER-903 test v3
    
    * RANCHER-903 test v4
    
    * RANCHER-903 test v5
    
    * RANCHER-903 test v6
    
    * RANCHER-903
    
    Parameter corrected according to comment and branch switched to origin
    
    * RANCHER-903 refactored according to comments.
    
    * RANCHER-903 final test
    
    * RANCHER-903 final test revert

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -176,6 +176,11 @@ String generateModuleValues(RancherNamespace ns, String moduleName, String modul
         moduleConfig << [consortiumEnabled: "true"]
     }
 
+    //Enable cross tenant extra env
+    if (moduleName == 'mod-authtoken' && ns.enableConsortia){
+       moduleConfig['javaOptions'] += ' -Dallow.cross.tenant.requests=true'
+    }
+
     // Enable ingress
     boolean enableIngress = moduleConfig.containsKey('ingress') ? moduleConfig['ingress']['enabled'] : false
     if (enableIngress) {
