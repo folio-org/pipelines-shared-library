3a57acdfb283082d76d08ba5f97941f04f05008f
gjalaghonia <guram_jalaghonia@epam.com>
2023-08-01 18:59:39 +0400

    add commend and re-undo shared library branch name

--- src/org/folio/models/InstallQueryParameters.groovy
+++ src/org/folio/models/InstallQueryParameters.groovy
@@ -59,6 +59,7 @@ class InstallQueryParameters implements Cloneable {
         this.tenantParameters = sb.toString()
     }
 
+    // method overloading, to accept boolean and string values
     void addTenantParameter(String key, boolean value) {
         addTenantParameter(key, String.valueOf(value))
     }
