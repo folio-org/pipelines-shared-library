2a78ce3281d10f2ff12a4fd0d23924d9db1faf9c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-08-22 19:14:04 +0600

    RANCHER-951 personal accessToken test v2

--- vars/folioStringScripts.groovy
+++ vars/folioStringScripts.groovy
@@ -8,7 +8,10 @@ return namespacesList[CLUSTER]
 
 static String getRepositoryBranches(String repository){
     return """import groovy.json.JsonSlurperClassic
-def get = new URL('${Constants.FOLIO_GITHUB_REPOS_URL}/${repository}/branches?per_page=100').openConnection()
+def credentialId = "id-jenkins-github-personal-token"
+def credential = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(credentialId) }
+def secretText = credential.getSecret().getPlainText()
+def get = new URL('${Constants.FOLIO_GITHUB_REPOS_URL}/${repository}/branches?per_page=100&Authorization=${secretText}').openConnection()
 if (get.getResponseCode().equals(200)) {
     return new JsonSlurperClassic().parseText(get.getInputStream().getText()).name
 }
@@ -21,7 +24,7 @@ import groovy.json.JsonSlurperClassic
 def credentialId = "id-jenkins-github-personal-token"
 def credential = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(credentialId) }
 def secretText = credential.getSecret().getPlainText()
-def installJson = new URL("${Constants.FOLIO_GITHUB_RAW_URL}/platform-complete/\${FOLIO_BRANCH}/install.json&Authorization=${secretText}").openConnection()
+def installJson = new URL("${Constants.FOLIO_GITHUB_RAW_URL}/platform-complete/\${FOLIO_BRANCH}/install.json").openConnection()
 if (installJson.getResponseCode().equals(200)) {
     String okapi = new JsonSlurperClassic().parseText(installJson.getInputStream().getText())*.id.find{it ==~ /okapi-.*/}
     if(okapi){
