754dc4d4c4fa1b35029d2613d3d00eebec86c4b4
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-08-22 20:13:46 +0600

    RANCHER-951 personal accessToken test v3 for branches

--- vars/folioStringScripts.groovy
+++ vars/folioStringScripts.groovy
@@ -11,19 +11,17 @@ static String getRepositoryBranches(String repository){
 def credentialId = "id-jenkins-github-personal-token"
 def credential = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(credentialId) }
 def secretText = credential.getSecret().getPlainText()
-def get = new URL("${Constants.FOLIO_GITHUB_REPOS_URL}/${repository}/branches?per_page=100&Authorization=${secretText}").openConnection()
-if (get.getResponseCode().equals(200)) {
-    return new JsonSlurperClassic().parseText(get.getInputStream().getText()).name
+def get = new URL("${Constants.FOLIO_GITHUB_REPOS_URL}/${repository}/branches?per_page=100")
+HttpURLConnection conn = (HttpURLConnection) get.openConnection()
+conn.setRequestProperty("Authorization","Bearer "+" ${secretText}");
+if(conn.responseCode.equals(200)){
+  return new JsonSlurperClassic().parseText(conn.getInputStream().getText()).name
 }
 """
 }
 
 static String getOkapiVersions(){
     return """import groovy.json.JsonSlurperClassic
-import groovy.json.JsonSlurperClassic
-def credentialId = "id-jenkins-github-personal-token"
-def credential = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(credentialId) }
-def secretText = credential.getSecret().getPlainText()
 def installJson = new URL("${Constants.FOLIO_GITHUB_RAW_URL}/platform-complete/\${FOLIO_BRANCH}/install.json").openConnection()
 if (installJson.getResponseCode().equals(200)) {
     String okapi = new JsonSlurperClassic().parseText(installJson.getInputStream().getText())*.id.find{it ==~ /okapi-.*/}
