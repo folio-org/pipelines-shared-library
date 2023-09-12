67501d22ca12aa7acf8a84ff13060131ae71f90f
vkapylou <vasili_kapylou@epam.com>
2023-09-12 13:48:37 +0200

    Update Constants.groovy with new rtr team

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -47,7 +47,8 @@ class Constants {
                                           'unam',
                                           'vega',
                                           'volaris',
-                                          'volaris-2nd']
+                                          'volaris-2nd',
+                                          'rtr']
     static Map AWS_EKS_NAMESPACE_MAPPING = ['folio-testing': AWS_EKS_TESTING_NAMESPACES,
                                             'folio-dev'    : AWS_EKS_DEV_NAMESPACES,
                                             'folio-perf'   : AWS_EKS_DEV_NAMESPACES,
