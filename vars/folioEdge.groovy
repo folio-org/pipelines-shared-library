7c95664420dfe482ac9294d4be6c8840e8d179d3
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-10-07 19:41:05 +0600

    RANCHER-984 logging enhanced.

--- vars/folioEdge.groovy
+++ vars/folioEdge.groovy
@@ -1,6 +1,7 @@
 import groovy.text.StreamingTemplateEngine
 import org.folio.models.OkapiTenant
 import org.folio.models.RancherNamespace
+import org.folio.rest_v2.Common
 import org.folio.utilities.Tools
 
 
@@ -34,7 +35,7 @@ void renderEphemeralProperties(OkapiTenant tenant, RancherNamespace ns) {
     ns.tenants.each { tenant_name, tenant_cm ->
       switch (tenant_cm.tenantId) {
         case "supertenant":
-          println("The ${tenant_cm} should not be presented in config, aborted!")
+          new Common(this, ns.getDomains()[tenant_cm.tenantId] as String).logger.warning("Tenant ${tenant_cm.tenantId} should not be presented in config, aborted!")
           break
         case "diku":
           admin_users += "${tenant_cm.tenantId + "=" + tenant_cm.adminUser.username + "," + tenant_cm.adminUser.password + "\n"}"
