3bf9c6407fbf2445e3bcb2aff30e8e54cc8756c0
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-10-09 00:05:14 +0600

    RANCHER-984 edge-fqm emergency include to avoid issues during environments provisioning.

--- vars/folioEdge.groovy
+++ vars/folioEdge.groovy
@@ -1,6 +1,7 @@
 import groovy.text.StreamingTemplateEngine
 import org.folio.models.OkapiTenant
 import org.folio.models.RancherNamespace
+import org.folio.rest_v2.Common
 import org.folio.utilities.Tools
 
 /**
@@ -13,6 +14,7 @@ import org.folio.utilities.Tools
 
 void renderEphemeralProperties(OkapiTenant tenant, RancherNamespace ns) {
   Tools tools = new Tools(this)
+  Common common = new Common(this, "${ns.getDomains()['okapi']}")
   Map edgeUsersConfig = tools.steps.readYaml file: tools.copyResourceFileToWorkspace("edge/config.yaml")
   String defaultTenantId = tenant.tenantId
   String config_template = tools.steps.readFile file: tools.copyResourceFileToWorkspace("edge/ephemeral-properties.tpl")
@@ -21,30 +23,34 @@ void renderEphemeralProperties(OkapiTenant tenant, RancherNamespace ns) {
     String institutional = ""
     String admin_users = ""
     Map edgeUserConfig = edgeUsersConfig[(name)]
-    if (edgeUserConfig['tenants']) {
-      edgeUserConfig['tenants'].each {
-        Map obj = [tenant  : it.tenant == "default" ? defaultTenantId : it.tenant,
-                   username: it.username,
-                   password: it.tenant == "default" ? defaultTenantId : it.password]
-        edgeTenantsId += it.tenant == "default" ? "" : "," + it.tenant
-        institutional += obj.tenant + "=" + obj.username + "," + obj.password + "\n"
+    try {
+      if (edgeUserConfig['tenants']) {
+        edgeUserConfig['tenants'].each {
+          Map obj = [tenant  : it.tenant == "default" ? defaultTenantId : it.tenant,
+                     username: it.username,
+                     password: it.tenant == "default" ? defaultTenantId : it.password]
+          edgeTenantsId += it.tenant == "default" ? "" : "," + it.tenant
+          institutional += obj.tenant + "=" + obj.username + "," + obj.password + "\n"
+        }
       }
-    }
-    ns.tenants.each { tenant_name, tenant_cm ->
-      switch (tenant_cm.tenantId) {
-        case "supertenant":
-          println("The ${tenant_cm} should not be presented in config, aborted!")
-          break
-        case "diku":
-          admin_users += "${tenant_cm.tenantId + "=" + tenant_cm.adminUser.username + "," + tenant_cm.adminUser.password + "\n"}"
-          break
-        default:
-          admin_users += "${tenant_cm.tenantId + "=" + tenant_cm.adminUser.username + "," + tenant_cm.adminUser.password + "\n"}"
-          edgeTenantsId += "," + tenant_cm.tenantId
-          break
+      ns.tenants.each { tenant_name, tenant_cm ->
+        switch (tenant_cm.tenantId) {
+          case "supertenant":
+            common.logger.warning("The ${tenant_cm} should not be presented in config, aborted!")
+            break
+          case "diku":
+            admin_users += "${tenant_cm.tenantId + "=" + tenant_cm.adminUser.username + "," + tenant_cm.adminUser.password + "\n"}"
+            break
+          default:
+            admin_users += "${tenant_cm.tenantId + "=" + tenant_cm.adminUser.username + "," + tenant_cm.adminUser.password + "\n"}"
+            edgeTenantsId += "," + tenant_cm.tenantId
+            break
+        }
       }
+      LinkedHashMap config_data = [edge_tenants: edgeTenantsId, edge_mappings: defaultTenantId, edge_users: admin_users, institutional_users: institutional]
+      tools.steps.writeFile file: "${name}-ephemeral-properties", text: (new StreamingTemplateEngine().createTemplate(config_template).make(config_data)).toString()
+    } catch (Exception e) {
+      common.logger.warning("Faulty module name: ${name}, error: ${e.getMessage()}")
     }
-    LinkedHashMap config_data = [edge_tenants: edgeTenantsId, edge_mappings: defaultTenantId, edge_users: admin_users, institutional_users: institutional]
-    tools.steps.writeFile file: "${name}-ephemeral-properties", text: (new StreamingTemplateEngine().createTemplate(config_template).make(config_data)).toString()
   }
 }
