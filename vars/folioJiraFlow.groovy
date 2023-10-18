10379e8de4c7f3a91cf7db309bef272007f55994
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-10-18 13:44:55 +0600

    RANCHER-927 required objects defined & credentials bound.

--- vars/folioJiraFlow.groovy
+++ vars/folioJiraFlow.groovy
@@ -1,3 +1,20 @@
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurperClassic
-//TBD
+import org.folio.utilities.RestClient
+/*
+***folioRancher general library for processing existing and new entries***
+* Functionality:
+* CRUD operations for Jira: tickets & comments.
+ */
+
+RestClient RestClient = new RestClient(this)
+JsonOutput JsonOutput = new JsonOutput()
+JsonSlurperClassic JsonSlurperClassic = new JsonSlurperClassic()
+
+withCredentials([usernamePassword(credentialsId: 'jenkins-jira', passwordVariable: 'jira_password', usernameVariable: 'jira_username')]) {
+  Map JiraCredentials = [
+    username: "${env.jira_username}",
+    password: "${env.jira_password}"
+  ]
+  return JiraCredentials
+}
