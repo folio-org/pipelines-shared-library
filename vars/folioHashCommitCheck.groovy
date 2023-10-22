27d5c72d0f24c3b8dc0154a805cc998fb8d46180
gjalaghonia <guram_jalaghonia@epam.com>
2023-10-22 19:52:38 +0400

    Test5

--- vars/folioHashCommitCheck.groovy
+++ vars/folioHashCommitCheck.groovy
@@ -2,6 +2,10 @@
 
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder
 import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest
+import org.folio.Constants
+import org.folio.utilities.HttpClient
+import org.folio.utilities.Logger
+import org.folio.utilities.Tools
 
 def checkHashChanges(branch, awsRegionHash) {
 
@@ -38,5 +42,17 @@ def getSavedHashFromSSM(awsRegionHash) {
   } catch (Exception e) {
     error "Error fetching saved hash from AWS SSM parameters: ${e.message}"
     return null
+  }
+}
+
+
+
+String getLastCommitHash(String repository, String branch) {
+  String url = "https://api.github.com/repos/${Constants.FOLIO_ORG}/${repository}/branches/${branch}"
+  def response = new HttpClient(this).getRequest(url)
+  if (response.status == HttpURLConnection.HTTP_OK) {
+    return new Tools(this).jsonParse(response.content).commit.sha
+  } else {
+    new Logger(this, 'common').error(new HttpClient(this).buildHttpErrorMessage(response))
   }
 }
