c6e858be2f485a2e80c79cdf02f7668bd4753c1e
gjalaghonia <guram_jalaghonia@epam.com>
2023-10-22 23:40:49 +0400

    Test18

--- vars/folioHashCommitCheck.groovy
+++ vars/folioHashCommitCheck.groovy
@@ -5,48 +5,62 @@ import org.folio.utilities.Tools
 
 def changeDetected(branch) {
 
-  def parameter_name = 'Hash-Commit'
-  def region = 'us-west-2'
-  def currentHash = getLastCommitHash(branch)
-  def savedHash = getSavedHashFromSSM(region,parameter_name)
-  echo "last commit hash ${currentHash}"
+  def awsParameterName = 'Hash-Commit'
+  def awsRegion = 'us-west-2'
+  def latestCommitHash = getLatestCommitHash(branch)
+  def previousSavedHash = getPreviousSavedHashFromSSM(awsRegion,awsParameterName)
+  echo "last commit hash ${latestCommitHash}"
   echo "saved hash from ssm ${savedHash}"
 
-  if (currentHash == savedHash) {
-    echo "No changes detected. Returning true."
+  if (latestCommitHash == previousSavedHash) {
+    echo "No changes detected. HashDiffDetected :false."
     return false
   } else {
-    echo "Changes detected. Returning true and updating ssm with new hash: ${currentHash}."
-    UpdateSsmParameterValue(region,parameter_name,currentHash)
+    echo "Changes detected. HashDiffDetected Returning true and updating ssm with new hash: ${latestCommitHash}."
+    withUpdateSsmParameterNewValue(awsRegion,awsParameterName,latestCommitHash)
     return true
   }
 }
 
-//def getCurrentGitHash(branch) {
-//  try {
-//    //return sh(script: "git ls-remote https://github.com/folio-org/pipelines-shared-library.git refs/heads/${branch} | cut -f 1", returnStdout: true).trim()
-//    def parameterValue1 = sh(script: 'aws ssm get-parameter --name Hash-Commit --region us-west-2 --query "Parameter.Value" --output text', returnStdout: true).trim()
-//    echo "Value of Last Hash-Commit: ${parameterValue1}"
-//  } catch (Exception e) {
-//    error "Error fetching Git hash: ${e.message}"
+//String getLatestCommitHash(String branch) {
+//  String url = "https://api.github.com/repos/folio-org/pipelines-shared-library/branches/RANCHER-999"
+//  def response = new HttpClient(this).getRequest(url)
+//  if (response.status == HttpURLConnection.HTTP_OK) {
+//    return new Tools(this).jsonParse(response.content).commit.sha
+//  } else {
+//    new Logger(this, 'folioHachCommitCheck').error(new HttpClient(this).buildHttpErrorMessage(response))
 //  }
 //}
 
 
-String getLastCommitHash(String branch) {
-  String url = "https://api.github.com/repos/folio-org/pipelines-shared-library/branches/RANCHER-999"
-  def response = new HttpClient(this).getRequest(url)
-  if (response.status == HttpURLConnection.HTTP_OK) {
-    return new Tools(this).jsonParse(response.content).commit.sha
-  } else {
-    new Logger(this, 'folioHachCommitCheck').error(new HttpClient(this).buildHttpErrorMessage(response))
+String getLatestCommitHash(String branch) {
+  String url = "https://api.github.com/repos/folio-org/pipelines-shared-library/branches/${branch}"
+  try {
+    def response = new HttpClient(this).getRequest(url)
+
+    if (response.status == HttpURLConnection.HTTP_OK) {
+      def parsedResponse = new Tools(this).jsonParse(response.content)
+      if (parsedResponse && parsedResponse.commit && parsedResponse.commit.sha) {
+        return parsedResponse.commit.sha
+      } else {
+        error "Failed to parse GitHub response or retrieve commit SHA."
+        return null
+      }
+    } else {
+      error "GitHub API request failed with status code ${response.status}: ${response.body}"
+      return null
+    }
+  } catch (Exception e) {
+    error "An error occurred while fetching GitHub data: ${e.message}"
+    return null
   }
 }
 
-String getSavedHashFromSSM(region, parameter_name) {
+
+String getPreviousSavedHashFromSSM(awsRegion, awsParameterName) {
   try {
     def parameterValue = sh(
-      script: "aws ssm get-parameter --name ${parameter_name} --region ${region} --query 'Parameter.Value' --output text",
+      script: "aws ssm get-parameter --name ${awsParameterName} --region ${awsRegion} --query 'Parameter.Value' --output text",
       returnStdout: true
     ).trim()
     echo "Value of Previous Saved Hash-Commit: ${parameterValue}"
@@ -58,6 +72,6 @@ String getSavedHashFromSSM(region, parameter_name) {
 }
 
 
-void UpdateSsmParameterValue(String region, String parameter_name, String currentHash) {
+void withUpdateSsmParameterNewValue(String region, String parameter_name, String currentHash) {
   sh(script: "aws ssm put-parameter --name ${parameter_name} --region ${region} --value ${currentHash} --type String --overwrite")
 }
