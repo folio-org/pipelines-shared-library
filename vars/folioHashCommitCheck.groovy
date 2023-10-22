179dffd7c10cc8cffed09731616317c72e26239a
gjalaghonia <guram_jalaghonia@epam.com>
2023-10-22 22:59:15 +0400

    Test13

--- vars/folioHashCommitCheck.groovy
+++ vars/folioHashCommitCheck.groovy
@@ -1,9 +1,15 @@
+import org.folio.Constants
+import org.folio.utilities.HttpClient
+import org.folio.utilities.Logger
+import org.folio.utilities.Tools
+
 def changeDetected(branch) {
 
   def parameter_name = 'Hash-Commit'
   def region = 'us-west-2'
-  def currentHash = getCurrentGitHash(branch)
+  def currentHash = getLastCommitHash(branch)
   def savedHash = getSavedHashFromSSM(region,parameter_name)
+  echo "last commit hash ${currentHash}"
 
   if (currentHash == savedHash) {
     echo "No changes detected. Returning true."
@@ -15,19 +21,30 @@ def changeDetected(branch) {
   }
 }
 
-def getCurrentGitHash(branch) {
-  try {
-    //return sh(script: "git ls-remote https://github.com/folio-org/pipelines-shared-library.git refs/heads/${branch} | cut -f 1", returnStdout: true).trim()
-    def parameterValue1 = sh(script: 'aws ssm get-parameter --name Hash-Commit --region us-west-2 --query "Parameter.Value" --output text', returnStdout: true).trim()
-    echo "Value of Hash-Commit: ${parameterValue1}"
-  } catch (Exception e) {
-    error "Error fetching Git hash: ${e.message}"
+//def getCurrentGitHash(branch) {
+//  try {
+//    //return sh(script: "git ls-remote https://github.com/folio-org/pipelines-shared-library.git refs/heads/${branch} | cut -f 1", returnStdout: true).trim()
+//    def parameterValue1 = sh(script: 'aws ssm get-parameter --name Hash-Commit --region us-west-2 --query "Parameter.Value" --output text', returnStdout: true).trim()
+//    echo "Value of Last Hash-Commit: ${parameterValue1}"
+//  } catch (Exception e) {
+//    error "Error fetching Git hash: ${e.message}"
+//  }
+//}
+
+
+String getLastCommitHash(String branch) {
+  String url = "https://api.github.com/repos/folio-org/pipelines-shared-library/branches/RANCHER-999"
+  def response = new HttpClient(this).getRequest(url)
+  if (response.status == HttpURLConnection.HTTP_OK) {
+    return new Tools(this).jsonParse(response.content).commit.sha
+  } else {
+    new Logger(this, 'folioHachCommitCheck').error(new HttpClient(this).buildHttpErrorMessage(response))
   }
 }
 
 def getSavedHashFromSSM(region,parameter_name) {
     def parameterValue = GetSsmParameterValue(region,parameter_name);
-    echo "${parameterValue}"
+    echo "Value of Previous Saved Hash-Commit: ${parameterValue}"
     return parameterValue
 }
 
