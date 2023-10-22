a414ba8894fa93033f90ab862a82afdd3ce7c1b7
gjalaghonia <guram_jalaghonia@epam.com>
2023-10-22 22:46:50 +0400

    Test12

--- vars/folioHashCommitCheck.groovy
+++ vars/folioHashCommitCheck.groovy
@@ -1,8 +1,3 @@
-import org.folio.Constants
-import org.folio.utilities.HttpClient
-import org.folio.utilities.Logger
-import org.folio.utilities.Tools
-
 def changeDetected(branch) {
 
   def parameter_name = 'Hash-Commit'
@@ -12,25 +7,27 @@ def changeDetected(branch) {
 
   if (currentHash == savedHash) {
     echo "No changes detected. Returning true."
-    return true
-  } else {
-    echo "Changes detected. Returning false and updating ssm with new hash: ${currentHash}."
-    UpdateSsmParameterValue(region,parameter_name,currentHash)
     return false
+  } else {
+    echo "Changes detected. Returning true and updating ssm with new hash: ${currentHash}."
+    UpdateSsmParameterValue(region,parameter_name,currentHash)
+    return true
   }
 }
 
 def getCurrentGitHash(branch) {
   try {
-    return sh(script: "git ls-remote https://github.com/folio-org/pipelines-shared-library.git refs/heads/${branch} | cut -f 1", returnStdout: true).trim()
+    //return sh(script: "git ls-remote https://github.com/folio-org/pipelines-shared-library.git refs/heads/${branch} | cut -f 1", returnStdout: true).trim()
+    def parameterValue1 = sh(script: 'aws ssm get-parameter --name Hash-Commit --region us-west-2 --query "Parameter.Value" --output text', returnStdout: true).trim()
+    echo "Value of Hash-Commit: ${parameterValue1}"
   } catch (Exception e) {
     error "Error fetching Git hash: ${e.message}"
-    return null
   }
 }
 
 def getSavedHashFromSSM(region,parameter_name) {
     def parameterValue = GetSsmParameterValue(region,parameter_name);
+    echo "${parameterValue}"
     return parameterValue
 }
 
