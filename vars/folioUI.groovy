0940b707432e2a42b316810be441fece7a1a542d
Renat Safiulin <rsafiulin@ebsco.com>
2023-10-24 14:22:05 +0500

    added deployUI job

--- vars/folioUI.groovy
+++ vars/folioUI.groovy
@@ -67,25 +67,17 @@ void build(params) {
 }
 
 void deploy(params) {
-  OkapiTenant tenant = new OkapiTenant(id: params.tenantId)
+  OkapiTenant tenant = new OkapiTenant(id: params.TENANT_ID)
   Project project_config = new Project(
-    clusterName: params.cluster,
-    projectName: params.namespace,
-    configType: 'development',
-    domains: [ui   : common.generateDomain(params.rancher_cluster_name, params.rancher_project_name, tenant.getId(), Constants.CI_ROOT_DOMAIN),
-              okapi: common.generateDomain(params.rancher_cluster_name, params.rancher_project_name, 'okapi', Constants.CI_ROOT_DOMAIN),
-              edge : common.generateDomain(params.rancher_cluster_name, params.rancher_project_name, 'edge', Constants.CI_ROOT_DOMAIN)]
+    clusterName: params.CLUSTER,
+    projectName: params.NAMESPACE,
+    domains: [ui   : common.generateDomain(params.CLUSTER, params.NAMESPACE, tenant.getId(), Constants.CI_ROOT_DOMAIN),
+              okapi: common.generateDomain(params.CLUSTER, params.NAMESPACE, 'okapi', Constants.CI_ROOT_DOMAIN),
+              edge : common.generateDomain(params.CLUSTER, params.NAMESPACE, 'edge', Constants.CI_ROOT_DOMAIN)]
   )
-  Module ui_bundle = new Module(
-    name: "ui-bundle",
-    hash: params.custom_hash?.trim() ? params.custom_hash : common.getLastCommitHash(params.repository, params.branch)
-  )
-  String okapi_url = params.custom_url?.trim() ? params.custom_url : "https://" + project_config.getDomains().okapi
-  ui_bundle.tag = params.custom_tag?.trim() ? params.custom_tag : "${project_config.getClusterName()}-${project_config.getProjectName()}.${tenant.getId()}.${ui_bundle.getHash().take(7)}"
-  ui_bundle.imageName = "${Constants.ECR_FOLIO_REPOSITORY}/${ui_bundle.getName()}:${ui_bundle.getTag()}"
 
   stage("Deploy UI bundle") {
-    folioDeploy.uiBundle(tenant, project_config)
+    folioDeploy.uiBundle(tenant.getId(), project_config)
   }
 }
 
