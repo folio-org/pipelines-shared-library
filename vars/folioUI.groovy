8bd378b36b335680c43fe682742535ee09146e03
Renat Safiulin <rsafiulin@ebsco.com>
2023-10-31 19:54:49 +0500

    apply function

--- vars/folioUI.groovy
+++ vars/folioUI.groovy
@@ -39,7 +39,8 @@ void build(params) {
 }
 
 void deploy(params) {
-  stage("Deploy UI bundle") {
-    folioDeploy.folioUiBundle(params)
+  stage("Deploy UI bundle")
+    folioHelm.withKubeConfig(params.CLUSTER()) {
+      folioHelm.deployFolioModule(params.NAMESPACE, 'ui-bundle', params.UI_BUNDLE_TAG, false, params.TENANT_ID)
   }
 }
