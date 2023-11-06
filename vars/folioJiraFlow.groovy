7379c99ce9b66018b111d6714db813469918e75c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-11-07 00:43:16 +0600

    Able to create ticket via Rest Api && cleared. RANCHER-927

--- vars/folioJiraFlow.groovy
+++ vars/folioJiraFlow.groovy
@@ -1,11 +1,6 @@
-import groovy.json.JsonOutput
 import groovy.json.JsonSlurperClassic
 import org.folio.Constants
 import org.folio.utilities.RestClient
-
-import groovy.json.JsonOutput
-
-JsonOutput.prettyPrint()
 
 /*
 ***folioRancher general library for processing existing and new entries***
@@ -13,43 +8,41 @@ JsonOutput.prettyPrint()
 * CRUD operations for Jira: tickets & comments.
 */
 
-void createJiraTicket(String summary, String DevTeam, String projectName, String type){
+void createJiraTicket(String summary, String DevTeamId, String projectName, String type) {
   withCredentials([string(credentialsId: 'JiraFlow', variable: 'JiraToken')]) {
     Map headers = [
-      "Content-Type": "application/json",
+      "Content-Type" : "application/json",
       "Authorization": "Bearer ${env.JiraToken}"
     ]
     String body = """{
     "fields": {
-       "project":
-       {"key": "${projectName}"},
+       "project": {"key": "${projectName}"},
        "summary": "${summary}",
        "description": "Automatic Jira ticket created via Jenkins",
-       "Development Team": "${DevTeam}",
-       "issuetype": {
-          "name": "${type}"
-       }
-   }
-}"""
-    new RestClient(this).post(Constants.FOLIO_JIRA_ISSUE_URL, body, headers)
+       "customfield_10501": {"id":"${DevTeamId}"},
+       "issuetype": {"name": "${type}"}
+        }
+    }"""
+    def body_data = new JsonSlurperClassic().parseText(body)
+    new RestClient(this).post(Constants.FOLIO_JIRA_ISSUE_URL, body_data, headers)
   }
 }
 
-def searchForExistingJiraTickets(String jql){
+def searchForExistingJiraTickets(String jql) {
   def existing_tickets = jiraIssueSelector(issueSelector: [$class: 'JqlIssueSelector', jql: "${jql}"])
   return existing_tickets
 }
 
-void addJiraComment(HashSet ticketNumbers, String body){
+void addJiraComment(HashSet ticketNumbers, String body) {
   ticketNumbers.each { ticketNumber ->
     jiraComment body: "${body}", issueKey: "${ticketNumber}"
   }
 }
 
-void updateJiraTicket(String ticketNumber){
+void updateJiraTicket(String ticketNumber) {
 
 }
 
-void closeJiraTicket(String ticketNumber){
+void closeJiraTicket(String ticketNumber) {
 
 }
