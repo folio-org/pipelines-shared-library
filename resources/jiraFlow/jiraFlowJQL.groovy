6e64b720b33dd72023592983ea860640ba3bc46c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-11-09 14:11:53 +0600

    RANCHER-927 add support of labeling, searching & commenting functionality via Rest Api.

--- resources/jiraFlow/jiraFlowJQL.groovy
+++ resources/jiraFlow/jiraFlowJQL.groovy
@@ -1,14 +1,11 @@
-static String getOpenTicketsByTeam(String teamName, String projectName, String summary) {
-  String jql = "Team = ${teamName} AND summary ~ ${summary}  AND status in (Open, 'In Progress')"
-  return jql
-}
-
 static String getOpenKarateTcikets(String moduleName){
   String jql = "summary ~ ${moduleName} AND labels in (karateRegressionPipeline)"
   return jql
 }
 
-static String getOpenKitFoxTickets(String summary){
-  String jql = "summary ~ ${summary} AND 'Development Team' = Kitfox AND status in (Backlog, Active, 'In progress')"
+static String getOpenTickets(String summary, String project, String team){
+  String jql = """
+  summary ~ ${summary} AND "Development Team" = ${team} AND status in (Active,"In progress","In Review","In Code Review") AND project = ${project}"
+  """
   return jql
 }
--- vars/folioJiraFlow.groovy
+++ vars/folioJiraFlow.groovy
@@ -8,6 +8,15 @@ import org.folio.utilities.RestClient
 * CRUD operations for Jira: tickets & comments.
 */
 
+Map status = [
+  "Open"          : 81,
+  "Closed"        : 61,
+  "Blocked"       : 71,
+  "In progress"   : 21,
+  "In review"     : 31,
+  "In code review": 91
+]
+
 void createJiraTicket(String summary, String DevTeamId, String projectName, String type) {
   withCredentials([string(credentialsId: 'JiraFlow', variable: 'JiraToken')]) {
     Map headers = [
@@ -29,17 +38,53 @@ void createJiraTicket(String summary, String DevTeamId, String projectName, Stri
 }
 
 def searchForExistingJiraTickets(String jql) {
-  def existing_tickets = jiraIssueSelector(issueSelector: [$class: 'JqlIssueSelector', jql: "${jql}"])
-  return existing_tickets
+  def issues
+  String search_url = "https://issues.folio.org/rest/api/2/search?jql=${jql}"
+  withCredentials([string(credentialsId: 'JiraFlow', variable: 'JiraToken')]) {
+    Map headers = [
+      "Content-Type" : "application/json",
+      "Authorization": "Bearer ${env.JiraToken}"
+    ]
+    def response = new RestClient(this).get(search_url, headers)
+    issues = response.body.issues.key
+  }
+  return issues
 }
 
-void addJiraComment(HashSet ticketNumbers, String body) {
-  ticketNumbers.each { ticketNumber ->
-    jiraComment body: "${body}", issueKey: "${ticketNumber}"
+void addJiraComment(String ticketNumber, String comment) {
+  String commentUrl = Constants.FOLIO_JIRA_ISSUE_URL + "${ticketNumber}/comment"
+  withCredentials([string(credentialsId: 'JiraFlow', variable: 'JiraToken')]) {
+    Map headers = [
+      "Content-Type" : "application/json",
+      "Authorization": "Bearer ${env.JiraToken}"
+    ]
+    String body = """
+      body: ${comment}
+      """
+    def comment_body = new JsonSlurperClassic().parseText(body)
+    new RestClient(this).post(commentUrl, headers, comment_body)
   }
 }
 
-void updateJiraTicket(String ticketNumber) {
+void addLabelJiraTicket(String ticketNumber, List labels) {
+  String labelUrl = Constants.FOLIO_JIRA_ISSUE_URL + "${ticketNumber}"
+  withCredentials([string(credentialsId: 'JiraFlow', variable: 'JiraToken')]) {
+    Map headers = [
+      "Content-Type" : "application/json",
+      "Authorization": "Bearer ${env.JiraToken}"
+    ]
+    labels.each { label ->
+      String body = """
+      { "update": { "labels": [{ "add" : "${label}"}]}}
+      """
+      def body_json = new JsonSlurperClassic().parseText(body)
+      new RestClient(this).put(labelUrl, headers, body_json)
+    }
+  }
+}
+
+void updateStatusJiraTicket(List ticketNumbers) {
+
 
 }
 
