bcc7ba95aaa9c4f9238e1dd011f595d82c05e5e1
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-11-10 16:02:46 +0600

    Mail function adjust. RANCHER-927

--- vars/folioJiraFlowMail.groovy
+++ vars/folioJiraFlowMail.groovy
@@ -1,4 +1,6 @@
-void sendMailNotification(List emails, String body, String subject) {
+import org.folio.rest_v2.Common
+
+void sendMailNotification(List emails, String subject, String body) {
   String pipelineInfo = """
 Pipeline: ${JOB_NAME}
 Build Status: ${currentBuild.currentResult}
@@ -6,6 +8,11 @@ Build Number: ${currentBuild.number}
 Build URL: ${BUILD_URL}
   """
   emails.each { mail ->
-    emailext body: """${pipelineInfo}\n${body}""", subject: "${subject}", to: "${mail}"
+    try {
+      emailext body: """${pipelineInfo}\n${body}""", subject: "${subject}", to: "${mail}"
+      new Common(this, "https://fakeUrl").logger.info("Mail sent to: ${mail}")
+    } catch (Exception e) {
+      new Common(this, "https://fakeUrl").logger.warning("Unable to send email to: ${mail}\nError: ${e.getMessage()}")
+    }
   }
 }
