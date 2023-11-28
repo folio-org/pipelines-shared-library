6c85f7227d02593ffe06e1cd88e593c20e66ae22
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-11-28 19:58:29 +0600

    fix-for-feature-backend-module-deploy | ready for PR.

--- pipelines/rancher/custom-backend-module.groovy
+++ pipelines/rancher/custom-backend-module.groovy
@@ -1,5 +1,5 @@
 #!groovy
-@Library('pipelines-shared-library@fix-for-feature-backend-module-deploy') _
+@Library('pipelines-shared-library') _
 
 import org.folio.Constants
 import org.folio.rest.Deployment
