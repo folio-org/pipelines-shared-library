2e545a12f0ac0c8c7ece25da591d16e41bd896fd
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-12-15 16:04:09 +0600

    RANCHER-1123 extend of Karate tests templates additional testing.

--- pipelines/folioRancher/folioScheduledTesting/karateTestsScheduledPipeline.groovy
+++ pipelines/folioRancher/folioScheduledTesting/karateTestsScheduledPipeline.groovy
@@ -1,6 +1,6 @@
 
 
-@Library('pipelines-shared-library@RANCHER-741-Jenkins-Enhancements') _
+@Library('pipelines-shared-library@RANCHER-1123') _
 
 import org.folio.karate.results.KarateTestsExecutionSummary
 import org.folio.karate.teams.TeamAssignment
@@ -45,33 +45,33 @@ pipeline {
     }
 
     stages {
-        stage("Check environment") {
-            steps {
-                script {
-                    try {
-                        def jobParameters = getDestroyEnvironmentJobParameters(clusterName, projectName)
-                        tearDownEnvironmentJob = build job: destroyEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-                    } catch (Exception new_ex) {
-                        println('Existing env: ' + new_ex)
-                    }
-                }
-            }
-        }
-
-        stage("Create environment") {
-            steps {
-                script {
-                    try {
-                        def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
-                            projectName, prototypeTenant, folio_repository, folio_branch)
-                        spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-                    } catch (Exception new_ex) {
-                        slackNotifications.sendPipelineFailSlackNotification("#rancher_tests_notifications")
-                        throw new Exception("Creation of the environment is failed: " + new_ex)
-                    }
-                }
-            }
-        }
+//        stage("Check environment") {
+//            steps {
+//                script {
+//                    try {
+//                        def jobParameters = getDestroyEnvironmentJobParameters(clusterName, projectName)
+//                        tearDownEnvironmentJob = build job: destroyEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
+//                    } catch (Exception new_ex) {
+//                        println('Existing env: ' + new_ex)
+//                    }
+//                }
+//            }
+//        }
+//
+//        stage("Create environment") {
+//            steps {
+//                script {
+//                    try {
+//                        def jobParameters = getEnvironmentJobParameters('apply', okapiVersion, clusterName,
+//                            projectName, prototypeTenant, folio_repository, folio_branch)
+//                        spinUpEnvironmentJob = build job: spinUpEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
+//                    } catch (Exception new_ex) {
+//                        slackNotifications.sendPipelineFailSlackNotification("#rancher_tests_notifications")
+//                        throw new Exception("Creation of the environment is failed: " + new_ex)
+//                    }
+//                }
+//            }
+//        }
 
         stage("Start tests") {
             when {
@@ -83,7 +83,7 @@ pipeline {
                 script {
                     def jobParameters = [branch         : params.branch,
                                          threadsCount   : "4",
-                                         modules        : "",
+                                         modules        : "mod-data-import",
                                          okapiUrl       : okapiUrl,
                                          edgeUrl        : edgeUrl,
                                          tenant         : 'supertenant',
