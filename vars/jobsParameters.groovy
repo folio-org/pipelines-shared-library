3e20a267337b632ff09e8c28c006bc41d678219d
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2023-12-28 13:27:13 +0600

    minor fix.RANCHER-741-Jenkins-Enhancements

--- vars/jobsParameters.groovy
+++ vars/jobsParameters.groovy
@@ -174,7 +174,7 @@ while (nextToken != '') {
     nextToken = res.nextToken ?: ''
 }
 
-result = result.findAll { it.startsWith(CLUSTER.trim() + '-' + NAMESPACE.trim()) }
+result = result.findAll { it.startsWith(CLUSTER.trim() + '-' + NAMESPACE.trim() + '.') }
         .sort()
         .reverse()
 
