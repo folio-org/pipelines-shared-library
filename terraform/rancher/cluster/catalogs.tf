d29fa0e9bf4630119a3bf6e154bdea3a5a84f95e
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-01-26 11:21:57 +0200

    Fix catalog

--- terraform/rancher/cluster/catalogs.tf
+++ terraform/rancher/cluster/catalogs.tf
@@ -134,7 +134,7 @@ resource "rancher2_catalog_v2" "sorry-cypress" {
 }
 
 # MockServer charts catalog
-resource "rancher2_catalog_v2" "sorry-cypress" {
+resource "rancher2_catalog_v2" "mock-server" {
   depends_on = [time_sleep.wait_300_seconds]
   count      = var.register_in_rancher ? 1 : 0
   cluster_id = rancher2_cluster_sync.this[0].cluster_id
