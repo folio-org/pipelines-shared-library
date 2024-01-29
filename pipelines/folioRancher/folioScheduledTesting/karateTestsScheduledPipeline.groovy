75d4289bbcd6a82ea530650f5a21f426e6746538
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-01-29 11:59:14 +0200

    Disable karate destroy

--- pipelines/folioRancher/folioScheduledTesting/karateTestsScheduledPipeline.groovy
+++ pipelines/folioRancher/folioScheduledTesting/karateTestsScheduledPipeline.groovy
@@ -137,14 +137,15 @@ pipeline {
                 }
               }
             }
-            stage("Destroy environment") {
-              steps {
-                script {
-                  def jobParameters = getDestroyEnvironmentJobParameters(clusterName, projectName)
-                  tearDownEnvironmentJob = build job: destroyEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
-                }
-              }
-            }
+              //TODO temporary disabled destroy for investigation
+//            stage("Destroy environment") {
+//              steps {
+//                script {
+//                  def jobParameters = getDestroyEnvironmentJobParameters(clusterName, projectName)
+//                  tearDownEnvironmentJob = build job: destroyEnvironmentJobName, parameters: jobParameters, wait: true, propagate: false
+//                }
+//              }
+//            }
             stage("Parse teams assignment") {
               steps {
                 script {
