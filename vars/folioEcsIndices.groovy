db1b5ae6522b5ae8bc818fe768ec6525f90bc256
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-02-16 20:54:35 +0600

    RANCHER-1216 ES testing v1.0.4

--- vars/folioEcsIndices.groovy
+++ vars/folioEcsIndices.groovy
@@ -1,11 +1,12 @@
 import org.folio.Constants
-import org.folio.utilities.Logger
+import org.folio.rest_v2.Common
 import org.folio.utilities.RestClient
 
 static void prepareEcsIndices(String username, String password) {
 
   RestClient client = new RestClient(this, true, 10800000)
-  Logger logger = new Logger(this, 'RestClient')
+  Common common = new Common(this, "https://fake-url.ecs-snapshot.env")
+
   Map indices = [
     "ecs-snapshot_instance_subject_cs00000int": "folio-testing-ecs-snapshot_instance_subject_cs00000int",
     "ecs-snapshot_instance_cs00000int"        : "folio-testing-ecs-snapshot_instance_cs00000int",
@@ -37,13 +38,13 @@ static void prepareEcsIndices(String username, String password) {
 
     try {
       client.delete(Constants.FOLIO_OPEN_SEARCH_URL + "/${destination}", headers_del)
-      logger.warning("Deleting this index: ${destination}...")
+      common.logger.warning("Deleting this index: ${destination}...")
       sleep(30000)
     } catch (Exception es) {
       logger.error("Unable to delete index: ${destination}, error: ${es.getMessage()}")
     }
     finally {
-      logger.info("Working on creation ${destination} index...")
+      common.logger.info("Working on creation ${destination} index...")
       client.post(Constants.FOLIO_OPEN_SEARCH_URL + "/_reindex", body, headers)
     }
   }
