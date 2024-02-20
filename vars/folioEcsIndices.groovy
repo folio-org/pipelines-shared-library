0a558f0e90f4526d52e3f7b978bc4e3967454e14
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-02-20 06:12:45 +0600

    RANCHER-1216 overall without update to snapshot versions + OS fixes

--- vars/folioEcsIndices.groovy
+++ vars/folioEcsIndices.groovy
@@ -16,13 +16,15 @@ String prepareEcsIndices(String username, String password) {
 
     try {
       logger.warning("Deleting this index: ${destination}...")
-      sh("curl -u \"${username}:${password}\" -X DELETE ${Constants.FOLIO_OPEN_SEARCH_URL}/${destination} > /dev/null 2>&1 &")
-      sleep(30)
+      sh(script: "curl -u \"${username}:${password}\" -X DELETE ${Constants.FOLIO_OPEN_SEARCH_URL}/${destination}?pretty", returnStdout: true)
+      sleep time: 10, unit: 'SECONDS'
     } catch (Error es) {
       logger.error("Unable to delete index: ${destination}, error: ${es.getMessage()}")
     } finally {
       logger.info("Working on creation ${destination} index...")
-      sh("curl -u \"${username}:${password}\" -X POST ${Constants.FOLIO_OPEN_SEARCH_URL}/${source}/_clone/${destination} > /dev/null 2>&1 &")
+      sh(script: "curl -u \"${username}:${password}\" -X PUT ${Constants.FOLIO_OPEN_SEARCH_URL}/${source}/_block/write?pretty", returnStdout: true)
+      sleep time: 10, unit: 'SECONDS'
+      sh(script: "curl -u \"${username}:${password}\" -X POST ${Constants.FOLIO_OPEN_SEARCH_URL}/${source}/_clone/${destination}?pretty", returnStdout: true)
     }
   }
 }
