a86ace0e7df1ae83ed55e5edfd53874f32c55476
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-12 00:55:06 +0300

    RANCHER-1004. The bad idea about the value of a test type called OTHER was revisited.

--- src/org/folio/client/reportportal/ReportPortalTestType.groovy
+++ src/org/folio/client/reportportal/ReportPortalTestType.groovy
@@ -4,15 +4,14 @@ import com.cloudbees.groovy.cps.NonCPS
 import org.folio.shared.TestType
 
 enum ReportPortalTestType{
-  KARATE(ReportPortalConstants.KARATE_PROJECT_NAME, ReportPortalConstants.KARATE_EXEC_PARAM_TEMPLATE, TestType.KARATE),
-  CYPRESS(ReportPortalConstants.CYPRESS_PROJECT_NAME, ReportPortalConstants.CYPRESS_EXEC_PARAM_TEMPLATE, TestType.CYPRESS),
-  OTHER("", "", TestType.OTHER)
+  KARATE(ReportPortalConstants.KARATE_PROJECT_NAME, ReportPortalConstants.KARATE_EXEC_PARAM_TEMPLATE, TestType.KARATE)
+  , CYPRESS(ReportPortalConstants.CYPRESS_PROJECT_NAME, ReportPortalConstants.CYPRESS_EXEC_PARAM_TEMPLATE, TestType.CYPRESS)
 
   final String projectName
   final String execParamTemplate
   final TestType baseType
 
-  ReportPortalTestType(String projectName, String execParamTemplate, TestType type) {
+  private ReportPortalTestType(String projectName, String execParamTemplate, TestType type) {
     this.projectName = projectName
     this.baseType = type
     this.execParamTemplate = execParamTemplate
