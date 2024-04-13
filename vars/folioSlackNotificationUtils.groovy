1314a389cf32d7cb3ca8f8ba0bea7e501790ca2d
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-14 01:28:46 +0300

    RANCHER-1323. Changes for test purposes.3

--- vars/folioSlackNotificationUtils.groovy
+++ vars/folioSlackNotificationUtils.groovy
@@ -1,7 +1,10 @@
 import org.folio.client.slack.SlackBuildResultRenderer
 
 String renderSlackBuildResultMessage(){
+  println("I'm in folioSlackNotificationUtils.groovy")
   SlackBuildResultRenderer slackBuildResult = SlackBuildResultRenderer.fromResult(currentBuild.result)
+
+  println("I'm in folioSlackNotificationUtils.groovy. SlackBuildResultRenderer was got.")
 
   return slackBuildResult.renderSection(
       env.JOB_NAME
