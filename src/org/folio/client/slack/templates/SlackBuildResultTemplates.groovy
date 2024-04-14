26ffc6bb38244d07edf16bd75a6f751697ca8d1c
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-14 17:26:31 +0300

    RANCHER-1323. Some minor issue was fixed.

--- src/org/folio/client/slack/templates/SlackBuildResultTemplates.groovy
+++ src/org/folio/client/slack/templates/SlackBuildResultTemplates.groovy
@@ -4,9 +4,9 @@ class SlackBuildResultTemplates {
 
   private SlackBuildResultTemplates(){}
 
-  static final SUCCESS_TEXT = 'Pipeline run status *SUCCESS* :white_check_mark: : `$BUILD_TXT_JOB_NAME` *#$BUILD_TXT_BUILD_NUMBER*'
-  static final FAILED_TEXT = 'Pipeline run status *FAILED* :x: : `$BUILD_TXT_JOB_NAME` *#$BUILD_TXT_BUILD_NUMBER* \\n:thinking_face: _Pipilene failed on stage_ - *$BUILD_TXT_STAGE_NAME*'
-  static final UNSTABLE_TEXT = 'Pipeline run status *UNSTABLE* :warning: : `$BUILD_TXT_JOB_NAME` *#$BUILD_TXT_BUILD_NUMBER*'
+  static final SUCCESS_TEXT = 'Pipeline run status *SUCCESS* :white_check_mark: : `${BUILD_TXT_JOB_NAME}` *#${BUILD_TXT_BUILD_NUMBER}*'
+  static final FAILED_TEXT = 'Pipeline run status *FAILED* :x: : `${BUILD_TXT_JOB_NAME}` *#${BUILD_TXT_BUILD_NUMBER}* \\n:thinking_face: _Pipilene failed on stage_ - *${BUILD_TXT_STAGE_NAME}*'
+  static final UNSTABLE_TEXT = 'Pipeline run status *UNSTABLE* :warning: : `${BUILD_TXT_JOB_NAME}` *#${BUILD_TXT_BUILD_NUMBER}*'
 
   static final ACTION_TEXT = '*Check out the console output* :page_facing_up:'
 
--- src/org/folio/client/slack/templates/SlackTestResultTemplates.groovy
+++ src/org/folio/client/slack/templates/SlackTestResultTemplates.groovy
@@ -6,10 +6,10 @@ class SlackTestResultTemplates {
 
   private SlackTestResultTemplates(){}
 
-  static final CYPRESS_TEXT = 'Build name: $TEST_TXT_BUILD_NAME. Passed tests: $TEST_TXT_PASS_CNT, Broken tests: $TEST_TXT_BRK_CNT, Failed tests: $TEST_TXT_FAIL_CNT, Pass rate: $TEST_TXT_PASS_RATE%'
+  static final CYPRESS_TEXT = 'Build name: ${TEST_TXT_BUILD_NAME}. Passed tests: ${TEST_TXT_PASS_CNT}, Broken tests: ${TEST_TXT_BRK_CNT}, Failed tests: ${TEST_TXT_FAIL_CNT}, Pass rate: ${TEST_TXT_PASS_RATE}%'
   static final CYPRESS_TITLE = 'Cypress tests results :arrow_forward:'
 
-  static final KARATE_TEXT = 'Passed tests: $TEST_TXT_PASS_CNT, Failed tests: $TEST_TXT_FAIL_CNT, Pass rate: $TEST_TXT_PASS_RATE%'
+  static final KARATE_TEXT = 'Passed tests: ${TEST_TXT_PASS_CNT}, Failed tests: ${TEST_TXT_FAIL_CNT}, Pass rate: ${TEST_TXT_PASS_RATE}%'
   static final KARATE_TITLE = 'Karate tests results :martial_arts_uniform:'
 
   static final ACTION_TEXT = '*Check out the tests report* :bar_chart: '
