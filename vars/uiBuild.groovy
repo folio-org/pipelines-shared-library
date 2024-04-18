e915d29085bc8f9915524446f28a54dc15d13868
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-04-18 20:48:29 +0600

    buildUI git clone depth remove

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -29,7 +29,7 @@ void call(Map params, boolean releaseVersion = false) {
         }
         checkout([$class           : 'GitSCM',
                   branches         : [[name: ui_bundle.hash]],
-                  extensions       : [[$class: 'CloneOption', depth: 10, noTags: true, reference: '', shallow: true, timeout: 20],
+                  extensions       : [[$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 20],
                                       [$class: 'CleanBeforeCheckout'],
                                       [$class: 'RelativeTargetDirectory', relativeTargetDir: "platform-complete-${params.tenant_id}"]],
                   userRemoteConfigs: [[url: 'https://github.com/folio-org/platform-complete.git']]])
