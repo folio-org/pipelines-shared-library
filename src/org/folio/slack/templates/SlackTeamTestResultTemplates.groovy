419d097552bb2e7bb95aebbc336107b40576d007
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-25 00:32:45 +0300

    RANCHER-1364. SlackTeamTestResultTemplates class field template parameters were fixed.

--- src/org/folio/slack/templates/SlackTeamTestResultTemplates.groovy
+++ src/org/folio/slack/templates/SlackTeamTestResultTemplates.groovy
@@ -5,7 +5,7 @@ class SlackTeamTestResultTemplates {
   private SlackTeamTestResultTemplates(){}
 
   static final SUCCESS_TEXT = 'All modules for ${TEAM_TEST_TXT_TEAM_NAME} team have successful result'
-  static final FAILED_TEXT = '${TEAM_TEST_TXT_FAILED_CNT} of ${TEAM_TEST_TXT_TOTAL_CNT} modules were failed'
+  static final FAILED_TEXT = '${TEAM_TEST_TXT_FAILED_CNT} of ${TEAM_TEST_TXT_TOTAL_CNT} modules were failed for ${TEAM_TEST_TXT_TEAM_NAME} team'
 
   static final EXISTING_ISSUES_ACTION_TEXT = '*Check out the existing issues* :information_source: '
   static final CREATED_ISSUES_ACTION_TEXT = '*Check out the created issues* :information_source: '
@@ -24,7 +24,7 @@ class SlackTeamTestResultTemplates {
   static Map<String, String> getFieldParams(String moduleName, String failedCount, String totalCount){
     [
       'TEST_FIELD_MODULE_NAME' : moduleName
-      , 'TEST_FIELD_FAILED_CNT' : failedCount
+      , 'TEAM_TEST_FIELD_FAILED_CNT' : failedCount
       , 'TEST_FIELD_TOTAL_CNT' : totalCount
     ]
   }
