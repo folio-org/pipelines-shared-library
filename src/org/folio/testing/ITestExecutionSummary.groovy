4a6cb256bbce502c790751aaf65b3e975943d0dc
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-24 17:54:15 +0300

    RANCHER-1365. ÐNew interfaces for the unification of the test result collection process have been added.

---
+++ src/org/folio/testing/ITestExecutionSummary.groovy
@@ -0,0 +1,10 @@
+package org.folio.testing
+
+import org.folio.testing.teams.Team
+import org.folio.testing.teams.TeamAssignment
+
+interface ITestExecutionSummary extends IExecutionSummary {
+
+  Map<Team, List<IModuleExecutionSummary>> getModuleResultByTeam(TeamAssignment teamAssignment)
+
+}
