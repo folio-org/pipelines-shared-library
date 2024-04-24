d35f68ff4fc30b5f0b6b4aee392f544c2ace9283
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-04-24 23:59:26 +0300

    RANCHER-1364. Changes were made for test purposes.13

--- src/org/folio/testing/TestExecutionResult.groovy
+++ src/org/folio/testing/TestExecutionResult.groovy
@@ -7,15 +7,8 @@ enum TestExecutionResult {
 
   static TestExecutionResult byPassRate(IExecutionSummary summary, int passRateLimit = 50){
     return summary.getPassRate() > passRateLimit ? SUCCESS : FAILED
-//    return SUCCESS
   }
 
-  static boolean test(IExecutionSummary summary, int passRateLimit = 50){
-    return summary.getPassRate() > passRateLimit
-//    return SUCCESS
-  }
-
-  @NonCPS
   static TestExecutionResult byTestResults(List<IExecutionSummary> results, int passRateLimit = 50){
     if (results.findAll({it.getExecutionResult(passRateLimit) == FAILED }).size() > 0)
       return FAILED
