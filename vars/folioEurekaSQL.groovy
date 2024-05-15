429fb17cb2a594b53b74641d5b82f920b7b2533d
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-15 15:50:34 +0600

    .RANCHER-1335 approach unification for all types of DBs aka sts || RDS

--- vars/folioEurekaSQL.groovy
+++ vars/folioEurekaSQL.groovy
@@ -3,6 +3,7 @@ import org.folio.Constants
 import org.folio.models.RancherNamespace
 import org.folio.utilities.Logger
 import org.folio.utilities.Tools
+import groovy.json.JsonSlurperClassic
 
 void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], Boolean type) {
   String arn = Constants.AWS_RDS_CLUSTER_ARN + namespace.getClusterName() + '-' + namespace.getNamespaceName()
@@ -14,21 +15,21 @@ void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], Boolea
     LinkedHashMap data = [db_name: "${db}", db_username: "${if (db == 'keycloak') { 'keycloak_admin' } else { 'kong_admin' }}", db_password: Constants.PG_ROOT_DEFAULT_PASSWORD]
     writeFile encoding: 'utf-8', file: "${db}.sql", text: (new StreamingTemplateEngine().createTemplate(tpl).make(data)).toString()
     folioHelm.withKubeConfig(namespace.getClusterName()) {
+      String pgadmin_pod = sh(script: "kubectl get pod -n ${namespace.getNamespaceName()} --no-headers | grep pgadmin | awk '{print \$1}'", returnStdout: true)
+      sh(script: "kubectl cp ./${db}.sql pod/${pgadmin_pod}:/tmp/${db}.sql -n ${namespace.getNamespaceName()}", returnStdout: true)
+      def connInfo = sh(script: "kubectl exec pod/${pgadmin_pod} -n ${namespace.getNamespaceName()} -- /bin/cat /pgadmin4/servers.json", returnStdout: true)
+      def connStr = new JsonSlurperClassic().parseText("${connInfo}")
       if (!type) {
         try {
           logger.info("Trying to init Eureka DBs on AWS RDS Cluster...")
-          def sql = readFile file: "./${db}.sql"
-          sql.each { sql_line ->
-            sh(script: "aws --region ${Constants.AWS_REGION} rds-data execute-statement --resource-arn \"$arn\" --database  \"folio\" --secret-arn \"$secret\" --sql \"$sql_line\"", returnStdout: true)
-          }
+          sh(script: "kubectl exec pod/${pgadmin_pod} -- export PGPASSWORD=${Constants.PG_ROOT_DEFAULT_PASSWORD};/usr/local/pgsql-13/psql -h ${connStr["Servers"]["pg"]["Host"]} -p \"5432\" -u \"postgres\" -a -f /tmp/${db}.sql", returnStdout: true)
         } catch (Exception e) {
           logger.error("Error: ${e.getMessage()}")
         }
       } else {
         try {
           logger.info("Trying to init Eureka DBs on built-in PostgresSQL...")
-          sh(script: "kubectl cp ./${db}.sql pod/postgresql-${namespace.getNamespaceName()}-0:/tmp/${db}.sql -n ${namespace.getNamespaceName()}", returnStdout: true)
-          sh(script: "kubectl exec pod/postgresql-${namespace.getNamespaceName()}-0 -n ${namespace.getNamespaceName()} -- /opt/bitnami/postgresql/bin/psql -a -f /tmp/${db}.sql", returnStdout: true)
+          sh(script: "kubectl exec pod/${pgadmin_pod} -- export PGPASSWORD=${Constants.PG_ROOT_DEFAULT_PASSWORD};/usr/local/pgsql-13/psql -h ${connStr["Servers"]["pg"]["Host"]} -p \"5432\" -u \"postgres\" -a -f /tmp/${db}.sql", returnStdout: true)
         } catch (Exception e) {
           logger.error("Error: ${e.getMessage()}")
         }
