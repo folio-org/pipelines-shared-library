4f664896ccd0f86d75878f9c4138b3f9aa0f2218
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-21 23:45:26 +0600

    .RANCHER-1334 tmp save

--- terraform/rancher/project/locals.tf
+++ terraform/rancher/project/locals.tf
@@ -7,6 +7,7 @@ locals {
   group_name            = join(".", [data.rancher2_cluster.this.name, var.rancher_project_name])
   okapi_url             = join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "okapi"]), var.root_domain])
   minio_url             = join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "minio"]), var.root_domain])
+  kong_url             = join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "kong"]), var.root_domain])
   minio_console_url     = join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "minio-console"]), var.root_domain])
   db_snapshot_arn       = "arn:aws:rds:${var.aws_region}:${data.aws_caller_identity.current.account_id}:cluster-snapshot:${var.pg_rds_snapshot_name}"
   system_user_modules   = ["mod-data-export-spring", "mod-inn-reach", "mod-search", "mod-consortia", "mod-remote-storage", "mod-entities-links", "mod-erm-usage-harvester", "mod-pubsub", "mod-dcb"]
