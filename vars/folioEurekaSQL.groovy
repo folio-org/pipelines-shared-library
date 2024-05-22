ff5752f337c14a9519abdb462f9d67190514e5b4
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-22 18:02:56 +0600

    .RANCHER-1334 DB init testing start

--- vars/folioEurekaSQL.groovy
+++ vars/folioEurekaSQL.groovy
@@ -5,9 +5,7 @@ import org.folio.utilities.Logger
 import org.folio.utilities.Tools
 import groovy.json.JsonSlurperClassic
 
-void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], Boolean type, String pgMajorVersion = '13') {
-  String arn = Constants.AWS_RDS_CLUSTER_ARN + namespace.getClusterName() + '-' + namespace.getNamespaceName()
-  String secret = Constants.AWS_RDS_CLUSTER_SECRET
+void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], String pgMajorVersion = '13') {
   Logger logger = new Logger(this, 'initSQL')
   new Tools(this).copyResourceFileToWorkspace("eureka/eureka_db.tpl")
   DBs.each { db ->
@@ -19,21 +17,12 @@ void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], Boolea
       sh(script: "kubectl cp ./${db}.sql pod/${pgadmin_pod}:/tmp/${db}.sql -n ${namespace.getNamespaceName()}", returnStdout: true)
       def connInfo = sh(script: "kubectl exec pod/${pgadmin_pod} -n ${namespace.getNamespaceName()} -- /bin/cat /pgadmin4/servers.json", returnStdout: true)
       def connStr = new JsonSlurperClassic().parseText("${connInfo}")
-      if (!type) {
         try {
           logger.info("Trying to init Eureka DBs on AWS RDS Cluster...")
           sh(script: "kubectl exec pod/${pgadmin_pod} -- export PGPASSWORD=${Constants.PG_ROOT_DEFAULT_PASSWORD};/usr/local/pgsql-${pgMajorVersion}/psql -h ${connStr["Servers"]["pg"]["Host"]} -p \"5432\" -u \"postgres\" -a -f /tmp/${db}.sql", returnStdout: true)
         } catch (Exception e) {
           logger.error("Error: ${e.getMessage()}")
         }
-      } else {
-        try {
-          logger.info("Trying to init Eureka DBs on built-in PostgresSQL...")
-          sh(script: "kubectl exec pod/${pgadmin_pod} -- export PGPASSWORD=${Constants.PG_ROOT_DEFAULT_PASSWORD};/usr/local/pgsql-${pgMajorVersion}/psql -h ${connStr["Servers"]["pg"]["Host"]} -p \"5432\" -u \"postgres\" -a -f /tmp/${db}.sql", returnStdout: true)
-        } catch (Exception e) {
-          logger.error("Error: ${e.getMessage()}")
-        }
-      }
     }
   }
 }
--- vars/folioNamespaceCreate.groovy
+++ vars/folioNamespaceCreate.groovy
@@ -52,6 +52,12 @@ void call(CreateNamespaceParameters args) {
       }
     }
 
+    if (args.eureka) {
+      stage('[Kong] Init DB') {
+        folioEurekaSQL.initSQL(namespace)
+      }
+    }
+
     if (args.namespaceOnly) {
       return
     }
