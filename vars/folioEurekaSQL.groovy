71c623c164a166cbe697fa7c3344d39a07a8abb2
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-22 18:28:29 +0600

    .RANCHER-1334 DB init testing start part2.2

--- vars/folioEurekaSQL.groovy
+++ vars/folioEurekaSQL.groovy
@@ -14,7 +14,7 @@ void initSQL(RancherNamespace namespace, List DBs = ['keycloak', 'kong'], String
     writeFile encoding: 'utf-8', file: "${db}.sql", text: (new StreamingTemplateEngine().createTemplate(tpl).make(data)).toString()
     folioHelm.withKubeConfig(namespace.getClusterName()) {
       String pgadmin_pod = sh(script: "kubectl get pod -n ${namespace.getNamespaceName()} --no-headers | grep pgadmin | awk '{print \$1}'", returnStdout: true)
-      sh(script: "kubectl cp ./${db}.sql pod/${pgadmin_pod + ':/tmp/${db}.sql'} -n ${namespace.getNamespaceName()}", returnStdout: true)
+      sh(script: "kubectl cp ./${db}.sql pod/${pgadmin_pod + ":/tmp/${db}.sql"} -n ${namespace.getNamespaceName()}", returnStdout: true)
       def connInfo = sh(script: "kubectl exec pod/${pgadmin_pod} -n ${namespace.getNamespaceName()} -- /bin/cat /pgadmin4/servers.json", returnStdout: true)
       def connStr = new JsonSlurperClassic().parseText("${connInfo}")
         try {
