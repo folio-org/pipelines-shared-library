c95ab6d667da80d4cdf1745e70ab7dadce74ce97
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-23 18:12:27 +0600

    .RANCHER-1334 TF part testing start | provider config adjust

--- terraform/rancher/project/eurekadb.tf
+++ terraform/rancher/project/eurekadb.tf
@@ -1,20 +1,20 @@
 resource "postgresql_role" "keycloak" {
-  count = var.eureka ? 1 : 0
+  count    = var.eureka ? 1 : 0
   name     = "keycloak_admin"
   login    = true
   password = var.pg_password
 }
 
 resource "postgresql_role" "kong" {
-  count = var.eureka ? 1 : 0
+  count    = var.eureka ? 1 : 0
   name     = "kong_admin"
   login    = true
   password = var.pg_password
 }
 
 resource "postgresql_database" "keycloak" {
-  count = var.eureka ? 1 : 0
-  depends_on = [postgresql_role.keycloak]
+  count             = var.eureka ? 1 : 0
+  depends_on        = [postgresql_role.keycloak]
   name              = "keycloak"
   owner             = postgresql_role.keycloak[0].id
   connection_limit  = -1
@@ -22,8 +22,8 @@ resource "postgresql_database" "keycloak" {
 }
 
 resource "postgresql_database" "kong" {
-  count = var.eureka ? 1 : 0
-  depends_on = [postgresql_role.kong]
+  count             = var.eureka ? 1 : 0
+  depends_on        = [postgresql_role.kong]
   name              = "kong"
   owner             = postgresql_role.kong[0].id
   connection_limit  = -1
