62a3bae5b84cc671a3caf5a7c4d9152705de27a2
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-23 15:33:08 +0600

    .RANCHER-1334 DB init testing start part4.1

--- terraform/rancher/project/postgresql.tf
+++ terraform/rancher/project/postgresql.tf
@@ -258,12 +258,12 @@ resources:
 env:
   email: ${var.pgadmin_username}
   password: ${var.pgadmin_password}
-envVarsExtra:
- - name: POSTGRES_USERNAME
-   valueFrom:
-     secretKeyRef:
-       name: db-credentials
-       key: DB_PASSWORD
+  variables:
+    - name: POSTGRES_USERNAME
+      valueFrom:
+        secretKeyRef:
+          name: db-credentials
+          key: DB_PASSWORD
 service:
   type: NodePort
 ingress:
