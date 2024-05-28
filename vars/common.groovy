32ba7f30669289e33a0725feeef7917cf11787b6
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-05-28 20:24:42 +0600

    .RANCHER-1338 input for a test v2.1

--- vars/common.groovy
+++ vars/common.groovy
@@ -57,6 +57,7 @@ def getOkapiLatestSnapshotVersion(String okapi_version){
 // Removing the image from the local machine.
 void removeImage(String image_name){
     String image_id = sh returnStdout: true, script: "docker images --format '{{.ID}} {{.Repository}}:{{.Tag}}' | grep '${image_name}' | cut -d' ' -f1"
+    new Logger(this, 'dockerBuild').info("${image_id.trim()}")
     sh(script: "docker rmi --force ${image_id.trim()}", returnStdout: true)
 }
 
