df35368bac1262c15420725ef2e69bf81539cded
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-05-29 18:46:59 +0300

    RANCHER-1336: add single quotes to annotations

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -57,7 +57,7 @@ resource "helm_release" "keycloak" {
           alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
           alb.ingress.kubernetes.io/success-codes: 200-399
           alb.ingress.kubernetes.io/healthcheck-path: /health/ready
-          alb.ingress.kubernetes.io/healthcheck-port: 80
+          alb.ingress.kubernetes.io/healthcheck-port: '80'
       auth:
         adminUser: "kc_admin"
         existingSecret: keycloak-credentials
