aa192a883bda5b26ef8951a493f88ea44d881b78
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-05-29 16:31:12 +0300

    RANCHER-1336: use list of values instead of string value

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -6,79 +6,81 @@ resource "helm_release" "keycloak" {
   namespace  = rancher2_namespace.this.id
   version    = "20.0.1"
   repository = "https://repository.folio.org/repository/helm-bitnami-proxy"
-  values     = <<-EOT
-    image:
-      registry: 732722833398.dkr.ecr.us-west-2.amazonaws.com
-      repository: keycloak
-      tag: latest
-      pullPolicy: IfNotPresent
-      debug: true
-    replicaCount: 1
-    containerSecurityContext:
-      enabled: true
-      seLinuxOptions: {}
-      runAsUser: 1001
-      runAsGroup: 1001
-      runAsNonRoot: true
-      privileged: false
-      readOnlyRootFilesystem: true
-      allowPrivilegeEscalation: false
-      capabilities:
-        drop: ["ALL"]
-      seccompProfile:
-        type: "RuntimeDefault"
-    database: postgresql
-    postgresql:
-      enabled: false
-    externalDatabase:
-      existingSecret: keycloak-credentials
-      existingSecretHostKey: KEYCLOAK_PG_HOST
-      existingSecretPortKey: KEYCLOAK_PG_PORT
-      existingSecretUserKey: KEYCLOAK_PG_USER
-      existingSecretDatabaseKey: KEYCLOAK_DATABASE
-      existingSecretPasswordKey: KEYCLOAK_PG_PASSWORD
-    networkPolicy:
-      enabled: false
-    service:
-      type: NodePort
-      http:
+  values = [
+    <<-EOF
+      image:
+        registry: 732722833398.dkr.ecr.us-west-2.amazonaws.com
+        repository: keycloak
+        tag: latest
+        pullPolicy: IfNotPresent
+        debug: true
+      replicaCount: 1
+      containerSecurityContext:
         enabled: true
-    ingress:
-      enabled: true
-      hostname: "${join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "keycloak"]), var.root_domain])}"
-      ingressClassName: ""
-      pathType: ImplementationSpecific
-      path: /*
-      annotations:
-        kubernetes.io/ingress.class: alb
-        alb.ingress.kubernetes.io/scheme: internet-facing
-        alb.ingress.kubernetes.io/group.name: "${local.group_name}"
-        alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
-        alb.ingress.kubernetes.io/success-codes: 200-399
-        alb.ingress.kubernetes.io/healthcheck-path: /health/ready
-        alb.ingress.kubernetes.io/healthcheck-port: 80
-    auth:
-      adminUser: "kc_admin"
-      existingSecret: keycloak-credentials
-      passwordSecretKey: KEYCLOAK_ADMIN_PASSWORD
-    extraEnvVars:
-      - name: KC_FOLIO_BE_ADMIN_CLIENT_SECRET
-        valueFrom:
-          secretKeyRef:
-            name: keycloak-credentials
-            key: KEYCLOAK_FOLIO_BE_ADMIN_CLIENT_SECRET
-      - name: KC_HTTPS_KEY_STORE_PASSWORD
-        valueFrom:
-          secretKeyRef:
-            name: keycloak-credentials
-            key: KEYCLOAK_HTTPS_KEY_STORE_PASSWORD
-      - name: KEYCLOAK_LOG_LEVEL
-        value: DEBUG
-    livenessProbe:
-      enabled: false
-    readinessProbe:
-      enabled: false
-    startupProbe:
-      enabled: false
-  EOT
+        seLinuxOptions: {}
+        runAsUser: 1001
+        runAsGroup: 1001
+        runAsNonRoot: true
+        privileged: false
+        readOnlyRootFilesystem: true
+        allowPrivilegeEscalation: false
+        capabilities:
+          drop: ["ALL"]
+        seccompProfile:
+          type: "RuntimeDefault"
+      database: postgresql
+      postgresql:
+        enabled: false
+      externalDatabase:
+        existingSecret: keycloak-credentials
+        existingSecretHostKey: KEYCLOAK_PG_HOST
+        existingSecretPortKey: KEYCLOAK_PG_PORT
+        existingSecretUserKey: KEYCLOAK_PG_USER
+        existingSecretDatabaseKey: KEYCLOAK_DATABASE
+        existingSecretPasswordKey: KEYCLOAK_PG_PASSWORD
+      networkPolicy:
+        enabled: false
+      service:
+        type: NodePort
+        http:
+          enabled: true
+      ingress:
+        enabled: true
+        hostname: "${join(".", [join("-", [data.rancher2_cluster.this.name, var.rancher_project_name, "keycloak"]), var.root_domain])}"
+        ingressClassName: ""
+        pathType: ImplementationSpecific
+        path: /*
+        annotations:
+          kubernetes.io/ingress.class: alb
+          alb.ingress.kubernetes.io/scheme: internet-facing
+          alb.ingress.kubernetes.io/group.name: "${local.group_name}"
+          alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
+          alb.ingress.kubernetes.io/success-codes: 200-399
+          alb.ingress.kubernetes.io/healthcheck-path: /health/ready
+          alb.ingress.kubernetes.io/healthcheck-port: 80
+      auth:
+        adminUser: "kc_admin"
+        existingSecret: keycloak-credentials
+        passwordSecretKey: KEYCLOAK_ADMIN_PASSWORD
+      extraEnvVars:
+        - name: KC_FOLIO_BE_ADMIN_CLIENT_SECRET
+          valueFrom:
+            secretKeyRef:
+              name: keycloak-credentials
+              key: KEYCLOAK_FOLIO_BE_ADMIN_CLIENT_SECRET
+        - name: KC_HTTPS_KEY_STORE_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: keycloak-credentials
+              key: KEYCLOAK_HTTPS_KEY_STORE_PASSWORD
+        - name: KEYCLOAK_LOG_LEVEL
+          value: DEBUG
+      livenessProbe:
+        enabled: false
+      readinessProbe:
+        enabled: false
+      startupProbe:
+        enabled: false
+    EOF
+  ]
 }
