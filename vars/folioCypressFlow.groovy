61a3c001d3d3f35d14a6f83e30013aac32ee619e
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-05-30 22:47:15 +0300

    RANCHER-1276. Changes were made for test purposes.14

--- vars/folioCypressFlow.groovy
+++ vars/folioCypressFlow.groovy
@@ -93,7 +93,7 @@ void call(params) {
                 break;
               case 'rancher':
                 workersLimit = 8
-                batchSize = 1
+                batchSize = 2
                 break;
               default:
                 error("Worker agent label unknown! '${agent}'")
