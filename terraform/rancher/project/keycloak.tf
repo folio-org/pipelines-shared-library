6d798968688658c7c27e9e100365c2f7169baadd
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-06-07 17:38:22 +0300

    Add Keycloak helm chart release

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -1,119 +1,172 @@
-# resource "helm_release" "keycloak" {
-#   count      = (var.eureka ? 1 : 0)
-#   chart      = "keycloak"
-#   depends_on = [rancher2_secret.keycloak-credentials, helm_release.postgresql]
-#   name       = "keycloak-${var.rancher_project_name}"
-#   namespace  = rancher2_namespace.this.id
-#   version    = "21.0.4"
-#   repository = "https://repository.folio.org/repository/helm-bitnami-proxy"
-#   values = [
-#     <<-EOF
-#       image:
-#         registry: 732722833398.dkr.ecr.us-west-2.amazonaws.com
-#         repository: keycloak
-#         tag: latest
-#         pullPolicy: IfNotPresent
-#         debug: true
-#       enableDefaultInitContainers: false
-#       replicaCount: 1
-#       containerSecurityContext:
-#         enabled: false
-#         seLinuxOptions: {}
-#         runAsUser: 1001
-#         runAsGroup: 1001
-#         runAsNonRoot: true
-#         privileged: false
-#         readOnlyRootFilesystem: true
-#         allowPrivilegeEscalation: false
-#         capabilities:
-#           drop: ["ALL"]
-#         seccompProfile:
-#           type: "RuntimeDefault"
-#       database: postgresql
-#       postgresql:
-#         enabled: false
-#       externalDatabase:
-#         host: '${var.pg_embedded ? local.pg_service_writer : module.rds[0].cluster_endpoint}'
-#         port: 5432
-#         user: keycloak
-#         database: keycloak
-#         password: postgres_password_123!
-#       networkPolicy:
-#         enabled: false
-#       service:
-#         type: NodePort
-#         http:
-#           enabled: true
-#       ingress:
-#         enabled: true
-#         hostname: ${local.keycloak_url}
-#         ingressClassName: ""
-#         pathType: ImplementationSpecific
-#         path: /*
-#         annotations:
-#           kubernetes.io/ingress.class: alb
-#           alb.ingress.kubernetes.io/scheme: internet-facing
-#           alb.ingress.kubernetes.io/group.name: ${local.group_name}
-#           alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
-#           alb.ingress.kubernetes.io/success-codes: 200-399
-#           alb.ingress.kubernetes.io/healthcheck-path: /health/ready
-#           alb.ingress.kubernetes.io/healthcheck-port: '80'
-#       auth:
-#         adminUser: admin
-#         adminPassword: admin
-#       extraEnvVars:
-#         - name: KC_FOLIO_BE_ADMIN_CLIENT_SECRET
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_FOLIO_BE_ADMIN_CLIENT_SECRET
-#         - name: KEYCLOAK_LOG_LEVEL
-#           value: DEBUG
-#         - name: KC_HOSTNAME_STRICT
-#           value: 'true'
-#         - name: KC_HOSTNAME
-#           value: ${local.keycloak_url}
-#         - name: FIPS
-#           value: 'false'
-#         - name: KC_DB_URL_HOST
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_PG_HOST
-#         - name: KC_DB_URL_PORT
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_PG_PORT
-#         - name: KC_DB_URL_DATABASE
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_DATABASE
-#         - name: KC_DB_USERNAME
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_PG_USER
-#         - name: KC_DB_PASSWORD
-#           valueFrom:
-#             secretKeyRef:
-#               name: keycloak-credentials
-#               key: KEYCLOAK_PG_PASSWORD
-#         - name: KC_HTTP_ENABLED
-#           value: 'true'
-#         - name: KC_PROXY
-#           value: edge
-#         - name: EUREKA_RESOLVE_SIDECAR_IP
-#           value: 'false'
-#         - name: CONFIG_FILE
-#           value: config.json
-#       livenessProbe:
-#         enabled: false
-#       readinessProbe:
-#         enabled: false
-#       startupProbe:
-#         enabled: false
-#     EOF
-#   ]
-# }
+resource "helm_release" "keycloak" {
+  count      = (var.eureka ? 1 : 0)
+  chart      = "keycloak"
+  depends_on = [rancher2_secret.keycloak-credentials, helm_release.postgresql]
+  name       = "keycloak-${var.rancher_project_name}"
+  namespace  = rancher2_namespace.this.id
+  version    = "21.0.4"
+  repository = "https://repository.folio.org/repository/helm-bitnami-proxy"
+  values = [
+    <<-EOF
+image:
+  registry: 732722833398.dkr.ecr.us-west-2.amazonaws.com
+  repository: keycloak
+  tag: latest
+  pullPolicy: Always
+  debug: true
+
+auth:
+  adminUser: admin
+  existingSecret: keycloak-credentials
+  passwordSecretKey: KEYCLOAK_ADMIN_PASSWORD
+
+extraEnvVars:
+  - name: FIPS
+    value: "false"
+  - name: EUREKA_RESOLVE_SIDECAR_IP
+    value: "false"
+  - name: PROXY_ADDRESS_FORWARDING
+    value: "true"
+  - name: KC_FOLIO_BE_ADMIN_CLIENT_SECRET
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_FOLIO_BE_ADMIN_CLIENT_SECRET
+  - name: KC_HTTPS_KEY_STORE_PASSWORD
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_HTTPS_KEY_STORE_PASSWORD
+  - name: KC_HOSTNAME
+    value: ${local.keycloak_url}
+  - name: KC_HOSTNAME_STRICT
+    value: "false"
+  - name: KC_HOSTNAME_STRICT_HTTPS
+    value: "false"
+  - name: KC_HOSTNAME_DEBUG
+    value: "true"
+  - name: KC_DB_PASSWORD
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_DB_PASSWORD
+  - name: KC_DB_URL_DATABASE
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_DB_URL_DATABASE
+  - name: KC_DB_URL_HOST
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_DB_URL_HOST
+  - name: KC_DB_URL_PORT
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_DB_URL_PORT
+  - name: KC_DB_USERNAME
+    valueFrom:
+      secretKeyRef:
+        name: keycloak-credentials
+        key: KC_DB_USERNAME
+  - name: KC_PROXY
+    value: edge
+  - name: KC_HTTP_ENABLED
+    value: "true"
+  - name: KC_HTTP_PORT
+    value: "8080"
+  - name: KC_HEALTH_ENABLED
+    value: "true"
+
+postgresql:
+  enabled: false
+
+externalDatabase:
+  existingSecret: keycloak-credentials
+  existingSecretHostKey: KC_DB_URL_HOST
+  existingSecretPortKey: KC_DB_URL_PORT
+  existingSecretUserKey: KC_DB_USERNAME
+  existingSecretDatabaseKey: KC_DB_URL_DATABASE
+  existingSecretPasswordKey: KC_DB_PASSWORD
+
+logging:
+  output: default
+  level: DEBUG
+
+proxy: edge
+
+enableDefaultInitContainers: false
+
+containerSecurityContext:
+  enabled: false
+
+service:
+  type: NodePort
+  http:
+    enabled: true
+  ports:
+    http: 80
+
+networkPolicy:
+  enabled: false
+
+livenessProbe:
+  enabled: false
+
+customLivenessProbe:
+  httpGet:
+    path: /health/live
+    port: 8080
+    scheme: HTTP
+  initialDelaySeconds: 0
+  periodSeconds: 1
+  timeoutSeconds: 5
+  failureThreshold: 3
+  successThreshold: 1
+
+readinessProbe:
+  enabled: false
+
+customReadinessProbe:
+  httpGet:
+    path: /health/ready
+    port: 8080
+    scheme: HTTP
+  initialDelaySeconds: 0
+  periodSeconds: 10
+  timeoutSeconds: 30
+  failureThreshold: 3
+  successThreshold: 1
+
+startupProbe:
+  enabled: false
+
+customStartupProbe:
+  httpGet:
+    path: /health/started
+    port: 8080
+    scheme: HTTP
+  initialDelaySeconds: 30
+  periodSeconds: 5
+  timeoutSeconds: 1
+  failureThreshold: 60
+  successThreshold: 1
+
+ingress:
+  enabled: true
+  hostname: ${local.keycloak_url}
+  ingressClassName: ""
+  pathType: ImplementationSpecific
+  path: /*
+  annotations:
+    kubernetes.io/ingress.class: alb
+    alb.ingress.kubernetes.io/scheme: internet-facing
+    alb.ingress.kubernetes.io/group.name: folio-tmp.test-2
+    alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'
+    alb.ingress.kubernetes.io/success-codes: 200-399
+    alb.ingress.kubernetes.io/healthcheck-path: /realms/master
+    alb.ingress.kubernetes.io/healthcheck-port: '8080'
+EOF
+  ]
+}
