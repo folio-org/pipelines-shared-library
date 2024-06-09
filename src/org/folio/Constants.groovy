dc7591ed94810805588a2a83c38f6b9c5f987efa
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-06-09 18:13:49 +0300

    Switch helm repository to deprecated

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -46,8 +46,8 @@ class Constants {
     static String FOLIO_HELM_HOSTED_REPO_URL = "https://repository.folio.org/repository/helm-hosted/"
     static String FOLIO_NPM_REPO_URL = "https://repository.folio.org/repository/npm-folioci/"
 
-    static String FOLIO_HELM_V2_REPO_URL = "https://repository.folio.org/repository/folio-helm-v2/"
-    static String FOLIO_HELM_V2_REPO_NAME = "folio-helm-v2"
+    static String FOLIO_HELM_V2_REPO_URL = "https://repository.folio.org/repository/folio-helm-v2-deprecated/"
+    static String FOLIO_HELM_V2_REPO_NAME = "folio-helm-v2-deprecated"
 
     // Docker
     static String DOCKER_DEV_REPOSITORY_CREDENTIALS_ID = "folio-docker-dev"
