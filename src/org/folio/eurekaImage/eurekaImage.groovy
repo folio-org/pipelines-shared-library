333a382e9590ecad0c0b8b5ffe41e12f960d0991
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-10 18:21:23 +0600

    .RANCHER-1502 to the module's repo v1.1

--- src/org/folio/eurekaImage/eurekaImage.groovy
+++ src/org/folio/eurekaImage/eurekaImage.groovy
@@ -9,41 +9,51 @@ class eurekaImage {
   String moduleName
 
   def prepare() {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: "${Constants.FOLIO_GITHUB_URL}/" + moduleName + ".git"]]])
+    try {
+      checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: "${Constants.FOLIO_GITHUB_URL}/" + moduleName + ".git"]]])
+    } catch (Exception e) {
+      logger.warning(e.getMessage())
+    }
   }
 
   def compile() {
-    withMaven(jdk: "openjdk-17-jenkins-slave-all", maven: Constants.MAVEN_TOOL_NAME, options: [artifactsPublisher(disabled: true)]) {
-      sh(script: "mvn clean install -DskipTests", returnStdOut: true)
+    try {
+      withMaven(jdk: "openjdk-17-jenkins-slave-all", maven: Constants.MAVEN_TOOL_NAME, options: [artifactsPublisher(disabled: true)]) {
+        sh(script: "mvn clean install -DskipTests", returnStdOut: true)
+      }
+    } catch (Exception e) {
+      logger.warning(e.getMessage())
     }
   }
 
-  def build(String ARGS) {
-    logger.info("Build started for ${moduleName} image...")
-    docker.withRegistry("https://${Constants.ECR_FOLIO_REPOSITORY}", "ecr:${Constants.AWS_REGION}:${Constants.ECR_FOLIO_REPOSITORY_CREDENTIALS_ID}") {
-      def image = docker.build(
-        moduleName,
-        ARGS
-      )
-      image.push()
-    }
-  }
 
-  def makeImage() {
-    switch (moduleName) {
-      case 'folio-kong':
-        prepare()
-        build("--build-arg TARGETARCH=amd64 -f ./Dockerfile .")
-        break
-      case 'folio-keycloak':
-        prepare()
-        build("-f ./Dockerfile .")
-        break
-      default:
-        prepare()
-        compile()
-        build("-f ./Dockerfile .")
-        break
-    }
+def build(String ARGS) {
+  logger.info("Build started for ${moduleName} image...")
+  docker.withRegistry("https://${Constants.ECR_FOLIO_REPOSITORY}", "ecr:${Constants.AWS_REGION}:${Constants.ECR_FOLIO_REPOSITORY_CREDENTIALS_ID}") {
+    def image = docker.build(
+      moduleName,
+      ARGS
+    )
+    image.push()
   }
+}
+
+def makeImage() {
+  switch (moduleName) {
+    case 'folio-kong':
+      prepare()
+      build("--build-arg TARGETARCH=amd64 -f ./Dockerfile .")
+      break
+    case 'folio-keycloak':
+      prepare()
+      build("-f ./Dockerfile .")
+      break
+    default:
+      prepare()
+      compile()
+      build("-f ./Dockerfile .")
+      break
+  }
+}
+
 }
