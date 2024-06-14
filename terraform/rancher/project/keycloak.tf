1f674e927276fe9127f36ae52d1a94960e28b5af
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-06-14 11:37:04 +0300

    RANCHER-1336: get rid of non-working code

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -199,13 +199,6 @@ ingress:
     alb.ingress.kubernetes.io/success-codes: 200-399
     alb.ingress.kubernetes.io/healthcheck-path: /health/ready
     alb.ingress.kubernetes.io/healthcheck-port: "${local.kc_target_http_port}"
-
-initdbScripts:
-  kc_init_script.sh: |
-    #!/bin/env bash
-    sleep 300
-    /opt/keycloak/bin/kcadm.sh config credentials --realm master --user admin --password SecretPassword --server http://localhost:8080
-    /opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE
 EOF
   ]
 }
