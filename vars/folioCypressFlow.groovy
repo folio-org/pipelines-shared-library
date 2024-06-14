215b68632dfb01e2ffe6ac1dc1261e127c555b2f
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-06-14 16:20:43 +0300

    RANCHER-1527. Updating original *.js files from the @reportportal packages with the changed ones in the workspace test environment.Fixed.2

--- vars/folioCypressFlow.groovy
+++ vars/folioCypressFlow.groovy
@@ -354,8 +354,8 @@ void tuneWorkspaceForRP(){
   String report_portal_client = "@reportportal/client-javascript/lib/report-portal-client.js"
   String config = "@reportportal/client-javascript/lib/commons/config.js"
 
-  writeFile file: "./node_modules/${report_portal_client}", text: libraryResource("reportportal/${report_portal_client}")
   writeFile file: "./node_modules/${cypressReporter}", text: libraryResource("reportportal/${cypressReporter}")
+  writeFile file: "./node_modules/${report_portal_client}", text: libraryResource("reportportal/${report_portal_client}")
   writeFile file: "./node_modules/${config}", text: libraryResource("reportportal/${config}")
 }
 
