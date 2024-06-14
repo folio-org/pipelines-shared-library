61c123ce81f51ae094831faabe88e22956e9e078
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-06-14 03:58:22 +0300

    RANCHER-1527. Updating original *.js files from the @reportportal packages with the changed ones in the workspace test environment.Fixed.

--- vars/folioCypressFlow.groovy
+++ vars/folioCypressFlow.groovy
@@ -350,12 +350,12 @@ void runInDocker(String cypressImageVersion, String containerNameSuffix, Closure
 //TODO: Temporary solution. Should be refactored via RANCHER-1528 and RANCHER-1529 tickets
 @Deprecated
 void tuneWorkspaceForRP(){
-  String cypressReporter = "reportportal/@reportportal/agent-js-cypress/lib/cypressReporter.js"
-  String report_portal_client = "reportportal/@reportportal/client-javascript/lib/report-portal-client.js"
-  String config = "reportportal/@reportportal/client-javascript/lib/commons/config.js"
+  String cypressReporter = "@reportportal/agent-js-cypress/lib/cypressReporter.js"
+  String report_portal_client = "@reportportal/client-javascript/lib/report-portal-client.js"
+  String config = "@reportportal/client-javascript/lib/commons/config.js"
 
-  writeFile file: "./node_modules/${report_portal_client}", text: libraryResource("${report_portal_client}")
-  writeFile file: "./node_modules/${cypressReporter}", text: libraryResource("${cypressReporter}")
-  writeFile file: "./node_modules/${config}", text: libraryResource("${config}")
+  writeFile file: "./node_modules/${report_portal_client}", text: libraryResource("reportportal/${report_portal_client}")
+  writeFile file: "./node_modules/${cypressReporter}", text: libraryResource("reportportal/${cypressReporter}")
+  writeFile file: "./node_modules/${config}", text: libraryResource("reportportal/${config}")
 }
 
