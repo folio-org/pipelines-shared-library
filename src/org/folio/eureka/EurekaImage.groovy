49aa41a665d7af09e26f2f2318b01799b8d31cd9
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-19 21:21:24 +0600

    RANCHER-1350-prerequisite debug review v1

--- src/org/folio/eureka/EurekaImage.groovy
+++ src/org/folio/eureka/EurekaImage.groovy
@@ -63,10 +63,9 @@ class EurekaImage implements Serializable {
     }
   }
 
-  String imageTag() {
-    context.sh(script: "ls -la" ,returnStdOut: true)
-    context.input("Paused for review...")
-    def tag = context.(((new FileNameFinder().getFileNames(".", "target/" + moduleName + "*.jar"))[0].split("/").find { it.endsWith(".jar") }).replace(".jar", ""))
+  final String imageTag() {
+    context.sh(script: "ls -la", returnStdOut: true)
+    def tag = context.(((new FileNameFinder().getFileNames(".", "target/${moduleName}*.jar"))[0].split("/").find { it.endsWith(".jar") }).replace(".jar", ""))
     return tag
   }
 
