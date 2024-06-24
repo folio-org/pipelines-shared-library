dfbf4ce8ec4fc8788719e5a2af7bd625014b4c05
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-24 19:25:51 +0600

    .RANCHER-1334 minor change

--- terraform/rancher/project/secrets.tf
+++ terraform/rancher/project/secrets.tf
@@ -66,7 +66,7 @@ resource "rancher2_secret" "eureka_common" {
     SECRET_STORE_AWS_SSM_ACCESS_KEY = base64encode(var.s3_postgres_backups_access_key)
     SECRET_STORE_AWS_SSM_SECRET_KEY = base64encode(var.s3_postgres_backups_secret_key)
     SECURITY_ENABLED                = base64encode("true")
-    'tenant.url'                    = base64encode("http://mgr-tenants")
+    "tenant.url"                    = base64encode("http://mgr-tenants")
     TE_URL                          = base64encode("http://mgr-tenant-entitlements")
   }
 }
