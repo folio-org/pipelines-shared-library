ad83275333a6eee9a4c2aec135a8c6853a1da98e
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-25 22:51:38 +0600

    RANCHER-1361 updatePL method draft, testing start v2.2

--- src/org/folio/eureka/EurekaImage.groovy
+++ src/org/folio/eureka/EurekaImage.groovy
@@ -96,8 +96,9 @@ class EurekaImage implements Serializable {
     try {
       logger.info("Starting git clone for platform-complete...")
       steps.script {
-        steps.sshagent([Constants.GITHUB_SSH_CREDENTIALS_ID]) {
-          steps.sh(script: "git clone ${Constants.FOLIO_SSH_GITHUB_URL}/platform-complete.git -b snapshot --single-branch", returnStdout: false)
+        steps.withCredentials([sshUserPrivateKey(credentialsId: Constants.GITHUB_SSH_CREDENTIALS_ID, keyFileVariable: 'SSH_KEY')]) {
+          String git_cmd="GIT_SSH_COMMAND=ssh -i $SSH_KEY"
+          steps.sh(script: "${git_cmd} git clone ${Constants.FOLIO_SSH_GITHUB_URL}/platform-complete.git -b snapshot --single-branch", returnStdout: false)
           logger.info("Checkout completed successfully for platform-complete:snapshot")
           def name = steps.sh(script: 'find target/ -name *.jar | cut -d "/" -f 2 | sed \'s/....$//\'', returnStdout: true).trim()
           def eureka_platform = steps.readJSON file: "platform-complete/eureka-platform.json"
@@ -107,7 +108,7 @@ class EurekaImage implements Serializable {
             }
           }
           steps.writeJSON(file: "platform-complete/eureka-platform.json", json: eureka_platform, pretty: 2)
-          steps.sh(script: "cd platform-complete && git commit -am 'eureka-platform update' && git push", returnStdout: true)
+          steps.sh(script: "cd platform-complete && ${git_cmd} git commit -am 'eureka-platform update' && ${git_cmd} git push", returnStdout: true)
         }
       }
     } catch (Error e) {
