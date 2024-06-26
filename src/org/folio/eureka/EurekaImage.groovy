3373a252a22c4b6888dd6040427379c994abf52c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-26 21:37:27 +0600

    EurekaImage-minor-change simplify overall name generation final + comment alignment

--- src/org/folio/eureka/EurekaImage.groovy
+++ src/org/folio/eureka/EurekaImage.groovy
@@ -102,8 +102,7 @@ class EurekaImage implements Serializable {
                 }
               }
               steps.writeJSON(file: "eureka-platform.json", json: eureka_platform, pretty: 0)
-              steps.sh(script: "mv eureka-platform.json data.json && jq '.' data.json > eureka-platform.json")
-              //beatify JSON
+              steps.sh(script: "mv eureka-platform.json data.json && jq '.' data.json > eureka-platform.json") // beatify JSON
               steps.sh(script: "rm -f data.json && git commit -am '[EPL] updated: ${pom.getArtifactId()}-${pom.getVersion()}'")
               steps.sh(script: "set +x && git push --set-upstream https://${steps.env.GIT_USER}:${steps.env.GIT_PASS}@github.com/folio-org/platform-complete.git snapshot")
               logger.info("Snapshot branch successfully updated\n${moduleName} version: ${pom.getArtifactId()}-${pom.getVersion()}")
