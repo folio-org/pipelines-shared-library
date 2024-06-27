cd9be7bbb198f580de0f8713a3ff2d25e664c6d6
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-06-27 17:32:36 +0600

    .RANCHER-1361 initial testing v1.5

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -162,7 +162,7 @@ ingress:
     kubernetes.io/ingress.class: alb
     alb.ingress.kubernetes.io/scheme: internet-facing
     alb.ingress.kubernetes.io/group.name: "${local.group_name}"
-    alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'
+    alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
     alb.ingress.kubernetes.io/success-codes: 200-399
     alb.ingress.kubernetes.io/healthcheck-path: /health/ready
     alb.ingress.kubernetes.io/healthcheck-port: "${local.kc_target_http_port}"
