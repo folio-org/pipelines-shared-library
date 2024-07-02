c9e5b140c598d47a74c502b858eb7a6323090c76
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-07-02 10:56:56 +0300

    RANCHER-1365. An unnecessary condition was removed in folioKarateFlow at the Archive artifacts step.

--- vars/folioKarateFlow.groovy
+++ vars/folioKarateFlow.groovy
@@ -75,22 +75,20 @@ void call(KarateTestsParameters args) {
     }
 
     stage('[Archive] Archive artifacts') {
-      if (currentBuild.getBuildCauses('org.jenkinsci.plugins.workflow.support.steps.build.BuildUpstreamCause')) {
-        sleep time: 1, unit: 'MINUTES'
+      sleep time: 1, unit: 'MINUTES'
 
-        zip zipFile: "cucumber.zip", glob: "**/target/karate-reports*/*.json"
-        zip zipFile: "junit.zip", glob: "**/target/karate-reports*/*.xml"
-        zip zipFile: "karate-summary.zip", glob: "**/target/karate-reports*/karate-summary-json.txt"
+      zip zipFile: "cucumber.zip", glob: "**/target/karate-reports*/*.json"
+      zip zipFile: "junit.zip", glob: "**/target/karate-reports*/*.xml"
+      zip zipFile: "karate-summary.zip", glob: "**/target/karate-reports*/karate-summary-json.txt"
 
-        sleep time: 1, unit: 'MINUTES'
+      sleep time: 1, unit: 'MINUTES'
 
-        archiveArtifacts allowEmptyArchive: true, artifacts: "cucumber.zip", fingerprint: true, defaultExcludes: false
-        archiveArtifacts allowEmptyArchive: true, artifacts: "junit.zip", fingerprint: true, defaultExcludes: false
-        archiveArtifacts allowEmptyArchive: true, artifacts: "karate-summary.zip", fingerprint: true, defaultExcludes: false
-        archiveArtifacts allowEmptyArchive: true, artifacts: "teams-assignment.json", fingerprint: true, defaultExcludes: false
+      archiveArtifacts allowEmptyArchive: true, artifacts: "cucumber.zip", fingerprint: true, defaultExcludes: false
+      archiveArtifacts allowEmptyArchive: true, artifacts: "junit.zip", fingerprint: true, defaultExcludes: false
+      archiveArtifacts allowEmptyArchive: true, artifacts: "karate-summary.zip", fingerprint: true, defaultExcludes: false
+      archiveArtifacts allowEmptyArchive: true, artifacts: "teams-assignment.json", fingerprint: true, defaultExcludes: false
 
-        sleep time: 1, unit: 'MINUTES'
-      }
+      sleep time: 1, unit: 'MINUTES'
     }
 
 //    if (args.syncWithJira) {
