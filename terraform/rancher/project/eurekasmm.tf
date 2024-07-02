751504a439cd1e584ca0d439ac8c569772fad76b
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-02 16:31:59 +0600

    RANCHER-1361 mgr-* ssm required initial ssm parameters v2.1

--- terraform/rancher/project/eurekasmm.tf
+++ terraform/rancher/project/eurekasmm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "ssm_param" {
-  for_each = var.eureka ? toset(local.ssm_params) : {}
+  for_each = var.eureka ? toset(local.ssm_params) : ""
   name  = join("_", [var.rancher_cluster_name, rancher2_namespace.this.name, each.value])
   type  = "SecureString"
   value = "SecretPassword"
