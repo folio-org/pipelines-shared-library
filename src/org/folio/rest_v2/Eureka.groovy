56cbb5e1469aaf52afbbac02c97e31b596e968a6
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-03 17:24:36 +0300

    RANCHER-1352 modify methods for register application

--- src/org/folio/rest_v2/Eureka.groovy
+++ src/org/folio/rest_v2/Eureka.groovy
@@ -1,6 +1,7 @@
 package org.folio.rest_v2
 
 import org.folio.models.OkapiTenant
+import org.folio.utilities.RequestException
 
 class Eureka extends Authorization {
 
@@ -8,36 +9,32 @@ class Eureka extends Authorization {
 
   Eureka(Object context, String okapiDomain, OkapiTenant superTenant, boolean debug = false) {
     super(context, okapiDomain, debug)
-    this.superTenant = superTenant
   }
 
-
-  void registerApplication(List descriptorsList, OkapiTenant tenant, String applicationId) {
+  boolean isApplicationRegistered(String applicationId) {
 
     String checkUrl = generateUrl("/applications/${applicationId}")
-    def response = restClient.get(checkUrl, headers).body
-    int statusCode = response.status
+    try {
+      def response = restClient.get(checkUrl, headers).body
+      int statusCode = response.status
+    } catch (RequestException e) {
+      if (statusCode == 200) {
+        def content = response.body
+        logger.info("Application already registered: ${content}")
+        return true
 
-    if (statusCode == 200) {
-      def content = response.body
-      logger.info("Application already registered: ${content}")
+      } else if (statusCode == 503) {
+        throw new RequestException("Application manager is unavailable", e.statusCode)
 
-    } else if (statusCode == 503) {
-      logger.error("Application manager is unavailable")
+      } else {
+        logger.info("Application id ${applicationId} not found in Application manager. Proceeding with registration.")
+        return false
 
-    } else {
-      logger.info("Application id ${applicationId} not found in Application manager. Proceeding with registration.")
-
-      String url = generateUrl("/applications?check=false")
-      Map<String, String> headers = getAuthorizedHeaders(tenant)
-
-
-      restClient.post(url, descriptorsList, headers)
-      logger.info("Application registered: ${descriptorsList}")
+      }
     }
   }
 
-  void registerApplicationDiscovery(List descriptorsList, String applicationId, OkapiTenant tenant) {
+  boolean isDiscoveryRegistered(OkapiTenant tenant, String applicationId) {
 
     String discoveryUrl = generateUrl("/applications/${applicationId}/discovery?limit=500")
     Map<String, String> headers = getAuthorizedHeaders(tenant)
@@ -45,16 +42,40 @@ class Eureka extends Authorization {
     def response = restClient.get(discoveryUrl, headers)
     def content = response.body
 
-    if (content == descriptorsList) {
+    if (isDiscoveryRegistered.content == descriptorsList) {
       logger.info("All module discovery information are registered. Nothing to do.")
+      return true
     } else {
       logger.info("Not all module discovery information is registered. Proceeding with registration.")
+      return false
+    }
+  }
+
+  void registerApplication(List descriptorsList, OkapiTenant tenant, String applicationId) {
+
+    if (!isApplicationRegistered(applicationId)) {
+
+      String url = generateUrl("/applications?check=false")
+      Map<String, String> headers = getAuthorizedHeaders(tenant)
+
+      restClient.post(url, descriptorsList, headers)
+      logger.info("Application registered: ${descriptorsList}")
+    } else {
+      logger.info("Application ${applicationId} is already registered.")
+    }
+  }
+
+  void registerApplicationDiscovery(List descriptorsList, String applicationId, OkapiTenant tenant) {
+
+    if (!isDiscoveryRegistered(tenant, applicationId)) {
 
       String registerUrl = generateUrl("/modules/discovery")
 
       restClient.post(registerUrl, descriptorsList, headers)
       logger.info("Application discovery registered: ${registerUrl}")
+
+    } else {
+      logger.info("All module discovery information are registered. Nothing to do.")
     }
   }
 }
-
