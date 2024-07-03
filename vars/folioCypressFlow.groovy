798e83533d61e3db39afe9ae77332fc031ae86e4
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-07-03 22:00:59 +0300

    RANCHER-1365-v3-test. Changes were made for test purposes.18

--- vars/folioCypressFlow.groovy
+++ vars/folioCypressFlow.groovy
@@ -216,7 +216,7 @@ void call(params) {
 
       input message: "Analyze results. Do you want to proceed?"
 
-      testRunExecutionSummary = CypressRunExecutionSummary.addFromJSON(jsonSuites)
+      testRunExecutionSummary = CypressRunExecutionSummary.addFromJSON(jsonSuites, this)
       testRunExecutionSummary.addDefectsFromJSON(jsonDefects)
     }
 
