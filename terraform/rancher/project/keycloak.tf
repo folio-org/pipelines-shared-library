f2c9773ff6f2e293db3905f12935b8d782830392
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-03 23:26:34 +0600

    RANCHER-1361 kcadm.sh remove

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -166,11 +166,6 @@ ingress:
     alb.ingress.kubernetes.io/success-codes: 200-399
     alb.ingress.kubernetes.io/healthcheck-path: /health/ready
     alb.ingress.kubernetes.io/healthcheck-port: "${local.kc_target_http_port}"
-
-lifecycleHooks:
-  postStart:
-    exec:
-      command: ["/bin/sh", "-c", "sleep 120 && /opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE"]
 EOF
   ]
 }
