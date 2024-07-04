927a365833bae4081fc788548b23dec24f5eb00c
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-04 03:39:04 +0300

    RANCHER-1352 modify methods for register application

--- src/org/folio/rest_v2/Eureka.groovy
+++ src/org/folio/rest_v2/Eureka.groovy
@@ -1,5 +1,6 @@
 package org.folio.rest_v2
 
+import groovy.json.JsonOutput
 import org.folio.models.OkapiTenant
 import org.folio.utilities.RequestException
 
@@ -41,9 +42,28 @@ class Eureka extends Authorization {
         logger.info("All module discovery information are registered. Nothing to do.")
         return true
       } else {
-      logger.info("Not all module discovery information is registered. Proceeding with registration.")
-      return false
+        logger.info("Not all module discovery information is registered. Proceeding with registration.")
+        return false
       }
+  }
+
+  boolean isDiscoveryModulesRegistered(List descriptorsList, OkapiTenant tenant) {
+
+    String url = generateUrl("/modules/discovery")
+    Map<String, String> headers = getAuthorizedHeaders(tenant)
+
+    try {
+      restClient.post(url, descriptorsList, headers)
+      logger.info("Application discovery registered: ${url}")
+      return true
+    } catch (RequestException e) {
+      if (e.statusCode ==! HttpURLConnection.HTTP_CREATED){
+        logger.info("Not all of the module discovery information is registered. Going to register one by one")
+        return false
+      } else {
+        throw new RequestException("Discovery modules is unavailable", e.statusCode)
+      }
+    }
   }
 
   void registerApplication(List descriptorsList, OkapiTenant tenant, String applicationId) {
@@ -60,17 +80,29 @@ class Eureka extends Authorization {
     logger.info("Application registered: ${descriptorsList}")
   }
 
-  void registerApplicationDiscovery(List descriptorsList, String applicationId, OkapiTenant tenant) {
+  void registerApplicationDiscovery(List descriptorsList, String applicationId, OkapiTenant tenant, String region) {
 
     if (isDiscoveryRegistered(tenant, applicationId, descriptorsList)) {
       logger.warning("All module discovery information are registered. Nothing to do.")
       return
+    } else (isDiscoveryModulesRegistered(tenant, descriptorsList)) {
+      logger.info("Application discovery registered")
+      return
     }
 
-    String url = generateUrl("/modules/discovery")
-    Map<String, String> headers = getAuthorizedHeaders(tenant)
+    def body = ['discovery':[]]
+    descriptorsList.modules.each() { module ->
+      def moduleUrl = "http://${module.name}-b.${folio}.folio-eis.${region}:8051/${module.name}".toString()
+      module.put('location', moduleUrl)
+      body.discovery.add(module)
+    }
+    body.discovery.each() { modDiscovery ->
+      String url = generateUrl("/modules/${modDiscovery.id}/discovery")
+      Map<String, String> headers = getAuthorizedHeaders(tenant)
+      def response = restClient.post(url, modDiscovery, headers)
+    }
 
-    restClient.post(registerUrl, descriptorsList, headers)
-    logger.info("Application discovery registered: ${url}")
+    logger.info("Info on the newly created discovery table for id ${applicationId}\\n" JsonOutput.prettyPrint(JsonOutput.toJson(response)))
   }
 }
+
