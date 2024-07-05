4565bef7b846ff3fc3ee64cf62edd164dc625fa8
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-07-06 01:57:35 +0300

    RANCHER-1365-v3-test. Changes were made for test purposes.79

--- src/org/folio/slack/templates/SlackTestResultTemplates.groovy
+++ src/org/folio/slack/templates/SlackTestResultTemplates.groovy
@@ -4,7 +4,7 @@ class SlackTestResultTemplates {
 
   private SlackTestResultTemplates() {}
 
-  static final CYPRESS_TEXT = 'Build name: ${TEST_TXT_BUILD_NAME}. Passed tests: ${TEST_TXT_PASS_CNT}, Broken tests: ${TEST_TXT_BRK_CNT}, Failed tests: ${TEST_TXT_FAIL_CNT}, Failed tests: ${TEST_TXT_SKIP_CNT}, Pass rate: ${TEST_TXT_PASS_RATE}%'
+  static final CYPRESS_TEXT = 'Build name: ${TEST_TXT_BUILD_NAME}. Passed tests: ${TEST_TXT_PASS_CNT}, Broken tests: ${TEST_TXT_BRK_CNT}, Failed tests: ${TEST_TXT_FAIL_CNT}, Skipped tests: ${TEST_TXT_SKIP_CNT}, Pass rate: ${TEST_TXT_PASS_RATE}%'
   static final CYPRESS_TITLE = 'Cypress tests results :arrow_forward:'
 
   static final KARATE_TEXT = 'Passed tests: ${TEST_TXT_PASS_CNT}, Failed tests: ${TEST_TXT_FAIL_CNT}, Pass rate: ${TEST_TXT_PASS_RATE}%'
