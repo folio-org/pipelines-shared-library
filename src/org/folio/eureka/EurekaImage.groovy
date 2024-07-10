421f9ca70a2b420a87b8547f393045ae749ddbab
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-10 19:30:26 +0600

    RANCHER-1576 unique image identifier v1.1

--- src/org/folio/eureka/EurekaImage.groovy
+++ src/org/folio/eureka/EurekaImage.groovy
@@ -33,7 +33,7 @@ class EurekaImage implements Serializable {
       logger.info("Starting Maven compile for ${moduleName}.")
       steps.withMaven(jdk: "openjdk-17-jenkins-slave-all",
         maven: Constants.MAVEN_TOOL_NAME) {
-        steps.sh(script: "mvn clean install -U -DskipTests -DbuildNumber=${steps.env.BUILD_NUMBER}", returnStdout: true)
+        steps.sh(script: "mvn clean install -DskipTests", returnStdout: true)
       }
       logger.info("Maven compile completed successfully for ${moduleName}")
     } catch (Error e) {
@@ -63,7 +63,7 @@ class EurekaImage implements Serializable {
     try {
       def pom = steps.readMavenPom file: 'pom.xml'
       if (pom) {
-        tag = "${pom.getVersion()}"
+        tag = "${pom.getVersion()}.${steps.env.BUILD_NUMBER}"
       }
     } catch (Exception e) {
       tag = 'latest'
@@ -77,8 +77,8 @@ class EurekaImage implements Serializable {
       try {
         def pom = steps.readMavenPom file: 'pom.xml'
         if (pom) {
-          steps.sh(script: "curl ${Constants.EUREKA_REGISTRY_URL}${pom.getArtifactId()}-${pom.getVersion()}.json --upload-file target/ModuleDescriptor.json")
-          logger.info("ModuleDescriptor: ${Constants.EUREKA_REGISTRY_URL}${pom.getArtifactId()}-${pom.getVersion()}.json")
+          steps.sh(script: "curl ${Constants.EUREKA_REGISTRY_URL}${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}.json --upload-file target/ModuleDescriptor.json")
+          logger.info("ModuleDescriptor: ${Constants.EUREKA_REGISTRY_URL}${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}.json")
         }
       } catch (Exception e) {
         logger.warning("Failed to publish MD for ${moduleName}\nError: ${e.getMessage()}")
@@ -97,20 +97,20 @@ class EurekaImage implements Serializable {
             steps.dir('platform-complete') {
               def eureka_platform = steps.readFile file: "eureka-platform.json"
               def check = new JsonSlurperClassic().parseText("${eureka_platform}")
-              def module = "${pom.getArtifactId()}-${pom.getVersion()}"
+              def module = "${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}"
               if (module.toString() in check['id']) {
-                logger.warning("${pom.getArtifactId()}-${pom.getVersion()} already exists!\nPlease update pom.xml to build a new image.")
+                logger.warning("${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}.${steps.env.BUILD_NUMBER} already exists!\nPlease update pom.xml to build a new image.")
               } else {
                 check.each {
                   if (it['id'] =~ /${moduleName}/) {
-                    it['id'] = "${pom.getArtifactId()}-${pom.getVersion()}" as String
+                    it['id'] = "${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}" as String
                   }
                 }
                 steps.writeJSON(file: "eureka-platform.json", json: check, pretty: 0)
                 steps.sh(script: "mv eureka-platform.json data.json && jq '.' data.json > eureka-platform.json")
-                steps.sh(script: "rm -f data.json && git commit -am '[EPL] updated: ${pom.getArtifactId()}-${pom.getVersion()}'")
+                steps.sh(script: "rm -f data.json && git commit -am '[EPL] updated: ${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}'")
                 steps.sh(script: "set +x && git push --set-upstream https://${steps.env.GIT_USER}:${steps.env.GIT_PASS}@github.com/folio-org/platform-complete.git snapshot")
-                logger.info("Snapshot branch successfully updated\n${moduleName} version: ${pom.getArtifactId()}-${pom.getVersion()}")
+                logger.info("Snapshot branch successfully updated\n${moduleName} version: ${pom.getArtifactId()}-${pom.getVersion()}.${steps.env.BUILD_NUMBER}")
               }
             }
           }
