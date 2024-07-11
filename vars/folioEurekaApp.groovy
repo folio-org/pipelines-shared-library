231df6f46881d20747fedb67a9c587560624c82e
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-11 12:44:14 +0300

    add functions to test application creation

--- vars/folioEurekaApp.groovy
+++ vars/folioEurekaApp.groovy
@@ -43,7 +43,7 @@ void generateApplicationDescriptorFile(String applicationId) {
   Logger logger = new Logger(this, 'folioEurekaApp')
 
   def publicMdr = "https://folio-registry.dev.folio.org"
-  def mdrBucket = "s3://eureka-application-registry/descriptors/"
+  def mdrBucket = "s3://eureka-application-registry/descriptors"
   logger.info("Going to build application descriptor for ${applicationId}")
 
   sh(script: "git clone -b master --single-branch ${org.folio.Constants.FOLIO_GITHUB_URL}/${applicationId}.git")
