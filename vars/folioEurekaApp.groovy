20c41a6d5f4db5f7e5aeda762f244e94134003ac
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-12 11:07:21 +0300

    Modify method for application descriptor generator

--- vars/folioEurekaApp.groovy
+++ vars/folioEurekaApp.groovy
@@ -4,6 +4,42 @@ import org.folio.utilities.Logger
 
 Logger logger = new Logger(this, 'folioEurekaApp')
 
+void generateApplicationDescriptorFile() {
+  Logger logger = new Logger(this, 'folioEurekaApp')
+
+  String platformComplete ='app-platform-complete'
+  String platformMinimal= 'app-platform-minimal'
+
+  logger.info("Going to build application descriptor for ${platformMinimal}")
+  applicationDescriptorFileGenerator(platformMinimal)
+  logger.info("Going to build application descriptor for ${platformComplete}")
+  applicationDescriptorFileGenerator(platformComplete)
+
+  }
+
+  def applicationDescriptorFileGenerator(String applicationId){
+
+    Logger logger = new Logger(this, 'folioEurekaApp')
+    String mdrBucket = "eureka-application-registry"
+
+    sh(script: "git clone -b master --single-branch ${org.folio.Constants.FOLIO_GITHUB_URL}/${applicationId}.git")
+    dir(applicationId) {
+      awscli.withAwsClient() {
+        sh(script: "mvn clean install -U -e -DbuildNumber=${BUILD_NUMBER} -DbeRegistries=\"s3::${mdrBucket}::descriptors/\" -DawsRegion=us-west-2")
+      }
+      dir('target') {
+        sh(script: "ls -la")
+        def applicationDescriptorFilename = sh(script: "find . -name '${applicationId}*.json' | head -1", returnStdout: true)
+        try {
+          sh(script: "curl ${org.folio.Constants.EUREKA_APPLICATIONS_URL} --upload-file ${applicationDescriptorFilename}")
+          logger.info("File ${applicationDescriptorFilename} successfully uploaded to: ${org.folio.Constants.EUREKA_APPLICATIONS_URL}")
+        } catch (Exception e) {
+          logger.warning("Failed to generat application descriptor\nError: ${e.getMessage()}")
+        }
+      }
+    }
+  }
+
 //def getApplicationDescriptor(String applicationId) {
 //  try {
 //    String response = steps.sh(script: "curl ${org.folio.Constants.EUREKA_APPLICATIONS_URL}${applicationId}", returnStdout: true)
@@ -38,30 +74,3 @@ Logger logger = new Logger(this, 'folioEurekaApp')
 //    logger.warning("Failed to get application dscriptors \nError: ${e.getMessage()}")
 //  }
 //}
-
-void generateApplicationDescriptorFile(String applicationId) {
-  Logger logger = new Logger(this, 'folioEurekaApp')
-
-  //def publicMdr = "https://folio-registry.dev.folio.org"
-  def mdrBucket = "eureka-application-registry"
-
-  logger.info("Going to build application descriptor for ${applicationId}")
-
-  sh(script: "git clone -b master --single-branch ${org.folio.Constants.FOLIO_GITHUB_URL}/${applicationId}.git")
-  dir(applicationId) {
-    awscli.withAwsClient() {
-      //sh(script: "mvn clean install -U -e -DbuildNumber=${BUILD_NUMBER} -DbeRegistries=\"s3::${mdrBucket}::descriptors/,okapi::${publicMdr}::/\" -DuiRegistries=\"okapi::${publicMdr}\" -DawsRegion=us-west-2 -DoverrideConfigRegistries=true")
-      sh(script: "mvn clean install -U -e -DbuildNumber=${BUILD_NUMBER} -DbeRegistries=\"s3::${mdrBucket}::descriptors/\" -DawsRegion=us-west-2")
-    }
-    dir('target') {
-      sh(script: "ls -la")
-      def applicationDescriptorFilename = sh(script: "find . -name '${applicationId}*.json' | head -1", returnStdout: true)
-      try {
-        sh(script: "curl ${org.folio.Constants.EUREKA_APPLICATIONS_URL} --upload-file ${applicationDescriptorFilename}")
-        logger.info("File ${applicationDescriptorFilename} successfully uploaded to: ${org.folio.Constants.EUREKA_APPLICATIONS_URL}")
-      } catch (Exception e) {
-        logger.warning("Failed to generat application descriptor\nError: ${e.getMessage()}")
-      }
-    }
-  }
-}
