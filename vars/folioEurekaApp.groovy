1773392b9f983264de3ec7faacefe915ad9a604d
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-12 10:52:37 +0300

    Modify method for application descriptor generator

--- vars/folioEurekaApp.groovy
+++ vars/folioEurekaApp.groovy
@@ -42,9 +42,9 @@ Logger logger = new Logger(this, 'folioEurekaApp')
 void generateApplicationDescriptorFile(String applicationId) {
   Logger logger = new Logger(this, 'folioEurekaApp')
 
-  def publicMdr = "https://folio-registry.dev.folio.org"
+  //def publicMdr = "https://folio-registry.dev.folio.org"
   def mdrBucket = "eureka-application-registry"
-  def
+
   logger.info("Going to build application descriptor for ${applicationId}")
 
   sh(script: "git clone -b master --single-branch ${org.folio.Constants.FOLIO_GITHUB_URL}/${applicationId}.git")
