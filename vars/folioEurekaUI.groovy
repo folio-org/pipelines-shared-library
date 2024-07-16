50954cb7368ae501544f0c36c9880e0a30503491
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-16 18:30:28 +0600

    RANCHER-1557 fs tests v1.0.1

--- vars/folioEurekaUI.groovy
+++ vars/folioEurekaUI.groovy
@@ -5,7 +5,18 @@ void call(Map params, boolean releaseVersion = false) {
   stage('Checkout') {
     sh(script: "git clone --branch ${params.branch} --single-branch ${Constants.FOLIO_GITHUB_URL}/platform-complete.git" as String)
   }
-
+  stage('Prepare') {
+    dir('platform-complete') {
+      sh(script: "cp -R -f eureka-tpl/* .")
+      //templating goes here...
+      input("Paused for review...")
+    }
+  }
+  stage('Build and Push') {
+    dir('platform-complete') {
+      // Docker & Helm stuff goes here
+    }
+  }
   stage('Cleanup') {
     //common.removeImage(image.getImageName()) TODO clean fresh image.
   }
