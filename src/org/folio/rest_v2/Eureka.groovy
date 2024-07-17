dc41d106b9deea229437ecc887f2991a664705f8
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-17 14:50:35 +0300

    Modify method for application registration

--- src/org/folio/rest_v2/Eureka.groovy
+++ src/org/folio/rest_v2/Eureka.groovy
@@ -63,7 +63,7 @@ class Eureka extends Authorization {
     String url = "https://folio-eureka-scout-kong.ci.folio.org/applications?check=false/applications?check=false"
     Map<String,String> headers = [
         "x-okapi-token": getEurekaToken(),
-        "Content-Type": "application/json"
+//        "Content-Type": "application/json"
       ]
 
     restClient.post(url, descriptorsList, headers)
