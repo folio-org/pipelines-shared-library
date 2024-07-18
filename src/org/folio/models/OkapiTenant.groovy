e5aadc9f9aa7ab76e34142af5bf854d2de597c64
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-07-18 15:37:07 +0300

    RANCHER-1486
    Add authority indexation during env provisioning
    Jira: https://folio-org.atlassian.net/browse/RANCHER-1486

--- src/org/folio/models/OkapiTenant.groovy
+++ src/org/folio/models/OkapiTenant.groovy
@@ -20,7 +20,9 @@ class OkapiTenant {
   /** Modules that are installed for the tenant. */
   Modules modules
 
-  /** Index information associated with the tenant. */
+  /** List of index information associated with the tenant. */
+  List<Index> indexes
+
   Index index
 
   /** Parameters for installation requests for the tenant. */
@@ -39,6 +41,7 @@ class OkapiTenant {
   OkapiTenant(String tenantId) {
     this.tenantId = tenantId
     this.modules = new Modules()
+    this.indexes = new ArrayList<>()
   }
 
   /**
@@ -84,12 +87,12 @@ class OkapiTenant {
   }
 
   /**
-   * Chainable setter for index information.
-   * @param index Index information associated with the tenant.
-   * @return The OkapiTenant object.
+   * Method to add an index to the tenant.
+   * @param index The Index object to add.
+   * @return The OkapiTenant instance for method chaining.
    */
   OkapiTenant withIndex(Index index) {
-    this.index = index
+    this.indexes.add(index)
     return this
   }
 
