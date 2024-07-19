6947e1393adefc7aa210ca482687c0966199a787
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-19 14:59:12 +0600

    .RANCHER-1557 full Eureka UI testing v1.4

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -95,7 +95,7 @@ void call(Map params, boolean releaseVersion = false) {
       ]
       Map updatesHeaders = ['Authorization': "Bearer " + token['access_token'], 'Content-Type': 'application/json']
       headers.put("Authorization", "Bearer ${token['access_token']}")
-      def realm = client.get("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients?query=name=${params.tenant_id}-application", headers).body
+      def realm = client.get("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients?query=name=${params.tenant_id}-application", headers)
       println(JsonOutput.prettyPrint(realm as String))
       client.put("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients/${realm['id']}", JsonOutput.toJson(updates), updatesHeaders)
     }
