dae1ab4fa9d77f0814b5d968e98510fe621fbece
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-19 16:48:20 +0600

    .RANCHER-1557 full Eureka UI testing v2.0

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -96,9 +96,7 @@ void call(Map params, boolean releaseVersion = false) {
       Map updatesHeaders = ['Authorization': "Bearer " + token['access_token'], 'Content-Type': 'application/json']
       headers.put("Authorization", "Bearer ${token['access_token']}")
       def realm = client.get("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients?clientId=${params.tenant_id}-application", headers).body
-      println(realm['id'])
-      input("Testing review...")
-      //client.put("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients/${realm['id']}", JsonOutput.toJson(updates), updatesHeaders)
+      client.put("${params.keycloakUrl}/admin/realms/${params.tenant_id}/clients/${realm['id']}", JsonOutput.toJson(updates), updatesHeaders)
     }
   }
 }
