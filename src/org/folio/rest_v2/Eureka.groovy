ec6f6326b26cd09121eb06d6dbc472505b9e29cc
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-22 13:02:27 +0300

    testing

--- src/org/folio/rest_v2/Eureka.groovy
+++ src/org/folio/rest_v2/Eureka.groovy
@@ -14,23 +14,23 @@ class Eureka extends Authorization {
     super(context, okapiDomain, debug)
   }
 
-//  boolean isApplicationRegistered(String applicationId) {
-//
-//    String url = generateKongUrl("/applications/${applicationId}")
-//
-//    try {
-//      restClient.get(url).body
-//      logger.info("Application ${applicationId} is already registered.")
-//      return true
-//    } catch (RequestException e) {
-//      if (e.statusCode == HttpURLConnection.HTTP_NOT_FOUND) {
-//        logger.info("Application id ${applicationId} not found in Application manager. Proceeding with registration.")
-//        return false
-//      } else {
-//        throw new RequestException("Application manager is unavailable", e.statusCode)
-//      }
-//    }
-//  }
+  boolean isApplicationRegistered(String applicationId) {
+
+    String url = generateKongUrl("/applications/${applicationId}")
+
+    try {
+      restClient.get(url).body
+      logger.info("Application ${applicationId} is already registered.")
+      return true
+    } catch (RequestException e) {
+      if (e.statusCode == HttpURLConnection.HTTP_NOT_FOUND) {
+        logger.info("Application id ${applicationId} not found in Application manager. Proceeding with registration.")
+        return false
+      } else {
+        throw new RequestException("Application manager is unavailable", e.statusCode)
+      }
+    }
+  }
 
   def getDescriptorsList(applicationId) {
 
@@ -65,25 +65,25 @@ class Eureka extends Authorization {
 //    }
 //  }
 //
-//  def registerApplication(String applicationId) {
-//    String descriptorsList = getDescriptorsList(applicationId)
-//    if (isApplicationRegistered(applicationId)) {
-//      logger.warning("Application ${applicationId} is already registered.")
-//      return
-//    }
-//
-//    String url = generateKongUrl("/applications?check=false")
-//    Map<String,String> headers = [
-//        'x-okapi-token': getEurekaToken(),
-//        'Content-Type': 'application/json'
-//      ]
-//    try {
-//    restClient.post(url, descriptorsList, headers)
-//    logger.info("Application registered: ${descriptorsList}")
-//    } catch (RequestException e) {
-//        throw new RequestException("Application is not registered", e.statusCode)
-//      }
-//    }
+  def registerApplication(String applicationId) {
+    String descriptorsList = getDescriptorsList(applicationId)
+    if (isApplicationRegistered(applicationId)) {
+      logger.warning("Application ${applicationId} is already registered.")
+      return
+    }
+
+    String url = generateKongUrl("/applications?check=false")
+    Map<String,String> headers = [
+        'x-okapi-token': getEurekaToken(),
+        'Content-Type': 'application/json'
+      ]
+    try {
+    restClient.post(url, descriptorsList, headers)
+    logger.info("Application registered: ${descriptorsList}")
+    } catch (RequestException e) {
+        throw new RequestException("Application is not registered", e.statusCode)
+      }
+    }
 
   String getEurekaToken() {
     logger.info("Getting access token from Keycloak service")
