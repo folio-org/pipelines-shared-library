8a8f2a5bb74933ee8ccb5b983b8cd7f6490df7eb
dmoroz-ebsco <dmoroz@ebsco.com>
2024-07-23 16:14:32 +0300

    Registering app

--- src/org/folio/rest_v2/Eureka.groovy
+++ src/org/folio/rest_v2/Eureka.groovy
@@ -1,6 +1,5 @@
 package org.folio.rest_v2
 
-import org.folio.models.OkapiTenant
 import org.folio.utilities.RequestException
 
 class Eureka extends Authorization {
@@ -34,29 +33,6 @@ class Eureka extends Authorization {
     }
     logger.info(steps.readJSON(file: "${applicationId}.json"))
     return steps.readJSON(file: "${applicationId}.json")
-  }
-
-  String getEurekaToken(String client_id, String client_secret, String grant_type) {
-    logger.info("Getting access token from Keycloak service")
-
-    String url = "${Constants.EUREKA_KEYCLOAK_URL}realms/master/protocol/openid-connect/token"
-    Map<String,String> headers = [
-      'Content-Type':'application/x-www-form-urlencoded'
-    ]
-    String requestBody = "client_id=$client_id&client_secret=$client_secret&grant_type=$grant_type"
-
-    try {
-      def response = restClient.post(url, requestBody, headers).body
-      logger.info("Access token received successfully from Keycloak service")
-      logger.info("${response.access_token}")
-      return response.access_token
-    } catch (RequestException e) {
-      if (e.statusCode == HttpURLConnection.HTTP_NOT_FOUND) {
-        logger.info("Cant get token.")
-      } else {
-        throw new RequestException("Keycloak is unavailable", e.statusCode)
-      }
-    }
   }
 
   def registerApplication(String applicationId) {
