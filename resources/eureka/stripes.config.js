c70a76e588b100c2a2b36f75bc8f3db99abf168b
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-25 01:54:22 +0600

    RANCHER-1334-Q release workarounds v4.3

--- resources/eureka/stripes.config.js
+++ resources/eureka/stripes.config.js
@@ -76,7 +76,6 @@ modules: {
     '@folio/plugin-query-builder' : {},
     '@folio/quick-marc' : {},
     '@folio/marc-authorities': {},
-    '@folio/reading-room' : {},
     '@folio/receiving' : {},
     '@folio/remote-storage' : {},
     '@folio/requests' : {},
