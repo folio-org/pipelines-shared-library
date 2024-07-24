9e15be555be44ca33c99d0595bf29f3685bb6c32
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-25 01:44:23 +0600

    RANCHER-1334-Q release workarounds v4.2

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -54,7 +54,7 @@ void call(Map params, boolean releaseVersion = true) {
         sh(script: "rm -f package.json")
         sh(script: "rm -f stripes.config.json")
         sh("curl https://raw.githubusercontent.com/folio-org/pipelines-shared-library/RANCHER-1334-Q/resources/eureka/package.json -o package.json")
-        sh("curl https://raw.githubusercontent.com/folio-org/pipelines-shared-library/RANCHER-1334-Q/resources/eureka/stripes.config.js -o stripes.config.json")
+        sh("curl https://raw.githubusercontent.com/folio-org/pipelines-shared-library/RANCHER-1334-Q/resources/eureka/stripes.config.js -o stripes.config.js")
         println("Parameters for UI:\n${JsonOutput.prettyPrint(JsonOutput.toJson(params))}")
         writeFile file: 'stripes.config.js', text: make_tpl(readFile(file: 'stripes.config.js', encoding: "UTF-8") as String, params), encoding: 'UTF-8'
       }
