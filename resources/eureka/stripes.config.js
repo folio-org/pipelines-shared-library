cb4a464fc241363c2efa8c17c91294c7ecba1f02
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-25 03:50:02 +0600

    RANCHER-1334-Q release workarounds v4.7.2

--- resources/eureka/stripes.config.js
+++ resources/eureka/stripes.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'uiUrl': '${tenantUrl}',
     'tenant': '${tenantId}',
     'authnUrl': '${keycloakUrl}',
-    'clientId': '${clientId}',
+    'clientId': '${clientId}'
   },
   config: {
     hasAllPerms: ${hasAllPerms},
