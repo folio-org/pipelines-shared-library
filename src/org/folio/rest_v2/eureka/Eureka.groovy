ee54f095e535537fddf0c77a511450a67fecfd93
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-03 21:01:47 +0300

    RANCHER-1359-test. Changes were made for test purposes.3

--- src/org/folio/rest_v2/eureka/Eureka.groovy
+++ src/org/folio/rest_v2/eureka/Eureka.groovy
@@ -1,22 +1,11 @@
 package org.folio.rest_v2.eureka
 
-import hudson.util.Secret
-import org.folio.models.EurekaTenant
-import org.folio.models.OkapiTenant
-import org.folio.models.OkapiUser
+import org.folio.models.Tenant
 import org.folio.rest_v2.Common
-import org.folio.utilities.RequestException
 
 class Eureka extends Common {
 
   private Kong kong
-
-  /**
-   * EurekaTenant object contains Master Tenant configuration for Eureka.
-   * @attribute 'tenantId'     Default: "master"
-   * @attribute 'clientId'     Default: "folio-backend-admin-client"
-   */
-  public EurekaTenant masterTenant
 
   Eureka(Object context, String kongUrl, String keycloakUrl, boolean debug = false) {
     super(context, kongUrl, debug)
@@ -24,74 +13,9 @@ class Eureka extends Common {
     this.kong = new Kong(context, kongUrl, keycloakUrl, debug)
   }
 
-  void initializeFromScratch(Map<String, OkapiTenant> tenants, boolean enableConsortia) {
-    //TODO
-  }
-
-  void createTenant(EurekaTenant tenant) {
-    // Get Authorization Headers for Master Tenant from Keycloak
-    Map<String, String> headers = getHttpHeaders(masterTenant)
-
-    Map body = [
-      name: tenant.tenantId,
-      description: tenant.tenantDescription
-    ]
-
-    logger.info("Creating tenant ${tenant.tenantId}...")
-
-    // Run POST request to create a new tenant
-    restClient.post(tenant.tenantManagerUrl, body, headers)
-
-    logger.info("Tenant (${tenant.tenantId}) successfully created")
-  }
-
-  boolean isTenantExist(String endpointUrl, String tenantId, Map<String, String> httpHeaders) {
-    String tenantUrl = "${endpointUrl}/${tenantId}"
-
-    try {
-      restClient.get(tenantUrl, httpHeaders)
-      logger.info("Tenant ${tenantId} exists")
-      return true
-    } catch (RequestException e) {
-        logger.warning("statusCode: ${e.statusCode}: Not able to check tenant ${tenantId} existence: ${e.getMessage()}")
-        return false
+  void initializeFromScratch(Map<String, Tenant> tenants, boolean enableConsortia) {
+    tenants.each { tenantId, tenant ->
+      kong.createTenant(tenant)
     }
-  }
-
-  def getHttpHeaders(EurekaTenant tenant) {
-    def tenantId = (tenant.tenantId == masterTenant.tenantId) ? "" : tenant.tenantId
-    def tenantToken = getAuthToken(tenant.keycloakUrl, tenant.tenantId, tenant.clientId, tenant.clientSecret)
-    return getAuthHeaders(tenantId, tenantToken)
-  }
-
-  String getAuthToken(String keycloakUrl, String tenantId, String clientId, String clientSecret) {
-    logger.info("Getting access token from Keycloak service")
-
-    String url = "${keycloakUrl}/realms/${tenantId}/protocol/openid-connect/token"
-    Map<String,String> headers = ['Content-Type':'application/x-www-form-urlencoded']
-    String requestBody = "client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials"
-
-    def response = restClient.post(url, requestBody, headers).body
-    logger.info("Access token received successfully from Keycloak service")
-
-    return response['access_token']
-  }
-
-  Map<String,String> getAuthHeaders(String tenantId, String token) {
-    Map<String,String> headers = [:]
-
-    if (tenantId != null && !tenantId.isEmpty()) {
-      headers.putAll(['x-okapi-tenant': tenantId])
-    }
-
-    if (token != null && !token.isEmpty()) {
-      headers.putAll(['Authorization' : "Bearer ${token}"])
-    }
-
-    if (!headers.isEmpty()) {
-      logger.info("Auth HTTP Headers are populated")
-    }
-
-    return headers
   }
 }
