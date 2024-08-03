8db68df2ff39831440eef6766792a0f2fbd7223b
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-03 20:22:52 +0300

    RANCHER-1359. The Tenant base class was added.

--- src/org/folio/models/Tenant.groovy
+++ src/org/folio/models/Tenant.groovy
@@ -10,76 +10,24 @@ class Tenant {
   /** Description of the tenant. */
   String tenantDescription
 
-  /** Modules that are installed for the tenant. */
-  Modules modules
-
-  /** Index information associated with the tenant. */
-  Index index
-
-  /** Parameters for installation requests for the tenant. */
-  InstallRequestParams installRequestParams
-
   /** User Interface (UI) details for the tenant. */
   TenantUi tenantUi
-
-  /**
-   * Constructor that sets the tenantId and initializes modules.
-   * @param tenantId Tenant's identifier.
-   */
-  Tenant(String tenantId, String tenantDescription = "") {
-    this.tenantId = tenantId
-    this.tenantDescription = tenantDescription
-    this.modules = new Modules()
-  }
-
-  /**
-   * Chainable setter for tenant's name.
-   * @param tenantName Name of the tenant.
-   * @return The Tenant object.
-   */
-  Tenant withTenantName(String tenantName) {
-    this.tenantName = tenantName
-    return this
-  }
-
-  /**
-   * Chainable setter for tenant's description.
-   * @param tenantDescription Description of the tenant.
-   * @return The Tenant object.
-   */
-  Tenant withTenantDescription(String tenantDescription) {
-    this.tenantDescription = tenantDescription
-    return this
-  }
-
-  /**
-   * Chainable setter for index information.
-   * @param index Index information associated with the tenant.
-   * @return The Tenant object.
-   */
-  Tenant withIndex(Index index) {
-    this.index = index
-    return this
-  }
-
-  /**
-   * Chainable setter for install request parameters.
-   * @param installRequestParams Parameters for installation requests for the tenant.
-   * @return The Tenant object.
-   */
-  Tenant withInstallRequestParams(InstallRequestParams installRequestParams) {
-    this.installRequestParams = installRequestParams
-    return this
-  }
 
   /**
    * Chainable setter for tenant UI.
    * @param tenantUi User Interface (UI) details for the tenant.
    * @return The Tenant object.
    */
-  Tenant withTenantUi(TenantUi tenantUi) {
+  void setTenantUi(TenantUi tenantUi) {
     this.tenantUi = tenantUi
     this.tenantUi.tenantId = this.tenantId
-    return this
+  }
+
+  static Tenant getTenantFromJson(def json){
+    return new Tenant(
+      tenantId: json.id
+      , tenantName: json.name
+      , tenantDescription: json.description
+    )
   }
 }
