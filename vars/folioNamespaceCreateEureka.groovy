319e39a7bfff031d293f58c3d21fc90428c6fcac
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-04 15:57:33 +0300

    RANCHER-1359-test. Changes were made for test purposes.24

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -84,7 +84,7 @@ void call(CreateNamespaceParameters args) {
       .withIndex(new Index(true, true))
       .withInstallRequestParams(installRequestParams.clone())
       .withTenantUi(tenantUi.clone())
-      .withTenantName("diku2")
+      .withTenantName("diku3")
     )
 
 //    stage('[Helm] Deploy mgr-*') {
