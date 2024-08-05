7e23969bb5fd5ca67dadb4065374f5e7bbdeaa1e
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-05 20:07:37 +0300

    RANCHER-1359-test. Changes were made for test purposes.42

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -59,10 +59,10 @@ void call(Map params, boolean releaseVersion = true) {
 //        sh("curl https://raw.githubusercontent.com/folio-org/pipelines-shared-library/RANCHER-1334-Q/resources/eureka/yarn.lock -o yarn.lock")
 //        sh("curl https://raw.githubusercontent.com/folio-org/pipelines-shared-library/RANCHER-1334-Q/resources/eureka/stripes.modules.js -o stripes.modules.js")
 
-        input message: "Let's wait"
-
         println("Parameters for UI:\n${JsonOutput.prettyPrint(JsonOutput.toJson(params))}")
         writeFile file: 'stripes.config.js', text: make_tpl(readFile(file: 'stripes.config.js', encoding: "UTF-8") as String, params), encoding: 'UTF-8'
+
+        input message: "Let's wait"
       }
     }
   }
