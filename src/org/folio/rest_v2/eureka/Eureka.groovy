d7897d3b7c4f55e9ee8a92e2141b85dc40d7777f
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-14 05:15:44 +0300

    RANCHER-1359. The Eureka class has been modified.

--- src/org/folio/rest_v2/eureka/Eureka.groovy
+++ src/org/folio/rest_v2/eureka/Eureka.groovy
@@ -1,96 +1,55 @@
 package org.folio.rest_v2.eureka
 
-
-import org.folio.models.EurekaTenant
 import org.folio.models.OkapiTenant
-import org.folio.rest_v2.Common
-import org.folio.utilities.RequestException
+import org.folio.models.Role
+import org.folio.models.Tenant
+import org.folio.models.User
+import org.folio.rest_v2.eureka.kong.Permissions
+import org.folio.rest_v2.eureka.kong.Tenants
+import org.folio.rest_v2.eureka.kong.UserGroups
+import org.folio.rest_v2.eureka.kong.Users
 
-class Eureka extends Common {
+class Eureka extends Base {
 
   private Kong kong
 
-  /**
-   * EurekaTenant object contains Master Tenant configuration for Eureka.
-   * @attribute 'tenantId'     Default: "master"
-   * @attribute 'clientId'     Default: "folio-backend-admin-client"
-   */
-  public EurekaTenant masterTenant
+  Eureka(def context, Kong kong, boolean debug = false) {
+    super(context, debug)
 
-  Eureka(Object context, String kongUrl, String keycloakUrl, boolean debug = false) {
-    super(context, kongUrl, debug)
+    this.kong = kong
+  }
 
-    this.kong = new Kong(context, kongUrl, keycloakUrl, debug)
+  void createTenantFlow(OkapiTenant tenant, List<String> applications) {
+    Tenant eurekaTenant = (kong as Tenants).createTenant(tenant)
+
+    (kong as Tenants).enableApplicationsOnTenant(eurekaTenant, applications)
+
+    //create tenant admin user
+    createUserFlow(eurekaTenant, tenant.adminUser
+      , new Role(name: "adminRole", desc: "Admin role")
+      , (kong as Permissions).getCapabilitiesId(tenant)
+      , (kong as Permissions).getCapabilitySetsId(tenant))
+  }
+
+  void createUserFlow(Tenant tenant, User user, Role role, List<String> permissions, List<String> permissionSets) {
+    user.patronGroup = (kong as UserGroups).createUserGroup(tenant, user.patronGroup)
+    user = (kong as Users).createUser(tenant, user)
+    (kong as Users).setUpdatePassword(tenant, user)
+
+    role = (kong as Permissions).createRole(tenant, role)
+    (kong as Permissions).assignCapabilitiesToRole(tenant, role, permissions)
+      .assignCapabilitySetsToRole(tenant, role, permissionSets)
+      .assignRolesToUser(tenant, user, [role])
   }
 
   void initializeFromScratch(Map<String, OkapiTenant> tenants, boolean enableConsortia) {
-    //TODO
-  }
-
-  void createTenant(EurekaTenant tenant) {
-    // Get Authorization Headers for Master Tenant from Keycloak
-    Map<String, String> headers = getHttpHeaders(masterTenant)
-
-    Map body = [
-      name: tenant.tenantId,
-      description: tenant.tenantDescription
-    ]
-
-    logger.info("Creating tenant ${tenant.tenantId}...")
-
-    // Run POST request to create a new tenant
-    restClient.post(tenant.tenantManagerUrl, body, headers)
-
-    logger.info("Tenant (${tenant.tenantId}) successfully created")
-  }
-
-  boolean isTenantExist(String endpointUrl, String tenantId, Map<String, String> httpHeaders) {
-    String tenantUrl = "${endpointUrl}/${tenantId}"
-
-    try {
-      restClient.get(tenantUrl, httpHeaders)
-      logger.info("Tenant ${tenantId} exists")
-      return true
-    } catch (RequestException e) {
-        logger.warning("statusCode: ${e.statusCode}: Not able to check tenant ${tenantId} existence: ${e.getMessage()}")
-        return false
+    tenants.each { tenantId, tenant ->
+      createTenantFlow(tenant,
+        [
+          "app-platform-minimal-1.0.0-SNAPSHOT.38"
+          , "app-platform-complete-1.0.0-SNAPSHOT.53"
+        ]
+      )
     }
-  }
-
-  def getHttpHeaders(EurekaTenant tenant) {
-    def tenantId = (tenant.tenantId == masterTenant.tenantId) ? "" : tenant.tenantId
-    def tenantToken = getAuthToken(tenant.keycloakUrl, tenant.tenantId, tenant.clientId, tenant.clientSecret)
-    return getAuthHeaders(tenantId, tenantToken)
-  }
-
-  String getAuthToken(String keycloakUrl, String tenantId, String clientId, String clientSecret) {
-    logger.info("Getting access token from Keycloak service")
-
-    String url = "${keycloakUrl}/realms/${tenantId}/protocol/openid-connect/token"
-    Map<String,String> headers = ['Content-Type':'application/x-www-form-urlencoded']
-    String requestBody = "client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials"
-
-    def response = restClient.post(url, requestBody, headers).body
-    logger.info("Access token received successfully from Keycloak service")
-
-    return response['access_token']
-  }
-
-  Map<String,String> getAuthHeaders(String tenantId, String token) {
-    Map<String,String> headers = [:]
-
-    if (tenantId != null && !tenantId.isEmpty()) {
-      headers.putAll(['x-okapi-tenant': tenantId])
-    }
-
-    if (token != null && !token.isEmpty()) {
-      headers.putAll(['Authorization' : "Bearer ${token}"])
-    }
-
-    if (!headers.isEmpty()) {
-      logger.info("Auth HTTP Headers are populated")
-    }
-
-    return headers
   }
 }
