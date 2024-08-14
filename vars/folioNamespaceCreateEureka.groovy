82205f26e2a9916acb993639184291f1b04f0e03
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-14 17:44:19 +0300

    RANCHER-1359. The new constructor has been added to the Eureka class to simplify its creating process. Fix.1

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -3,7 +3,6 @@ import org.folio.models.*
 import org.folio.models.parameters.CreateNamespaceParameters
 import org.folio.rest.GitHubUtility
 import org.folio.rest_v2.eureka.Eureka
-import org.folio.rest_v2.eureka.Kong
 
 import static groovy.json.JsonOutput.prettyPrint
 import static groovy.json.JsonOutput.toJson
