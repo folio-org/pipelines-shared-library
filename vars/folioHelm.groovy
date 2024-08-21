31f72aa5093db182588629313015974e0e587c56
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-08-21 14:47:29 +0300

    RANCHER-1358: remove debug code

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -6,25 +6,19 @@ import org.folio.utilities.Logger
 import java.time.LocalDateTime
 
 void withK8sClient(Closure closure) {
-  println("in withK8sClient")
   withCredentials([[$class           : 'AmazonWebServicesCredentialsBinding',
                     credentialsId    : Constants.AWS_CREDENTIALS_ID,
                     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-    println("in withK8sClient/withCredentials")
     docker.image(Constants.DOCKER_K8S_CLIENT_IMAGE).inside("-u 0:0 --entrypoint=") {
-      println("in withK8sClient/docker.image.inside")
-//      closure()
+      closure()
     }
   }
 }
 
 void withKubeConfig(String clusterName, Closure closure) {
-  println("in withKubeConfig")
   withK8sClient {
-    println("in withKubeConfig/withK8sClient")
     awscli.getKubeConfig(Constants.AWS_REGION, clusterName)
-    println("with Helm Repo")
     addHelmRepository(Constants.FOLIO_HELM_V2_REPO_NAME, Constants.FOLIO_HELM_V2_REPO_URL, true)
     closure.call()
   }
