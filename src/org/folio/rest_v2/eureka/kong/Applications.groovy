ab26d247e0fab2f47b47696412181188823a091a
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-29 15:40:09 +0300

    RANCHER-1359. The Applications API class has been fully implemented.

--- src/org/folio/rest_v2/eureka/kong/Applications.groovy
+++ src/org/folio/rest_v2/eureka/kong/Applications.groovy
@@ -1,8 +1,6 @@
 package org.folio.rest_v2.eureka.kong
 
 import com.cloudbees.groovy.cps.NonCPS
-import org.folio.models.Tenant
-import org.folio.models.User
 import org.folio.rest_v2.eureka.Keycloak
 import org.folio.rest_v2.eureka.Kong
 
@@ -20,95 +18,81 @@ class Applications extends Kong{
     this(kong.context, kong.kongUrl, kong.keycloak, kong.restClient.debugValue())
   }
 
-  User registerApplication(Map descriptors) {
-    logger.info("Creating user for ${tenant.tenantName}...")
+  String registerApplication(def jsonAppDefinition) {
+    logger.info("Register application \"${jsonAppDefinition.id}\"")
 
-    Map<String, String> headers = getTenantHttpHeaders(tenant)
+    Map<String, String> headers = getMasterHttpHeaders()
 
-    Map body = user.toMap()
-
-    def response = restClient.post(generateUrl("/users-keycloak/users"), body, headers)
+    def response = restClient.post(generateUrl("/applications"), jsonAppDefinition, headers, [201, 409])
+    String contentStr = response.body.toString()
     Map content = response.body as Map
 
+    if (response.responseCode == 409) {
+      if (contentStr.contains("Application descriptor already created")) {
+        logger.info("""
+          Application \"${jsonAppDefinition.id}\" already exists
+          Status: ${response.responseCode}
+          Response content:
+          ${contentStr}""")
+
+        // Check twice
+        def app = getRegisteredApplication(jsonAppDefinition.id as String)
+        return app.id
+      } else {
+        logger.error("""
+          The application registering result
+          Status: ${response.responseCode}
+          Response content:
+          ${contentStr}""")
+
+        throw new Exception("Build failed: " + contentStr)
+      }
+    }
+
     logger.info("""
-      Info on the newly created user \"${content.id}\"
+      Info on the newly created application \"${jsonAppDefinition.id}\"
       Status: ${response.responseCode}
       Response content:
       ${content.toString()}""")
 
-    return User.getUserFromContent(content, tenant, this as UserGroups)
+    return content.id
   }
 
-  User getUser(Tenant tenant, String userId){
-    return getUsers(tenant, userId)[0]
+  def getRegisteredApplication(String appId, boolean fullInfo = false){
+    return getRegisteredApplications("id==$appId", fullInfo)[0]
   }
 
-  User getUserByName(Tenant tenant, String name){
-    return getUsers(tenant, "", "name==${name}")[0]
+  boolean isApplicationRegistered(String appId) {
+    return getRegisteredApplications("id==$appId").size() > 0
   }
 
-  boolean isUserExist(Tenant tenant, String userId) {
-    return getUser(tenant, userId) ? true : false
-  }
+  List getRegisteredApplications(String query = "", boolean fullInfo = false, int limit = 500){
+    logger.info("Get application(s)${query ? " with query=${query}" : ""}...")
 
-  List<User> getUsers(Tenant tenant, String userId = "", String query = ""){
-    logger.info("Get users${userId ? " with ,userId=${userId}" : ""}${query ? " with query=${query}" : ""} for tenant ${tenant.tenantName}...")
+    Map<String, String> headers = getMasterHttpHeaders()
 
-    Map<String, String> headers = getTenantHttpHeaders(tenant)
-
-    String url = generateUrl("/users${userId ? "/${userId}" : ""}${query ? "?query=${query}" : ""}")
+    String url = generateUrl("/applications?${query ? "query=$query" : ""}&fullInfo=$fullInfo&limit=$limit")
 
     Map response = restClient.get(url, headers).body as Map
 
     if (response.totalRecords > 0) {
-      logger.debug("Found users: ${response.users}")
-      List<User> users = []
-      response.users.each { userContent ->
-        users.add(User.getUserFromContent(userContent as Map, tenant, this as UserGroups))
-      }
-      return users
+      logger.debug("Found application(s): ${response.applicationDescriptors}")
+      return response.applicationDescriptors as List
     } else {
-      logger.debug("Buy the url ${url} user(s) not found")
+      logger.debug("By the url ${url} registered application(s) not found")
       logger.debug("HTTP response is: ${response}")
-      throw new Exception("User(s) not found")
+      throw new Exception("Application(s) is(are) not registered")
     }
   }
 
-  User setUpdatePassword(Tenant tenant, User user){
-    logger.info("Setting or updating user password for user ${user.username}(${user.uuid}) for ${tenant.tenantName}...")
+  Applications registerModules(def jsonModuleList) {
+    logger.info("Register module list...")
 
-    Map<String, String> headers = getTenantHttpHeaders(tenant)
+    Map<String, String> headers = getMasterHttpHeaders()
 
-    Map body = [
-      "username": user.username,
-      "userId": user.uuid,
-      "password": user.password.getPlainText()
-    ]
+    restClient.post(generateUrl("/modules/discovery"), jsonAppDefinition, headers)
 
-    def response = restClient.post(generateUrl("/authn/credentials"), body, headers, [201, 400])
-    Map content = response.body as Map
-
-    if (response.responseCode == 400 && content.toString().contains("already exists credentials")) {
-      logger.info("""
-        Credential for user \"${user.uuid}\" already exists
-        Status: ${response.responseCode}
-        Response content:
-        ${content.toString()}""")
-
-      return setUpdatePassword(tenant, unsetPassword(tenant, user))
-     }
-
-    return user
-  }
-
-  User unsetPassword(Tenant tenant, User user){
-    logger.info("Unset user password for user ${user.username}(${user.uuid}) for ${tenant.tenantName}...")
-
-    Map<String, String> headers = getTenantHttpHeaders(tenant)
-
-    restClient.delete(generateUrl("/authn/credentials?userId=${user.uuid}"), headers)
-
-    return user
+    return this
   }
 
   @NonCPS
