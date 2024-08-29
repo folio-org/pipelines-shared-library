b3c8ec4b78f8a4b4f728d3231eb39aaa48810704
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-29 13:45:10 +0300

    RANCHER-1359. The new DTO class has been added as a temporary sollution fixing the Tenant(s) classes cast issue.

---
+++ src/org/folio/models/DTO.groovy
@@ -0,0 +1,24 @@
+package org.folio.models
+
+import org.codehaus.groovy.runtime.InvokerHelper
+
+class DTO {
+  <T> T convertTo(Class<T> classTo){
+    T converted = classTo.getDeclaredConstructor().newInstance()
+
+    InvokerHelper.setProperties(converted, properties)
+
+    return converted
+  }
+
+  static <T, K extends DTO> Map<? extends String, T> convertMapTo(Map<? extends String, K> mapToConvert
+                                                                  , Class<T> classTo){
+    Map<? extends String, T> convertedMap = [:]
+
+    mapToConvert.each {key, value ->
+      convertedMap.put(key, value.convertTo(classTo))
+    }
+
+    return convertedMap
+  }
+}
