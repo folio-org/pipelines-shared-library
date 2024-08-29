03c6a8f8d4dd9324fa5ab99216f58b939464dcaf
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-29 22:33:32 +0300

    RANCHER-1359-test. Changes which made for test purposes have been removed.3

--- src/org/folio/models/DTO.groovy
+++ src/org/folio/models/DTO.groovy
@@ -6,7 +6,7 @@ class DTO {
 
   DTO(){}
 
-  public <T> T convertTo(Class<T> classTo){
+  <T> T convertTo(Class<T> classTo){
     T converted = classTo.getDeclaredConstructor().newInstance()
 
     InvokerHelper.setProperties(converted, properties)
@@ -14,14 +14,14 @@ class DTO {
     return converted
   }
 
-//  static <T, K extends DTO> Map<? extends String, T> convertMapTo(Map<? extends String, K> mapToConvert
-//                                                                  , Class<T> classTo){
-//    Map<? extends String, T> convertedMap = [:]
-//
-//    mapToConvert.each {key, value ->
-//      convertedMap.put(key, value.convertTo(classTo))
-//    }
-//
-//    return convertedMap
-//  }
+  static <T, K extends DTO> Map<? extends String, T> convertMapTo(Map<? extends String, K> mapToConvert
+                                                                  , Class<T> classTo){
+    Map<? extends String, T> convertedMap = [:]
+
+    mapToConvert.each {key, value ->
+      convertedMap.put(key, value.convertTo(classTo))
+    }
+
+    return convertedMap
+  }
 }
