7a4ed0f89c6a4258eee4e3d9dd4e78849ff9abfc
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-08-31 16:28:53 +0300

    RANCHER-1359-test. Changes were made for test purposes.155

--- vars/folioEurekaAppGenerator.groovy
+++ vars/folioEurekaAppGenerator.groovy
@@ -27,40 +27,52 @@ def generateApplicationDescriptor(String appName, Map<String, String> moduleList
   }
 }
 
-def setModuleLatestVersion(def template, Map<String, String> moduleList){
+def setTemplateModuleLatestVersion(def template, Map<String, String> moduleList){
   logger.info("Updated template latest module version with exact value...")
-
-  List updatedModules = template.modules
-
-  logger.info("""
-  Default template module list:
-  $updatedModules
-  """)
 
   logger.info("""
   Module Map with all modules and exact version:
   $moduleList
   """)
 
-  template.modules.eachWithIndex{module, index ->
-    logger.debug("Index: $index Module: $module")
-    logger.debug("Module.name: $module.name Module.version: $module.version")
-    logger.debug("updatedModules[index] ${updatedModules[index]}")
-    logger.debug("updatedModules[index].name ${updatedModules[index].name} updatedModules[index].name ${updatedModules[index].version}")
-    logger.debug("moduleList[module.name] ${moduleList[module.name]}")
+  logger.info("""
+  Default template backend module list:
+  $template.modules
+  """)
 
+  template.modules = setModuleLatestVersion(template.modules, moduleList)
+
+  logger.info("""
+  Updated backend module list with latest version:
+  $template.modules
+  """)
+
+  logger.info("""
+  Default template UI module list:
+  $template.uiModules
+  """)
+
+  template.uiModules = setModuleLatestVersion(template.uiModules, moduleList)
+
+  logger.info("""
+  Updated UI module list with latest version:
+  $template.uiModules
+  """)
+
+  return template
+}
+
+def setModuleLatestVersion(def templateModules, Map<String, String> moduleList){
+  logger.info("Updated latest module version with exact value...")
+
+  def updatedModules = templateModules
+
+  templateModules.eachWithIndex{module, index ->
     if(!moduleList[module.name])
       logger.info("Module Map with all modules and exact version doesn't contain $module.name")
     else
       updatedModules[index].version = module.version.trim() == "latest" ? moduleList[module.name] : module.version
   }
 
-  logger.info("""
-  Updated module list with latest version:
-  $updatedModules
-  """)
-
-  template.modules = updatedModules
-
-  return template
+  return updatedModules
 }
