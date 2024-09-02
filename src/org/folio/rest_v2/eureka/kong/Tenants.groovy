7f00c036efbda7964b6925798cbe120931b10d10
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-09-02 21:40:12 +0300

    RANCHER-1359-test. Changes were made for test purposes.201

--- src/org/folio/rest_v2/eureka/kong/Tenants.groovy
+++ src/org/folio/rest_v2/eureka/kong/Tenants.groovy
@@ -73,7 +73,7 @@ class Tenants extends Kong{
 
     logger.debug("Tenants.createTenant before EurekaTenant.getTenantFromContent(content)")
 
-//    EurekaTenant ttt = EurekaTenant.getTenantFromContent(content)
+    EurekaTenant ttt = EurekaTenant.getTenantFromContent(content)
 
     logger.debug("Tenants.createTenant after EurekaTenant.getTenantFromContent(content)")
 
@@ -81,7 +81,9 @@ class Tenants extends Kong{
 
     logger.debug("Tenants.createTenant after retrieveTenantClientSecret")
 
-    return tenant
+    ttt.withClientSecret(Secret.fromString("sdfs"))
+
+    return ttt
 //      .withClientSecret(Secret.fromString(sss))
   }
 
