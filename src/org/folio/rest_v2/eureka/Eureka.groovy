09f9854c381832c8a7886e8fe27445cc1e651aab
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-09-02 22:11:20 +0300

    RANCHER-1359-test. Changes were made for test purposes.210

--- src/org/folio/rest_v2/eureka/Eureka.groovy
+++ src/org/folio/rest_v2/eureka/Eureka.groovy
@@ -38,6 +38,8 @@ class Eureka extends Base {
     tenant.withUUID(createdTenant.getUuid())
       .withClientSecret(retrieveTenantClientSecretFromAWSSSM(tenant))
 
+    logger.debug("createTenantFlow tenant password: ${tenant.getClientSecret().getPlainText()}")
+
     Tenants.get(kong).enableApplicationsOnTenant(tenant)
 
     //create tenant admin user
