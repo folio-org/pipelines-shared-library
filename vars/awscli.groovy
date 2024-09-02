1e2815536753d69036054159363c758d8d989c45
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-09-02 20:57:39 +0300

    RANCHER-1359-test. Changes were made for test purposes.191

--- vars/awscli.groovy
+++ vars/awscli.groovy
@@ -84,6 +84,11 @@ void updateSsmParameter(String awsRegion, String awsParameterName, String latest
  */
 String getSsmParameterValue(String awsRegion, String awsParameterName, boolean withDecryption = false) {
   println("I'm in awscli.getSsmParameterValue")
+
+  def ttt = sh(script: "aws ssm get-parameter --name ${awsParameterName} --region ${awsRegion} --query 'Parameter.Value' --output text ${withDecryption ? '--with-decryption' : ''}", returnStdout: true)
+
+  println("I'm in awscli.getSsmParameterValue secret: $ttt")
+
   return sh(script: "aws ssm get-parameter --name ${awsParameterName} --region ${awsRegion} --query 'Parameter.Value' --output text ${withDecryption ? '--with-decryption' : ''}", returnStdout: true).trim()
 }
 
