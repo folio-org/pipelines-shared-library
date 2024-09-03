1df94e81337287ce0a9099202f8bfc64c3e0e2be
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-03 17:11:12 +0600

    RANCHER-1359-test keycloak healthcheck path correction

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -159,7 +159,7 @@ ingress:
     alb.ingress.kubernetes.io/group.name: "${local.group_name}"
     alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
     alb.ingress.kubernetes.io/success-codes: 200-399
-    alb.ingress.kubernetes.io/healthcheck-path: /health/ready
+    alb.ingress.kubernetes.io/healthcheck-path: /
     alb.ingress.kubernetes.io/healthcheck-port: "8080"
 EOF
   ]
