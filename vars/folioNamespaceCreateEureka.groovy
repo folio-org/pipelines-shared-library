88b45e570ec9a486e5bdba44ff9b3549e420db63
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-05 20:26:14 +0600

    RANCHER-1359-test edge modules include

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -89,9 +89,6 @@ void call(CreateNamespaceParameters args) {
     //TODO: Temporary solution. Unused by Eureka modules have been removed.
     namespace.getModules().removeModule('mod-login')
     namespace.getModules().removeModule('mod-authtoken')
-    namespace.getModules().removeModule('edge-inventory')
-    namespace.getModules().removeModule('edge-erm')
-    namespace.getModules().removeModule('edge-users')
 
     namespace.addTenant(
       folioDefault.tenants()[namespace.getDefaultTenantId()]
