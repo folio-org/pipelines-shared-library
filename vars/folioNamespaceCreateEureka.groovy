d53957c9119ff011cee376c8664879484bb42dc3
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-09-07 09:43:51 +0300

    RANCHER-1359-test. Changes were made for test purposes.305

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -31,29 +31,29 @@ void call(CreateNamespaceParameters args) {
     tfConfig.addVar('pg_version', args.pgVersion)
     tfConfig.addVar('eureka', args.eureka)
 
-//    stage('[Terraform] Provision') {
-//      folioTerraformFlow.manageNamespace('apply', tfConfig)
-//    }
-//
-//    if (args.greenmail) {
-//      stage('[Helm] Deploy greenmail') {
-//        folioHelm.withKubeConfig(namespace.getClusterName()) {
-//          folioHelmFlow.deployGreenmail(namespace.getNamespaceName())
-//        }
-//      }
-//    }
-//
-//    if (args.mockServer) {
-//      stage('[Helm] Deploy mock-server') {
-//        folioHelm.withKubeConfig(namespace.getClusterName()) {
-//          folioHelmFlow.deployMockServer(namespace)
-//        }
-//      }
-//    }
-//
-//    if (args.namespaceOnly) {
-//      return
-//    }
+    stage('[Terraform] Provision') {
+      folioTerraformFlow.manageNamespace('apply', tfConfig)
+    }
+
+    if (args.greenmail) {
+      stage('[Helm] Deploy greenmail') {
+        folioHelm.withKubeConfig(namespace.getClusterName()) {
+          folioHelmFlow.deployGreenmail(namespace.getNamespaceName())
+        }
+      }
+    }
+
+    if (args.mockServer) {
+      stage('[Helm] Deploy mock-server') {
+        folioHelm.withKubeConfig(namespace.getClusterName()) {
+          folioHelmFlow.deployMockServer(namespace)
+        }
+      }
+    }
+
+    if (args.namespaceOnly) {
+      return
+    }
 
     //Set install configuration
     String defaultTenantId = 'diku'
