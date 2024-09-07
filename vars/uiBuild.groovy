411b83d31bd9d7832642006a8c68bd641e423935
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-09-07 21:37:01 +0300

    RANCHER-1359-test. Changes were made for test purposes.317

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -70,8 +70,6 @@ void call(Map params, boolean releaseVersion = false) {
               "-f docker/Dockerfile  " +
               "."
           )
-
-          input "Check ui build"
           image.push()
         }
       }
@@ -83,8 +81,6 @@ void call(Map params, boolean releaseVersion = false) {
 
   if (params.eureka) {
     stage('KC and UI') {
-      input "Wait for tenant..."
-
       RestClient client = new RestClient(this)
       Map headers = ['Content-Type': 'application/x-www-form-urlencoded']
       def body = "grant_type=password&username=admin&password=SecretPassword&client_id=admin-cli"
