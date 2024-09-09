54b129651e28cd6ab52c04d94d89cd4f444ebd44
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-09 19:30:46 +0600

    kafka config adjust

--- terraform/rancher/project/kafka.tf
+++ terraform/rancher/project/kafka.tf
@@ -21,9 +21,13 @@ resource "helm_release" "kafka" {
 image:
   tag: 3.5
 config: |-
+  listeners=PLAINTEXT://:9092
+  advertised.listeners=PLAINTEXT://"kafka-${rancher2_namespace.this.name}-zookeeper:9092"
   heartbeat.interval.ms=600000
   max.poll.interval.ms=600000
   session.timeout.ms=600000
+  zookeeper.connect="kafka-${rancher2_namespace.this.name}-zookeeper"
+  zookeeper.connection.timeout.ms=6000
 metrics:
   kafka:
     enabled: true
