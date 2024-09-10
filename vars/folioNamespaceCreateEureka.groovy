f020d08009511a4609beaf16c171b8cf10df96e0
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-11 04:09:38 +0600

    RANCHER-1359-test ASG adjust v1.0.3

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -132,8 +132,7 @@ void call(CreateNamespaceParameters args) {
         stage('[ASG] configure') {
             folioHelm.withKubeConfig(namespace.getClusterName()) {
                 def asg_json = sh(script: "aws autoscaling describe-auto-scaling-groups --filters \"Name=tag:\"eks:cluster-name\",Values=${namespace.getClusterName()}\" --region ${Constants.AWS_REGION}", returnStdout: true)
-                def asg_data = readJSON file: asg_json
-                sh(script: "aws autoscaling set-desired-capacity --auto-scaling-group-name ${asg_data.AutoScalingGroupName} --desired-capacity ${(asg_data.DesiredCapacity).toInt() + 1} --region ${Constants.AWS_REGION}")
+                sh(script: "aws autoscaling set-desired-capacity --auto-scaling-group-name ${asg_json.AutoScalingGroupName} --desired-capacity ${(asg_json.DesiredCapacity).toInt() + 1} --region ${Constants.AWS_REGION}")
             }
         }
 
