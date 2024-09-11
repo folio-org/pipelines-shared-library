0956426ffa94504618bbfa8f292eaffacbf24d8e
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-11 14:44:07 +0600

    RANCHER-1359-test folio-etesting namespaces

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -56,12 +56,13 @@ class Constants {
                                         'volaris',
                                         'volaris-2nd',
                                         'rtr']
-  static Map AWS_EKS_NAMESPACE_MAPPING = ['folio-dev'    : AWS_EKS_DEV_NAMESPACES,
-                                          'folio-testing': AWS_EKS_TESTING_NAMESPACES,
-                                          'folio-perf'   : AWS_EKS_DEV_NAMESPACES + AWS_EKS_RELEASE_NAMESPACES,
-                                          'folio-tmp'    : AWS_EKS_TMP_NAMESPACES,
-                                          'folio-eureka' : AWS_EKS_DEV_NAMESPACES,
-                                          'folio-edev'   : AWS_EKS_DEV_NAMESPACES]
+  static Map AWS_EKS_NAMESPACE_MAPPING = ['folio-dev'      : AWS_EKS_DEV_NAMESPACES,
+                                          'folio-testing'  : AWS_EKS_TESTING_NAMESPACES,
+                                          'folio-perf'     : AWS_EKS_DEV_NAMESPACES + AWS_EKS_RELEASE_NAMESPACES,
+                                          'folio-tmp'      : AWS_EKS_TMP_NAMESPACES,
+                                          'folio-eureka'   : AWS_EKS_DEV_NAMESPACES,
+                                          'folio-edev'     : AWS_EKS_DEV_NAMESPACES,
+                                          'folio-etesting' : AWS_EKS_TESTING_NAMESPACES]
 
   static List AWS_EKS_NAMESPACE_CONFIGS = ['development',
                                            'performance',
