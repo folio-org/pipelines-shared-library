dd955d28af16c26810817ae74d413d9e347c7e4f
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-12 12:18:27 +0600

    RANCHER-1359-test KC test v2.5

--- terraform/rancher/project/keycloak.tf
+++ terraform/rancher/project/keycloak.tf
@@ -45,7 +45,7 @@ extraEnvVars:
   - name: KC_HOSTNAME
     value: "https://${local.keycloak_url}"
   - name: KC_HOSTNAME_BACKCHANNEL
-    value: "keycloak-${rancher2_namespace.this.id}.${rancher2_namespace.this.id}.svc.cluster.local"
+    value: "https://${local.keycloak_url}"
   - name: KC_HOSTNAME_STRICT
     value: "false"
   - name: KC_HOSTNAME_STRICT_HTTPS
