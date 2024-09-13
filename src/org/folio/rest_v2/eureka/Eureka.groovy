645f5ef053b399f910264daca05bdcd0cf72662a
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-09-13 11:40:39 +0300

    RANCHER-1700: configure debug run

--- src/org/folio/rest_v2/eureka/Eureka.groovy
+++ src/org/folio/rest_v2/eureka/Eureka.groovy
@@ -274,19 +274,6 @@ class Eureka extends Base {
    * @return Updated Application Descriptor as a Map
    */
   Map getUpdatedApplicationDescriptor(Map appDescriptor, FolioModule module, String buildNumber) {
-    // Update Application Descriptor with incremented Application Version
-    String currentAppVersion = appDescriptor.version
-    String newAppVersion = currentAppVersion.replaceFirst(/SNAPSHOT\.\d+/, "SNAPSHOT.${buildNumber}")
-    appDescriptor.version = newAppVersion
-    appDescriptor.id = "${appDescriptor.name}-${newAppVersion}"
-
-    // Update Application Descriptor with new Module Version
-    appDescriptor.modules.findAll { it.name == module.name }.each {
-      it.url = "${Constants.EUREKA_REGISTRY_URL}${module.name}-${module.version}"
-      it.id = "${module.name}-${module.version}"
-      it.version = module.version
-    }
-
     // Remove broken module version from the Application Descriptor
     appDescriptor.modules.eachWithIndex { mod, index ->
       if(mod.id.contains("mod-roles-keycloak-1.4.5-SNAPSHOT.121")) {
@@ -295,13 +282,26 @@ class Eureka extends Base {
       }
     }
 
-    // Remove broken module version from the Application Descriptor
-    appDescriptor.modules.eachWithIndex { mod, index ->
-      if(mod.id.contains("mod-lists-2.1.0-SNAPSHOT.95")) {
-        logger.debug("Found Broken Module: ${mod}")
-        mod.url = "https://folio-registry.dev.folio.org/_/proxy/modules/mod-lists-2.1.0-SNAPSHOT.95"
-      }
-    }
+    // Update Application Descriptor with incremented Application Version
+    String currentAppVersion = appDescriptor.version
+    String newAppVersion = currentAppVersion.replaceFirst(/SNAPSHOT\.\d+/, "SNAPSHOT.${buildNumber}")
+    appDescriptor.version = newAppVersion
+    appDescriptor.id = "${appDescriptor.name}-${newAppVersion}"
+
+//    // Update Application Descriptor with new Module Version
+//    appDescriptor.modules.findAll { it.name == module.name }.each {
+//      it.url = "${Constants.EUREKA_REGISTRY_URL}${module.name}-${module.version}"
+//      it.id = "${module.name}-${module.version}"
+//      it.version = module.version
+//    }
+
+//    // Remove broken module version from the Application Descriptor
+//    appDescriptor.modules.eachWithIndex { mod, index ->
+//      if(mod.id.contains("mod-lists-2.1.0-SNAPSHOT.95")) {
+//        logger.debug("Found Broken Module: ${mod}")
+//        mod.url = "https://folio-registry.dev.folio.org/_/proxy/modules/mod-lists-2.1.0-SNAPSHOT.95"
+//      }
+//    }
 
     logger.info("Updated Application Descriptor with new Module Version: ${module.name}-${module.version}")
 
