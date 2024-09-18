7cbce696827beaba58e44f15c19ba38d882a6a3d
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-09-18 11:46:54 +0300

    RANCHER-1700: remove not used code block

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -166,11 +166,6 @@ String generateModuleValues(RancherNamespace ns, String moduleName, String modul
 //    }
 
   if (ns instanceof EurekaNamespace) {
-//    String sidecarRepository = determineModulePlacement(
-//      "folio-module-sidecar"
-//      , ns.getModules().allModules['folio-module-sidecar']
-//    )
-
     switch (moduleName) {
     //TODO: Temporary solution just to bug avoiding workaround
       case "mod-data-export-spring":
