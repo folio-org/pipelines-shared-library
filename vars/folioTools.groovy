7678d007f05d1cc458ef0a52bca7d05cdad00ed6
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-19 15:15:34 +0600

    RANCHER-1748 kafka lag test v1.0.2

--- vars/folioTools.groovy
+++ vars/folioTools.groovy
@@ -28,14 +28,16 @@ void deleteKafkaTopics(String cluster, String namespace) {
   kubectl.deletePod('kafka')
 }
 
-static void stsKafkaLag(String cluster, String namespace, String tenantId) {
-  String lag = "kafka-consumer-groups.sh --bootstrap-server kafka-${namespace}:9092 --describe --group ${cluster}-${namespace}-mod-roles-keycloak-capability-group | grep ${tenantId} | awk '" + '''{print $6}''' + "'"
-  kubectl.runPodWithCommand("${namespace}", 'kafka-sh', 'bitnami/kafka:3.5.0')
-  kubectl.waitPodIsRunning("${namespace}", 'kafka-sh')
-  def check = kubectl.execCommand("${namespace}", 'kafka-sh', "${lag}")
-  while (check.toInteger() != 0) {
-    sleep time: 30, unit: 'SECONDS'
-    check = kubectl.execCommand("${namespace}", 'kafka-sh', "${lag}")
+void stsKafkaLag(String cluster, String namespace, String tenantId) {
+  folioHelm.withKubeConfig(cluster) {
+    String lag = "kafka-consumer-groups.sh --bootstrap-server kafka-${namespace}:9092 --describe --group ${cluster}-${namespace}-mod-roles-keycloak-capability-group | grep ${tenantId} | awk '" + '''{print $6}''' + "'"
+    kubectl.runPodWithCommand("${namespace}", 'kafka-sh', 'bitnami/kafka:3.5.0')
+    kubectl.waitPodIsRunning("${namespace}", 'kafka-sh')
+    def check = kubectl.execCommand("${namespace}", 'kafka-sh', "${lag}")
+    while (check.toInteger() != 0) {
+      sleep time: 30, unit: 'SECONDS'
+      check = kubectl.execCommand("${namespace}", 'kafka-sh', "${lag}")
+    }
   }
 }
 
