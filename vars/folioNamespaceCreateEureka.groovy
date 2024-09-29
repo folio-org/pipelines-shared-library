68437814540ebab1c2ce2aac5fc99ee32a589e5b
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-09-29 12:56:36 +0600

    RANCHER-1735-v2 modules - mgr-* v2.1.2

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -101,7 +101,7 @@ void call(CreateNamespaceParameters args) {
     namespace.getModules().removeModule('mod-login')
     namespace.getModules().removeModule('mod-authtoken')
 
-    List tenants = ['fs09000000', 'fs09000002, fs09000003', 'cs00000int', 'cs00000int_0001', 'cs00000int_0004', 'cs00000int_0005', 'cs00000int_0006']
+    List tenants = ['fs09000000', 'fs09000002', 'fs09000003', 'cs00000int', 'cs00000int_0001', 'cs00000int_0004', 'cs00000int_0005', 'cs00000int_0006']
 
     tenants.each { newTenant ->
       println("***Working on tenant: ${newTenant}")
