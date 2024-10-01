88faed8c300889ba9ad275dcfae55c9e9139ea24
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-10-01 16:57:18 +0600

    RANCHER-1359-test KONG upstream timeouts increase testing v1.0.2

--- terraform/rancher/project/kong.tf
+++ terraform/rancher/project/kong.tf
@@ -104,6 +104,8 @@ kong:
        secretKeyRef:
          name: kong-credentials
          key: KONG_PASSWORD
+   - name: KONG_UPSTREAM_TIMEOUT
+     value: "600000"
    - name: KONG_UPSTREAM_READ_TIMEOUT
      value: "600000"
    - name: KONG_UPSTREAM_KEEPALIVE_TIMEOUT
