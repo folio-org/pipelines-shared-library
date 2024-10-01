f02dfc4cc5494a77f2184d4c9fa3440a542f2795
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-10-01 12:52:00 +0600

    RANCHER-1359-test timeout increase

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -231,7 +231,7 @@ void call(CreateNamespaceParameters args) {
       int counter = 0
       retry(10) {
         // The first wait time should be at least 10 minutes due to module's long time instantiation
-        sleep time: (counter == 0 ? 5 : 2), unit: 'MINUTES'
+        sleep time: (counter == 0 ? 10 : 2), unit: 'MINUTES'
         counter++
 
         eureka.initializeFromScratch(
