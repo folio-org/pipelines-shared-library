f2f9d41113589b16b326d8cf6955d47380d19eee
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-10-11 11:17:05 +0300

    Update folioCypress.groovy

--- vars/folioCypress.groovy
+++ vars/folioCypress.groovy
@@ -1,1 +1,39 @@
-//TBD
+import org.folio.Constants
+
+void setupCommonEnvironmentVariables(String tenantUrl, String okapiUrl, String tenantId, String adminUsername, String adminPassword) {
+  env.CYPRESS_BASE_URL = tenantUrl
+  env.CYPRESS_OKAPI_HOST = okapiUrl
+  env.CYPRESS_OKAPI_TENANT = tenantId
+  env.CYPRESS_diku_login = adminUsername
+  env.CYPRESS_diku_password = adminPassword
+  env.AWS_DEFAULT_REGION = Constants.AWS_REGION
+}
+
+void runInDocker(String cypressImageVersion, String containerNameSuffix, Closure<?> closure) {
+  String containerName = "cypress-${containerNameSuffix}"
+  def containerObject
+  try {
+    docker.withRegistry("https://${Constants.ECR_FOLIO_REPOSITORY}", "ecr:${Constants.AWS_REGION}:${Constants.ECR_FOLIO_REPOSITORY_CREDENTIALS_ID}") {
+      containerObject = docker.image("732722833398.dkr.ecr.us-west-2.amazonaws.com/cypress/browsers:latest").inside("--init --name=${containerName} --entrypoint=") {
+        withCredentials([[$class           : 'AmazonWebServicesCredentialsBinding',
+                          credentialsId    : Constants.AWS_S3_SERVICE_ACCOUNT_ID,
+                          accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+          closure()
+        }
+      }
+    }
+  } catch (e) {
+    println(e)
+    if (containerName.contains('cypress-compile')) {
+      currentBuild.result = 'FAILED'
+      error('Unable to compile tests')
+    } else {
+      currentBuild.result = 'UNSTABLE'
+    }
+  } finally {
+    if (containerObject) {
+      containerObject.stop()
+    }
+  }
+}
