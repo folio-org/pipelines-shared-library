6d1032b108119d2e69c38c17e30d867654d78f7a
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-11-02 03:13:10 +0200

    RANCHER-1877. System user support has been implemented for mod-marc-migrations as well. Fix previous cherry-pick.

--- terraform/rancher/project/locals.tf
+++ terraform/rancher/project/locals.tf
@@ -14,7 +14,7 @@ locals {
 
   system_user_modules = ["mod-data-export-spring", "mod-inn-reach", "mod-search", "mod-consortia",
     "mod-remote-storage", "mod-entities-links", "mod-erm-usage-harvester", "mod-pubsub", "mod-dcb", "mod-lists",
-    "mod-linked-data", "mod-tlr", "mod-marc-migrations", "mod-linked-data", "mod-consortia-keycloak", "mod-scheduler",
+    "mod-linked-data", "mod-tlr", "mod-marc-migrations", "mod-consortia-keycloak", "mod-scheduler",
     "mod-users-keycloak", "mod-roles-keycloak"
   ]
 
