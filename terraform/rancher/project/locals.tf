ac87eac7a6a00927e24fe37a3ec73c45b93d027e
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-11-06 22:21:26 +0200

    RANCHER-1890. System user support has been added for mod-circulation-bff.

--- terraform/rancher/project/locals.tf
+++ terraform/rancher/project/locals.tf
@@ -14,8 +14,8 @@ locals {
 
   system_user_modules = ["mod-data-export-spring", "mod-inn-reach", "mod-search", "mod-consortia",
     "mod-remote-storage", "mod-entities-links", "mod-erm-usage-harvester", "mod-pubsub", "mod-dcb", "mod-lists",
-    "mod-linked-data", "mod-tlr", "mod-marc-migrations", "mod-requests-mediated", "mod-consortia-keycloak",
-    "mod-scheduler", "mod-users-keycloak", "mod-roles-keycloak"
+    "mod-linked-data", "mod-tlr", "mod-marc-migrations", "mod-requests-mediated", "mod-circulation-bff",
+    "mod-consortia-keycloak", "mod-scheduler", "mod-users-keycloak", "mod-roles-keycloak"
   ]
 
   s3_integrated_modules = ["mod-data-export", "mod-data-export-worker", "mod-data-import", "mod-lists",
