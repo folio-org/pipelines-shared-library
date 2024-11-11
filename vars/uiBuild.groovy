66a23784baba4265a147be0f8c2c68f659d0266c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-11-11 15:02:15 +0600

    RANCHER-1897 tmp solution for PoC + manual build testing v2.1

--- vars/uiBuild.groovy
+++ vars/uiBuild.groovy
@@ -44,6 +44,8 @@ void call(Map params, boolean releaseVersion = false) {
         if (params.consortia) {
           params.kongUrl = "https://ecs-${params.custom_url}" // TODO Temporary solution
           params.custom_url = "https://ecs-${params.custom_url}"
+          params.isSingleTenant = false
+          okapi_url = "ecs-${params.custom_url}"
         }
         println("Parameters for UI:\n${JsonOutput.prettyPrint(JsonOutput.toJson(params))}")
         writeFile file: 'stripes.config.js', text: make_tpl(readFile(file: 'stripes.config.js', encoding: "UTF-8") as String, params), encoding: 'UTF-8'
