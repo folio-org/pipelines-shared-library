b950b5635cf720835c7cfa9e9c56d63f69e32221
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-11-19 16:00:05 +0200

    RANCHER-1916. The Eureka specific extension has been introduced into the FolioModule and ModuleType classes.

--- src/org/folio/models/module/FolioModule.groovy
+++ src/org/folio/models/module/FolioModule.groovy
@@ -103,7 +103,7 @@ class FolioModule {
 
     // Adjust request parameters based on module type
     switch (moduleName) {
-      case ~/mod-.*/:
+      case ~/(mod-|mgr)-.*/:
         reqParams += "&preRelease=${isRelease ? 'false' : 'only'}"
         break
       case ~/folio_.*/:
@@ -150,6 +150,8 @@ class FolioModule {
         return ModuleType.EDGE
       case ~/^folio_.*/:
         return ModuleType.FRONTEND
+      case ~/^mgr-.*/:
+        return ModuleType.MGR
       case 'okapi':
         return ModuleType.OKAPI
       default:
