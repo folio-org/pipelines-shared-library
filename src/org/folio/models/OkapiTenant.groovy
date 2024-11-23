4a0e344d62a129aa3337c77de10deb71a17c4b1d
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-11-23 13:06:39 +0200

    RANCHER-1921-test. Changes were made for test purposes.19

--- src/org/folio/models/OkapiTenant.groovy
+++ src/org/folio/models/OkapiTenant.groovy
@@ -94,7 +94,7 @@ class OkapiTenant extends DTO{
    * @return The OkapiTenant object for method chaining.
    */
   OkapiTenant withInstallJson(Object installJson, def context = null) {
-    this.modules.setInstallJsonObject(installJson)
+    this.modules.setInstallJsonObject(installJson, context)
 
     if(context)
       context.println("I'm in the OkapiTenant.withInstallJson after the this.modules.setInstallJsonObject")
