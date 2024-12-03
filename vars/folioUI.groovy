2aa373a0646b5d8901fdb58dacccfdb59f7b6ef6
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2024-12-03 21:46:36 +0200

    RANCHER-1838 Create logic to install mod-linked-data and ui-ld-folio-wrapper on eureka-based environment
    Jira: https://folio-org.atlassian.net/browse/RANCHER-1838

--- vars/folioUI.groovy
+++ vars/folioUI.groovy
@@ -1,11 +1,16 @@
 #!groovy
-
+import com.cloudbees.groovy.cps.NonCPS
+import groovy.json.JsonOutput
+import groovy.text.StreamingTemplateEngine
 import org.folio.Constants
 import org.folio.models.OkapiTenant
 import org.folio.models.RancherNamespace
 import org.folio.models.TenantUi
+import org.folio.models.module.FolioModule
+import org.folio.utilities.RestClient
 
-void build(String okapiUrl, OkapiTenant tenant) {
+void build(String okapiUrl, OkapiTenant tenant, boolean isEureka = false, String kongDomain = ''
+           , String keycloakDomain = '', boolean enableEcsRequests = false) {
   final String baseDir = "platform-complete-${tenant.getTenantId()}"
   TenantUi tenantUi = tenant.getTenantUi()
 
@@ -23,6 +28,42 @@ void build(String okapiUrl, OkapiTenant tenant) {
   stage('[UI] Add folio extensions') {
     if (tenantUi.getCustomUiModules()) {
       dir(baseDir) {
+        //TODO refactoring and reviewing required.
+        if (isEureka) {
+          String tenantId = tenant.getTenantId()
+          okapiUrl = "https://${kongDomain}"
+          sh "cp -R -f eureka-tpl/* ."
+          Map binding = [
+            kongUrl          : "https://${kongDomain}",
+            tenantUrl        : "https://${tenantUi.getDomain()}",
+            keycloakUrl      : "https://${keycloakDomain}",
+            hasAllPerms      : false,
+            isSingleTenant   : true,
+            tenantOptions    : """{${tenantId}: {name: "${tenantId}", clientId: "${tenantId}-application"}}""",
+            enableEcsRequests: enableEcsRequests
+          ]
+
+          if (tenantUi.getCustomUiModules()*.name.contains('folio_consortia-settings')) {
+            binding.kongUrl = "https://ecs-${kongDomain}"
+            binding.isSingleTenant = false
+            okapiUrl = binding.kongUrl
+          }
+
+          writeFile(file: 'stripes.config.js'
+            , text: make_tpl(readFile(file: 'stripes.config.js', encoding: "UTF-8") as String, binding)
+            , encoding: 'UTF-8')
+
+          List eurekaCustomUiModules = ["folio_authorization-policies",
+                                        "folio_authorization-roles",
+                                        "folio_plugin-select-application"]
+          eurekaCustomUiModules.each { moduleName ->
+            FolioModule uiModule = new FolioModule()
+            uiModule.setName(moduleName)
+            uiModule.setVersion(">=1.0.0")
+            tenantUi.customUiModules.add(uiModule)
+          }
+        }
+
         List uiModulesToAdd = _updatePackageJsonFile(tenantUi)
         _updateStripesConfigJsFile(uiModulesToAdd)
       }
@@ -30,7 +71,8 @@ void build(String okapiUrl, OkapiTenant tenant) {
   }
   stage('[UI] Build and Push') {
     dir(baseDir) {
-      docker.withRegistry("https://${Constants.ECR_FOLIO_REPOSITORY}", "ecr:${Constants.AWS_REGION}:${Constants.ECR_FOLIO_REPOSITORY_CREDENTIALS_ID}") {
+      docker.withRegistry("https://${Constants.ECR_FOLIO_REPOSITORY}"
+        , "ecr:${Constants.AWS_REGION}:${Constants.ECR_FOLIO_REPOSITORY_CREDENTIALS_ID}") {
         def image = docker.build(
           tenantUi.getImageName(),
           "--build-arg OKAPI_URL=${okapiUrl} " +
@@ -46,6 +88,39 @@ void build(String okapiUrl, OkapiTenant tenant) {
   stage('[UI] Cleanup') {
     common.removeImage(tenantUi.getImageName())
   }
+
+  //TODO Refactoring required
+  stage('Update keycloak redirect') {
+    if (isEureka) {
+      String tenantId = tenant.getTenantId()
+      RestClient client = new RestClient(this)
+      Map headers = ['Content-Type': 'application/x-www-form-urlencoded']
+      String tokenUrl = "https://${keycloakDomain}/realms/master/protocol/openid-connect/token"
+      String tokenBody = "grant_type=password&username=admin&password=SecretPassword&client_id=admin-cli"
+
+      Map response = client.post(tokenUrl, tokenBody, headers).body
+      String token = response['access_token']
+      headers.put("Authorization", "Bearer ${token}")
+
+      String getRealmUrl = "https://${keycloakDomain}/admin/realms/${tenantId}/clients?clientId=${tenantId}-application"
+      List realm = client.get(getRealmUrl, headers).body
+
+      String updateRealmUrl = "https://${keycloakDomain}/admin/realms/${tenantId}/clients/${realm['id'].get(0)}"
+      headers['Content-Type'] = 'application/json'
+      String tenantUrl = "https://${tenantUi.getDomain()}"
+      Map updateContent = [
+        rootUrl                     : tenantUrl,
+        baseUrl                     : tenantUrl,
+        adminUrl                    : tenantUrl,
+        redirectUris                : ["${tenantUrl}/*", "http://localhost:3000/*"], //Requested by AQA Team
+        webOrigins                  : ["/*"],
+        authorizationServicesEnabled: true,
+        serviceAccountsEnabled      : true,
+        attributes                  : ['post.logout.redirect.uris': "/*##${tenantUrl}/*", login_theme: 'custom-theme']
+      ]
+      client.put(updateRealmUrl, JsonOutput.toJson(updateContent), headers)
+    }
+  }
 }
 
 void deploy(RancherNamespace namespace, OkapiTenant tenant) {
@@ -57,8 +132,9 @@ void deploy(RancherNamespace namespace, OkapiTenant tenant) {
   }
 }
 
-void buildAndDeploy(RancherNamespace namespace, OkapiTenant tenant) {
-  build("https://${namespace.getDomains()['okapi']}", tenant)
+void buildAndDeploy(RancherNamespace namespace, OkapiTenant tenant, boolean isEureka = false, String kongDomain = ''
+                    , String keycloakDomain = '', boolean enableEcsRequests = false) {
+  build("https://${namespace.getDomains()['okapi']}", tenant, isEureka, kongDomain, keycloakDomain, enableEcsRequests)
   deploy(namespace, tenant)
 }
 
@@ -118,4 +194,11 @@ private List<String> _updatePackageJsonFile(TenantUi tenantUi) {
   echo "${packageJsonFile} updated with UI modules: ${uiModulesToAdd}"
 
   return uiModulesToAdd
+}
+
+//TODO refactoring and reviewing required.
+@NonCPS
+static def make_tpl(String tpl, Map data) {
+  def ui_tpl = ((new StreamingTemplateEngine().createTemplate(tpl)).make(data)).toString()
+  return ui_tpl
 }
