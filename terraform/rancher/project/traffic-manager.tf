9f27f58ab564b2295fb1948becfe894c7cc7e04a
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-12-17 18:57:54 +0600

    .RANCHER-1971 helm update for telepresence v2.1

--- terraform/rancher/project/traffic-manager.tf
+++ terraform/rancher/project/traffic-manager.tf
@@ -3,7 +3,7 @@ resource "helm_release" "traffic-manager" {
   namespace = rancher2_namespace.this.name
   name      = "traffic-manager-${rancher2_namespace.this.name}"
   chart     = "oci://ghcr.io/telepresenceio/telepresence-oss"
-  version   = "2.20.1"
+  version   = "2.21.1"
   values = [<<-EOF
 managerRbac:
   create: true
