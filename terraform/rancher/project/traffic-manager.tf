64966f4a98c2d50a51aa6b2f9aea48776d4f2cb7
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-12-17 20:21:56 +0600

    RANCHER-1971 helm telepresence + RBAC

--- terraform/rancher/project/traffic-manager.tf
+++ terraform/rancher/project/traffic-manager.tf
@@ -4,7 +4,8 @@ resource "helm_release" "traffic-manager" {
   name      = "traffic-manager-${rancher2_namespace.this.name}"
   chart     = "oci://ghcr.io/telepresenceio/telepresence-oss"
   version   = "2.21.1"
-  values = [<<-EOF
+  values = [
+    <<-EOF
 managerRbac:
   create: true
   namespaced: true
@@ -12,4 +13,27 @@ managerRbac:
   - ${rancher2_namespace.this.name}
     EOF
   ]
+}
+
+data "rancher2_user" "cluster_this" {
+  name = var.rancher_cluster_name
+}
+
+resource "rancher2_role_template" "port_forward" {
+  name         = "port-forward-access"
+  context      = "project"
+  default_role = false
+  description  = "Terraform role for telepresence"
+  rules {
+    api_groups = ["*"]
+    resources  = ["pods/portforward"]
+    verbs      = ["create"]
+  }
+}
+
+resource "rancher2_project_role_template_binding" "access_port_forward" {
+  name             = "access-port-forward"
+  role_template_id = rancher2_role_template.port_forward.id
+  project_id       = rancher2_project.this.id
+  user_id          = data.rancher2_user.cluster_this.id
 }
