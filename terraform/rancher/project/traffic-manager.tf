b504b96581425722471c8d20370c8743cf36a276
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-12-17 18:17:30 +0600

    .RANCHER-1971 name correction

--- terraform/rancher/project/traffic-manager.tf
+++ terraform/rancher/project/traffic-manager.tf
@@ -3,7 +3,7 @@ resource "helm_release" "traffic-manager" {
   namespace = rancher2_namespace.this.name
   name      = "traffic-manager-${rancher2_namespace.this.name}"
   repository = local.catalogs.wiredata
-  chart     = "telepresence-oss"
+  chart     = "telepresence"
   version   = "2.20.1"
   values = [<<-EOF
 image:
