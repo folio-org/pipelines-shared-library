71cd71ebb91dcf6b356ae168be3808cfed00f7de
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-12-17 18:13:57 +0600

    .RANCHER-1971 catalog include

--- terraform/rancher/project/traffic-manager.tf
+++ terraform/rancher/project/traffic-manager.tf
@@ -2,6 +2,7 @@ resource "helm_release" "traffic-manager" {
   count     = 1
   namespace = rancher2_namespace.this.name
   name      = "traffic-manager-${rancher2_namespace.this.name}"
+  repository = local.catalogs.wiredata
   chart     = "telepresence-oss"
   version   = "2.20.1"
   values = [<<-EOF
