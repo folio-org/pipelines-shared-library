dc783bb73967f2f3f81cd694504f6538084a895a
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-12-17 17:58:09 +0600

    .RANCHER-1971 initial commit

---
+++ terraform/rancher/project/traffic-manager.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "traffic-manager" {
+  count     = 1
+  namespace = rancher2_namespace.this.name
+  name      = "traffic-manager-${rancher2_namespace.this.name}"
+  chart     = "telepresence-oss"
+  version   = "2.20.1"
+  values = [<<-EOF
+image:
+  registry: ghcr.io/telepresenceio
+  tag: 2.20.1
+managerRbac:
+  create: true
+  namespaced: true
+  namespaces:
+  - ${rancher2_namespace.this.name}
+    EOF
+  ]
+}
