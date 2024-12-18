b2345d7b7dff011174aa67fb81418ef9ef2489e2
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-12-18 10:58:07 +0200

    Add namespaces for debug runs to folio-tmp cluster

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -12,7 +12,7 @@ class Constants {
   static String AWS_EKS_VPC_NAME = 'folio-rancher-vpc'
   static String AWS_EKS_ADMIN_USERS = 'oleksandrhaimanov,eldiiarduishenaliev,tarasspashchenko,stanislav,arsenatoyan,dmytromoroz,vasylavramenko,yaroslavishchenko,sergiimasiuk'
   static List AWS_EKS_CLUSTERS = ['folio-testing', 'folio-dev', 'folio-perf', 'folio-tmp', 'folio-etesting', 'folio-edev', 'folio-eperf', 'folio-etmp']
-  static List AWS_EKS_TMP_NAMESPACES = ['test', 'test-1', 'test-2', 'tdspora']
+  static List AWS_EKS_TMP_NAMESPACES = ['cikarate', 'cicypress', 'test', 'test-1', 'test-2', 'tdspora']
   static List AWS_EKS_TESTING_NAMESPACES = ['cypress', 'data-migration', 'ecs-snapshot', 'karate', 'snapshot', 'snapshot2', 'sprint', 'pre-bugfest', 'orchid-migration']
   static List AWS_EKS_RELEASE_NAMESPACES = ['poppy', 'quesnelia']
   static List AWS_EKS_DEV_NAMESPACES = ['aggies',
