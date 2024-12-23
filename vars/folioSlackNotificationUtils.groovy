fd57c06df1fd7a518bd9dc51faeca952fefed498
Yaroslav Ishchenko <yaroslav_ishchenko@epam.com>
2024-12-23 11:31:20 +0200

    RANCHER-1692: suck in changes for parallel execution section rendering

--- vars/folioSlackNotificationUtils.groovy
+++ vars/folioSlackNotificationUtils.groovy
@@ -116,9 +116,24 @@ String renderTeamTestResultSection(TestType type, Team team, List<IModuleExecuti
     .renderSection(team, results, existingIssuesUrl, createdIssuesUrl)
 }
 
+/**
+ * Render a section for a sub-job build result
+ * @param jobName Name of sub job
+ * @param buildNumber Number of sub job build
+ * @param buildUrl URL of sub job build
+ * @param buildStatus Status of sub job build
+ * @param errorMessage Error message for sub job build
+ * @return String Rendered section for Slack notification
+ */
 @SuppressWarnings('GrMethodMayBeStatic')
-String renderSubJobBuildResultSection(String jobName, String buildNumber, String stageName, String buildUrl, String buildStatus, String errorMessage) {
-  SlackBuildResultRenderer buildResult = SlackBuildResultRenderer.fromResult(buildStatus)
+String renderSubJobBuildResultSection(String jobName, String buildNumber, String buildUrl, String buildStatus, String errorMessage) {
+  def message = "*${buildNumber} - ${buildStatus}* \\n _Error message_ - `${errorMessage}`\\n"
 
-  return buildResult.renderSubJobErrorSection(jobName, buildNumber, stageName, errorMessage, buildUrl)
+  List<String> actions = [
+    SlackHelper.renderAction(buildUrl, ":page_facing_up: Check *${buildNumber}* console output")
+  ]
+
+  String section = SlackHelper.renderSection("Failed to create Environment with '${jobName}' pipeline", message, '#FF0000', actions, [])
+
+  return section
 }
