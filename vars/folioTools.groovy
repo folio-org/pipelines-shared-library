5d8c40a74120f1181c0e77f1a6bc963f1ccd2fc0
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2024-12-26 23:07:11 +0600

    Qg status fix (#756)
    * .QG-status-fix
    
    * .QG-status-fix
    
    * .QG-status-fix version bump up

--- vars/folioTools.groovy
+++ vars/folioTools.groovy
@@ -20,12 +20,12 @@ void deleteKafkaTopics(String cluster, String namespace) {
   String kafka_port = kubectl.getSecretValue(namespace, 'kafka-credentials', 'KAFKA_PORT')
   String delete_topic_command = "kafka-topics.sh --bootstrap-server ${kafka_host}:${kafka_port} --delete --topic ${cluster}-${namespace}.*"
 
-  kubectl.runPodWithCommand("${namespace}", 'kafka', 'bitnami/kafka:2.8.0')
-  kubectl.waitPodIsRunning('kafka')
+  kubectl.runPodWithCommand("${namespace}", 'kafka', 'bitnami/kafka:3.5.0')
+  kubectl.waitPodIsRunning("${namespace}", 'kafka')
   retry(3) {
-    kubectl.execCommand('kafka', delete_topic_command)
+    kubectl.execCommand("${namespace}", 'kafka', delete_topic_command)
   }
-  kubectl.deletePod('kafka')
+  kubectl.deletePod("${namespace}", 'kafka')
 }
 
 List getGitHubTeamsIds(String teams) {
