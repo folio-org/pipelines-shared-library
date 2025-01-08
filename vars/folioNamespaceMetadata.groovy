0f4b01d7e485b0df231cd6c14f92fcf85afee26b
Sergii Masiuk <sergii_masiuk@epam.com>
2025-01-08 16:07:59 +0200

    RANCHER-1778: add createmetadata stage with return

--- vars/folioNamespaceMetadata.groovy
+++ vars/folioNamespaceMetadata.groovy
@@ -1,5 +1,7 @@
+import groovy.json.JsonSlurper
 import org.folio.Constants
 import org.folio.models.parameters.CreateNamespaceParameters
+
 
 void create(CreateNamespaceParameters param) {
   def configMapName = Constants.AWS_EKS_NS_METADATA
@@ -33,7 +35,7 @@ boolean isExist(String namespace) {
     return false
   }
 }
-//temp method
+//temp method to print createNamespaceParameters
 void printParams(CreateNamespaceParameters param) {
   println "*******************METADATA******************"
   param.properties.each { key, value ->
@@ -45,6 +47,54 @@ void printParams(CreateNamespaceParameters param) {
   }
   println"*****************************************"
 }
+
+//compare with existing
+void compare(CreateNamespaceParameters param) {
+  def configMapName = Constants.AWS_EKS_NS_METADATA
+  def namespace = param.namespaceName
+  def configMapJson = sh(
+    script: "kubectl get configmap ${configMapName} -n ${namespace} -o json",
+    returnStdout: true
+  ).trim()
+  def configMapData = readJSON text: configMapJson
+  def existingData = configMapData?.data ?: [:]
+
+
+// Шаг 2: Сравниваем свойства объекта с ключами ConfigMap
+  def changes = [:] // Храним изменения
+  paramInstance.properties.each { key, value ->
+    if (key in ['class', 'metaClass']) return // Пропускаем технические свойства
+
+    def configValue = configMapData[key]
+    def objectValue = value instanceof List ? value.join("\n") : value.toString()
+
+    if (configValue != objectValue) {
+      changes[key] = [current: configValue, expected: objectValue]
+    }
+  }
+
+// Шаг 3: Выводим сообщение об изменениях
+  if (changes) {
+    println("Найдены отличия между объектом и ConfigMap:")
+    changes.each { key, diff ->
+      println("Ключ '${key}': текущее значение '${diff.current}', ожидаемое значение '${diff.expected}'")
+    }
+    def userInput = input(
+      message: 'Обнаружены изменения в ConfigMap. Продолжить обновление?',
+      ok: 'Да',
+      parameters: [
+        string(defaultValue: 'Да', description: 'Введите "Да" для продолжения или "Нет" для прерывания', name: 'response')
+      ]
+    )
+
+    if (userInput.toLowerCase() != 'да') {
+      error "Пайплайн прерван пользователем."
+    }
+
+  }
+}
+
+
 
 //void printMetadata() {
 //  def configMapName = Constants.AWS_EKS_NS_METADATA
