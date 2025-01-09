64304ea0812f90645ef2bf3a4d9adf845c31ca3a
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-01-10 04:33:51 +0600

    .kong-ui V4.11

--- terraform/rancher/project/kong.tf
+++ terraform/rancher/project/kong.tf
@@ -223,9 +223,40 @@ resource "kubernetes_service" "kong_admin_ui" {
       "app.kubernetes.io/name"      = "kong"
     }
     port {
+      name        = "kong-admin-ui"
       port        = 80
       target_port = 8002
     }
-    type = "ClusterIP"
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_ingress" "kong" {
+
+  count = var.eureka ? 1 : 0
+  metadata {
+    name      = "kong-ingress-${rancher2_namespace.this.id}"
+    namespace = rancher2_namespace.this.id
+    annotations = {
+      "kubernetes.io/ingress.class"                = "alb"
+      "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+      "alb.ingress.kubernetes.io/group.name"       = local.group_name
+      "alb.ingress.kubernetes.io/listen-ports"     = "[{\"HTTPS\":443}]"
+      "alb.ingress.kubernetes.io/success-codes"    = "200-399"
+      "alb.ingress.kubernetes.io/healthcheck-path" = "/version"
+    }
+  }
+  spec {
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service_name = "kong-admin-ui-${rancher2_namespace.this.id}"
+            service_port = "kong-admin-ui"
+          }
+        }
+      }
+    }
   }
 }
