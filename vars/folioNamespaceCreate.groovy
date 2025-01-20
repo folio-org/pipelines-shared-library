6f4361f6d4fbbb056ca1a70ea8939928f73f232b
Sergii Masiuk <sergii_masiuk@epam.com>
2025-01-20 12:48:34 +0200

    RANCHER-1RANCHER-1778: final test

--- vars/folioNamespaceCreate.groovy
+++ vars/folioNamespaceCreate.groovy
@@ -48,21 +48,6 @@ void call(CreateNamespaceParameters args) {
         ldpConfig.dbHost = kubectl.getSecretValue(namespace.getNamespaceName(), 'db-credentials', 'DB_HOST')
       }
     }
-    stage('Create metadata ConfigMap') {
-      folioHelm.withKubeConfig(namespace.getClusterName()) {
-        println(
-          'Current !isMetadataExist is: ' + !folioNamespaceMetadata.isMetadataExist(namespace.getNamespaceName())
-        )
-        if (!folioNamespaceMetadata.isMetadataExist(namespace.getNamespaceName())) {
-          folioNamespaceMetadata.create(args)
-        } else {
-          folioNamespaceMetadata.compare(args)
-        }
-        folioNamespaceMetadata.getMetadataAll(args)
-        folioNamespaceMetadata.getMetadataKey(args, 'clusterName')
-        folioNamespaceMetadata.updateConfigMap(args, ["Arg":"false", "ARG2":"true"])
-      }
-    }
 
     if (args.greenmail) {
       stage('[Helm] Deploy greenmail') {
