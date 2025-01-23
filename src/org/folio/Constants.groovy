6352db57e9e769e18f1be78cbd03dda7fe699baa
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2025-01-24 04:21:37 +0600

    RANCHER-1284 LSD (#791)
    * .RANCHER-1284 initial commit
    
    * .RANCHER-1284 wrongly copied stage remove
    
    * .RANCHER-1284 missing stage include
    
    * .RANCHER-1284 RP project change
    
    * .RANCHER-1284 destroy before
    
    * .RANCHER-1284 destroy before V2
    
    * .RANCHER-1284 destroy before V2.1
    
    * RANCHER-1284 stable state
    
    * RANCHER-1284 stable state + RP
    
    * RANCHER-1284 stable state + RP + 2V suggestions

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -37,7 +37,7 @@ class Constants {
   static List AWS_EKS_CLUSTERS = ['folio-testing', 'folio-dev', 'folio-perf', 'folio-tmp', 'folio-etesting', 'folio-edev']
   //tmp excluded: 'folio-eperf', 'folio-etmp
   static List AWS_EKS_TMP_NAMESPACES = ['test', 'test-1', 'test-2', 'tdspora']
-  static List AWS_EKS_TESTING_NAMESPACES = ['cypress', 'data-migration', 'ecs-snapshot', 'karate', 'snapshot', 'snapshot2', 'sprint', 'pre-bugfest', 'orchid-migration']
+  static List AWS_EKS_TESTING_NAMESPACES = ['cypress', 'data-migration', 'ecs-snapshot', 'karate', 'snapshot', 'snapshot2', 'sprint', 'pre-bugfest', 'orchid-migration', 'lsdi']
   static List AWS_EKS_RELEASE_NAMESPACES = ['poppy', 'quesnelia']
   static List AWS_EKS_DEV_NAMESPACES = ['aggies',
                                         'bama',
