f62df6950d1f76483cd32cda9bc2fcd93d3fbf6b
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-01-25 15:09:50 +0200

    RANCHER-2024-test. Changes were made for test purposes.33

--- src/org/folio/rest_v2/Constants.groovy
+++ src/org/folio/rest_v2/Constants.groovy
@@ -178,6 +178,6 @@ class Constants {
 
   static final Map APPLICATION_SETS_APPLICATIONS =
     APPLICATION_SETS.collectEntries { appSet, appList ->
-      [ appSet, appList.collect { it.name } ]
+      [ appSet, appList.collect { it.name + "blabla" } ]
     }
 }
