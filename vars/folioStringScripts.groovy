4a243f4ce705480c6fa7b423f97dd7192d88daf8
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-02-03 23:21:30 +0200

    RANCHER-2013-test. Changes were made for test purposes.19

--- vars/folioStringScripts.groovy
+++ vars/folioStringScripts.groovy
@@ -197,7 +197,7 @@ def selectors = ${hideMap.inspect()}[${reference}.toString()]?.collect {
     "div.jenkins-form-item:has(input[value='\$it']):not(:has([id^=hiddenPanel]))"
   }?.join(", \\n")
 
-def test = ${reference}.getClass().getName()
+def test = ${reference}
 
 return selectors ? \"\"\"
   <style>
