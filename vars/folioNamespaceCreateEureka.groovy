021a78f371601ad8dbf9c29b24fceeaf9ac7ef6c
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-02-05 05:15:25 +0600

    RANCHER-1735 proper env name in notification

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -314,8 +314,10 @@ void call(CreateNamespaceParameters args) {
 
     //TODO: Add adequate slack notification https://folio-org.atlassian.net/browse/RANCHER-1892
     stage('[Notify] Eureka') {
-      slackSend(color: 'good', message: 'eureka-snapshot env successfully built\n' + "1. https://${namespace.generateDomain('diku')}\n" +
-        "2. https://${namespace.generateDomain('consortium')}", channel: '#rancher_tests_notifications')
+      slackSend(color: 'good', message: args.clusterName + "-" + args.namespaceName + " env successfully built\n" +
+        "1. https://${namespace.generateDomain('diku')}\n" +
+        "2. https://${namespace.generateDomain('consortium')}",
+        channel: '#rancher_tests_notifications')
     }
 
 //    stage('Deploy ldp') {
