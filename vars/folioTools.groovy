7d0b20b7b7f70b74703b5195db71ebf437252c66
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-02-07 07:34:03 +0600

    RANCHER-1735 workarounds v2.1

--- vars/folioTools.groovy
+++ vars/folioTools.groovy
@@ -41,6 +41,9 @@ void stsKafkaLag(String cluster, String namespace, String tenantId) {
       kubectl.waitPodIsRunning("${namespace}", 'kafka-sh')
     }
     def check = kubectl.execCommand("${namespace}", 'kafka-sh', "${lag}")
+    if (check.contains("0\n0")) {
+      check = check.tokenize()[0]
+    }
     while (check.toInteger() != 0) {
       logger.debug("Waiting for capabilities to be propagated on tenant: ${tenantId}")
       sleep time: 30, unit: 'SECONDS'
