2f6fdde9cbaec9e53e7cc4ea59f87ac3b0793d62
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-02-09 14:00:36 +0600

    .RANCHER-1735 consortia system user workaround v2

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -333,7 +333,7 @@ class Constants {
 
   //RDS
   static String BUGFEST_SNAPSHOT_DBNAME = 'folio'
-  static String BUGFEST_SNAPSHOT_NAME = 'folio-etesting-sprint-february-2025-ecs'
+  static String BUGFEST_SNAPSHOT_NAME = 'folio-etesting-sprint-february-2025-ecs-users-clean'
 
   static Map JMX_METRICS_AVAILABLE = ['mod-bulk-operations': '2.0.0-SNAPSHOT.71']
 
