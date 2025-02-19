0755c1d6d604b95c00b5b387cca81953baf939fe
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-02-20 00:19:16 +0200

    RANCHER-2057-test. Changes were made for test purposes.5

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -204,7 +204,7 @@ class Constants {
 
   //Eureka base constants
   static String EUREKA_REGISTRY_URL = 'https://eureka-registry.ci.folio.org/descriptors/'
-  static List<String> EUREKA_MODULE_SOURCES = ['github/folio-org', 'dockerhub/folioci', 'dockerhub/folioorg']
+  static List<String> EUREKA_MODULE_SOURCES = ['GitHub/folio-org', 'DockerHub/folioci', 'DockerHub/folioorg', 'ECR']
 
   static String RANCHER_API_URL = 'https://rancher.ci.folio.org/v3'
 
