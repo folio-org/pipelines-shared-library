82b7b179552786d0f5b3433841766793f9ae4528
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-02-22 00:02:18 +0200

    RANCHER-2057-test. Changes were made for test purposes.14

--- vars/folioStringScripts.groovy
+++ vars/folioStringScripts.groovy
@@ -4,12 +4,12 @@ import org.folio.rest_v2.PlatformType
 import org.folio.rest_v2.Constants as RestConstants
 
 static String getClusters(String platformParam = null, String platformValue = null) {
-  return "return [\"$platformValue\"]"
-//  return """
-//return ${platformParam} && ${PlatformType.values().collect{it.name() }.inspect()}.contains(${platformParam}.trim()) ?
-//${Constants.AWS_EKS_PLATFORM_CLUSTERS().inspect()}[${platformParam}.trim()] :
-//${Constants.AWS_EKS_CLUSTERS_LIST.inspect()}
-//"""
+  return """
+  def clusters = $platformValue ? ${Constants.AWS_EKS_PLATFORM_CLUSTERS().inspect()}["${platformValue}"] : ${Constants.AWS_EKS_CLUSTERS_LIST.inspect()}
+
+  return ${platformParam} && ${PlatformType.values().collect{it.name() }.inspect()}.contains(${platformParam}.trim()) ?
+  ${Constants.AWS_EKS_PLATFORM_CLUSTERS().inspect()}[${platformParam}.trim()] : clusters
+"""
 }
 
 static String getNamespaces() {
