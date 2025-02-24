600ac28d61d774e029d3a724448eff51464875ea
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2025-02-25 00:09:55 +0200

    Update PodTemplates.groovy

--- src/org/folio/jenkins/PodTemplates.groovy
+++ src/org/folio/jenkins/PodTemplates.groovy
@@ -11,31 +11,31 @@ class PodTemplates {
 
   void java11Template(Closure body) {
     steps.podTemplate(inheritFrom: BASE_AGENT, label: 'java11-agent',
-      containers: [steps.containerTemplate(name: 'java', image: 'amazoncorretto:11-alpine-jdk')]) {
+      containers: [steps.containerTemplate(name: 'java', image: 'amazoncorretto:11-alpine-jdk',
+        command: 'sleep', args: '99d')]) {
       body.call()
     }
   }
 
   void java17Template(Closure body) {
-    podTemplate(inheritFrom: BASE_AGENT, label: 'java17-agent', containers: [
-      containerTemplate(name: 'java', image: 'amazoncorretto:17-alpine-jdk')
-    ]) {
+    podTemplate(inheritFrom: BASE_AGENT, label: 'java17-agent',
+      containers: [containerTemplate(name: 'java', image: 'amazoncorretto:17-alpine-jdk',
+        command: 'sleep', args: '99d')]) {
       body.call()
     }
   }
 
   void java21Template(Closure body) {
-    podTemplate(inheritFrom: BASE_AGENT, label: 'java21-agent', containers: [
-      containerTemplate(name: 'java', image: 'amazoncorretto:21-alpine-jdk')
-    ]) {
+    podTemplate(inheritFrom: BASE_AGENT, label: 'java21-agent',
+      containers: [containerTemplate(name: 'java', image: 'amazoncorretto:21-alpine-jdk',
+        command: 'sleep', args: '99d')]) {
       body.call()
     }
   }
 
   void stripesTemplate(Closure body) {
-    podTemplate(inheritFrom: BASE_AGENT, label: 'stripes-agent', containers: [
-      containerTemplate(name: 'jnlp', resourceRequestMemory: '8Gi', resourceLimitMemory: '9Gi')
-    ]) {
+    podTemplate(inheritFrom: BASE_AGENT, label: 'stripes-agent',
+      containers: [containerTemplate(name: 'jnlp', resourceRequestMemory: '8Gi', resourceLimitMemory: '9Gi')]) {
       body.call()
     }
   }
