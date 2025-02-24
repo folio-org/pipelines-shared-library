7b9226f0c3353f78920201a9c7b64f68ea36b9cc
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2025-02-24 23:51:59 +0200

    Update PodTemplates.groovy

--- src/org/folio/jenkins/PodTemplates.groovy
+++ src/org/folio/jenkins/PodTemplates.groovy
@@ -3,13 +3,12 @@ package org.folio.jenkins
 class PodTemplates {
   static final String BASE_AGENT = "base-agent"
 
-  void java11Template(Closure body) {
+  void java11Template(body) {
 //    body.setDelegate(this)
 //    body.setResolveStrategy(Closure.DELEGATE_FIRST)
 
-    podTemplate(inheritFrom: BASE_AGENT, label: 'java11-agent', containers: [
-      containerTemplate(name: 'java', image: 'amazoncorretto:11-alpine-jdk')
-    ]) {
+    podTemplate(inheritFrom: BASE_AGENT, label: 'java11-agent',
+      containers: [containerTemplate(name: 'java', image: 'amazoncorretto:11-alpine-jdk')]) {
       body.call()
     }
   }
