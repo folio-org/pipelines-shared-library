f13907eabf545a59f12ad38072c7bcda1d06052f
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2025-02-25 00:32:33 +0200

    Update PodTemplates.groovy

--- src/org/folio/jenkins/PodTemplates.groovy
+++ src/org/folio/jenkins/PodTemplates.groovy
@@ -7,26 +7,33 @@ class PodTemplates {
 
   private Object steps
 
+  private boolean debug
+
   private Logger logger
 
-  PodTemplates(context) {
+  PodTemplates(context, debug = false) {
     this.steps = context
+    this.debug = debug
     this.logger = new Logger(context, 'PodTemplates')
   }
 
   void javaTemplate(String javaVersion, Closure body) {
-    steps.podTemplate(inheritFrom: BASE_AGENT, label: 'java-agent',
+    steps.podTemplate(inheritFrom: BASE_AGENT, label: 'java-agent', showRawYaml: debug,
       containers: [steps.containerTemplate(name: 'java', image: "amazoncorretto:${javaVersion}-alpine-jdk",
         command: 'sleep', args: '99d')]) {
-      logger.info('\n' + '-' * 30 + "\nJava ${javaVersion} pod template in use!\n" + '-' * 30)
       body.call()
     }
   }
 
   void stripesTemplate(Closure body) {
-    steps.podTemplate(inheritFrom: BASE_AGENT,
+    steps.podTemplate(inheritFrom: BASE_AGENT, label: 'stripes-agent', showRawYaml: debug,
       containers: [steps.containerTemplate(name: 'jnlp', resourceRequestMemory: '8Gi', resourceLimitMemory: '9Gi')]) {
       body.call()
     }
   }
+
+  /**
+   * To be implemented in scope of cypress tests adoption
+   */
+  void cypressTemplate(Closure body) {}
 }
