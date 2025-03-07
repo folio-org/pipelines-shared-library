4a67e2246cda17a7ea734ba7ae162e7b71516b71
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-03-07 02:13:34 +0200

    RANCHER-2057-test. Changes were made for test purposes.132

---
+++ src/org/folio/models/parameters/ModuleBuildPushPipeline.groovy
@@ -0,0 +1,46 @@
+package org.folio.models.parameters
+
+class ModuleBuildPushPipeline {
+  static String JOB_NAME = "/v2/pipelines/v2/modules/buildPush"
+
+  List parameters = []
+  def context
+
+  ModuleBuildPushPipeline(def context) {
+    this.context = context
+  }
+
+  ModuleBuildPushPipeline withModuleName(String name) {
+    parameters.add(string(name: 'MODULE_NAME', value: name))
+    return this
+  }
+
+  ModuleBuildPushPipeline withModuleBranch(String branch) {
+    parameters.add(string(name: 'MODULE_BRANCH', value: branch))
+    return this
+  }
+
+  ModuleBuildPushPipeline withMavenArgs(String args) {
+    parameters.add(string(name: 'MAVEN_ARGS', value: args))
+    return this
+  }
+
+  ModuleBuildPushPipeline doPushImage(boolean isPush) {
+    parameters.add(booleanParam(name: 'PUSH_IMAGE_TO_ECR', value: isPush))
+    return this
+  }
+
+  ModuleBuildPushPipeline doPushDescriptor(boolean isPush) {
+    parameters.add(booleanParam(name: 'PUSH_DESCRIPTOR_TO_ECR', value: isPush))
+    return this
+  }
+
+  ModuleBuildPushPipeline withAgent(String agent) {
+    parameters.add(string(name: 'AGENT', value: agent))
+    return this
+  }
+
+  def run(boolean waitJobComplete = true, boolean propagateJobRunStatus = true) {
+    return context.build(job: v2.modules.buildPush.JOB_NAME, wait: waitJobComplete, propagate: propagateJobRunStatus, parameters: parameters)
+  }
+}
