d37ddfbf8a4149ddedc87d28ea4adfa556dda31f
Sergii Masiuk <sergii_masiuk@epam.com>
2025-03-12 12:03:18 +0200

    RANCHER-2162: add bulk-edit and oai-pmh apps

--- src/org/folio/rest_v2/Constants.groovy
+++ src/org/folio/rest_v2/Constants.groovy
@@ -89,7 +89,7 @@ class Constants {
     ],
     [
       name: "app-platform-complete"
-      , branch: "revert-11-revert-10-MODOAIPMH-603_MODBULKOPS-487"
+      , branch: "master"
       , consortia: false
       , core     : false
       , byDefault: true
@@ -137,7 +137,7 @@ class Constants {
     ],
     [
       name: "app-edge-complete"
-      , branch: "MODOAIPMH-603"
+      , branch: "master"
       , consortia: false
       , core     : false
       , byDefault: true
