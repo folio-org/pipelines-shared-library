a96513dae9428cf47d897c3ccf5ddc015eee9763
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-03-12 14:18:49 +0600

    RANCHER-2182 proper job update v2.1 | Final variant v2.6

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -397,10 +397,10 @@ String generateModuleValues(RancherNamespace ns, String moduleName, String modul
   if (enableIngress) {
     moduleConfig['ingress']['hosts'][0] += [host: domain]
     if (moduleName == 'ui-bundle' && ns.defaultTenantId == 'diku' && ns.clusterName == 'folio-etesting' && (ns.namespaceName == 'snapshot' || ns.namespaceName == 'snapshot2')) {
-      moduleConfig['ingress']['hosts'] << [host: "eureka-snapshot-diku.${Constants.CI_ROOT_DOMAIN}"]
+      moduleConfig['ingress']['hosts'] << [host: "eureka-snapshot-diku.${Constants.CI_ROOT_DOMAIN}", paths: [[path: "/*", pathType: "ImplementationSpecific"]]]
     }
     if (moduleName == 'ui-bundle' && ns.defaultTenantId == 'consortium' && ns.clusterName == 'folio-etesting' && (ns.namespaceName == 'snapshot' || ns.namespaceName == 'snapshot2')) {
-      moduleConfig['ingress']['hosts'] << [host: "eureka-snapshot-consortium.${Constants.CI_ROOT_DOMAIN}"]
+      moduleConfig['ingress']['hosts'] << [host: "eureka-snapshot-consortium.${Constants.CI_ROOT_DOMAIN}", paths: [[path: "/*", pathType: "ImplementationSpecific"]]]
     }
     moduleConfig['ingress']['annotations'] += ['alb.ingress.kubernetes.io/group.name': "${ns.clusterName}.${ns.namespaceName}"]
     moduleConfig['ingress']['annotations'] += ['alb.ingress.kubernetes.io/target-type': 'ip']
