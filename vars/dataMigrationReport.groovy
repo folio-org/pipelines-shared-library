ceaf3146827f5e38d4e7d92a132475a0ba4a4bdb
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-03-14 12:52:24 +0600

    .RANCHER-2148 new Eureka approach v1.0.2

--- vars/dataMigrationReport.groovy
+++ vars/dataMigrationReport.groovy
@@ -38,7 +38,7 @@ void call(CreateNamespaceParameters args) {
     tfConfig.addVar('eureka', args.platform == PlatformType.EUREKA)
     tfConfig.addVar('kong_version', args.kongVersion)
     tfConfig.addVar('keycloak_version', args.keycloakVersion)
-    tfConfig.addVar('pg_rds_snapshot_name', Constants.DATA_MIGRATION_SNAPSHOT_NAME)
+    tfConfig.addVar('pg_rds_snapshot_name', args.dmSnapshot)
     tfConfig.addVar('pg_dbname', Constants.BUGFEST_SNAPSHOT_DBNAME)
 
     stage('[Terraform] Provision') {
