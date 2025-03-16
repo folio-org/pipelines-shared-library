e709f8a9c9b2ae777a1620b3c434bc43a39e3b9a
Bill Avramenko <Vasyl_Avramenko@epam.com>
2025-03-16 13:46:51 +0200

    RANCHER-2057-test. Changes were made for test purposes.175

--- vars/folioStringScripts.groovy
+++ vars/folioStringScripts.groovy
@@ -134,6 +134,8 @@ def fetchModules(String url) {
   processResponse(new URL(url).openConnection())
 
   return modules.findAll {
+    return it.startsWith('mod-') || it.startsWith('edge-')
+
     bool isOKAPISpecific = it == 'okapi'
     bool isEUREKASpecific = it == 'folio-kong' || it == 'folio-keycloak' || it == 'folio-module-sidecar' || it.startsWith('mgr-')
 
