adaf7fb7216a4fba2fcd430047918517fb92daec
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-03-18 12:17:15 +0600

    RANCHER-2182 condition simplify

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -396,24 +396,10 @@ String generateModuleValues(RancherNamespace ns, String moduleName, String modul
   boolean enableIngress = moduleConfig.containsKey('ingress') ? moduleConfig['ingress']['enabled'] : false
   if (enableIngress) {
     moduleConfig['ingress']['hosts'][0] += [host: domain]
-    if (moduleName == 'ui-bundle' && ns.defaultTenantId == 'diku' && ns.clusterName == 'folio-etesting' && (ns.namespaceName == 'snapshot' || ns.namespaceName == 'snapshot2')) {
+    if (moduleName == 'ui-bundle' && ns.clusterName == 'folio-etesting' && ns.namespaceName ==~ /snapshot.*/ ) {
       moduleConfig['ingress']['hosts'] += [
         [
-          host : "eureka-snapshot-diku.${Constants.CI_ROOT_DOMAIN}",
-          paths: [
-            [
-              path       : "/*",
-              pathType   : "ImplementationSpecific",
-              servicePort: 80
-            ]
-          ]
-        ]
-      ]
-    }
-    if (moduleName == 'ui-bundle' && ns.defaultTenantId == 'consortium' && ns.clusterName == 'folio-etesting' && (ns.namespaceName == 'snapshot' || ns.namespaceName == 'snapshot2')) {
-      moduleConfig['ingress']['hosts'] += [
-        [
-          host : "eureka-snapshot-consortium.${Constants.CI_ROOT_DOMAIN}",
+          host : "eureka-snapshot-${ns.defaultTenantId}.${Constants.CI_ROOT_DOMAIN}",
           paths: [
             [
               path       : "/*",
