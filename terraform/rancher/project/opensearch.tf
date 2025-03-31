712ca8af3bd80bea90ad684e883261d40805cdd1
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-03-31 17:51:50 +0600

    RANCHER-2189 plan is OK | testing part v1.0.6

--- terraform/rancher/project/opensearch.tf
+++ terraform/rancher/project/opensearch.tf
@@ -1,3 +1,23 @@
+resource "null_resource" "config-executor" {
+  count = var.opensearch_shared ? 0 : 1
+  depends_on = [
+    rancher2_namespace.this,
+    rancher2_project.this,
+    helm_release.opensearch-single-node,
+    helm_release.opensearch-master,
+    helm_release.opensearch-data,
+    helm_release.opensearch-client,
+  ]
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "Configuring OpenSearch based on RANCHER-2189"
+      kubectl port-forward -n ${rancher2_namespace.this.name} svc/opensearch-${var.rancher_project_name} 9200:9200 &
+      curl -X PUT "localhost:9200/_cluster/settings" -H 'Content-Type: application/json' -d '{"persistent": {"action": {"auto_create_index": false}}}'
+    EOT
+  }
+}
+
+
 resource "rancher2_secret" "opensearch-credentials" {
   name         = "opensearch-credentials"
   project_id   = rancher2_project.this.id
@@ -51,17 +71,6 @@ plugins:
   enabled: true
   installList: [analysis-icu, analysis-kuromoji, analysis-smartcn, analysis-nori, analysis-phonetic, https://github.com/aiven/prometheus-exporter-plugin-for-opensearch/releases/download/2.11.0.0/prometheus-exporter-2.11.0.0.zip]
 ${local.schedule_value}
-opensearchConfig:
-  opensearch.yml: |
-    action:
-      auto_create_index: false
-extraInitContainers:
-  - name: block-index-creation-by-default
-    image: busybox
-    command:
-      - "sh"
-      - "-c"
-      - "chown -R 1000:1000 /usr/share/opensearch/config"
 EOF
   ]
 }
