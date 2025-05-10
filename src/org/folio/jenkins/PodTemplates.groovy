fd716de89a043c4271388801ede6c54ceb9b972a
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-05-11 00:22:21 +0300

    Increase memory limits for Karate test Java containers.
    Adjusted memory allocations to improve performance and accommodate larger workloads. The container memory was increased from 3072Mi/3584Mi to 6144Mi/8192Mi, and Maven's `-Xmx` was updated from 2048m to 5120m.

--- src/org/folio/jenkins/PodTemplates.groovy
+++ src/org/folio/jenkins/PodTemplates.groovy
@@ -298,7 +298,7 @@ spec:
       label: JenkinsAgentLabel.JAVA_KARATE_AGENT.getLabel(),
       volumes: [steps.persistentVolumeClaim(claimName: MAVEN_CACHE_PVC, mountPath: "${WORKING_DIR}/.m2/repository")],
       containers: [
-        buildJavaContainer(javaVersion, [], '3072Mi', '3584Mi')
+        buildJavaContainer(javaVersion, [], '6144Mi', '8192Mi')
       ]
     )) {
       steps.node(JenkinsAgentLabel.JAVA_KARATE_AGENT.getLabel()) {
--- vars/folioKarateFlow.groovy
+++ vars/folioKarateFlow.groovy
@@ -45,7 +45,7 @@ KarateRunExecutionSummary call(KarateTestsParameters args) {
         timeout(time: args.timeout, unit: 'HOURS') {
           container('java') {
             withMaven(jdk: args.javaToolName, maven: args.mavenToolName,
-              mavenOpts: '-XX:MaxRAMPercentage=75 -Xmx2048m -XX:MaxMetaspaceSize=512m',
+              mavenOpts: '-XX:MaxRAMPercentage=75 -Xmx5120m -XX:MaxMetaspaceSize=512m',
               mavenLocalRepo: "${podTemplates.WORKING_DIR}/.m2/repository",
               traceability: true,
               options: [artifactsPublisher(disabled: true)]) {
