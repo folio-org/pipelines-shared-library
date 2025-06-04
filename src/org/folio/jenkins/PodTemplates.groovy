acf9454f61263ddfddb9bf2b25d130dde572b975
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2025-06-04 17:05:49 +0300

    [DEBUG] Decrease memory for dind container for sidecar build

--- src/org/folio/jenkins/PodTemplates.groovy
+++ src/org/folio/jenkins/PodTemplates.groovy
@@ -281,7 +281,7 @@ spec:
       containers: [
         buildKanikoContainer([], '512Mi', '768Mi'),
         buildJavaContainer(javaVersion, [new KeyValueEnvVar('DOCKER_HOST', 'tcp://localhost:2375')], '768Mi', '1024Mi'),
-        buildDindContainer([], '8192Mi', '9216Mi')
+        buildDindContainer([], '4096Mi', '5120Mi')
       ]
     )) {
       steps.node(JenkinsAgentLabel.JAVA_BUILD_AGENT.getLabel()) {
