11a1f6cd7f168d085df242242a01894dcbca6767
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-06-05 12:57:41 +0600

    RANCHER-2116 simple native + beautify v4.8

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -120,7 +120,7 @@ void call(CreateNamespaceParameters args) {
         String tag = (awscli.listEcrImages(Constants.AWS_REGION, 'folio-module-sidecar')).replaceAll('"', '')
         if (tag.contains('native')) {
           logger.info("Previously built SC image is 'NATIVE'. Using it for Eureka env.")
-          installJson.remove { module -> module.id =~ /folio-module-sidecar-.*/ }
+          installJson.removeAll { module -> module.id =~ /folio-module-sidecar-.*/ }
           installJson.add([id: "folio-module-sidecar-" + tag, action: 'enable'])
           writeJSON (file: 'used-install.json', json: installJson, pretty: 4)
           archiveArtifacts 'used-install.json' // Archive used modules version for review
