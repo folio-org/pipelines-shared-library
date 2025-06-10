d051e0b187d21c241e4df92ad94440284476eacf
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2025-06-10 15:49:20 +0600

    RANCHER-2379 includes app-linked-data (#986)

--- src/org/folio/rest_v2/Constants.groovy
+++ src/org/folio/rest_v2/Constants.groovy
@@ -116,7 +116,7 @@ class Constants {
       , branch: "snapshot"
       , consortia: false
       , core     : false
-      , byDefault: false
+      , byDefault: true
       , dependsOn: ["app-platform-minimal", "app-platform-complete"]
     ],
     [
