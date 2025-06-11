947fc0aa3d3178576cb86600ccadf1e0d7b0559a
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2025-06-11 18:04:07 +0600

    RANCHER-2375-v2 additional permissions v2.2 + SC

--- vars/folioNamespaceCreateEureka.groovy
+++ vars/folioNamespaceCreateEureka.groovy
@@ -118,15 +118,12 @@ void call(CreateNamespaceParameters args) {
 
       if (args.scNative) {
         String tag = (awscli.listEcrImages(Constants.AWS_REGION, 'folio-module-sidecar')).replaceAll('"', '')
-        if (tag.contains('native')) {
-          logger.info("Previously built SC image is 'NATIVE'. Using it for Eureka env.")
+          logger.info("Previously built SC image is 'CUSTOM/NATIVE'. Using it for Eureka env.")
           installJson.removeAll { module -> module.id =~ /folio-module-sidecar-.*/ }
           installJson.add([id: "folio-module-sidecar-" + tag, action: 'enable'])
           writeJSON (file: 'used-install.json', json: installJson, pretty: 4)
           archiveArtifacts 'used-install.json' // Archive used modules version for review
-        } else {
-          logger.warning("Previously built SC image is not 'NATIVE'. Falling back to default SC image.")
-        }
+
       }
 
       //TODO: Temporary solution. Unused by Eureka modules have been removed.
