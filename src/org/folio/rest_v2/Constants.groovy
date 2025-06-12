072c73085dfb0cd3760545303c59cb18bb4bd63e
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-06-12 15:56:05 +0300

    MODCONSKC-78. Changes were made for test purposes.1

--- src/org/folio/rest_v2/Constants.groovy
+++ src/org/folio/rest_v2/Constants.groovy
@@ -97,11 +97,11 @@ class Constants {
     ],
     [
       name: "app-consortia"
-      , branch: "snapshot"
+      , branch: "MODCONSKC-78"
       , consortia: true
       , core     : false
       , byDefault: true
-      , dependsOn: ["app-platform-minimal", "app-platform-complete", "app-consortia-manager"]
+      , dependsOn: ["app-platform-minimal"]
     ],
     [
       name: "app-consortia-manager"
@@ -109,7 +109,7 @@ class Constants {
       , consortia: true
       , core     : false
       , byDefault: true
-      , dependsOn: ["app-platform-minimal", "app-platform-complete", "app-consortia"]
+      , dependsOn: ["app-platform-minimal", "app-consortia"]
     ],
     [
       name: "app-linked-data"
