90bfebe4bfc3a88d294b3d3f98dd904670939136
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-06-14 00:26:48 +0300

    RANCHER-2374-bienen-test. Changes were made for test purposes.1

--- src/org/folio/rest_v2/Constants.groovy
+++ src/org/folio/rest_v2/Constants.groovy
@@ -89,7 +89,7 @@ class Constants {
     ],
     [
       name: "app-platform-complete"
-      , branch: "snapshot"
+      , branch: "erm_cleanup"
       , consortia: false
       , core     : false
       , byDefault: true
