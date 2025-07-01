a6b157f89e064e9387d718a7441efe0c409cccca
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2025-07-01 13:49:43 +0300

    RANCHER-2414   Scheduled timer doesn't work on etesting-sprint environment
    Jira: https://folio-org.atlassian.net/browse/RANCHER-2414

--- src/org/folio/rest_v2/eureka/Kong.groovy
+++ src/org/folio/rest_v2/eureka/Kong.groovy
@@ -1,5 +1,6 @@
 package org.folio.rest_v2.eureka
 
+import hudson.util.Secret
 import org.folio.models.EurekaTenant
 
 class Kong extends Base {
@@ -7,14 +8,14 @@ class Kong extends Base {
   protected Keycloak keycloak
   protected String kongUrl
 
-  Kong(def context, String kongUrl, Keycloak keycloak, boolean debug = false){
+  Kong(def context, String kongUrl, Keycloak keycloak, boolean debug = false) {
     super(context, debug)
 
     this.keycloak = keycloak
     this.kongUrl = kongUrl
   }
 
-  Kong(def context, String kongUrl, String keycloakUrl, boolean debug = false){
+  Kong(def context, String kongUrl, String keycloakUrl, boolean debug = false) {
     this(context, kongUrl, new Keycloak(context, keycloakUrl, debug), debug)
   }
 
@@ -45,5 +46,25 @@ class Kong extends Base {
 
   Map<String, String> getTenantHttpHeaders(EurekaTenant tenant, boolean addOkapiAuth = false) {
     return getDefaultHeaders() + keycloak.getAuthTenantHeaders(tenant, addOkapiAuth)
+  }
+
+  Map<String, String> getTenantHttpHeaders(String tenantId, String token, boolean addOkapiAuth = false) {
+    return getDefaultHeaders() + keycloak.getAuthorizedHeaders(token, addOkapiAuth) + ["X-Okapi-Tenant": tenantId]
+  }
+
+  String getAuthUserToken(String tenantId, String username, Secret password) {
+    logger.info("Getting access token via login for user " + username)
+
+    String url = generateUrl("/authn/login")
+
+    Map<String, String> headers = ['Content-Type': 'application/json', 'x-okapi-tenant': tenantId]
+
+    Map requestBody = ["username": username, "password": password.getPlainText()]
+
+    def response = restClient.post(url, requestBody, headers).body
+
+    logger.info("Access token obtained successfully via login for user " + username)
+
+    return response['okapiToken']
   }
 }
