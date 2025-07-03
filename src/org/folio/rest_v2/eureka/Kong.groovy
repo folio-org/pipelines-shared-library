dfac98dc0bd94bcbd4a3c7b7831c042c7f62151b
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-07-03 19:25:58 +0300

    RANCHER-2414. Refactor getAuthUserToken method to use Keycloak direct endpoint for user authorization. Add.1

--- src/org/folio/rest_v2/eureka/Kong.groovy
+++ src/org/folio/rest_v2/eureka/Kong.groovy
@@ -8,14 +8,14 @@ class Kong extends Base {
   protected Keycloak keycloak
   protected String kongUrl
 
-  Kong(def context, String kongUrl, Keycloak keycloak, boolean debug = false) {
+  Kong(def context, String kongUrl, Keycloak keycloak, boolean debug = false){
     super(context, debug)
 
     this.keycloak = keycloak
     this.kongUrl = kongUrl
   }
 
-  Kong(def context, String kongUrl, String keycloakUrl, boolean debug = false) {
+  Kong(def context, String kongUrl, String keycloakUrl, boolean debug = false){
     this(context, kongUrl, new Keycloak(context, keycloakUrl, debug), debug)
   }
 
@@ -45,7 +45,7 @@ class Kong extends Base {
   }
 
   Map<String, String> getTenantHttpHeaders(EurekaTenant tenant, boolean addOkapiAuth = false) {
-    return getDefaultHeaders() + keycloak.getAuthTenantHeaders(tenant, addOkapiAuth)
+    return getDefaultHeaders() + keycloak.getAuthTenantHeaders(tenant, null, addOkapiAuth)
   }
 
   Map<String, String> getTenantUserHttpHeaders(EurekaTenant tenant, User user = tenant.getAdminUser(), boolean addOkapiAuth = false) {
