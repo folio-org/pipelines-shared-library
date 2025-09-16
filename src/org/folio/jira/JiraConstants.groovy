525135f4bdde29062bdb1ed9414afce1f5316d2a
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2025-09-16 18:11:57 +0600

    RANCHER-2542 nightly karate tests

--- src/org/folio/jira/JiraConstants.groovy
+++ src/org/folio/jira/JiraConstants.groovy
@@ -5,7 +5,7 @@ import org.folio.jira.model.JiraResources
 class JiraConstants {
 
   static final String URL = 'https://folio-org.atlassian.net'
-  static final String API_URL_PART = "/rest/api/2/"
+  static final String API_URL_PART = "/rest/api/3/"
   static final String API_URL = "${JiraConstants.URL}${API_URL_PART}"
 
   static final String ISSUE_URL = "${API_URL}${JiraResources.ISSUE}/"
