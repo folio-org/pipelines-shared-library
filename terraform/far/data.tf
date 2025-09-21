52528312a6f5d946eda4a0ee66079c0951e76960
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-09-21 03:46:11 +0300

    RANCHER-2507. Fix cluster ID retrieval for Rancher data source in `data.tf`

--- terraform/far/data.tf
+++ terraform/far/data.tf
@@ -4,9 +4,10 @@ data "aws_eks_cluster" "this" {
 
 # Getting the EKS cluster data from the Rancher cluster name.
 data "rancher2_cluster" "this" {
+  count = var.cluster_name == "rancher" ? 0 : 1
   name = var.cluster_name
 }
 
 locals {
-  cluster_id = var.cluster_name == "rancher" ? "local" : data.rancher2_cluster.this.id
+  cluster_id = var.cluster_name == "rancher" ? "local" : data.rancher2_cluster.this[0].id
 }
