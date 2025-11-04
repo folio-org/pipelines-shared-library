b7ef26abdd09ff6a78cf85686ed13aebc9da3ed8
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2025-11-04 18:10:55 +0600

    RANCHER-2438 Additional fix v2.3

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -45,7 +45,7 @@ void upgrade(String release_name, String namespace, String values_path, String c
       sh "helm upgrade --install ${release_name} --namespace=${namespace} ${valuesPathOption(values_path)} ${chart_repo}/${chart_name}"
     } catch (Exception e) {
       println("Error: $e\nUpgrade failed for ${release_name}, attempting to remove and reinstall")
-      //Without this change Headless svc will brake all existing workflows...
+      
       sh "helm uninstall ${release_name} --namespace=${namespace} --wait --timeout=10m"
       sh "helm upgrade --install ${release_name} --namespace=${namespace} ${valuesPathOption(values_path)} ${chart_repo}/${chart_name}"
     }
