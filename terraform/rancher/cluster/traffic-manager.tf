31dd59403453e5e87878529b5fa006999c47d626
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2025-11-11 19:01:14 +0600

    RANCHER-2623 Telepresence user configuration in manager-namespace (#1139)

--- terraform/rancher/cluster/traffic-manager.tf
+++ terraform/rancher/cluster/traffic-manager.tf
@@ -111,3 +111,66 @@ resource "null_resource" "patch_traffic_manager_host_network" {
     cluster_name         = module.eks_cluster.cluster_name
   }
 }
+
+resource "kubernetes_role" "port_forward_role" {
+  metadata {
+    name      = "port-forward-role"
+    namespace = rancher2_namespace.traffic-manager[0].name
+  }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "replicasets", "statefulsets"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "pods/log", "services"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods/portforward"]
+    verbs      = ["create", "get", "list", "update"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["configmaps"]
+    verbs      = ["get", "list"]
+  }
+}
+
+resource "kubernetes_role_binding" "port_forward_binding" {
+  metadata {
+    name      = "port-forward-binding"
+    namespace = rancher2_namespace.traffic-manager[0].name
+  }
+  subject {
+    kind      = "User"
+    name      = "rancher-port-forward"
+    api_group = "rbac.authorization.k8s.io"
+  }
+  role_ref {
+    kind      = "Role"
+    name      = "port-forward-role"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+data "aws_secretsmanager_secret" "telepresence_key" {
+  name = "telepresence"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.telepresence_key.id
+}
+
+resource "kubernetes_config_map" "telepresence-cm" {
+  metadata {
+    namespace = rancher2_namespace.traffic-manager[0].name
+    name      = "telepresence-${rancher2_namespace.traffic-manager[0].name}"
+  }
+  data = {
+    AWS_KEY_ID     = tostring(jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["KEY"])
+    AWS_SECRET_KEY = tostring(jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["SECRET"])
+  }
+}
