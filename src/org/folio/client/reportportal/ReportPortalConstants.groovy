1ed458407ccddbecf8dccb0bd279fc180c17f9fd
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2025-11-15 18:09:34 +0600

    RANCHER-2631 align Slack notifications with real RP project name for karate tests results (#1150)

--- src/org/folio/client/reportportal/ReportPortalConstants.groovy
+++ src/org/folio/client/reportportal/ReportPortalConstants.groovy
@@ -6,7 +6,7 @@ class ReportPortalConstants {
   public static final String API_URL = "${ReportPortalConstants.URL}/api/v1"
   public static final String KARATE_CRED_TEMPLATE_FILE_PATH = "testrail-integration/src/main/resources/reportportal.properties"
 
-  public static final String KARATE_PROJECT_NAME = "junit5-integration"
+  public static final String KARATE_PROJECT_NAME = "karate-eureka"
   public static final String CYPRESS_PROJECT_NAME = "cypress-nightly"
 
   public static final String CREDENTIALS_ID = "report-portal-api-key-1"
