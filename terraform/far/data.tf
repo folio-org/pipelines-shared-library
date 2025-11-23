b0c8856905d533811f7f58d4d154f5403d061749
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2025-11-23 14:13:36 +0600

    RANCHER-2655 enable support for mod-rtac-cache

--- terraform/far/data.tf
+++ terraform/far/data.tf
@@ -5,7 +5,7 @@ data "aws_eks_cluster" "this" {
 # Getting the EKS cluster data from the Rancher cluster name.
 data "rancher2_cluster" "this" {
   count = var.cluster_name == "rancher" ? 0 : 1
-  name = var.cluster_name
+  name  = var.cluster_name
 }
 
 locals {
