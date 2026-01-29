7952a0607c2065fefa9843011bb6e4fb7e58547b
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-01-29 12:45:24 +0600

    RANCHER-2139 s3 global simplification

--- terraform/rancher/project/minio.tf
+++ terraform/rancher/project/minio.tf
@@ -22,7 +22,7 @@ resource "helm_release" "minio" {
   chart      = "minio"
   version    = "11.8.1"
   values = [<<-EOF
-defaultBuckets: ${join(",", [local.s3_buckets_string, "local-files"])}
+defaultBuckets: ${local.s3_bucket_name},local-files
 image:
   repository: minio
   registry: 732722833398.dkr.ecr.us-west-2.amazonaws.com
@@ -73,18 +73,10 @@ EOF
 }
 
 resource "rancher2_secret" "s3-credentials" {
-  for_each = local.s3_buckets_map
-
-  name         = "s3-${each.key}-credentials"
+  name         = "s3-credentials"
   project_id   = rancher2_project.this.id
   namespace_id = rancher2_namespace.this.name
   data = {
-    AWS_SDK               = base64encode(var.s3_embedded ? "false" : "true")
-    AWS_URL               = base64encode(var.s3_embedded ? join("", ["https://", local.minio_url]) : "https://s3.amazonaws.com")
-    AWS_REGION            = base64encode(var.aws_region)
-    AWS_BUCKET            = base64encode(each.value)
-    AWS_ACCESS_KEY_ID     = base64encode(var.s3_embedded ? random_string.access_key[0].result : var.s3_access_key)
-    AWS_SECRET_ACCESS_KEY = base64encode(var.s3_embedded ? random_password.secret_access_key[0].result : var.s3_secret_key)
 
     LOCAL_FS_COMPOSE_WITH_AWS_SDK = base64encode(var.s3_embedded ? "false" : "true")
     LOCAL_FS_URL                  = base64encode(var.s3_embedded ? join("", ["https://", local.minio_url]) : "https://s3.amazonaws.com")
@@ -93,16 +85,24 @@ resource "rancher2_secret" "s3-credentials" {
     LOCAL_FS_ACCESS_KEY_ID        = base64encode(var.s3_embedded ? random_string.access_key[0].result : var.s3_access_key)
     LOCAL_FS_SECRET_ACCESS_KEY    = base64encode(var.s3_embedded ? random_password.secret_access_key[0].result : var.s3_secret_key)
 
-    USE_AWS_SDK          = base64encode(var.s3_embedded ? "false" : "true")
     S3_IS_AWS            = base64encode(var.s3_embedded ? "false" : "true")
     S3_URL               = base64encode(var.s3_embedded ? join("", ["https://", local.minio_url]) : "https://s3.amazonaws.com")
     S3_REGION            = base64encode(var.aws_region)
-    S3_BUCKET            = base64encode(each.value)
+    S3_BUCKET            = base64encode(local.s3_bucket_name)
     S3_ACCESS_KEY_ID     = base64encode(var.s3_embedded ? random_string.access_key[0].result : var.s3_access_key)
     S3_SECRET_ACCESS_KEY = base64encode(var.s3_embedded ? random_password.secret_access_key[0].result : var.s3_secret_key)
 
-    LIST_APP_S3_BUCKET = base64encode(each.value)
-
     GLOBAL_S3_SECRET_KEY = base64encode(var.s3_embedded ? random_password.secret_access_key[0].result : var.s3_secret_key)
+  }
+}
+
+resource "aws_s3_bucket" "s3-bucket-for-backend-modules" {
+  count  = var.s3_embedded ? 0 : 1
+  bucket = local.s3_bucket_name
+  tags = {
+    Cluster   = data.rancher2_cluster.this.name
+    Project   = var.rancher_project_name
+    Name      = rancher2_namespace.this.name
+    ManagedBy = "Terraform"
   }
 }
