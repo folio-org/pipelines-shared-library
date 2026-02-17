785c8da3d37330dd9addb9e24cba035866df27f4
Oleksandr Haimanov <oleksandr_haimanov@epam.com>
2026-02-17 09:51:51 +0200

    tmp

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -235,8 +235,10 @@ class Constants {
 
   static final String JENKINS_FOLIO_RANCHER_FOLDER = '/folioRancher'
 
+//  static final String JENKINS_CREATE_NAMESPACE_FROM_BRANCH_JOB =
+//    "$JENKINS_FOLIO_RANCHER_FOLDER/manageNamespace/createNamespaceFromBranchNEW"
   static final String JENKINS_CREATE_NAMESPACE_FROM_BRANCH_JOB =
-    "$JENKINS_FOLIO_RANCHER_FOLDER/manageNamespace/createNamespaceFromBranchNEW"
+    "/tmpFolderForDraftPipelines/Haimanov/createNamespaceFromBranch"
 
   static final String JENKINS_DELETE_NAMESPACE_JOB = "$JENKINS_FOLIO_RANCHER_FOLDER/manageNamespace/deleteNamespace"
   static final String JENKINS_DEPLOY_MODULES_FROM_JSON = "$JENKINS_FOLIO_RANCHER_FOLDER/folioDevTools/moduleDeployment/deployModulesFromJson"
