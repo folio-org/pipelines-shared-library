7e5e382bf1632f8b48272c223f3126c7581af283
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-03-25 17:04:40 +0600

    RANCHER-2895 folio-metrics s3 bucket

---
+++ terraform/shared/s3/terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "folio-terraform"
+    region         = "us-east-1"
+    key            = "folio-shared/folio-s3-metrics/terraform.tfstate"
+    dynamodb_table = "folio-terraform-lock"
+  }
+}
