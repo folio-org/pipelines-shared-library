160a5ddbd6f933fe03c6c0d5ef5679efe365a8b3
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-05-28 23:24:17 +0600

    RANCHER-2981 + mod-*-keycloak to a separate kafka instance

--- vars/folioHelm.groovy
+++ vars/folioHelm.groovy
@@ -277,7 +277,7 @@ String generateModuleValues(RancherNamespace ns, String moduleName, String modul
     moduleConfig.integrations.db.existingSecret = "db-credentials-${ns.getNamespaceName()}-eureka"
   }
 
-  if (ns.getNamespaceName() == 'cikarate' && moduleName.startsWith('mgr-') && moduleConfig.integrations?.kafka?.existingSecret == 'kafka-credentials') {
+  if (ns.getNamespaceName() == 'cikarate' && (moduleName.startsWith('mgr-') || moduleName ==~ /mod-.*-keycloak/) && moduleConfig.integrations?.kafka?.existingSecret == 'kafka-credentials') {
     moduleConfig.integrations.kafka.existingSecret = 'kafka-credentials-2'
   }
 
