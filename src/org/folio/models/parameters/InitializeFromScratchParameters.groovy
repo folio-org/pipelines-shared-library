e478837c296859061179e5b284dd53e7d44442a2
epam-avramenko <Vasyl_Avramenko@epam.com>
2026-05-30 00:52:24 +0300

    RANCHER-3005. Add default field application logic to InitializeFromScratchParameters
    Introduced a new `applyDefaults` method to set default values for fields based on `FolioRelease` unless explicitly modified. Enhanced tracking of modified fields with a `modifiedFields` Set.

--- src/org/folio/models/parameters/InitializeFromScratchParameters.groovy
+++ src/org/folio/models/parameters/InitializeFromScratchParameters.groovy
@@ -1,8 +1,11 @@
 package org.folio.models.parameters
 
 import org.folio.rest_v2.EntitlementApproach
+import org.folio.rest_v2.FolioRelease
 
 class InitializeFromScratchParameters {
+
+  Set<String> modifiedFields = [] as Set
 
   EntitlementApproach entitlementApproach
   boolean setBaseUrl = true
@@ -10,16 +13,31 @@ class InitializeFromScratchParameters {
 
   InitializeFromScratchParameters withEntitlementApproach(EntitlementApproach entitlementApproach) {
     this.entitlementApproach = entitlementApproach
+    this.modifiedFields << 'entitlementApproach'
     return this
   }
 
   InitializeFromScratchParameters withSetBaseUrl(boolean setBaseUrl) {
     this.setBaseUrl = setBaseUrl
+    this.modifiedFields << 'setBaseUrl'
     return this
   }
 
   InitializeFromScratchParameters withMigrate(boolean migrate) {
     this.migrate = migrate
+    this.modifiedFields << 'migrate'
+    return this
+  }
+
+  InitializeFromScratchParameters applyDefaults(FolioRelease releaseType) {
+    if (releaseType == null) return this
+
+    if (!modifiedFields.contains('entitlementApproach'))
+      this.entitlementApproach = (releaseType == FolioRelease.SUNFLOWER) ? EntitlementApproach.CREATE : EntitlementApproach.STATE
+
+    if (!modifiedFields.contains('setBaseUrl'))
+      this.setBaseUrl = (releaseType != FolioRelease.SUNFLOWER)
+
     return this
   }
 }
