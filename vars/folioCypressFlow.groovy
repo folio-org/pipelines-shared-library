e4dc2d929ab806293e4d5207a6a12d77c5b2eb20
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-07-03 20:57:58 +0600

    RANCHER-3038 + retry count == 2

--- vars/folioCypressFlow.groovy
+++ vars/folioCypressFlow.groovy
@@ -71,101 +71,103 @@ CypressRunExecutionSummary call(String ciBuildId, List<CypressTestsParameters> t
 
   try {
     testsToRun.each { CypressTestsParameters testParams ->
-      List execBatches = []
+      retry(2) {
+        List execBatches = []
 
-      podTemplates.cypressAgent {
-        if (reportPortalUse) {
-          folioCypress.validateParameter(reportPortalRunType, 'reportPortalRunType')
+        podTemplates.cypressAgent {
+          if (reportPortalUse) {
+            folioCypress.validateParameter(reportPortalRunType, 'reportPortalRunType')
 
-          reportPortalClient = new ReportPortalClient(this,
-            TestType.CYPRESS,
-            ciBuildId,
-            env.BUILD_NUMBER,
-            env.WORKSPACE,
-            reportPortalRunType)
+            reportPortalClient = new ReportPortalClient(this,
+              TestType.CYPRESS,
+              ciBuildId,
+              env.BUILD_NUMBER,
+              env.WORKSPACE,
+              reportPortalRunType)
 
-          reportPortalExecParameters = folioCypress.setupReportPortal(reportPortalClient)
-        }
-
-        container('cypress') {
-          testParams.addExecParameter(reportPortalExecParameters)
-
-          folioCypress.cloneCypressRepo(testParams.testsSrcBranch)
-
-          folioCypress.setupCommonEnvironmentVariables(testParams.tenantUrl,
-            testParams.okapiUrl,
-            testParams.tenant.tenantId,
-            testParams.tenant.adminUser.username,
-            testParams.tenant.adminUser.getPasswordPlainText())
-
-          folioCypress.compileCypressTests()
-
-          execBatches = folioCypress.compileExecBatches(testParams.compileExecParameters, testParams.numberOfWorkers)
-
-          if (testParams.prepare) {
-            folioCypress.prepareTenantForCypressTests(testParams)
+            reportPortalExecParameters = folioCypress.setupReportPortal(reportPortalClient)
           }
 
-          stage('[Stash] Archive tests') {
-            sh """
+          container('cypress') {
+            testParams.addExecParameter(reportPortalExecParameters)
+
+            folioCypress.cloneCypressRepo(testParams.testsSrcBranch)
+
+            folioCypress.setupCommonEnvironmentVariables(testParams.tenantUrl,
+              testParams.okapiUrl,
+              testParams.tenant.tenantId,
+              testParams.tenant.adminUser.username,
+              testParams.tenant.adminUser.getPasswordPlainText())
+
+            folioCypress.compileCypressTests()
+
+            execBatches = folioCypress.compileExecBatches(testParams.compileExecParameters, testParams.numberOfWorkers)
+
+            if (testParams.prepare) {
+              folioCypress.prepareTenantForCypressTests(testParams)
+            }
+
+            stage('[Stash] Archive tests') {
+              sh """
             touch ${cypressStash('archive')}
             tar --exclude=${cypressStash('archive')} -zcf ${cypressStash('archive')} .
             md5sum ${cypressStash('archive')} > ${cypressStash('checksum')}
           """.stripIndent()
-            stash(name: cypressStash('name'),
-              includes: "${cypressStash('archive')},${cypressStash('checksum')}")
-          }
-        }
-      }
-
-      stage('Run tests') {
-        def workers = [failFast: false]
-        String runId = folioCypress.generateRandomId(3)
-        testParams.ciBuildId = "${ciBuildId}-${runId}"
-        List localAllureResults = []
-
-        execBatches.size().times { int batchIndex ->
-          String workerId = "${runId}${batchIndex}"
-          workers["Worker#${workerId}"] = {
-            podTemplates.cypressAgent {
-              container('cypress') {
-                stage('[Stash] Extract tests') {
-                  unstash name: cypressStash('name')
-                  sh """
-                    md5sum -c ${cypressStash('checksum')}
-                    tar -zxf ${cypressStash('archive')}
-                    rm -rf ${cypressStash('archive')} ${cypressStash('checksum')}
-                  """
-                }
-
-                folioCypress.setupCommonEnvironmentVariables(testParams.tenantUrl,
-                  testParams.okapiUrl,
-                  testParams.tenant.tenantId,
-                  testParams.tenant.adminUser.username,
-                  testParams.tenant.adminUser.getPasswordPlainText())
-
-                String execParameters = "${execBatches[batchIndex]} ${testParams.execParameters}"
-
-                folioCypress.executeTests(testParams.ciBuildId,
-                  testParams.browserName,
-                  execParameters,
-                  testParams.testrailProjectID,
-                  testParams.testrailRunID)
-
-                String stashName = folioCypress.archiveTestResults(workerId)
-                if (stashName) {
-                  localAllureResults.add(stashName)
-                }
-              }
+              stash(name: cypressStash('name'),
+                includes: "${cypressStash('archive')},${cypressStash('checksum')}")
             }
           }
         }
 
-        timeout(time: testParams.timeout, unit: 'MINUTES') {
-          parallel(workers)
-        }
+        stage('Run tests') {
+          def workers = [failFast: false]
+          String runId = folioCypress.generateRandomId(3)
+          testParams.ciBuildId = "${ciBuildId}-${runId}"
+          List localAllureResults = []
 
-        allureResultsList.addAll(localAllureResults)
+          execBatches.size().times { int batchIndex ->
+            String workerId = "${runId}${batchIndex}"
+            workers["Worker#${workerId}"] = {
+              podTemplates.cypressAgent {
+                container('cypress') {
+                  stage('[Stash] Extract tests') {
+                    unstash name: cypressStash('name')
+                    sh """
+                    md5sum -c ${cypressStash('checksum')}
+                    tar -zxf ${cypressStash('archive')}
+                    rm -rf ${cypressStash('archive')} ${cypressStash('checksum')}
+                  """
+                  }
+
+                  folioCypress.setupCommonEnvironmentVariables(testParams.tenantUrl,
+                    testParams.okapiUrl,
+                    testParams.tenant.tenantId,
+                    testParams.tenant.adminUser.username,
+                    testParams.tenant.adminUser.getPasswordPlainText())
+
+                  String execParameters = "${execBatches[batchIndex]} ${testParams.execParameters}"
+
+                  folioCypress.executeTests(testParams.ciBuildId,
+                    testParams.browserName,
+                    execParameters,
+                    testParams.testrailProjectID,
+                    testParams.testrailRunID)
+
+                  String stashName = folioCypress.archiveTestResults(workerId)
+                  if (stashName) {
+                    localAllureResults.add(stashName)
+                  }
+                }
+              }
+            }
+          }
+
+          timeout(time: testParams.timeout, unit: 'MINUTES') {
+            parallel(workers)
+          }
+
+          allureResultsList.addAll(localAllureResults)
+        }
       }
     }
     mainPhaseSucceeded = true
