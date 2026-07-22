8fc82bbc1231c93331b2bbbcc04cbf899e3f7ef3
Eldiiar <138369232+eldiiar-duishenaliev@users.noreply.github.com>
2026-07-22 14:46:08 +0600

    RANCHER-818 Implement Standard Naming Convention and S3 Bucket Folder Structure (#1383)

--- src/org/folio/Constants.groovy
+++ src/org/folio/Constants.groovy
@@ -244,7 +244,7 @@ class Constants {
 
   //PostgreSQL
   static String PSQL_DUMP_HELM_CHART_NAME = 'psql-dump'
-  static String PSQL_DUMP_HELM_INSTALL_CHART_VERSION = '1.0.3'
+  static String PSQL_DUMP_HELM_INSTALL_CHART_VERSION = '1.0.10'
   static String PSQL_DUMP_BACKUPS_BUCKET_NAME = 'folio-postgresql-backups'
 
   //Tools
