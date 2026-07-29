9c70253d898bf0893fe7c3894cb3741a36c37c56
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-07-29 19:29:00 +0600

    RANCHER-700 Conditional PostgreSQL resource limits based on cluster

--- terraform/rancher/project/postgresql.tf
+++ terraform/rancher/project/postgresql.tf
@@ -120,9 +120,9 @@ primary:
   name: main
   resources:
     requests:
-      memory: 8Gi
+      memory: ${local.pg_memory_request}
     limits:
-      memory: 10Gi
+      memory: ${local.pg_memory_limit}
   persistence:
     enabled: true
     size: '${var.pg_vol_size}Gi'
